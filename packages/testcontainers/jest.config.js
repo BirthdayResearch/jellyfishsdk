207246546a02aef5bab7bba7c34859ce9c9d9b1e
Fuxing Loh <git@fuxing.dev>
2021-03-02 22:22:24 +0800

    fixed husky setup

--- packages/testcontainers/jest.config.js
+++ packages/testcontainers/jest.config.js
@@ -1,10 +1,10 @@
 module.exports = {
   testEnvironment: 'node',
   testMatch: [
-    '**/__tests__/**/*.ts',
+    '**/__tests__/**/*.ts'
   ],
   transform: {
-    '^.+\\.ts$': 'ts-jest',
+    '^.+\\.ts$': 'ts-jest'
   },
   verbose: true,
   clearMocks: true
