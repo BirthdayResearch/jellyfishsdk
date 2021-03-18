2416699d9ea12bfbca75eda8ce0b710e2c18638b
Fuxing Loh <git@fuxing.dev>
2021-03-18 16:39:53 +0800

    check in wallet todos

--- packages/wallet-core/src/index.ts
+++ packages/wallet-core/src/index.ts
@@ -1,5 +1,28 @@
 export class Wallet {
-  getName (): string {
-    return 'wallet'
-  }
+
+  // TODO(fuxingloh): constructor -> ledger/mnemonic/node?
+  // TODO(fuxingloh): getAddress
+  // TODO(fuxingloh): getNewAddress
+  // TODO(fuxingloh): getBalance
+
+  // TODO(fuxingloh): watchOnly?
+  // TODO(fuxingloh): send
+
+  // TODO(fuxingloh): sign message?
+  // TODO(fuxingloh): list wallet
+  // TODO(fuxingloh): list transaction
+
+
+}
+
+// TODO(fuxingloh): create customTx
+
+export class NodeWallet extends Wallet {
+  // TODO(fuxingloh): lock
+  // TODO(fuxingloh): password
+  // TODO(fuxingloh): passwordChange
+
+  // TODO(fuxingloh): backup
+
+
 }
--- packages/wallet-mnemonic/src/index.ts
+++ packages/wallet-mnemonic/src/index.ts
@@ -1,5 +1,67 @@
 import { Wallet } from '@defichain/wallet-core'
+import bip39 from 'bip39'
+import bip32 from 'bip32'
+
+type Network = 'main' | 'test' | 'regtest'
+
+function getBip32Network(network: Network) {
+  switch (network) {
+    case "main":
+    case "test":
+    case "regtest":
+      // TODO(fuxingloh): return {}
+  }
+}
+
+// const BITCOIN = {
+//     messagePrefix: '\x18Bitcoin Signed Message:\n',
+//     bech32: 'bc',
+//     bip32: {
+//         public: 0x0488b21e, // mainnet public
+//         private: 0x0488ade4, // mainnet private
+//     },
+//     pubKeyHash: 0x00,
+//     scriptHash: 0x05,
+//     wif: 0x80, // wallet import format
+// };
 
 export class MnemonicWallet extends Wallet {
 
+  static validate (mnemonic: string): boolean {
+    return bip39.validateMnemonic(mnemonic)
+  }
+
+  static generate (): string {
+    return bip39.generateMnemonic(24)
+  }
+
+  constructor (mnemonic: string, network: Network) {
+    super();
+    const seed: Buffer = bip39.mnemonicToSeedSync(mnemonic)
+    bip32.fromSeed(seed)
+  }
+
+  getAddress () {
+
+  }
+
+  getBalance () {
+
+  }
+
+  //   createRoot = (seed: Buffer, network: any) => {
+  //     return bip32.fromSeed(seed, network);
+  //   };
+  //
+  //   getRootPrivateKey = (root: any) => {
+  //     return root.privateKey.toString('hex');
+  //   };
+  //
+  //   getRootPublicKey = (root: any) => {
+  //     return root.publicKey.toString('hex');
+  //   };
+  //
+  //   getPrivateKeyInWIF = (root: any) => {
+  //     return root.toWIF();
+  //   };
 }
