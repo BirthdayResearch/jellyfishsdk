01aae05496d2c584942b8bb8dc4b3b6d4f1e370a
Fuxing Loh <git@fuxing.dev>
2021-03-23 10:02:00 +0800

    hd_node docs rename

--- packages/wallet-coin/src/hd_node.ts
+++ packages/wallet-coin/src/hd_node.ts
@@ -7,25 +7,25 @@ export interface HdNode<T extends HdNode<any>> {
   /**
    * Get the public key Buffer of HdNode
    */
-  publicKey (): Promise<Buffer>
+  publicKey: () => Promise<Buffer>
 
   /**
    * Get the private key Buffer of HdNode
    * Allowed to fail when it is neutered or if it is hardware key.
    */
-  privateKey (): Promise<Buffer>
+  privateKey: () => Promise<Buffer>
 
   /**
    * Derive a node in this hierarchical key tree.
    * @param index of the node to derive
    */
-  derive (index: number): Promise<T>
+  derive: (index: number) => Promise<T>
 
   /**
    * Derive a hardened node in this hierarchical key tree.
    * @param index of the hardened node to derive
    */
-  deriveHardened (index: number): Promise<T>
+  deriveHardened: (index: number) => Promise<T>
 
   /**
    * @param path to derive
@@ -34,17 +34,14 @@ export interface HdNode<T extends HdNode<any>> {
    * m/0'/100
    * m/44'/0'/0'/0/0
    */
-  derivePath (path: string): Promise<T>
+  derivePath: (path: string) => Promise<T>
 
   /**
-   * Sign the hash
-   *
    * @param hash to sign
-   * @param lowR whether to create signature with Low R values,
-   * should be defaulted to true
+   * @param lowR whether to create signature with Low R values, should be defaulted to true
    * can be ignored if implementation class is unable to specific this option.
    */
-  sign (hash: Buffer, lowR?: boolean): Promise<Buffer>;
+  sign: (hash: Buffer, lowR?: boolean) => Promise<Buffer>
 
   /**
    * Verify the signature of a hash with this node/address
@@ -52,7 +49,7 @@ export interface HdNode<T extends HdNode<any>> {
    * @param hash to verify
    * @param signature to verify
    */
-  verify (hash: Buffer, signature: Buffer): Promise<boolean>;
+  verify: (hash: Buffer, signature: Buffer) => Promise<boolean>
 
   // TODO(fuxingloh): Additional interface that can be implemented, ignored for now to keep it lite
   //  - neuter
