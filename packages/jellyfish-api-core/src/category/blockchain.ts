2faa6d4ff55a9eb6f25b0d00b1f2b6b6474af5d2
canonbrother <w.canonbrother@gmail.com>
2021-04-06 10:37:46 +0800

    blockchain.getBlock, getBlockHeight, getBlockHash (#91)
    Co-authored-by: Fuxing Loh <4266087+fuxingloh@users.noreply.github.com>

--- packages/jellyfish-api-core/src/category/blockchain.ts
+++ packages/jellyfish-api-core/src/category/blockchain.ts
@@ -17,6 +17,57 @@ export class Blockchain {
   async getBlockchainInfo (): Promise<BlockchainInfo> {
     return await this.client.call('getblockchaininfo', [], 'number')
   }
+
+  /**
+   * Get a hash of block in best-block-chain at height provided.
+   * @param height
+   * @return Promise<string>
+   */
+  async getBlockHash (height: number): Promise<string> {
+    return await this.client.call('getblockhash', [height], 'number')
+  }
+
+  /**
+   * Get the height of the most-work fully-validated chain.
+   * @return Promise<number>
+   */
+  async getBlockCount (): Promise<number> {
+    return await this.client.call('getblockcount', [], 'number')
+  }
+
+  /**
+   * Get block data with particular header hash.
+   * Returns a string that is serialized, hex-encoded data for block 'hash'
+   *
+   * @param hash of the block
+   * @param verbosity 0
+   * @return Promise<string>
+   */
+  getBlock (hash: string, verbosity: 0): Promise<string>
+
+  /**
+   * Get block data with particular header hash.
+   * Returns an Object with information about the block 'hash'.
+   *
+   * @param hash of the block
+   * @param verbosity 1
+   * @return Promise<Block<string>>
+   */
+  getBlock (hash: string, verbosity: 1): Promise<Block<string>>
+
+  /**
+   * Get block data with particular header hash.
+   * Returns an Object with information about block 'hash' and information about each transaction.
+   *
+   * @param hash of the block
+   * @param verbosity 2
+   * @return Promise<Block<Transaction>>
+   */
+  getBlock (hash: string, verbosity: 2): Promise<Block<Transaction>>
+
+  async getBlock<T> (hash: string, verbosity: 0 | 1 | 2): Promise<string | Block<T>> {
+    return await this.client.call('getblock', [hash, verbosity], 'number')
+  }
 }
 
 /**
@@ -42,4 +93,67 @@ export interface BlockchainInfo {
     }
   }
   warnings: string
+}
+
+export interface Block<T> {
+  hash: string
+  confirmations: number
+  strippedsize: number
+  size: number
+  weight: number
+  height: number
+  masternode: string
+  minter: string
+  mintedBlocks: number
+  stakeModifier: string
+  version: number
+  versionHex: string
+  merkleroot: string
+  time: number
+  mediantime: number
+  bits: string
+  difficulty: number
+  chainwork: string
+  tx: T[]
+  nTx: number
+  previousblockhash: string
+  nextblockhash: string
+}
+
+export interface Transaction {
+  txid: string
+  hash: string
+  version: number
+  size: number
+  vsize: number
+  weight: number
+  locktime: number
+  vin: Vin[]
+  vout: Vout[]
+  hex: string
+}
+
+export interface Vin {
+  coinbase: string
+  txid: string
+  vout: number
+  scriptSig: {
+    asm: string
+    hex: string
+  }
+  txinwitness: string[]
+  sequence: string
+}
+
+export interface Vout {
+  value: number
+  n: number
+  scriptPubKey: {
+    asm: string
+    hex: string
+    type: string
+    reqSigs: number
+    addresses: string[]
+    tokenId: string
+  }
 }
