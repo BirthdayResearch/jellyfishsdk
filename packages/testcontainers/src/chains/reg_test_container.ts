6b7f9ba74c452f5c005bba794632bacc37afe687
Fuxing Loh <git@fuxing.dev>
2021-04-15 14:43:34 +0800

    new testcontainers utils for raw tx QOL testing

--- packages/testcontainers/src/chains/reg_test_container.ts
+++ packages/testcontainers/src/chains/reg_test_container.ts
@@ -110,4 +110,53 @@ export class MasterNodeRegTestContainer extends RegTestContainer {
   async waitForWalletCoinbaseMaturity (): Promise<void> {
     await this.generate(100)
   }
+
+  /**
+   * Wait for in wallet balance to be greater than an amount.
+   * This allow test that require fund to wait for fund to be filled up before running the tests.
+   *
+   * @param balance {number} to wait for in wallet to be greater than or equal
+   * @param timeout {number} default to 30000ms
+   * @see waitForWalletCoinbaseMaturity
+   */
+  async waitForWalletBalanceGTE (balance: number, timeout = 30000): Promise<void> {
+    return await this.waitForCondition(async () => {
+      const getbalance = await this.call('getbalance')
+      return getbalance >= balance
+    }, timeout)
+  }
+
+  /**
+   * Fund an address with an amount for testing.
+   * Funded address don't have to be tracked within the node wallet.
+   * This allow for light wallet implementation testing.
+   *
+   * Note, please use whole number as BigNumber is not used here.
+   *
+   * @param address {string} to fund
+   * @param amount {number} to fund address
+   * @return string txid of the transaction
+   * @see waitForWalletCoinbaseMaturity
+   * @see waitForWalletBalanceGTE
+   */
+  async fundAddress (address: string, amount: number): Promise<string> {
+    return await this.call('sendtoaddress', [address, amount])
+  }
+
+  /**
+   * Create a new bech32 address and get the associated priv key for it.
+   * The address is created in the wallet and the priv key is dumped out.
+   * This is to facilitate raw tx feature testing, if you need an address that is not associated with the wallet,
+   * use jellyfish-crypto instead.
+   *
+   * This is not a deterministic feature, each time you run this, you get a different set of address and priv key.
+   *
+   * @return {Promise<{ address: string, privKey: string }>} a new address and it's associated privKey
+   */
+  async newAddressAndPrivKey (): Promise<{ address: string, privKey: string }> {
+    const address = await this.call('getnewaddress', ['', 'bech32'])
+    const privKey = await this.call('dumpprivkey', [address])
+
+    return { address, privKey }
+  }
 }
