8d7057286221d08ace6824681bdc7b57b0f7f614
Fuxing Loh <4266087+fuxingloh@users.noreply.github.com>
2021-04-17 16:29:23 +0800

    added bech32 & wif into jellyfish-crypto (#129)
    * added wif
    
    * added bech32 to jellyfish-crypto
    
    * added lowr as default for getEllipticPairFromPrivateKey
    
    * added bech32 to index for export
    
    * added docs clarification for HRP

---
+++ packages/jellyfish-crypto/__tests__/wif.test.ts
@@ -0,0 +1,40 @@
+import { encode, decode, decodeAsEllipticPair } from '../src'
+
+const REG_TEST_WIF = 'cQSsfYvYkK5tx3u1ByK2ywTTc9xJrREc1dd67ZrJqJUEMwgktPWN'
+const privKey = '557c4bdff86e59015987c1c7f3328a1fb4c2177b5e834f09c8cd10fae51af93b'
+const pubKey = '03987aec2e508e124468f0f07a836d185b329026e7aaf75be48cf12be8f18cbe81'
+
+it('should decode without version', () => {
+  const decoded = decode(REG_TEST_WIF)
+
+  expect(decoded.version).toBe(0xef)
+  expect(decoded.compressed).toBe(true)
+  expect(decoded.privateKey.toString('hex')).toBe(privKey)
+})
+
+it('should decode with failed version', () => {
+  expect(() => {
+    decode(REG_TEST_WIF, 0x80)
+  }).toThrow('Invalid network version')
+})
+
+it('should decode with correct version', () => {
+  const decoded = decode(REG_TEST_WIF, 0xef)
+
+  expect(decoded.version).toBe(0xef)
+  expect(decoded.compressed).toBe(true)
+  expect(decoded.privateKey.toString('hex')).toBe(privKey)
+})
+
+it('should decode decodeAsEllipticPair', async () => {
+  const pair = decodeAsEllipticPair(REG_TEST_WIF, 0xef)
+  const privateKey = await pair.privateKey()
+  const publicKey = await pair.publicKey()
+  expect(privateKey.toString('hex')).toBe(privKey)
+  expect(publicKey.toString('hex')).toBe(pubKey)
+})
+
+it('should encode', () => {
+  const wif = encode(0xef, Buffer.from(privKey, 'hex'))
+  expect(wif).toBe(REG_TEST_WIF)
+})
--- packages/jellyfish-crypto/src/index.ts
+++ packages/jellyfish-crypto/src/index.ts
@@ -1,3 +1,5 @@
+export * from './bech32'
 export * from './der'
 export * from './elliptic'
 export * from './hash'
+export * from './wif'
