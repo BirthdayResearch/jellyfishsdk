2388da448cbd8868cb9043809657573c568f0ea9
Fuxing Loh <git@fuxing.dev>
2021-04-18 15:35:06 +0800

    added wallet_hd_node tests

---
+++ packages/jellyfish-wallet/__tests__/test_node.mock.ts
@@ -0,0 +1,53 @@
+import { WalletHdNode, WalletHdNodeProvider } from '../src'
+import {
+  SignInputOption,
+  Transaction,
+  TransactionSegWit,
+  TransactionSigner,
+  Vout
+} from '@defichain/jellyfish-transaction'
+import { EllipticPair, getEllipticPairFromPrivateKey } from '@defichain/jellyfish-crypto'
+
+/**
+ * This is for testing only, please don't use this for anything else.
+ */
+export class TestNode implements WalletHdNode {
+  private readonly path: string
+  private readonly ellipticPair: EllipticPair
+
+  constructor (path: string) {
+    this.path = path
+    this.ellipticPair = getEllipticPairFromPrivateKey(
+      Buffer.alloc(32, path, 'ascii')
+    )
+  }
+
+  async publicKey (): Promise<Buffer> {
+    return await this.ellipticPair.publicKey()
+  }
+
+  async privateKey (): Promise<Buffer> {
+    return await this.ellipticPair.privateKey()
+  }
+
+  async sign (hash: Buffer): Promise<Buffer> {
+    return await this.ellipticPair.sign(hash)
+  }
+
+  async verify (hash: Buffer, derSignature: Buffer): Promise<boolean> {
+    return await this.ellipticPair.verify(hash, derSignature)
+  }
+
+  async signTx (transaction: Transaction, prevouts: Vout[]): Promise<TransactionSegWit> {
+    const inputs: SignInputOption[] = prevouts.map(prevout => {
+      return { prevout: prevout, ellipticPair: this }
+    })
+    return TransactionSigner.sign(transaction, inputs)
+  }
+}
+
+export class TestNodeProvider implements WalletHdNodeProvider<TestNode> {
+  derive (path: string): TestNode {
+    return new TestNode(path)
+  }
+}
