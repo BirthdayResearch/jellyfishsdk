8f68f7993090dc9a978d7cd6e0f7ccf5572b16f4
Fuxing Loh <git@fuxing.dev>
2021-04-18 12:21:39 +0800

    init jellyfish-wallet-ocean

---
+++ packages/jellyfish-wallet-ocean/README.md
@@ -0,0 +1,4 @@
+[![npm](https://img.shields.io/npm/v/@defichain/jellyfish-wallet-ocean)](https://www.npmjs.com/package/@defichain/jellyfish-wallet-ocean/v/latest)
+[![npm@next](https://img.shields.io/npm/v/@defichain/jellyfish-wallet-ocean/next)](https://www.npmjs.com/package/@defichain/jellyfish-wallet-ocean/v/next)
+
+# @defichain/jellyfish-wallet-ocean
---
+++ packages/jellyfish-wallet-ocean/src/index.ts
@@ -0,0 +1,40 @@
+import { WalletAccount, WalletAccountProvider, WalletHdNode } from '@defichain/jellyfish-wallet'
+import { Network } from '@defichain/jellyfish-network'
+import { HRP, toBech32 } from '@defichain/jellyfish-crypto'
+
+/**
+ * jellyfish-api-ocean implementation of WalletAccount.
+ * All stateless and stateful node service is provided by an ocean instance.
+ */
+export class OceanWalletAccount implements WalletAccount {
+  private readonly hdNode: WalletHdNode
+  private readonly network: Network
+
+  constructor (hdNode: WalletHdNode, network: Network) {
+    this.hdNode = hdNode
+    this.network = network
+  }
+
+  async getAddress (): Promise<string> {
+    const pubKey = await this.hdNode.publicKey()
+    return toBech32(pubKey, this.network.bech32.hrp as HRP)
+  }
+
+  async isActive (): Promise<boolean> {
+    throw new Error('to be implemented')
+  }
+}
+
+export class OceanWalletAccountProvider implements WalletAccountProvider<OceanWalletAccount> {
+  private readonly network: Network
+
+  // TODO(fuxingloh): to implement 'jellyfish-api-ocean'
+
+  constructor (network: Network) {
+    this.network = network
+  }
+
+  async map (hdNode: WalletHdNode): Promise<OceanWalletAccount> {
+    return new OceanWalletAccount(hdNode, this.network)
+  }
+}
