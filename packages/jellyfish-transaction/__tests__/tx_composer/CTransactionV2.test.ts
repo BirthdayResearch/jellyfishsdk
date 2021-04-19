03f88fc5cd7122d591bf2902f460ecbe6a0d4356
Fuxing Loh <git@fuxing.dev>
2021-04-19 12:50:21 +0800

    added better test coverage for v2

--- packages/jellyfish-transaction/__tests__/tx_composer/CTransactionV2.test.ts
+++ packages/jellyfish-transaction/__tests__/tx_composer/CTransactionV2.test.ts
@@ -1,24 +1,60 @@
 import { SmartBuffer } from 'smart-buffer'
-import { CTransactionSegWit } from '../../src'
+import { CTransaction, CTransactionSegWit } from '../../src'
 
-const v2 = '020000000001010000000000000000000000000000000000000000000000000000000000000000ffffffff050393700500ffffffff038260498a040000001976a9143db7aeb218455b697e94f6ff00c548e72221231d88ac7e67ce1d0000000017a914dd7730517e0e4969b4e43677ff5bee682e53420a870000000000000000266a24aa21a9ede2f61c3f71d1defd3fa999dfa36953755c690689799962b48bebd836974e8cf90120000000000000000000000000000000000000000000000000000000000000000000000000'
+describe('Transaction', () => {
+  const v2 = '02000000010000000000000000000000000000000000000000000000000000000000000000ffffffff050393700500ffffffff038260498a040000001976a9143db7aeb218455b697e94f6ff00c548e72221231d88ac7e67ce1d0000000017a914dd7730517e0e4969b4e43677ff5bee682e53420a870000000000000000266a24aa21a9ede2f61c3f71d1defd3fa999dfa36953755c690689799962b48bebd836974e8cf900000000'
 
-it('should parse v2 txn with 3 vout', () => {
-  const buffer = SmartBuffer.fromBuffer(Buffer.from(v2, 'hex'))
-  const composable = new CTransactionSegWit(buffer)
+  it('should parse v2 txn with 3 vout', () => {
+    const buffer = SmartBuffer.fromBuffer(Buffer.from(v2, 'hex'))
+    const composable = new CTransaction(buffer)
 
-  expect(composable.toObject().vout.length).toBe(3)
+    expect(composable.vin[0].txid).toBe('0000000000000000000000000000000000000000000000000000000000000000')
+    expect(composable.vout.length).toBe(3)
+
+    expect(composable.vout[0].dct_id).toBeUndefined()
+    expect(composable.vout[0].value.toString()).toBe('194.99933826')
+    expect(composable.vout[0].script.stack.length).toBe(5)
+  })
+
+  it('should bi-directional buffer to object to buffer', () => {
+    const fromBuffer = SmartBuffer.fromBuffer(Buffer.from(v2, 'hex'))
+    const composable = new CTransaction(fromBuffer)
+
+    const object = composable.toObject()
+
+    const txn = new CTransaction(object)
+    const toBuffer = new SmartBuffer()
+    txn.toBuffer(toBuffer)
+
+    expect(toBuffer.toBuffer().toString('hex')).toBe(v2)
+  })
 })
 
-it('should bi-directional buffer to object to buffer', () => {
-  const fromBuffer = SmartBuffer.fromBuffer(Buffer.from(v2, 'hex'))
-  const composable = new CTransactionSegWit(fromBuffer)
+describe('TransactionSegWit', () => {
+  const v2 = '020000000001010000000000000000000000000000000000000000000000000000000000000000ffffffff050393700500ffffffff038260498a040000001976a9143db7aeb218455b697e94f6ff00c548e72221231d88ac7e67ce1d0000000017a914dd7730517e0e4969b4e43677ff5bee682e53420a870000000000000000266a24aa21a9ede2f61c3f71d1defd3fa999dfa36953755c690689799962b48bebd836974e8cf90120000000000000000000000000000000000000000000000000000000000000000000000000'
 
-  const object = composable.toObject()
+  it('should parse v2 txn with 3 vout', () => {
+    const buffer = SmartBuffer.fromBuffer(Buffer.from(v2, 'hex'))
+    const composable = new CTransactionSegWit(buffer)
 
-  const txn = new CTransactionSegWit(object)
-  const toBuffer = new SmartBuffer()
-  txn.toBuffer(toBuffer)
+    expect(composable.vin.length).toBe(1)
 
-  expect(toBuffer.toBuffer().toString('hex')).toBe(v2)
+    expect(composable.vout.length).toBe(3)
+    expect(composable.vout[0].dct_id).toBeUndefined()
+    expect(composable.vout[0].value.toString()).toBe('194.99933826')
+    expect(composable.vout[0].script.stack.length).toBe(5)
+  })
+
+  it('should bi-directional buffer to object to buffer', () => {
+    const fromBuffer = SmartBuffer.fromBuffer(Buffer.from(v2, 'hex'))
+    const composable = new CTransactionSegWit(fromBuffer)
+
+    const object = composable.toObject()
+
+    const txn = new CTransactionSegWit(object)
+    const toBuffer = new SmartBuffer()
+    txn.toBuffer(toBuffer)
+
+    expect(toBuffer.toBuffer().toString('hex')).toBe(v2)
+  })
 })
