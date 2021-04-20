c6319dde15ae3fc1db23ff2f3cb755f88af3cc9b
Fuxing Loh <4266087+fuxingloh@users.noreply.github.com>
2021-04-20 14:19:15 +0800

    added DfTx PoolAddLiquidity (#145)
    * refactor dftx_pool.test.ts to PoolSwap.test.ts
    
    * added PoolAddLiquidity dftx
    
    * tests rename
    
    * added more test for pool add liquidity

--- packages/jellyfish-transaction/src/script/defi/dftx_pool.ts
+++ packages/jellyfish-transaction/src/script/defi/dftx_pool.ts
@@ -8,7 +8,7 @@ import { SmartBuffer } from 'smart-buffer'
 /* eslint-disable no-return-assign */
 
 /**
- * A poolswap transaction.
+ * PoolSwap DeFi Transaction
  */
 export interface PoolSwap {
   fromScript: Script // ----------------| n = VarUInt{1-9 bytes}, + n bytes
@@ -49,6 +49,66 @@ export class CPoolSwap extends ComposableBuffer<PoolSwap> {
           buffer.writeBigUInt64LE(BigInt(fraction.toString(10)))
         }
       }
+    ]
+  }
+}
+
+/**
+ * PoolAddLiquidity DeFi Transaction
+ */
+export interface PoolAddLiquidity {
+  from: ScriptBalances[] // ------------| c = VarUInt{1-9 bytes}, + c x ScriptBalances
+  shareAddress: Script // --------------| n = VarUInt{1-9 bytes}, + n bytes
+}
+
+export interface ScriptBalances {
+  script: Script // --------------------| n = VarUInt{1-9 bytes}, + n bytes
+  balances: TokenBalance[] // ----------| c = VarUInt{1-9 bytes}, + c x TokenBalance
+}
+
+export interface TokenBalance {
+  token: number // ---------------------| 4 bytes unsigned
+  amount: BigNumber // -----------------| 8 bytes unsigned
+}
+
+/**
+ * Composable PoolAddLiquidity, C stands for Composable.
+ * Immutable by design, bi-directional fromBuffer, toBuffer deep composer.
+ */
+export class CPoolAddLiquidity extends ComposableBuffer<PoolAddLiquidity> {
+  static OP_CODE = 0x6c
+  static OP_NAME = 'DEFI_OP_POOL_ADD_LIQUIDITY'
+
+  composers (p: PoolAddLiquidity): BufferComposer[] {
+    return [
+      ComposableBuffer.varUIntArray(() => p.from, v => p.from = v, v => new CScriptBalances(v)),
+      ComposableBuffer.single<Script>(() => p.shareAddress, v => p.shareAddress = v, v => new CScript(v))
+    ]
+  }
+}
+
+/**
+ * Composable ScriptBalances, C stands for Composable.
+ * Immutable by design, bi-directional fromBuffer, toBuffer deep composer.
+ */
+export class CScriptBalances extends ComposableBuffer<ScriptBalances> {
+  composers (sb: ScriptBalances): BufferComposer[] {
+    return [
+      ComposableBuffer.single<Script>(() => sb.script, v => sb.script = v, v => new CScript(v)),
+      ComposableBuffer.varUIntArray(() => sb.balances, v => sb.balances = v, v => new CTokenBalance(v))
+    ]
+  }
+}
+
+/**
+ * Composable TokenBalance, C stands for Composable.
+ * Immutable by design, bi-directional fromBuffer, toBuffer deep composer.
+ */
+export class CTokenBalance extends ComposableBuffer<TokenBalance> {
+  composers (tb: TokenBalance): BufferComposer[] {
+    return [
+      ComposableBuffer.uInt32(() => tb.token, v => tb.token = v),
+      ComposableBuffer.satoshiAsBigNumber(() => tb.amount, v => tb.amount = v)
     ]
   }
 }
