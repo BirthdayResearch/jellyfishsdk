f933f2ba610f093ab3bc17f766a467fe003f6ea5
Fuxing Loh <git@fuxing.dev>
2021-04-20 11:42:17 +0800

    refactor and deps upgrade

--- packages/jellyfish-transaction/src/index.ts
+++ packages/jellyfish-transaction/src/index.ts
@@ -2,7 +2,7 @@
  * DeFi Blockchain Transaction Constants
  * https://github.com/DeFiCh/ain/blob/master/src/primitives/transaction.h
  */
-export const DeFiTransaction = {
+export const DeFiTransactionConstants = {
   Version: 0x00000004, // 4 bytes
   WitnessMarker: 0x00, // 1 byte
   WitnessFlag: 0x01 // 1 byte
