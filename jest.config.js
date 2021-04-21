9f4e26e0a25d20cfe846bd637dba07f7cc2a703d
Fuxing Loh <git@fuxing.dev>
2021-04-21 23:42:30 +0800

    refactor testcontainers to separate masternode

--- jest.config.js
+++ jest.config.js
@@ -2,9 +2,5 @@ module.exports = {
   projects: [
     '<rootDir>/packages/*'
   ],
-  testTimeout: 240000,
-  collectCoverageFrom: [
-    'packages/**/*.{ts,js}',
-    '!packages/**/__tests__/**'
-  ]
+  testTimeout: 240000
 }
--- packages/testcontainers/src/chains/reg_test_container/masternode.ts
+++ packages/testcontainers/src/chains/reg_test_container/masternode.ts
@@ -1,36 +1,7 @@
+import { GenesisKeys, MasterNodeKey } from '../../testkeys'
 import { DockerOptions } from 'dockerode'
-import { DeFiDContainer, StartOptions } from './container'
-import { GenesisKeys, MasterNodeKey } from '../testkeys'
-
-export class RegTestContainer extends DeFiDContainer {
-  constructor (options?: DockerOptions) {
-    super('regtest', options)
-  }
-
-  protected getCmd (opts: StartOptions): string[] {
-    return [...super.getCmd(opts),
-      '-regtest=1',
-      '-txnotokens=0',
-      '-logtimemicros',
-      '-txindex=1',
-      '-acindex=1',
-      '-amkheight=0',
-      '-bayfrontheight=1',
-      '-bayfrontgardensheight=2',
-      '-clarkequayheight=3',
-      '-dakotaheight=4',
-      '-dakotacrescentheight=5'
-    ]
-  }
-
-  async getNewAddress (label: string = '', addressType: 'legacy' | 'p2sh-segwit' | 'bech32' | string = 'bech32'): Promise<string> {
-    return await this.call('getnewaddress', [label, addressType])
-  }
-
-  async getRpcPort (): Promise<string> {
-    return await this.getPort('19554/tcp')
-  }
-}
+import { StartOptions } from '../container'
+import { RegTestContainer } from './index'
 
 /**
  * RegTest with MasterNode preconfigured
