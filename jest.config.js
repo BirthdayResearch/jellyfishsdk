badb7e694cca8d4e551e7e230e95b0efc343b823
Fuxing Loh <git@fuxing.dev>
2021-04-21 22:55:37 +0800

    "jest" coverage should be in jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,9 @@ module.exports = {
   projects: [
     '<rootDir>/packages/*'
   ],
-  testTimeout: 240000
+  testTimeout: 240000,
+  collectCoverageFrom: [
+    'packages/**/*.{ts,js}',
+    '!packages/**/__tests__/**'
+  ]
 }
