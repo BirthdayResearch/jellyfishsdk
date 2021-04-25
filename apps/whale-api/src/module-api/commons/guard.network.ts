5c82ecbc7bad5e0a5c793239415c3b8857c428be
Fuxing Loh <4266087+fuxingloh@users.noreply.github.com>
2021-04-25 23:08:40 +0800

    setup module-api with /v1.0/:network/call/:method (#8)
    * add required deps
    
    * added NetworkGuard
    
    * added project modules
    
    * added module for e2e testing
    
    * changed guard to throw NotFoundException
    
    * changed to fastify as the http provider
    
    * added call controller with unit and e2e tests
    
    * refactor test to silent logging during test
    
    * renamed to adapter.fastify.ts
    
    * updated governance.yml
    
    * updated codeclimate to exclude test files
    
    * updated ci to build, test & lint
    
    * fixed lint
    
    * install and run standard
    
    * removed 'standard' from lint
    
    * fixed nest build

---
+++ apps/whale-api/src/module-api/commons/guard.network.ts
@@ -0,0 +1,36 @@
+import { CanActivate, ExecutionContext, Injectable, NotFoundException } from '@nestjs/common'
+import { Observable } from 'rxjs'
+import { ConfigService } from '@nestjs/config'
+
+/**
+ * Whale endpoints are exposed as /v1.0/:network/...
+ * Each whale server can only run a single network for separation of concerns.
+ * This provides global request guard to ensure request are routed to the correct endpoint.
+ * Incorrect network will raise NotFoundException and handled as a 404 response.
+ */
+@Injectable()
+export class NetworkGuard implements CanActivate {
+  static available: string[] = [
+    'mainnet',
+    'testnet',
+    'regtest'
+  ]
+
+  private readonly network: string
+
+  constructor (private readonly configService: ConfigService) {
+    const network = configService.get<string>('network')
+    if (network === undefined || !NetworkGuard.available.includes(network)) {
+      throw new Error('bootstrapping error: missing config in configuration.ts - network is not configured')
+    }
+    this.network = network
+  }
+
+  canActivate (context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
+    const request = context.switchToHttp().getRequest()
+    if (request.params.network !== this.network) {
+      throw new NotFoundException('Network not found')
+    }
+    return true
+  }
+}
