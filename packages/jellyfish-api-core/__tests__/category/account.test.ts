6e104aa1443923dc4ec71f9b1503bdf53f4e5188
websterlcl <w.canonbrother@gmail.com>
2021-04-26 17:18:45 +0800

    test listaccounts

--- packages/jellyfish-api-core/__tests__/category/account.test.ts
+++ packages/jellyfish-api-core/__tests__/category/account.test.ts
@@ -1,6 +1,7 @@
 import { RegTestContainer, MasterNodeRegTestContainer } from '@defichain/testcontainers'
 import { ContainerAdapterClient } from '../container_adapter_client'
 import waitForExpect from 'wait-for-expect'
+import { AccountOwner, AccountAmount } from '../../src'
 
 describe('non masternode', () => {
   const container = new RegTestContainer()
@@ -25,13 +26,30 @@ describe('non masternode', () => {
   })
 })
 
-describe.only('masternode', () => {
+describe('masternode', () => {
   const container = new MasterNodeRegTestContainer()
   const client = new ContainerAdapterClient(container)
+
+  async function utxosToAccount (): Promise<void> {
+    const address = await container.call('getnewaddress')
+    const payload: { [key: string]: string } = {}
+    payload[address] = '100@0'
+    await container.call('utxostoaccount', [payload])
+    await container.generate(1)
+  }
+
+  // async function mintTokens (symbol: string): Promise<void> {
+
+  //   await container.call('minttokens', [`2000@${symbol}`])
+
+  //   await container.generate(1)
+  // }
 
   beforeAll(async () => {
     await container.start()
     await container.waitForReady()
+    await container.waitForWalletCoinbaseMaturity()
+    await container.waitForWalletBalanceGTE(300)
   })
 
   afterAll(async () => {
@@ -39,11 +57,236 @@ describe.only('masternode', () => {
   })
 
   describe('listAccounts', () => {
+    beforeAll(async () => {
+      await utxosToAccount()
+      await utxosToAccount()
+      await utxosToAccount()
+    })
+
     it('should listAccounts', async () => {
       await waitForExpect(async () => {
         const accounts = await client.account.listAccounts()
-        console.log('accounts: ', accounts)
+        expect(accounts.length).toBeGreaterThan(0)
       })
+
+      const accounts = await client.account.listAccounts()
+      for (let i = 0; i < accounts.length; i += 1) {
+        const account = accounts[i]
+        expect(typeof account.key).toBe('string')
+        expect(typeof account.owner === 'object').toBe(true)
+        expect(typeof (account.owner as AccountOwner).asm).toBe('string')
+        expect(typeof (account.owner as AccountOwner).reqSigs).toBe('number')
+        expect((account.owner as AccountOwner).type).toBe('scripthash')
+        expect((account.owner as AccountOwner).addresses.length).toBeGreaterThan(0)
+        expect(account.amount).toBe('100.00000000@DFI')
+      }
+    })
+
+    it('should listAccounts with pagination start and including_start', async () => {
+      let accounts: any[] = []
+
+      await waitForExpect(async () => {
+        accounts = await client.account.listAccounts()
+        expect(accounts.length).toBeGreaterThan(0)
+      })
+
+      const pagination = {
+        start: accounts[0].owner.key,
+        including_start: true
+      }
+
+      accounts = await client.account.listAccounts(pagination)
+      for (let i = 0; i < accounts.length; i += 1) {
+        const account = accounts[i]
+        expect(typeof account.key).toBe('string')
+        expect(typeof account.owner === 'object').toBe(true)
+        expect(typeof (account.owner as AccountOwner).asm).toBe('string')
+        expect(typeof (account.owner as AccountOwner).reqSigs).toBe('number')
+        expect((account.owner as AccountOwner).type).toBe('scripthash')
+        expect((account.owner as AccountOwner).addresses.length).toBeGreaterThan(0)
+        expect(account.amount).toBe('100.00000000@DFI')
+      }
+    })
+
+    it('should listAccounts with pagination.limit', async () => {
+      let accounts: any[] = []
+
+      await waitForExpect(async () => {
+        accounts = await client.account.listAccounts()
+        expect(accounts.length).toBeGreaterThan(0)
+      })
+
+      const pagination = {
+        limit: 2
+      }
+      accounts = await client.account.listAccounts(pagination)
+      expect(accounts.length).toBe(2)
+    })
+
+    it('should listAccounts with verbose false', async () => {
+      await waitForExpect(async () => {
+        const accounts = await client.account.listAccounts()
+        expect(accounts.length).toBeGreaterThan(0)
+      })
+
+      const accounts = await client.account.listAccounts({}, false)
+      for (let i = 0; i < accounts.length; i += 1) {
+        const account = accounts[i]
+        expect(typeof account.key).toBe('string')
+        expect(typeof account.owner).toBe('string')
+        expect(account.amount).toBe('100.00000000@DFI')
+      }
+    })
+
+    it('should listAccounts with indexed_amounts true', async () => {
+      await waitForExpect(async () => {
+        const accounts = await client.account.listAccounts()
+        expect(accounts.length).toBeGreaterThan(0)
+      })
+
+      const options = {
+        indexedAmounts: true
+      }
+
+      const accounts = await client.account.listAccounts({}, true, options)
+      for (let i = 0; i < accounts.length; i += 1) {
+        const account = accounts[i]
+        expect(typeof account.key).toBe('string')
+        expect(typeof account.owner === 'object').toBe(true)
+        expect(typeof (account.owner as AccountOwner).asm).toBe('string')
+        expect(typeof (account.owner as AccountOwner).reqSigs).toBe('number')
+        expect((account.owner as AccountOwner).type).toBe('scripthash')
+        expect((account.owner as AccountOwner).addresses.length).toBeGreaterThan(0)
+
+        expect(typeof account.amount === 'object').toBe(true)
+        expect((account.amount as AccountAmount)['0'].toString()).toBe('100')
+      }
+    })
+
+    it('should listAccounts with isMineOnly true', async () => {
+      await waitForExpect(async () => {
+        const accounts = await client.account.listAccounts()
+        expect(accounts.length).toBeGreaterThan(0)
+      })
+
+      const options = {
+        isMineOnly: true
+      }
+
+      const accounts = await client.account.listAccounts({}, true, options)
+      for (let i = 0; i < accounts.length; i += 1) {
+        const account = accounts[i]
+        expect(typeof account.key).toBe('string')
+        expect(typeof account.owner === 'object').toBe(true)
+        expect(typeof (account.owner as AccountOwner).asm).toBe('string')
+        expect(typeof (account.owner as AccountOwner).reqSigs).toBe('number')
+        expect((account.owner as AccountOwner).type).toBe('scripthash')
+        expect((account.owner as AccountOwner).addresses.length).toBeGreaterThan(0)
+
+        expect(typeof account.owner === 'object').toBe(true)
+        expect(typeof (account.amount as AccountAmount)['0']).toBe('string')
+      }
     })
   })
+
+  describe.only('getAccount', () => {
+    beforeAll(async () => {
+      await utxosToAccount()
+    })
+
+    it.only('should getAccount', async () => {
+      let accounts: any[] = []
+
+      await waitForExpect(async () => {
+        accounts = await client.account.listAccounts({}, false)
+        expect(accounts.length).toBeGreaterThan(0)
+      })
+
+      const account = await client.account.getAccount(accounts[0].owner)
+      console.log('account: ', account)
+      // expect(account.length).toBeGreaterThan(0)
+      // expect(account[0]).toBe('100.00000000@DFI')
+    })
+
+    it('should getAccount with pagination start and including_start', async () => {
+      let accounts: any[] = []
+
+      await waitForExpect(async () => {
+        accounts = await client.account.listAccounts()
+        expect(accounts.length).toBeGreaterThan(0)
+      })
+
+      const pagination = {
+        start: accounts[0].owner.key,
+        including_start: true
+      }
+
+      const account = await client.account.getAccount(accounts[0].owner, pagination)
+      console.log('account 1: ', account)
+
+      expect(account.length).toBeGreaterThan(0)
+      expect(account[0]).toBe('100.00000000@DFI')
+    })
+
+    it('should getAccount with pagination.limit', async () => {
+      let accounts: any[] = []
+
+      await waitForExpect(async () => {
+        accounts = await client.account.listAccounts()
+        expect(accounts.length).toBeGreaterThan(0)
+      })
+
+      const pagination = {
+        limit: 2
+      }
+      const account = await client.account.getAccount(accounts[0].owner, pagination)
+      console.log('account 2: ', account)
+    })
+
+    it('should getAccount with indexedAmount true', async () => {
+      let accounts: any[] = []
+
+      await waitForExpect(async () => {
+        accounts = await client.account.listAccounts()
+        expect(accounts.length).toBeGreaterThan(0)
+      })
+
+      const options = {
+        indexedAmounts: true
+      }
+
+      const account = await client.account.getAccount(accounts[0].owner, {}, options)
+      console.log('account 3: ', account)
+    })
+  })
+
+  describe('getTokenBalances', () => {
+    beforeAll(async () => {
+      await utxosToAccount()
+    })
+
+    it('should getTokenBalances', async () => {
+      let accounts: any[] = []
+
+      await waitForExpect(async () => {
+        accounts = await client.account.listAccounts()
+        expect(accounts.length).toBeGreaterThan(0)
+      })
+
+      const tokenBalances = await client.account.getTokenBalances(accounts[0].owner)
+      console.log('tokenBalances: ', tokenBalances)
+    })
+  })
+
+  // describe('listAccountHistory', () => {
+  //   beforeAll(async () => {
+  //     await utxosToAccount()
+  //     await utxosToAccount()
+  //     await utxosToAccount()
+  //   })
+
+  //   it('should listAccountHistory', async () => {
+
+  //   })
+  // })
 })
--- packages/jellyfish-api-core/src/category/account.ts
+++ packages/jellyfish-api-core/src/category/account.ts
@@ -13,22 +13,22 @@ export class Account {
   /**
    * Returns information about all accounts on chain
    *
-   * @param {AccountPagination=} pagination
-   * @param {string} pagination.start
-   * @param {boolean} pagination.including_start
-   * @param {number} pagination.limit
-   * @param {boolean} verbose default = true, otherwise limited objects are listed
-   * @param {boolean} indexedAmounts format of amount output, default = false (true: {tokenid:amount}, false: amount@tokenid)
-   * @param {boolean} isMineOnly get balances about all accounts belonging to the wallet
-   * @return Promise<IAccount>
+   * @param {AccountPagination} [pagination]
+   * @param {string} [pagination.start]
+   * @param {boolean} [pagination.including_start]
+   * @param {number} [pagination.limit]
+   * @param {boolean} [verbose=true] default = true, otherwise limited objects are listed
+   * @param {ListAccountOptions} [options] default = true, otherwise limited objects are listed
+   * @param {boolean} [options.indexedAmounts=false] format of amount output, default = false (true: {tokenid:amount}, false: amount@tokenid)
+   * @param {boolean} [options.isMineOnly=false] get balances about all accounts belonging to the wallet
+   * @return {Promise<AccountResult[]>}
    */
   async listAccounts (
-    pagination: AccountPagination = {
-      // start: '',
-      including_start: false,
-      limit: 100
-    }, verbose = true, indexedAmounts = false, isMineOnly = false
-  ): Promise<any> {
+    pagination: AccountPagination = {},
+    verbose = true,
+    options: ListAccountOptions = {}
+  ): Promise<AccountResult[]> {
+    const { indexedAmounts = false, isMineOnly = false } = options
     return await this.client.call('listaccounts', [pagination, verbose, indexedAmounts, isMineOnly], 'number')
   }
 
@@ -36,38 +36,32 @@ export class Account {
    * Returns information about account
    *
    * @param {string} owner address in base58/bech32/hex encoding
-   * @param {AccountPagination} pagination
-   * @param {string} pagination.start
-   * @param {boolean} pagination.including_start
-   * @param {number} pagination.limit
-   * @param {boolean} indexedAmounts format of amount output, default = false (true: {tokenid:amount}, false: amount@tokenid)
-   * @return Promise<IAccount>
+   * @param {AccountPagination} [pagination]
+   * @param {string} [pagination.start]
+   * @param {boolean} [pagination.including_start]
+   * @param {number} [pagination.limit]
+   * @param {GetAccountOptions} [options]
+   * @param {boolean} [options.indexedAmounts=false] format of amount output, default = false (true: {tokenid:amount}, false: amount@tokenid)
+   * @return {Promise<string[]>}
    */
-  async getAccount (owner: string, pagination: AccountPagination = {
-    start: '',
-    including_start: true,
-    limit: 100
-  }, indexedAmounts = false): Promise<any> {
-    return await this.client.call('getaccount', [owner, pagination, indexedAmounts], 'number')
+  async getAccount (owner: string, pagination: AccountPagination = {}, options: GetAccountOptions = {}): Promise<any> {
+    return await this.client.call('getaccount', [owner, pagination, options], 'number')
   }
 
   /**
    * Returns the balances of all accounts that belong to the wallet
    *
-   * @param {AccountPagination} pagination
-   * @param {string} pagination.start
-   * @param {boolean} pagination.including_start
-   * @param {number} pagination.limit
-   * @param {boolean} indexedAmounts format of amount output, default = false (true: {tokenid:amount}, false: amount@tokenid)
-   * @param {boolean} symbolLookup use token symbols in output, default = false
+   * @param {AccountPagination} [pagination]
+   * @param {string} [pagination.start]
+   * @param {boolean} [pagination.including_start]
+   * @param {number} [pagination.limit]
+   * @param {boolean} [indexedAmounts=false] format of amount output, default = false (true: {tokenid:amount}, false: amount@tokenid)
+   * @param {boolean} [options] format of amount output, default = false (true: {tokenid:amount}, false: amount@tokenid)
+   * @param {boolean} [options.symbolLookup=false] use token symbols in output, default = false
    * @return
    */
   async getTokenBalances (
-    pagination: AccountPagination = {
-      start: '',
-      including_start: true,
-      limit: 100
-    }, indexedAmounts = false, symbolLookup = false
+    pagination: AccountPagination = {}, indexedAmounts = false, symbolLookup = false
   ): Promise<any> {
     return await this.client.call('gettokenbalances', [pagination, indexedAmounts, symbolLookup], 'number')
   }
@@ -75,14 +69,14 @@ export class Account {
   /**
    * Returns information about account history
    *
-   * @param {string=} owner
-   * @param {AccountHistoryOptions=} options
-   * @param {number=} options.maxBlockHeight Optional height to iterate from (downto genesis block), (default = chaintip).
-   * @param {number=} options.depth Maximum depth, from the genesis block is the default
-   * @param {boolean=} options.no_rewards Filter out rewards
-   * @param {string=} options.token Filter by token
-   * @param {string=} options.txtype Filter by transaction type, supported letter from 'CRTMNnpuslrUbBG
-   * @param {number=} options.limit Maximum number of records to return, 100 by default
+   * @param {string} [owner]
+   * @param {AccountHistoryOptions} [options]
+   * @param {number} [options.maxBlockHeight] Optional height to iterate from (downto genesis block), (default = chaintip).
+   * @param {number} [options.depth] Maximum depth, from the genesis block is the default
+   * @param {boolean} [options.no_rewards] Filter out rewards
+   * @param {string} [options.token] Filter by token
+   * @param {string} [options.txtype] Filter by transaction type, supported letter from 'CRTMNnpuslrUbBG
+   * @param {number} [options.limit] Maximum number of records to return, 100 by default
    * @return
    */
   async listAccountHistory (owner?: string, options?: AccountHistoryOptions): Promise<any> {
@@ -96,8 +90,31 @@ export interface AccountPagination {
   limit?: number
 }
 
-// export interface IAccount {
-// }
+export interface AccountResult {
+  key: string
+  owner: string | AccountOwner
+  amount: string | AccountAmount
+}
+
+export interface AccountOwner {
+  asm: string
+  reqSigs: number
+  type: string
+  addresses: string[]
+}
+
+export interface AccountAmount {
+  [id: string]: string
+}
+
+export interface ListAccountOptions {
+  indexedAmounts?: boolean
+  isMineOnly?: boolean
+}
+
+export interface GetAccountOptions {
+  indexedAmounts?: boolean
+}
 
 export interface AccountHistoryOptions {
   maxBlockHeight: number
