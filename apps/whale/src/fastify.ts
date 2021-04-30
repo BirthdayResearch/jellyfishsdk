d6e26812af13809abe335a194ccc16da36eeb555
Fuxing Loh <4266087+fuxingloh@users.noreply.github.com>
2021-04-30 13:15:06 +0800

    added transactions.controller.ts (https://github.com/DeFiCh/whale/pull/23)
    * remove addContentTypeParser to keep the body validation brief, DeFi whale should not support BigNumber in body too. (will revisit if need to in the future)
    
    * added class-validator
    
    * updated CONTRIBUTING.md
    
    * added jellyfish-crypto
    
    * updated jellyfish deps to 0.0.17
    
    * updated jellyfish to v0.0.18
    
    * added transactions.controller.ts with tests
    
    * updated deps
    
    * added transactions.controller.ts
    
    * added missing waitForWalletCoinbaseMaturity
    
    Revert "separated transactions.e2e.ts"
    
    This reverts commit 3d4e787b
    
    separated transactions.e2e.ts
    
    separated transactions.e2e.ts

--- apps/whale/src/fastify.ts
+++ apps/whale/src/fastify.ts
@@ -17,20 +17,10 @@ export function newFastifyAdapter (options: AdapterOptions = {
 }
 
 /**
- * @param {FastifyAdapter} adapter to setup and use JellyfishJSON for setReplySerializer & addContentTypeParser
+ * @param {FastifyAdapter} adapter to setup and use JellyfishJSON for setReplySerializer
  */
 function setupJellyfishJSON (adapter: FastifyAdapter): void {
   adapter.getInstance().setReplySerializer(payload => {
     return JellyfishJSON.stringify(payload)
-  })
-
-  adapter.getInstance().addContentTypeParser('application/json', { parseAs: 'string' }, (req, body, done) => {
-    try {
-      const json = JellyfishJSON.parse(body as string, 'lossless')
-      done(null, json)
-    } catch (err) {
-      err.statusCode = 400
-      done(err, undefined)
-    }
   })
 }
