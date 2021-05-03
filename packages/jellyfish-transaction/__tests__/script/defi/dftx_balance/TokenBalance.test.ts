276ad5ced0b11bbede016bb1cb4a5fce967a4b54
ivan <ivan@zynesis.com>
2021-05-03 12:36:11 +0800

    add test for TokenBalance and ScriptBalances as they are reused now

---
+++ packages/jellyfish-transaction/__tests__/script/defi/dftx_balance/TokenBalance.test.ts
@@ -0,0 +1,24 @@
+import BigNumber from 'bignumber.js'
+import { SmartBuffer } from 'smart-buffer'
+import { CTokenBalance, TokenBalance } from '../../../../src/script/defi/dftx_balance'
+
+const data = '000000003da5a50300000000'
+const tokenBalance: TokenBalance = {
+  token: 0,
+  amount: new BigNumber('0.61187389')
+}
+
+describe('Composable', () => {
+  it('should compose from buffer to composable', () => {
+    const buffer = SmartBuffer.fromBuffer(Buffer.from(data, 'hex'))
+    const composable = new CTokenBalance(buffer)
+    expect(composable.toObject()).toEqual(tokenBalance)
+  })
+
+  it('should compose from composable to buffer', () => {
+    const composable = new CTokenBalance(tokenBalance)
+    const buffer = new SmartBuffer()
+    composable.toBuffer(buffer)
+    expect(buffer.toBuffer().toString('hex')).toEqual(data)
+  })
+})
