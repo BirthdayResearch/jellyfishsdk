db95cd03fe85a6b395472214631da14c1ade58a2
jingyi2811 <jingyi2811@gmail.com>
2021-05-03 15:12:51 +0800

    Add getNetworkInfo rpc (#180)
    * Add getnetworkinfo rpc
    
    * Remove commas

--- packages/jellyfish-api-core/src/category/net.ts
+++ packages/jellyfish-api-core/src/category/net.ts
@@ -18,4 +18,43 @@ export class Net {
   async getConnectionCount (): Promise<number> {
     return await this.client.call('getconnectioncount', [], 'number')
   }
+
+  /**
+   * Returns an object containing various state info regarding P2P networking.
+   *
+   * @return {Promise<NetworkInfo>}
+   */
+  async getNetworkInfo (): Promise<NetworkInfo> {
+    return await this.client.call('getnetworkinfo', [], 'number')
+  }
+}
+
+export interface NetworkInfo {
+  version: number
+  subversion: string
+  protocolversion: number
+  localservices: string
+  localrelay: boolean
+  timeoffset: number
+  connections: number
+  networkactive: boolean
+  networks: Network[]
+  relayfee: number
+  incrementalfee: number
+  localaddresses: LocalAddress[]
+  warnings: string
+}
+
+export interface Network {
+  name: string
+  limited: boolean
+  reachable: boolean
+  proxy: string
+  proxy_randomize_credentials: boolean
+}
+
+export interface LocalAddress {
+  address: string
+  port: number
+  score: number
 }
