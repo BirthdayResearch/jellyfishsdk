749a7e2eb18c23d390692040205f0c9c7b89cffb
Fuxing Loh <4266087+fuxingloh@users.noreply.github.com>
2021-05-12 16:02:41 +0800

    whale-api-client: query pagination chaining (https://github.com/DeFiCh/whale/pull/62)
    * added query pagination spec
    
    * refactor call.call tp rpc.call
    also fixed cherry pick errors
    
    * fixed test
    
    * updated sanity test

--- apps/whale/src/module.api/_module.ts
+++ apps/whale/src/module.api/_module.ts
@@ -1,6 +1,6 @@
 import { APP_PIPE } from '@nestjs/core'
 import { Module } from '@nestjs/common'
-import { CallController } from '@src/module.api/call.controller'
+import { RpcController } from '@src/module.api/rpc.controller'
 import { HealthController } from '@src/module.api/health.controller'
 import { TransactionsController } from '@src/module.api/transactions.controller'
 import { ApiValidationPipe } from '@src/module.api/_validation'
@@ -10,7 +10,7 @@ import { ApiValidationPipe } from '@src/module.api/_validation'
  */
 @Module({
   controllers: [
-    CallController,
+    RpcController,
     HealthController,
     TransactionsController
   ],
