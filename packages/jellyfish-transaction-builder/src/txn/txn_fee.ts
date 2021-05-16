7151bd954d01478f5090e7b44062d5742b01e8b6
Fuxing Loh <git@fuxing.dev>
2021-05-16 21:35:27 +0800

    added txn_fee.test.ts for fee calculation

--- packages/jellyfish-transaction-builder/src/txn/txn_fee.ts
+++ packages/jellyfish-transaction-builder/src/txn/txn_fee.ts
@@ -1,13 +1,11 @@
 import { BigNumber } from 'bignumber.js'
+import { SmartBuffer } from 'smart-buffer'
 import {
-  OP_PUSHDATA,
-  Script,
-  Transaction,
-  Vin,
-  Vout,
+  CTransaction,
+  CTransactionSegWit,
+  Transaction, TransactionSegWit,
   Witness
 } from '@defichain/jellyfish-transaction'
-import { byteLength } from '@defichain/jellyfish-transaction/dist/buffer/buffer_varuint'
 
 /**
  * Consider dust if output is < 0.00003000
@@ -23,7 +21,7 @@ const DUST_AMOUNT = new BigNumber('0.00003000')
 const P2WPKH_WITNESS_EXAMPLE: Witness = {
   scripts: [
     { hex: '3044022000000000000000000000000000000000000000000000000000000000000000000220000000000000000000000000000000000000000000000000000000000000000001' },
-    { hex: '000000000000000000000000000000000000000000000000000000000000000000000000000000' }
+    { hex: '000000000000000000000000000000000000000000000000000000000000000000' }
   ]
 }
 
@@ -34,31 +32,31 @@ const P2WPKH_WITNESS_EXAMPLE: Witness = {
  * @return {boolean}
  */
 export function isDustAmount (amount: BigNumber): boolean {
-  return amount.lt(DUST_AMOUNT)
+  return amount.lte(DUST_AMOUNT)
 }
 
 /**
  * Calculate fee of a transaction where inputs are all P2WPKH.
  *
- * @param {BigNumber} feeRate
+ * @param {BigNumber} feeRate in DFI/kb
  * @param {Transaction} transaction
  * @return {BigNumber} fee amount to use for transaction with provided fee rate
  */
 export function calculateFeeP2WPKH (feeRate: BigNumber, transaction: Transaction): BigNumber {
   const witness = transaction.vin.map(_ => P2WPKH_WITNESS_EXAMPLE)
   const size = calculateVirtual(transaction, witness)
-  return feeRate.multipliedBy(size)
+  return feeRate.multipliedBy(size).dividedBy(1000)
 }
 
 /**
- * @param {BigNumber} feeRate
+ * @param {BigNumber} feeRate in DFI/kb
  * @param {Transaction} transaction
  * @param {Witness} witness of the transaction, separated to allow unsigned calculation
  * @return {BigNumber} fee amount to use for transaction with provided fee rate
  */
 export function calculateFee (feeRate: BigNumber, transaction: Transaction, witness: Witness[]): BigNumber {
   const size = calculateVirtual(transaction, witness)
-  return feeRate.multipliedBy(size)
+  return feeRate.multipliedBy(size).dividedBy(1000)
 }
 
 /**
@@ -76,75 +74,27 @@ export function calculateVirtual (transaction: Transaction, witness: Witness[]):
  * @return {number} calculated weight of transaction
  */
 export function calculateWeight (transaction: Transaction, witness: Witness[]): number {
-  const base = calculateBase(transaction)
-  const total = calculateTotal(transaction, witness)
+  const base = calculate(transaction)
+  const total = calculateWitness({
+    version: transaction.version,
+    marker: 0x00,
+    flag: 0x01,
+    vin: transaction.vin,
+    vout: transaction.vout,
+    lockTime: transaction.lockTime,
+    witness: witness
+  })
   return base * 3 + total
 }
 
-function calculateBase (transaction: Transaction): number {
-  return (
-    4 + // version
-    calculateVin(transaction.vin) +
-    calculateVout(transaction.vout) +
-    4 // locktime
-  )
+function calculate (transaction: Transaction): number {
+  const buffer = new SmartBuffer()
+  new CTransaction(transaction).toBuffer(buffer)
+  return buffer.length
 }
 
-function calculateTotal (transaction: Transaction, witness: Witness[]): number {
-  return (
-    4 + // version
-    1 + // marker
-    1 + // flag
-    calculateVin(transaction.vin) +
-    calculateVout(transaction.vout) +
-    calculateWitness(witness) +
-    4 // locktime
-  )
-}
-
-function calculateVin (vin: Vin[]): number {
-  return (
-    byteLength(vin.length) +
-    vin.reduce((sum, input) => {
-      return sum + 40 + calculateScript(input.script)
-    }, 0)
-  )
-}
-
-function calculateVout (vout: Vout[]): number {
-  return (
-    byteLength(vout.length) +
-    vout.reduce((sum, output) => {
-      return sum +
-        8 + // value
-        calculateScript(output.script) +
-        byteLength(output.tokenId)
-    }, 0)
-  )
-}
-
-function calculateWitness (witness: Witness[]): number {
-  return witness.reduce((sum, witness) => {
-    return (
-      byteLength(witness.scripts.length) +
-      witness.scripts.reduce((sum, script) => {
-        const length = script.hex.length / 2
-        return (
-          byteLength(length) +
-          length
-        )
-      }, 0)
-    )
-  }, 0)
-}
-
-function calculateScript (script: Script): number {
-  const length = script.stack.reduce((sum, code) => {
-    if (code instanceof OP_PUSHDATA) {
-      return sum + code.asBuffer().length
-    }
-    return sum + 1
-  }, 0)
-
-  return byteLength(length) + length
+function calculateWitness (transaction: TransactionSegWit): number {
+  const buffer = new SmartBuffer()
+  new CTransactionSegWit(transaction).toBuffer(buffer)
+  return buffer.length
 }
