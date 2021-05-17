e7739f48a632f6776f68632bdd893432f2f102e3
Fuxing Loh <git@fuxing.dev>
2021-05-18 04:02:59 +0800

    added missing test coverage

--- packages/jellyfish-wallet/__tests__/wallet_account.test.ts
+++ packages/jellyfish-wallet/__tests__/wallet_account.test.ts
@@ -41,3 +41,44 @@ describe('WalletAccount: 0/0/0', () => {
     expect(active).toBe(true)
   })
 })
+
+describe('get script', () => {
+  const nodeProvider = new TestNodeProvider()
+  const accountProvider = new TestAccountProvider([])
+
+  it('should get script for 0', async () => {
+    const node = nodeProvider.derive('0')
+    const account = accountProvider.provide(node)
+    expect(await account.getScript()).toEqual({
+      stack: [
+        { code: 0, type: 'OP_0' },
+        { hex: 'df01eaac7d4f3e28cf3b8929590766d3559e7a69', type: 'OP_PUSHDATA' }
+      ]
+    })
+  })
+
+  it('should get script for 0/0', async () => {
+    const node = nodeProvider.derive('0/0')
+    const account = accountProvider.provide(node)
+    expect(await account.getScript()).toEqual({
+      stack: [
+        { code: 0, type: 'OP_0' },
+        { hex: '938a24a531e4ee0fd4ea12f95058292ed8f3eed4', type: 'OP_PUSHDATA' }
+      ]
+    })
+  })
+})
+
+it('address to script', () => {
+  const nodeProvider = new TestNodeProvider()
+  const accountProvider = new TestAccountProvider([])
+
+  const node = nodeProvider.derive('0')
+  const account = accountProvider.provide(node)
+  expect(account.addressToScript('bcrt1qur2tmednr6e52u9du972nqvua60egwqkf98ps8')).toEqual({
+    stack: [
+      { code: 0, type: 'OP_0' },
+      { hex: 'e0d4bde5b31eb34570ade17ca9819cee9f943816', type: 'OP_PUSHDATA' }
+    ]
+  })
+})
