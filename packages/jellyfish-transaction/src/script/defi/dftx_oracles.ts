bd5d409c408502cbefb6cb42b014bdf1abfb0af2
monstrobishi <monstrobishi@gmail.com>
2021-05-18 22:50:31 +0800

    Added implementation for other types

--- packages/jellyfish-transaction/src/script/defi/dftx_oracles.ts
+++ packages/jellyfish-transaction/src/script/defi/dftx_oracles.ts
@@ -52,3 +52,83 @@ export class CAppointOracle extends ComposableBuffer<AppointOracle> {
     ]
   }
 }
+
+/**
+ * RemoveOracle DeFi Transaction
+ */
+ export interface RemoveOracle {
+  script: Script // --------------------| n = VarUInt{1-9 bytes}, + n bytes
+  oracleId: string // -------------------| string
+}
+
+/**
+ * Composable RemoveOracle, C stands for Composable.
+ * Immutable by design, bi-directional fromBuffer, toBuffer deep composer.
+ */
+export class CRemoveOracle extends ComposableBuffer<RemoveOracle> {
+  static OP_CODE = 0x68
+  static OP_NAME = 'DEFI_OP_REMOVE_ORACLE'
+
+  composers (ao: RemoveOracle): BufferComposer[] {
+    return [
+      ComposableBuffer.single<Script>(() => ao.script, v => ao.script = v, v => new CScript(v)),
+      ComposableBuffer.single<string>(() => ao.oracleId, v => ao.oracleId = v, v => SmartBuffer.fromBuffer(Buffer.from(v, 'ascii')))
+    ]
+  }
+}
+
+
+/**
+ * UpdateOracle DeFi Transaction
+ */
+ export interface UpdateOracle {
+  oracleId: string // -------------------| string
+  script: Script // --------------------| n = VarUInt{1-9 bytes}, + n bytes
+  weightage: number // -------------------| 1 byte unsigned
+  pricefeeds: CurrencyPair[] // -----------------| array of strings each max 8 bytes
+}
+
+/**
+ * Composable UpdateOracle, C stands for Composable.
+ * Immutable by design, bi-directional fromBuffer, toBuffer deep composer.
+ */
+export class CUpdateOracle extends ComposableBuffer<UpdateOracle> {
+  static OP_CODE = 0x74
+  static OP_NAME = 'DEFI_OP_UPDATE_ORACLE'
+
+  composers (ao: UpdateOracle): BufferComposer[] {
+    return [
+      ComposableBuffer.single<string>(() => ao.oracleId, v => ao.oracleId = v, v => SmartBuffer.fromBuffer(Buffer.from(v, 'ascii'))),
+      ComposableBuffer.single<Script>(() => ao.script, v => ao.script = v, v => new CScript(v)),
+      ComposableBuffer.uInt8(() => ao.weightage, v => ao.weightage = v),
+      ComposableBuffer.varUIntArray(() => ao.pricefeeds, v => ao.pricefeeds = v, v => new CCurrencyPair(v))
+    ]
+  }
+}
+
+
+/**
+ * SetOracleData DeFi Transaction
+ */
+ export interface SetOracleData {
+  oracleId: string // -------------------| string
+  timestamp: number // -------------------| 4 bytes unsigned
+  prices: CurrencyPair[] // -----------------| array of strings each max 8 bytes
+}
+
+/**
+ * Composable SetOracleData, C stands for Composable.
+ * Immutable by design, bi-directional fromBuffer, toBuffer deep composer.
+ */
+export class CSetOracleData extends ComposableBuffer<SetOracleData> {
+  static OP_CODE = 0x79
+  static OP_NAME = 'DEFI_OP_SET_ORACLE_DATA'
+
+  composers (ao: SetOracleData): BufferComposer[] {
+    return [
+      ComposableBuffer.single<string>(() => ao.oracleId, v => ao.oracleId = v, v => SmartBuffer.fromBuffer(Buffer.from(v, 'ascii'))),
+      ComposableBuffer.uInt32(() => ao.timestamp, v => ao.timestamp = v),
+      ComposableBuffer.varUIntArray(() => ao.prices, v => ao.prices = v, v => new CCurrencyPair(v))
+    ]
+  }
+}
