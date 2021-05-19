0d4c45d82f9c46c7c7ea43554f5e6109e3d07f87
ivan <ivan@zynesis.com>
2021-05-19 14:06:08 +0800

    try fix build error

--- packages/jellyfish-transaction-builder/src/txn/txn_builder_account.ts
+++ packages/jellyfish-transaction-builder/src/txn/txn_builder_account.ts
@@ -1,4 +1,4 @@
-import { OP_CODES, Script, TransactionSegWit } from '@defichain/jellyfish-transaction'
+import { OP_CODES, Script, TransactionSegWit } from '@defichain/jellyfish-transaction/dist'
 import { UtxosToAccount } from '@defichain/jellyfish-transaction/dist/script/defi/dftx_account'
 import { P2WPKHTxnBuilder } from './txn_builder'
 import { TxnBuilderError, TxnBuilderErrorType } from './txn_builder_error'
