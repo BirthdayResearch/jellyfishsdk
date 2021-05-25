0aee6fb8d362df32232e3eda181e404bd067253b
Ahmed Hilali <monstrobishi@gmail.com>
2021-05-25 12:16:55 +0800

    Updated test

--- packages/jellyfish-transaction-builder/__tests__/txn/txn_builder_appoint_oracle.test.ts
+++ packages/jellyfish-transaction-builder/__tests__/txn/txn_builder_appoint_oracle.test.ts
@@ -1,120 +1,67 @@
-// import BigNumber from 'bignumber.js'
-// import { MasterNodeRegTestContainer } from '@defichain/testcontainers'
-// import {
-//   appointOracle
-// } from '@defichain/testing'
-// import { getProviders, MockProviders } from '../provider.mock'
-// import { P2WPKHTransactionBuilder } from '../../src'
-// import { fundEllipticPair, sendTransaction } from '../test.utils'
-// import { JsonRpcClient } from '@defichain/jellyfish-api-jsonrpc'
+import { MasterNodeRegTestContainer } from '@defichain/testcontainers'
+import { getProviders, MockProviders } from '../provider.mock'
+import { P2WPKHTransactionBuilder } from '../../src'
+import { fundEllipticPair, sendTransaction } from '../test.utils'
 
-// const container = new MasterNodeRegTestContainer()
-// let providers: MockProviders
-// let builder: P2WPKHTransactionBuilder
-// let jsonRpc: JsonRpcClient
+const container = new MasterNodeRegTestContainer()
+let providers: MockProviders
+let builder: P2WPKHTransactionBuilder
 
-// beforeAll(async () => {
-//   await container.start()
-//   await container.waitForReady()
-//   await container.waitForWalletCoinbaseMaturity()
-//   jsonRpc = new JsonRpcClient(await container.getCachedRpcUrl())
+beforeAll(async () => {
+  await container.start()
+  await container.waitForReady()
+  await container.waitForWalletCoinbaseMaturity()
 
-//   providers = await getProviders(container)
-//   builder = new P2WPKHTransactionBuilder(providers.fee, providers.prevout, providers.elliptic)
+  providers = await getProviders(container)
+  builder = new P2WPKHTransactionBuilder(providers.fee, providers.prevout, providers.elliptic)
 
-//   // creating DFI-* pool pairs and funding liquidity
-//   for (const symbol of Object.keys(pairs)) {
-//     pairs[symbol].tokenB = await createToken(container, symbol)
-//     await mintTokens(container, symbol, { mintAmount: 10000 })
-//     await createPoolPair(container, 'DFI', symbol)
-//   }
+  // Prep 1000 DFI Token for testing
+  await container.waitForWalletBalanceGTE(1001)
+})
 
-//   // Prep 1000 DFI Token for testing
-//   await container.waitForWalletBalanceGTE(1001)
-//   await utxosToAccount(container, 1000)
-// })
+afterAll(async () => {
+  await container.stop()
+})
 
-// afterAll(async () => {
-//   await container.stop()
-// })
+describe('appoint oracle', () => {
+  beforeEach(async () => {
+    await providers.randomizeEllipticPair()
+    await container.waitForWalletBalanceGTE(1)
+  })
 
-// describe('DFI to DOG', () => {
-//   let addressLP: string
-//   let amountLP: BigNumber
+  afterEach(async () => {
+    await container.waitForWalletBalanceGTE(1)
+  })
 
-//   beforeEach(async () => {
-//     await providers.randomizeEllipticPair()
-//     await container.waitForWalletBalanceGTE(1)
+  it('should appoint oracle', async () => {
+    // Fund 10 DFI UTXO
+    await fundEllipticPair(container, providers.ellipticPair, 10)
+    await providers.setupMocks() // required to move utxos
 
-//     addressLP = await container.getNewAddress()
-//     amountLP = await addPoolLiquidity(container, {
-//       tokenA: 'DFI',
-//       amountA: 100,
-//       tokenB: 'DOG',
-//       amountB: 100,
-//       shareAddress: addressLP
-//     })
-//   })
+    // Appoint Oracle
+    const script = await providers.elliptic.script()
+    const txn = await builder.oracles.appointOracle({
+      script: script,
+      weightage: 1,
+      pricefeeds: [
+        {
+          token: 'TEST',
+          currency: 'USD'
+        }
+      ]
+    }, script)
 
-//   afterEach(async () => {
-//     await container.waitForWalletBalanceGTE(1)
-//     await removePoolLiquidity(container, {
-//       address: addressLP,
-//       amountLP: amountLP,
-//       tokenLP: 'DFI-DOG'
-//     })
-//   })
+    // Ensure the created txn is correct.
+    const outs = await sendTransaction(container, txn)
+    expect(outs[0].value).toStrictEqual(0)
+    expect(outs[1].value).toBeLessThan(10)
+    expect(outs[1].value).toBeGreaterThan(9.999)
+    expect(outs[1].scriptPubKey.addresses[0]).toStrictEqual(await providers.getAddress())
 
-//   it('should poolSwap from DFI to DOG', async () => {
-//     // Fund 10 DFI UTXO
-//     await fundEllipticPair(container, providers.ellipticPair, 10)
-//     // Fund 10 DFI TOKEN
-//     await providers.setupMocks() // required to move utxos
-//     await utxosToAccount(container, 100, { address: await providers.getAddress() })
-//     // Fund 10 DOG TOKEN
-//     await sendTokensToAddress(container, await providers.getAddress(), 10, 'DOG')
-
-//     // Perform SWAP
-//     const script = await providers.elliptic.script()
-//     const txn = await builder.dex.poolSwap({
-//       fromScript: script,
-//       fromTokenId: pairs.DOG.tokenA,
-//       fromAmount: new BigNumber('10'),
-//       toScript: script,
-//       toTokenId: pairs.DOG.tokenB,
-//       maxPrice: {
-//         integer: new BigNumber('9223372036854775807'),
-//         fraction: new BigNumber('9223372036854775807')
-//       }
-//     }, script)
-
-//     // Ensure the created txn is correct.
-//     const outs = await sendTransaction(container, txn)
-//     expect(outs[0].value).toStrictEqual(0)
-//     expect(outs[1].value).toBeLessThan(10)
-//     expect(outs[1].value).toBeGreaterThan(9.999)
-//     expect(outs[1].scriptPubKey.addresses[0]).toStrictEqual(await providers.getAddress())
-
-//     // Ensure your balance is correct
-//     const account = await jsonRpc.account.getAccount(await providers.getAddress())
-//     expect(account).toContain('90.00000000@DFI')
-//     expect(account).toContain('19.09090910@DOG')
-
-//     const poolPair = await jsonRpc.poolpair.getPoolPair('DFI-DOG', true)
-//     const pair = Object.values(poolPair)[0]
-
-//     // Ensure correct pp liquidity and reverse balances
-//     expect(pair.totalLiquidity.toFixed(8)).toStrictEqual('100.00000000')
-//     expect(pair.reserveA.toFixed(8)).toStrictEqual('110.00000000')
-//     expect(pair.reserveB.toFixed(8)).toStrictEqual('90.90909090') // 100-9.09090910
-
-//     // Ensure you don't send all your balance away during poolswap
-//     const prevouts = await providers.prevout.all()
-//     expect(prevouts.length).toStrictEqual(1)
-//     expect(prevouts[0].value.toNumber()).toBeLessThan(10)
-//     expect(prevouts[0].value.toNumber()).toBeGreaterThan(9.999)
-//   })
-// })
-
-// // TODO(jellyfish): test alternative poolpair
-// // TODO(jellyfish): test alternative maxPrice
+    // Ensure you don't send all your balance away during appoint oracle
+    const prevouts = await providers.prevout.all()
+    expect(prevouts.length).toStrictEqual(1)
+    expect(prevouts[0].value.toNumber()).toBeLessThan(10)
+    expect(prevouts[0].value.toNumber()).toBeGreaterThan(9.999)
+  })
+})
