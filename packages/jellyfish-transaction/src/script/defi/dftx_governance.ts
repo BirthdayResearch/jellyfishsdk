3753b22a862e4c8d108a49a81e3eb20ed100e0bb
ivan <ivan@zynesis.com>
2021-05-27 12:32:24 +0800

    refactor slight better depth

--- packages/jellyfish-transaction/src/script/defi/dftx_governance.ts
+++ packages/jellyfish-transaction/src/script/defi/dftx_governance.ts
@@ -11,9 +11,64 @@ export interface LiqPoolSplit {
   value: BigNumber // ------------------| 8 bytes unsigned
 }
 
+export class CLiqPoolSplit extends ComposableBuffer<LiqPoolSplit> {
+  composers (lps: LiqPoolSplit): BufferComposer[] {
+    return [
+      ComposableBuffer.uInt32(() => lps.tokenId, v => lps.tokenId = v),
+      ComposableBuffer.satoshiAsBigNumber(() => lps.value, v => lps.value = v)
+    ]
+  }
+}
+
 export interface GovernanceVar {
   key: string // -----------------------| VarUInt{1-9 bytes}
   value: BigNumber | LiqPoolSplit[] // -| VarUInt{8 OR 1 + n * 12 bytes}, case LiqPoolSplit: first byte = array len
+}
+
+export class CGovernanceVar extends ComposableBuffer<GovernanceVar> {
+  private keyLength!: number
+
+  composers (gv: GovernanceVar): BufferComposer[] {
+    return [
+      ComposableBuffer.uInt8(() => gv.key.length, v => this.keyLength = v),
+      {
+        fromBuffer: (buffer: SmartBuffer): void => {
+          gv.key = Buffer.from(buffer.readBuffer(this.keyLength)).toString('utf-8')
+        },
+        toBuffer: (buffer: SmartBuffer): void => {
+          buffer.writeBuffer(Buffer.from(gv.key, 'utf-8'))
+        }
+      },
+      {
+        fromBuffer: (buffer: SmartBuffer): void => {
+          if (gv.key === 'LP_DAILY_DFI_REWARD') {
+            gv.value = readBigNumberUInt64(buffer).div('1e8')
+          } else if (gv.key === 'LP_SPLITS') {
+            gv.value = []
+            const configLen = buffer.readUInt8()
+            for (let i = 0; i < configLen; i++) {
+              gv.value.push(new CLiqPoolSplit(buffer).toObject())
+            }
+          } else {
+            throw new Error(`Unrecognized Governance Variable type: ${gv.key}`)
+          }
+        },
+        toBuffer: (buffer: SmartBuffer): void => {
+          if (gv.key === 'LP_DAILY_DFI_REWARD') {
+            writeBigNumberUInt64((gv.value as BigNumber).times('1e8'), buffer)
+          } else if (gv.key === 'LP_SPLITS') {
+            const lpss = gv.value as LiqPoolSplit[]
+            buffer.writeUInt8(lpss.length)
+            lpss.forEach(lps =>
+              new CLiqPoolSplit(lps).toBuffer(buffer)
+            )
+          } else {
+            throw new Error(`Unrecognized Governance Variable type: ${gv.key}`)
+          }
+        }
+      }
+    ]
+  }
 }
 
 export interface SetGovernance {
@@ -34,56 +89,13 @@ export class CSetGovernance extends ComposableBuffer<SetGovernance> {
         fromBuffer: (buffer: SmartBuffer): void => {
           gvs.governanceVars = []
           while (buffer.remaining() > 0) {
-            const keyLen = buffer.readUInt8()
-            const key = Buffer.from(buffer.readBuffer(keyLen)).toString('utf-8')
-
-            if (key === 'LP_DAILY_DFI_REWARD') {
-              gvs.governanceVars.push({
-                key,
-                value: readBigNumberUInt64(buffer).div('1e8')
-              })
-            } else if (key === 'LP_SPLITS') {
-              const liqSplits: LiqPoolSplit[] = []
-
-              const lpsArrLen = buffer.readUInt8()
-              for (let i = 0; i < lpsArrLen; i++) {
-                const tokenId = buffer.readUInt32LE()
-                const split = readBigNumberUInt64(buffer).div('1e8')
-                liqSplits.push({
-                  tokenId,
-                  value: split
-                })
-              }
-              gvs.governanceVars.push({
-                key,
-                value: liqSplits
-              })
-            } else {
-              throw new Error(`Unrecognized Governance Variable type: ${key}`)
-            }
+            gvs.governanceVars.push(new CGovernanceVar(buffer).toObject())
           }
         },
         toBuffer: (buffer: SmartBuffer): void => {
-          for (let i = 0; i < gvs.governanceVars.length; i++) {
-            const { key, value } = gvs.governanceVars[i]
-            buffer.writeUInt8(key.length)
-            buffer.writeBuffer(Buffer.from(key, 'utf-8'))
-
-            if (key === 'LP_DAILY_DFI_REWARD') {
-              writeBigNumberUInt64((value as BigNumber).times('1e8'), buffer)
-            } else if (key === 'LP_SPLITS') {
-              const liqSplits = value as LiqPoolSplit[]
-              buffer.writeUInt8(liqSplits.length)
-
-              for (let i = 0; i < liqSplits.length; i++) {
-                const ls = liqSplits[i]
-                buffer.writeUInt32LE(ls.tokenId)
-                writeBigNumberUInt64(ls.value.times('1e8'), buffer)
-              }
-            } else {
-              throw new Error(`Unrecognized Governance Variable type: ${key}`)
-            }
-          }
+          gvs.governanceVars.forEach(gv =>
+            new CGovernanceVar(gv).toBuffer(buffer)
+          )
         }
       }
     ]
