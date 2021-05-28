30b848331f89cb8b4582c42caf5bfa52b3952424
jingyi2811 <jingyi2811@gmail.com>
2021-05-28 16:41:53 +0800

    Minor

--- packages/jellyfish-api-core/__tests__/category/account/accountHistory.test.ts
+++ packages/jellyfish-api-core/__tests__/category/account/accountHistory.test.ts
@@ -1,9 +1,9 @@
-import waitForExpect from 'wait-for-expect'
-import { AccountHistoryCountOptions, TxType } from '../../../src/category/account'
 import { MasterNodeRegTestContainer } from '@defichain/testcontainers'
 import { ContainerAdapterClient } from '../../container_adapter_client'
+import waitForExpect from 'wait-for-expect'
+import { AccountHistoryCountOptions, TxType } from '../../../src/category/account'
 
-describe('accountHistoryCount', () => {
+describe('masternode', () => {
   const container = new MasterNodeRegTestContainer()
   const client = new ContainerAdapterClient(container)
 
@@ -11,13 +11,23 @@ describe('accountHistoryCount', () => {
     await container.start()
     await container.waitForReady()
     await container.waitForWalletCoinbaseMaturity()
-    await container.waitForWalletBalanceGTE(100)
-    await createToken(await container.getNewAddress(), 'DBTC', 200)
+    await setup()
   })
 
   afterAll(async () => {
     await container.stop()
   })
+
+  async function setup (): Promise<void> {
+    const from = await container.call('getnewaddress')
+    await createToken(from, 'DBTC', 200)
+
+    const to = await accountToAccount('DBTC', 5, from)
+    await accountToAccount('DBTC', 18, from, to)
+
+    await createToken(from, 'DETH', 200)
+    await accountToAccount('DETH', 46, from)
+  }
 
   async function createToken (address: string, symbol: string, amount: number): Promise<void> {
     const metadata = {
@@ -28,6 +38,7 @@ describe('accountHistoryCount', () => {
       tradeable: true,
       collateralAddress: address
     }
+    await container.waitForWalletBalanceGTE(101)
     await container.call('createtoken', [metadata])
     await container.generate(1)
 
@@ -35,79 +46,90 @@ describe('accountHistoryCount', () => {
     await container.generate(1)
   }
 
-  it('should get accountHistoryCount', async () => {
-    await waitForExpect(async () => {
-      const count = await client.account.historyCount()
+  async function accountToAccount (symbol: string, amount: number, from: string, _to = ''): Promise<string> {
+    const to = _to !== '' ? _to : await container.call('getnewaddress')
 
-      expect(typeof count).toBe('number')
-      expect(count).toBeGreaterThanOrEqual(0)
+    await container.call('accounttoaccount', [from, { [to]: `${amount.toString()}@${symbol}` }])
+    await container.generate(1)
+
+    return to
+  }
+
+  describe('accountHistoryCount', () => {
+    it('should get accountHistoryCount', async () => {
+      await waitForExpect(async () => {
+        const count = await client.account.historyCount()
+
+        expect(typeof count).toBe('number')
+        expect(count).toBeGreaterThanOrEqual(0)
+      })
     })
-  })
 
-  it('should get accountHistoryCount with owner as all', async () => {
-    await waitForExpect(async () => {
-      const count = await client.account.historyCount('all')
+    it('should get accountHistoryCount with owner as all', async () => {
+      await waitForExpect(async () => {
+        const count = await client.account.historyCount('all')
 
-      expect(typeof count).toBe('number')
-      expect(count).toBeGreaterThanOrEqual(0)
+        expect(typeof count).toBe('number')
+        expect(count).toBeGreaterThanOrEqual(0)
+      })
     })
-  })
 
-  it('should get accountHistoryCount with no_rewards option', async () => {
-    await waitForExpect(async () => {
-      const options: AccountHistoryCountOptions = {
-        no_rewards: true
-      }
-      const count = await client.account.historyCount('mine', options)
+    it('should get accountHistoryCount with no_rewards option', async () => {
+      await waitForExpect(async () => {
+        const options: AccountHistoryCountOptions = {
+          no_rewards: true
+        }
+        const count = await client.account.historyCount('mine', options)
 
-      expect(typeof count).toBe('number')
-      expect(count).toBeGreaterThanOrEqual(0)
+        expect(typeof count).toBe('number')
+        expect(count).toBeGreaterThanOrEqual(0)
+      })
     })
-  })
 
-  it('should get accountHistoryCount with token option', async () => {
-    await waitForExpect(async () => {
-      const options1: AccountHistoryCountOptions = {
-        token: 'DBTC'
-      }
-      const options2: AccountHistoryCountOptions = {
-        token: 'DFI'
-      }
-      const countWithDBTC = await client.account.historyCount('mine', options1)
-      const countWithDFI = await client.account.historyCount('mine', options2)
+    it('should get accountHistoryCount with token option', async () => {
+      await waitForExpect(async () => {
+        const options1: AccountHistoryCountOptions = {
+          token: 'DBTC'
+        }
+        const options2: AccountHistoryCountOptions = {
+          token: 'DETH'
+        }
+        const countWithDBTC = await client.account.historyCount('mine', options1)
+        const countWithDETH = await client.account.historyCount('mine', options2)
 
-      expect(typeof countWithDBTC).toBe('number')
-      expect(typeof countWithDFI).toBe('number')
-      expect(countWithDBTC).toStrictEqual(1)
-      expect(countWithDFI).toStrictEqual(111)
+        expect(typeof countWithDBTC).toBe('number')
+        expect(typeof countWithDETH).toBe('number')
+        expect(countWithDBTC).toStrictEqual(5)
+        expect(countWithDETH).toStrictEqual(3)
+      })
     })
-  })
 
-  it('should get accountHistory with txtype option', async () => {
-    await waitForExpect(async () => {
-      const options: AccountHistoryCountOptions = {
-        txtype: TxType.MINT_TOKEN
-      }
-      const count = await client.account.historyCount('mine', options)
+    it('should get accountHistory with txtype option', async () => {
+      await waitForExpect(async () => {
+        const options: AccountHistoryCountOptions = {
+          txtype: TxType.MINT_TOKEN
+        }
+        const count = await client.account.historyCount('mine', options)
 
-      expect(typeof count).toBe('number')
-      expect(count).toBeGreaterThanOrEqual(0)
+        expect(typeof count).toBe('number')
+        expect(count).toBeGreaterThanOrEqual(0)
+      })
     })
-  })
 
-  it('should get different count for different txtypes', async () => {
-    await waitForExpect(async () => {
-      const options1: AccountHistoryCountOptions = {
-        txtype: TxType.MINT_TOKEN
-      }
-      const options2: AccountHistoryCountOptions = {
-        txtype: TxType.POOL_SWAP
+    it('should get different count for different txtypes', async () => {
+      await waitForExpect(async () => {
+        const options1: AccountHistoryCountOptions = {
+          txtype: TxType.MINT_TOKEN
+        }
+        const options2: AccountHistoryCountOptions = {
+          txtype: TxType.POOL_SWAP
 
-      }
-      const count1 = await client.account.historyCount('mine', options1)
-      const count2 = await client.account.historyCount('mine', options2)
+        }
+        const count1 = await client.account.historyCount('mine', options1)
+        const count2 = await client.account.historyCount('mine', options2)
 
-      expect(count1 === count2).toStrictEqual(false)
+        expect(count1 === count2).toStrictEqual(false)
+      })
     })
   })
 })
