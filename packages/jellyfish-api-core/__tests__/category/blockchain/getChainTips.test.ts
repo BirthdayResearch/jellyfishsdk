a7a9637ed48c13e150ebe37bcd55396a61615868
jingyi2811 <jingyi2811@gmail.com>
2021-05-28 15:12:17 +0800

    Refactor blockchain RPCs tests (#305)
    * Refactor account RPCs tests
    
    * Refactor blockchain RPCs tests
    
    * minor
    
    * test
    
    * Refactor blockchain rpcs
    
    * Add blockchain rpcs
    
    * minor
    
    * test
    
    * Refactor blockchain rpcs
    
    * Add blockchain rpcs
    
    * Refactor blockchain rpcs
    
    * Add blockchain rpcs
    
    * Add missing rpcs
    
    * Remove file
    
    * Remove describe

---
+++ packages/jellyfish-api-core/__tests__/category/blockchain/getChainTips.test.ts
@@ -0,0 +1,29 @@
+import { MasterNodeRegTestContainer } from '@defichain/testcontainers'
+import { ContainerAdapterClient } from '../../container_adapter_client'
+import { blockchain } from '../../../src'
+
+describe('ChainTips', () => {
+  const container = new MasterNodeRegTestContainer()
+  const client = new ContainerAdapterClient(container)
+
+  beforeAll(async () => {
+    await container.start()
+    await container.waitForReady()
+  })
+
+  afterAll(async () => {
+    await container.stop()
+  })
+
+  it('should getChainTips', async () => {
+    const chainTips: blockchain.ChainTip[] = await client.blockchain.getChainTips()
+    for (let i = 0; i < chainTips.length; i += 1) {
+      const data = chainTips[i]
+      expect(data.height).toBeGreaterThan(0)
+      expect(typeof data.hash).toStrictEqual('string')
+      expect(data.hash.length).toStrictEqual(64)
+      expect(data.branchlen).toBeGreaterThanOrEqual(0)
+      expect(['invalid', 'headers-only', 'valid-headers', 'valid-fork', 'active'].includes(data.status)).toStrictEqual(true)
+    }
+  })
+})
