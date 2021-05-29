383a8bfee0391e71a2acebd43d54e45e097ec5f8
websterlcl <w.canonbrother@gmail.com>
2021-05-30 00:04:56 +0800

    refine desc

--- packages/jellyfish-transaction/__tests__/script/defi/dftx_pool/PoolUpdatePair.test.ts
+++ packages/jellyfish-transaction/__tests__/script/defi/dftx_pool/PoolUpdatePair.test.ts
@@ -8,7 +8,7 @@ import { toBuffer, toOPCodes } from '../../../../src/script/_buffer'
 it('should bi-directional buffer-object-buffer', () => {
   const fixtures = [
     // regtest fixtures
-    // height >= ClarkeQuayHeight (include customRewards)
+    // height >= ClarkeQuayHeight (encoding includes customRewards)
     // status, commission
     '6a144466547875020000000100e1f505000000000000',
     // ownerAddress
@@ -20,7 +20,7 @@ it('should bi-directional buffer-object-buffer', () => {
     // all
     '6a434466547875020000000120a107000000000017a914fd190704714762e6c30eb5b39071c1a52e6130ad87020000000000ca9a3b00000000010000000094357700000000',
 
-    // height < ClarkeQuayHeight (exclude customRewards)
+    // height < ClarkeQuayHeight (exclude customRewards from being encoded)
     // all
     '6a2a44665478750300000001c0cf6a000000000017a9141205616a55fbbdd91ec97e222fae709acc4fd2c887'
   ]
