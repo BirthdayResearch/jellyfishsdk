9b85cba6bf591d300f2c84bb586ecddee84efa1d
Siradji <siradjiawoual@gmail.com>
2021-06-02 00:20:35 +0100

    Control.ts OP_CODES

--- packages/jellyfish-transaction/__tests__/script/mapping.test.ts
+++ packages/jellyfish-transaction/__tests__/script/mapping.test.ts
@@ -153,4 +153,40 @@ describe('All mapped OP_CODES are setup properly: (static, hex, num, asm)', () =
   it('OP_CHECKMULTISIGVERIFY', () => {
     expectOPCode(script.OP_CODES.OP_CHECKMULTISIGVERIFY, script.OP_CHECKMULTISIGVERIFY, 'OP_CHECKMULTISIGVERIFY', 0xaf, 'af')
   })
+
+  it('OP_NOP', () => {
+    expectOPCode(script.OP_CODES.OP_NOP, script.OP_NOP, 'OP_NOP', 0X61, '61')
+  })
+
+  it('OP_VER', () => {
+    expectOPCode(script.OP_CODES.OP_VER, script.OP_VER, 'OP_VER', 0X62, '62')
+  })
+
+  it('OP_IF', () => {
+    expectOPCode(script.OP_CODES.OP_IF, script.OP_IF, 'OP_IF', 0X63, '63')
+  })
+
+  it('OP_NOTIF', () => {
+    expectOPCode(script.OP_CODES.OP_NOTIF, script.OP_NOTIF, 'OP_NOTIF', 0X64, '64')
+  })
+
+  it('OP_VERIF', () => {
+    expectOPCode(script.OP_CODES.OP_VERIF, script.OP_VERIF, 'OP_VERIF', 0X65, '65')
+  })
+
+  it('OP_VERNOTIF', () => {
+    expectOPCode(script.OP_CODES.OP_VERNOTIF, script.OP_VERNOTIF, 'OP_VERNOTIF', 0X66, '66')
+  })
+
+  it('OP_ELSE', () => {
+    expectOPCode(script.OP_CODES.OP_ELSE, script.OP_ELSE, 'OP_ELSE', 0X67, '67')
+  })
+
+  it('OP_ENDIF', () => {
+    expectOPCode(script.OP_CODES.OP_ENDIF, script.OP_ENDIF, 'OP_ENDIF', 0X68, '68')
+  })
+
+  it('OP_VERIFY', () => {
+    expectOPCode(script.OP_CODES.OP_VERIFY, script.OP_VERIFY, 'OP_VERIFY', 0X69, '69')
+  })
 })
--- packages/jellyfish-transaction/src/script/control.ts
+++ packages/jellyfish-transaction/src/script/control.ts
@@ -8,3 +8,57 @@ export class OP_RETURN extends StaticCode {
     super(0x6a, 'OP_RETURN')
   }
 }
+
+export class OP_NOP extends StaticCode {
+  constructor () {
+    super(0x61, 'OP_NOP')
+  }
+}
+
+export class OP_VER extends StaticCode {
+  constructor () {
+    super(0x62, 'OP_VER')
+  }
+}
+
+export class OP_IF extends StaticCode {
+  constructor () {
+    super(0x63, 'OP_IF')
+  }
+}
+
+export class OP_NOTIF extends StaticCode {
+  constructor () {
+    super(0x64, 'OP_NOTIF')
+  }
+}
+
+export class OP_VERIF extends StaticCode {
+  constructor () {
+    super(0x65, 'OP_VERIF')
+  }
+}
+
+export class OP_VERNOTIF extends StaticCode {
+  constructor () {
+    super(0x66, 'OP_VERNOTIF')
+  }
+}
+
+export class OP_ELSE extends StaticCode {
+  constructor () {
+    super(0x67, 'OP_ELSE')
+  }
+}
+
+export class OP_ENDIF extends StaticCode {
+  constructor () {
+    super(0x68, 'OP_ENDIF')
+  }
+}
+
+export class OP_VERIFY extends StaticCode {
+  constructor () {
+    super(0x69, 'OP_VERIFY')
+  }
+}
