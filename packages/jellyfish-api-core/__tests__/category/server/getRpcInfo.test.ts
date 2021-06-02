28afbbe4f4de6ebf2859bfb1619f211163ea4b75
aikchun <iam@simaikchun.com>
2021-06-02 15:15:27 +0800

    Removed unnecessary comments.

--- packages/jellyfish-api-core/__tests__/category/server/getRpcInfo.test.ts
+++ packages/jellyfish-api-core/__tests__/category/server/getRpcInfo.test.ts
@@ -20,29 +20,6 @@ describe('Server on masternode', () => {
       const info = await client.server.getRpcInfo()
       expect(info.active_commands.length).toBeGreaterThan(0)
     })
-
-    // const info = await client.mining.getMiningInfo()
-    // const mn1 = info.masternodes[0]
-    //
-    // expect(info.blocks).toBeGreaterThan(0)
-    //
-    // expect(info.currentblockweight).toBeGreaterThan(0)
-    // expect(info.currentblocktx).toStrictEqual(0)
-    //
-    // expect(info.difficulty).toBeDefined()
-    // expect(info.isoperator).toStrictEqual(true)
-    //
-    // expect(mn1.masternodeid).toBeDefined()
-    // expect(mn1.masternodeoperator).toBeDefined()
-    // expect(mn1.masternodestate).toStrictEqual('ENABLED')
-    // expect(mn1.generate).toStrictEqual(true)
-    // expect(mn1.mintedblocks).toStrictEqual(0)
-    // expect(mn1.lastblockcreationattempt).not.toStrictEqual('0')
-    //
-    // expect(info.networkhashps).toBeGreaterThan(0)
-    // expect(info.pooledtx).toStrictEqual(0)
-    // expect(info.chain).toStrictEqual('regtest')
-    // expect(info.warnings).toStrictEqual('')
   })
 })
 
@@ -64,28 +41,5 @@ describe('Server not on masternode', () => {
       const info = await client.server.getRpcInfo()
       expect(info.active_commands.length).toBeGreaterThan(0)
     })
-
-    // const info = await client.mining.getMiningInfo()
-    // const mn1 = info.masternodes[0]
-    //
-    // expect(info.blocks).toBeGreaterThan(0)
-    //
-    // expect(info.currentblockweight).toBeGreaterThan(0)
-    // expect(info.currentblocktx).toStrictEqual(0)
-    //
-    // expect(info.difficulty).toBeDefined()
-    // expect(info.isoperator).toStrictEqual(true)
-    //
-    // expect(mn1.masternodeid).toBeDefined()
-    // expect(mn1.masternodeoperator).toBeDefined()
-    // expect(mn1.masternodestate).toStrictEqual('ENABLED')
-    // expect(mn1.generate).toStrictEqual(true)
-    // expect(mn1.mintedblocks).toStrictEqual(0)
-    // expect(mn1.lastblockcreationattempt).not.toStrictEqual('0')
-    //
-    // expect(info.networkhashps).toBeGreaterThan(0)
-    // expect(info.pooledtx).toStrictEqual(0)
-    // expect(info.chain).toStrictEqual('regtest')
-    // expect(info.warnings).toStrictEqual('')
   })
 })
