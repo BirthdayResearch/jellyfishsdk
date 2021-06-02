837428a1accfd1f27f8f2222f8e50c91d731c745
Fuxing Loh <git@fuxing.dev>
2021-06-02 13:46:39 +0800

    fix tests relating to this change

--- packages/jellyfish-api-core/__tests__/category/blockchain/getBlockHash.test.ts
+++ packages/jellyfish-api-core/__tests__/category/blockchain/getBlockHash.test.ts
@@ -1,6 +1,5 @@
 import { MasterNodeRegTestContainer } from '@defichain/testcontainers'
 import { ContainerAdapterClient } from '../../container_adapter_client'
-import waitForExpect from 'wait-for-expect'
 
 describe('BlockHash', () => {
   const container = new MasterNodeRegTestContainer()
@@ -9,6 +8,7 @@ describe('BlockHash', () => {
   beforeAll(async () => {
     await container.start()
     await container.waitForReady()
+    await container.waitForBlock(1)
   })
 
   afterAll(async () => {
@@ -16,13 +16,13 @@ describe('BlockHash', () => {
   })
 
   it('should getBlockHash', async () => {
-    await waitForExpect(async () => {
-      const info = await client.blockchain.getBlockchainInfo()
-      expect(info.blocks).toBeGreaterThan(1)
-    })
-
     const blockHash: string = await client.blockchain.getBlockHash(1)
     expect(typeof blockHash).toStrictEqual('string')
     expect(blockHash.length).toStrictEqual(64)
+  })
+
+  it('should get regtest genesis block hash', async () => {
+    const genesisHash = await client.blockchain.getBlockHash(0)
+    expect(genesisHash).toStrictEqual('d744db74fb70ed42767ae028a129365fb4d7de54ba1b6575fb047490554f8a7b')
   })
 })
--- packages/jellyfish-api-core/__tests__/category/blockchain/getTxOut.test.ts
+++ packages/jellyfish-api-core/__tests__/category/blockchain/getTxOut.test.ts
@@ -1,4 +1,4 @@
-import { MasterNodeRegTestContainer } from '@defichain/testcontainers'
+import { GenesisKeys, MasterNodeRegTestContainer } from '@defichain/testcontainers'
 import { ContainerAdapterClient } from '../../container_adapter_client'
 import { BigNumber } from '../../../src'
 
@@ -9,25 +9,63 @@ describe('TxOut', () => {
   beforeAll(async () => {
     await container.start()
     await container.waitForReady()
+    await container.waitForBlock(1)
   })
 
   afterAll(async () => {
     await container.stop()
   })
 
-  it('should getTxOut', async () => {
-    const txId = '00eed320c213f506038fa29f77d4d2535232fa97b7789ff6fb516c63201c5e44'
+  it('should getTxOut of regtest 100M DFI - 0', async () => {
+    const txId = '9fb9c46b1d12dae8a4a35558f7ef4b047df3b444b1ead61d334e4f187f5f58b7'
     const txOut = await client.blockchain.getTxOut(txId, 0)
+
     expect(txOut).toHaveProperty('bestblock')
     expect(txOut.confirmations).toBeGreaterThanOrEqual(1)
     expect(txOut.value instanceof BigNumber).toStrictEqual(true)
-    expect(txOut.value.toString()).toStrictEqual('38')
+    expect(txOut.value.toString()).toStrictEqual('100000000')
     expect(txOut.scriptPubKey).toHaveProperty('asm')
     expect(txOut.scriptPubKey).toHaveProperty('hex')
     expect(txOut.scriptPubKey.reqSigs).toBeGreaterThanOrEqual(1)
     expect(txOut.scriptPubKey.type).toStrictEqual('pubkeyhash')
     expect(txOut.scriptPubKey.addresses.length).toBeGreaterThanOrEqual(1)
-    expect(txOut.scriptPubKey.addresses[0]).toStrictEqual('mwsZw8nF7pKxWH8eoKL9tPxTpaFkz7QeLU')
+    expect(txOut.scriptPubKey.addresses[0]).toStrictEqual(GenesisKeys[0].owner.address)
+    expect(txOut.coinbase).toStrictEqual(true)
+  })
+
+  it('should getTxOut of regtest 100M DFI - 1', async () => {
+    const txId = '9fb9c46b1d12dae8a4a35558f7ef4b047df3b444b1ead61d334e4f187f5f58b7'
+    const txOut = await client.blockchain.getTxOut(txId, 1)
+
+    expect(txOut.value.toString()).toStrictEqual('100000000')
+    expect(txOut.scriptPubKey.addresses[0]).toStrictEqual(GenesisKeys[0].operator.address)
+    expect(txOut.coinbase).toStrictEqual(true)
+  })
+
+  it('should getTxOut of regtest 100M DFI - 2', async () => {
+    const txId = '9fb9c46b1d12dae8a4a35558f7ef4b047df3b444b1ead61d334e4f187f5f58b7'
+    const txOut = await client.blockchain.getTxOut(txId, 2)
+
+    expect(txOut.value.toString()).toStrictEqual('100000000')
+    expect(txOut.scriptPubKey.addresses[0]).toStrictEqual(GenesisKeys[1].owner.address)
+    expect(txOut.coinbase).toStrictEqual(true)
+  })
+
+  it('should getTxOut of regtest 100M DFI - 3', async () => {
+    const txId = '9fb9c46b1d12dae8a4a35558f7ef4b047df3b444b1ead61d334e4f187f5f58b7'
+    const txOut = await client.blockchain.getTxOut(txId, 3)
+
+    expect(txOut.value.toString()).toStrictEqual('100000000')
+    expect(txOut.scriptPubKey.addresses[0]).toStrictEqual(GenesisKeys[1].operator.address)
+    expect(txOut.coinbase).toStrictEqual(true)
+  })
+
+  it('should getTxOut of regtest 100M DFI - 4', async () => {
+    const txId = '9fb9c46b1d12dae8a4a35558f7ef4b047df3b444b1ead61d334e4f187f5f58b7'
+    const txOut = await client.blockchain.getTxOut(txId, 4)
+
+    expect(txOut.value.toString()).toStrictEqual('100')
+    expect(txOut.scriptPubKey.addresses[0]).toStrictEqual(GenesisKeys[5].owner.address)
     expect(txOut.coinbase).toStrictEqual(true)
   })
 })
