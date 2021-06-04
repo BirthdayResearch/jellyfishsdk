fd25416070e883dd35e94687fb52496cc9b8421a
ivan <ivan@zynesis.com>
2021-06-04 12:21:55 +0800

    allow custom scrypt param for scryptsy implementation, reduce test difficulty and time

--- packages/jellyfish-wallet-encrypted/src/scryptsy.ts
+++ packages/jellyfish-wallet-encrypted/src/scryptsy.ts
@@ -1,21 +1,32 @@
 import scrypt from 'scryptsy'
 import { ScryptProvider } from './scrypt-storage'
 
-const BIP38_SCRYPT_PARAMS = {
+export interface ScryptParams {
+  N: number
+  r: number
+  p: number
+}
+
+const DEFAULT_SCRYPT_PARAMS: ScryptParams = {
   N: 16384,
   r: 8,
   p: 8
 }
 
 export class SimpleScryptsy implements ScryptProvider {
+  readonly scryptParams: ScryptParams
+  constructor (scryptParams?: ScryptParams) {
+    this.scryptParams = scryptParams !== undefined ? scryptParams : DEFAULT_SCRYPT_PARAMS
+  }
+
   passphraseToKey (passphrase: string, salt: Buffer, keyLength: number): Buffer {
     const secret = Buffer.from(passphrase.normalize('NFC'), 'utf8')
     return scrypt(
       secret,
       salt,
-      BIP38_SCRYPT_PARAMS.N,
-      BIP38_SCRYPT_PARAMS.r,
-      BIP38_SCRYPT_PARAMS.p,
+      this.scryptParams.N,
+      this.scryptParams.r,
+      this.scryptParams.p,
       keyLength
     )
   }
