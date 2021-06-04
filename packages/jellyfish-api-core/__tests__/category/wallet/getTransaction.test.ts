97d2f45e12d8179536ac8f808648122b3e350366
aikchun <iam@simaikchun.com>
2021-06-04 16:07:59 +0800

    Used sendToAddress to set up the transaction to test

--- packages/jellyfish-api-core/__tests__/category/wallet/getTransaction.test.ts
+++ packages/jellyfish-api-core/__tests__/category/wallet/getTransaction.test.ts
@@ -9,27 +9,33 @@ describe('Server on masternode', () => {
   beforeAll(async () => {
     await container.start()
     await container.waitForReady()
+    await container.waitForWalletCoinbaseMaturity()
   })
 
   afterAll(async () => {
     await container.stop()
   })
 
-  it('should getTransaction', async () => {
-    const txid = 'e86c027861cc0af423313f4152a44a83296a388eb51bf1a6dde9bd75bed55fb4'
+  it('should getTransaction from sendToAddress', async () => {
+    const address = 'mwsZw8nF7pKxWH8eoKL9tPxTpaFkz7QeLU'
+    const amount = 0.00001
+    const txid = await client.wallet.sendToAddress(address, amount)
+
     const inWalletTransaction = await client.wallet.getTransaction(txid)
-    expect(inWalletTransaction.details[0].address).toStrictEqual('mwsZw8nF7pKxWH8eoKL9tPxTpaFkz7QeLU')
+
+    expect(inWalletTransaction.details[0].address).toStrictEqual(address)
     expect(typeof inWalletTransaction.txid).toStrictEqual('string')
     expect(typeof inWalletTransaction.amount).toStrictEqual('number')
     expect(typeof inWalletTransaction.confirmations).toStrictEqual('number')
-    expect(typeof inWalletTransaction.blockindex).toStrictEqual('number')
     expect(typeof inWalletTransaction.time).toStrictEqual('number')
     expect(typeof inWalletTransaction.timereceived).toStrictEqual('number')
-    expect(typeof inWalletTransaction.blockhash).toStrictEqual('string')
     expect(typeof inWalletTransaction.hex).toBe('string')
 
-    expect(typeof inWalletTransaction.details[0].vout).toStrictEqual('number')
     expect(typeof inWalletTransaction.details[0].address).toStrictEqual('string')
     expect(Object.values(InWalletTransactionCategory).includes(inWalletTransaction.details[0].category)).toBe(true)
+    expect(typeof inWalletTransaction.details[0].amount).toStrictEqual('number')
+    expect(typeof inWalletTransaction.details[0].label).toStrictEqual('string')
+    expect(typeof inWalletTransaction.details[0].vout).toStrictEqual('number')
+    expect(typeof inWalletTransaction.details[0].abandoned).toStrictEqual('boolean')
   })
 })
