a0b71778605bc0e294795ffd05a7f18c79be5717
jingyi2811 <jingyi2811@gmail.com>
2021-06-06 16:14:07 +0800

    Improve comment text

--- packages/jellyfish-api-core/src/category/oracle.ts
+++ packages/jellyfish-api-core/src/category/oracle.ts
@@ -20,7 +20,7 @@ export class Oracle {
    * @param {UTXO[]} [options.utxos = []]
    * @param {string} [options.utxos.txid]
    * @param {number} [options.utxos.vout]
-   * @return {Promise<string>} txn id for txn created to appoint oracle
+   * @return {Promise<string>} oracleid, also the txn id for txn created to appoint oracle
    */
   async appointOracle (address: string, priceFeeds: PriceFeed[], options: AppointOracleOptions = {}): Promise<string> {
     const { utxos = [] } = options
@@ -34,7 +34,7 @@ export class Oracle {
    * @param {UTXO[]} [utxos = []]
    * @param {string} [utxos.txid]
    * @param {number} [utxos.vout]
-   * @return {Promise<string>} txn id for txn created to remove oracle
+   * @return {Promise<string>} oracleid, also the txn id for txn created to remove oracle
    */
   async removeOracle (oracleid: string, utxos: UTXO[] = []): Promise<string> {
     return await this.client.call('removeoracle', [oracleid, utxos], 'number')
@@ -51,7 +51,7 @@ export class Oracle {
    * @param {UTXO[]} [options.utxos = []]
    * @param {string} [options.utxos.txid]
    * @param {number} [options.utxos.vout]
-   * @return {Promise<string>} txn id for txn created to update oracle
+   * @return {Promise<string>} oracleid, also the txn id for txn created to update oracle
    */
   async updateOracle (oracleid: string, address: string, options: UpdateOracleOptions = {}): Promise<string> {
     const { utxos = [] } = options
