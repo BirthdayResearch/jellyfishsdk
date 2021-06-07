d01237d008f754337745dc650da780404fad03bf
aikchun <iam@simaikchun.com>
2021-06-07 22:52:01 +0800

    Replaced the amount variable used in the it block.

--- packages/jellyfish-api-core/__tests__/category/wallet/getTransaction.test.ts
+++ packages/jellyfish-api-core/__tests__/category/wallet/getTransaction.test.ts
@@ -17,10 +17,9 @@ describe('Server on masternode', () => {
   })
 
   const address = 'mwsZw8nF7pKxWH8eoKL9tPxTpaFkz7QeLU'
-  const amount = 0.00001
 
   it('should getTransaction', async () => {
-    const txid = await client.wallet.sendToAddress(address, amount)
+    const txid = await client.wallet.sendToAddress(address, 0.0001)
     await container.generate(1)
 
     const inWalletTransaction = await client.wallet.getTransaction(txid)
@@ -49,7 +48,7 @@ describe('Server on masternode', () => {
   })
 
   it('should getTransaction with includesWatch false', async () => {
-    const txid = await client.wallet.sendToAddress(address, amount)
+    const txid = await client.wallet.sendToAddress(address, 0.0001)
     await container.generate(1)
 
     const inWalletTransaction = await client.wallet.getTransaction(txid, false)
