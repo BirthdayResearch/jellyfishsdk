6d8ba9f42344578c1314e18e7945c291f691a4a6
Fuxing Loh <git@fuxing.dev>
2021-06-09 01:18:59 +0800

    fixed tsconfig.json

--- jest.config.js
+++ jest.config.js
@@ -2,8 +2,6 @@ module.exports = {
   preset: 'ts-jest',
   testRegex: '((\\.|/)(e2e|test|spec))\\.[jt]sx?$',
   moduleNameMapper: {
-    // NOTE(canonbrother): uncomment it only while test running with this dependency
-    // '@defichain/jellyfish': '<rootDir>/packages/jellyfish/src/jellyfish/src',
     '@defichain/jellyfish-address': '<rootDir>/packages/jellyfish-address/src',
     '@defichain/jellyfish-api-core': '<rootDir>/packages/jellyfish-api-core/src',
     '@defichain/jellyfish-api-jsonrpc': '<rootDir>/packages/jellyfish-api-jsonrpc/src',
