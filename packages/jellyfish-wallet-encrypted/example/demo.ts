825e829ff7704cd743e837e5ac703eb31a8cf5bf
ivan <ivan@zynesis.com>
2021-06-08 17:43:41 +0800

    update some comments in demo

--- packages/jellyfish-wallet-encrypted/example/demo.ts
+++ packages/jellyfish-wallet-encrypted/example/demo.ts
@@ -2,29 +2,44 @@ import { Transaction } from '@defichain/jellyfish-transaction/dist'
 import { EncryptedMnemonicProvider, ScryptStorage, ScryptProvider, Storage, SimpleScryptsy } from '../src'
 
 // Implementation of ScryptProvider depend on your platform, eg: react-native-scrypt
-// if npm package `scryptsy` available on your platform, can use `SimpleScryptsy` direct
 class MyScryptProvider implements ScryptProvider {
   passphraseToKey (passphrase: string, salt: Buffer, keyLength: number): Buffer {
+    // eg: react-native
+    // return require('react-native-scrypt')(passphrase, salt[, N=16384, r=8, p=8, dkLen=keyLength, encoding='legacy'])
     return new SimpleScryptsy().passphraseToKey(passphrase, salt, keyLength)
   }
 }
 
-class MockSecuredStorage implements Storage {
+// a persistent simple string storage (secured) on your platform
+class MockSecuredStorage {
   // use secured storage on your platform, eg: https://github.com/oblador/react-native-keychain
-  inMemory: string | undefined
-  async getter (): Promise<string|undefined> {
-    return this.inMemory
+  encryptedSeed: string | undefined
+  seedHash: string | undefined
+  async getter (key: 'data' | 'hash'): Promise<string|undefined> {
+    return key === 'data' ? this.encryptedSeed : this.seedHash
   }
 
-  async setter (data: string|undefined): Promise<void> {
-    this.inMemory = data
+  async setter (key: 'data' | 'hash', data: string|undefined): Promise<void> {
+    if (key === 'data') {
+      this.encryptedSeed = data
+    } else {
+      this.seedHash = data
+    }
   }
 }
-// create 2 string storages, for encrypted data and dSHA256(data) storing purpose
-const mySecuredStorage = new MockSecuredStorage()
-const mySecuredStorage2 = new MockSecuredStorage()
 
-// eslint-disable-next-line
+const mySecuredStorage = new MockSecuredStorage()
+// create 2 string storages, for encrypted data and dSHA256(data) storing purpose
+const encryptedSeedStorage: Storage = {
+  getter: async (): Promise<string|undefined> => await mySecuredStorage.getter('data'),
+  setter: async (value: string): Promise<void> => await mySecuredStorage.setter('data', value)
+}
+const seedHashStorage: Storage = {
+  getter: async (): Promise<string|undefined> => await mySecuredStorage.getter('hash'),
+  setter: async (value: string): Promise<void> => await mySecuredStorage.setter('hash', value)
+}
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 class CreateMnemoniceWalletPage {
   state = {
     textInput: 'user input new desired passphrase'
@@ -37,11 +52,13 @@ class CreateMnemoniceWalletPage {
       passphrase: this.state.textInput, // to encrypt the seed
       seed: mnemonic.seed,
       options: {
+        // this is DeFiChain testnet prefixes, they should be envvar decided config
+        // find acceptable config from jellyfish-network
         bip32: {
-          public: 0x00000000,
-          private: 0x00000000
+          public: 0x043587cf,
+          private: 0x04358394
         },
-        wif: 0x00
+        wif: 0xef
       }
     })
 
@@ -57,20 +74,21 @@ class CreateMnemoniceWalletPage {
   getScryptStorage (): ScryptStorage {
     return new ScryptStorage(
       new MyScryptProvider(),
-      mySecuredStorage, // to store encrypted mnemonic seed
-      mySecuredStorage2 // to store mnemonic seed hash, for decryption result check
+      encryptedSeedStorage, // to store encrypted mnemonic seed
+      seedHashStorage // to store mnemonic seed hash, for decryption result check
     )
   }
 
   randomMnemonicSeed (): { words: string, seed: Buffer } {
     return {
+      // can use `jellyfish-wallet-mnemonic` `generateMnemonic()`
       words: '24 words to be kept by user',
       seed: Buffer.from('private key')
     }
   }
 }
 
-// eslint-disable-next-line
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 class SignTransactionPage {
   state
 
@@ -86,11 +104,13 @@ class SignTransactionPage {
       scryptStorage: this.getScryptStorage(),
       passphrase: this.state.textInput,
       options: {
+        // this is DeFiChain testnet prefixes, they should be envvar decided config
+        // find acceptable config from jellyfish-network
         bip32: {
-          public: 0x00000000,
-          private: 0x00000000
+          public: 0x043587cf,
+          private: 0x04358394
         },
-        wif: 0x00
+        wif: 0xef
       }
     })
 
@@ -104,8 +124,8 @@ class SignTransactionPage {
   getScryptStorage (): ScryptStorage {
     return new ScryptStorage(
       new MyScryptProvider(),
-      mySecuredStorage, // to store encrypted mnemonic seed
-      mySecuredStorage2 // to store mnemonic seed hash, for decryption result check
+      encryptedSeedStorage, // to store encrypted mnemonic seed
+      seedHashStorage // to store mnemonic seed hash, for decryption result check
     )
   }
 }
