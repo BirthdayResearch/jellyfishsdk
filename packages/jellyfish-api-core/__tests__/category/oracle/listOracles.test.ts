debaa0f3a3626744cef640524740e6f419d2db8c
jingyi2811 <jingyi2811@gmail.com>
2021-06-14 14:42:20 +0800

    Minor

--- packages/jellyfish-api-core/__tests__/category/oracle/listOracles.test.ts
+++ packages/jellyfish-api-core/__tests__/category/oracle/listOracles.test.ts
@@ -5,13 +5,13 @@ describe('Oracle', () => {
   const container = new MasterNodeRegTestContainer()
   const client = new ContainerAdapterClient(container)
 
-  beforeAll(async () => {
+  beforeEach(async () => {
     await container.start()
     await container.waitForReady()
     await container.waitForWalletCoinbaseMaturity()
   })
 
-  afterAll(async () => {
+  afterEach(async () => {
     const data = await container.call('listoracles')
 
     for (let i = 0; i < data.length; i += 1) {
@@ -29,8 +29,8 @@ describe('Oracle', () => {
       { token: 'TESLA', currency: 'USD' }
     ]
 
-    const oracleid1 = await container.call('appointoracle', [await container.getNewAddress(), priceFeeds, 1])
-    const oracleid2 = await container.call('appointoracle', [await container.getNewAddress(), priceFeeds, 2])
+    await container.call('appointoracle', [await container.getNewAddress(), priceFeeds, 1])
+    await container.call('appointoracle', [await container.getNewAddress(), priceFeeds, 2])
 
     await container.generate(1)
 
@@ -42,11 +42,6 @@ describe('Oracle', () => {
     expect(data[0].length).toStrictEqual(64)
     expect(typeof data[1]).toStrictEqual('string')
     expect(data[1].length).toStrictEqual(64)
-
-    await container.call('removeoracle', [oracleid1])
-    await container.call('removeoracle', [oracleid2])
-
-    await container.generate(1)
   })
 
   it('should listOracles with empty array if there is no oracle available', async () => {
