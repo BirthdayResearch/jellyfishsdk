eb7b869b0bcc5898a4a6088b72a3f65d4c036220
Suraj Auwal <siradjiawoual@gmail.com>
2021-06-24 00:47:20 +0100

    Added resignMasternode RPC

---
+++ packages/jellyfish-api-core/__tests__/category/masternode/resignMasternode.test.ts
@@ -0,0 +1,75 @@
+import { MasterNodeRegTestContainer } from '@defichain/testcontainers'
+import { ContainerAdapterClient } from '../../container_adapter_client'
+import { MasternodeState } from '../../../src/category/masternode'
+import { RpcApiError } from '../../../src'
+
+describe('Masternode', () => {
+  const container = new MasterNodeRegTestContainer()
+  const client = new ContainerAdapterClient(container)
+
+  beforeAll(async () => {
+    await container.start()
+    await container.waitForReady()
+    await container.waitForWalletCoinbaseMaturity()
+  })
+
+  afterAll(async () => {
+    await container.stop()
+  })
+
+  it('should resignMasternode', async () => {
+    const ownerAddress = await client.wallet.getNewAddress()
+    const masternodeId = await client.masternode.createMasternode(ownerAddress)
+
+    await container.generate(1)
+
+    const hex = await client.masternode.resignMasternode(masternodeId)
+    expect(typeof hex).toStrictEqual('string')
+    expect(hex.length).toStrictEqual(64)
+
+    await container.generate(1)
+
+    const resignedMasternode = Object.values(await client.masternode.listMasternodes()).filter(mn => mn.ownerAuthAddress === ownerAddress)
+    for (const masternode of resignedMasternode) {
+      expect(masternode.state).toStrictEqual(MasternodeState.PRE_RESIGNED)
+      expect(masternode.resignTx).toStrictEqual(hex)
+    }
+  })
+
+  it('should resignMasternode with utxos', async () => {
+    const ownerAddress = await client.wallet.getNewAddress()
+    const masternodeId = await client.masternode.createMasternode(ownerAddress)
+    const { txid } = await container.fundAddress(ownerAddress, 10)
+
+    await container.generate(1)
+
+    const utxos = (await client.wallet.listUnspent())
+      .filter((utxo: any) => utxo.txid === txid)
+      .map((utxo: any) => {
+        return {
+          txid: utxo.txid,
+          vout: utxo.vout
+        }
+      })
+
+    const hex = await client.masternode.resignMasternode(masternodeId, utxos)
+    expect(typeof hex).toStrictEqual('string')
+    expect(hex.length).toStrictEqual(64)
+
+    await container.generate(1)
+
+    const resignedMasternode = Object.values(await client.masternode.listMasternodes()).filter(mn => mn.ownerAuthAddress === ownerAddress)
+    for (const masternode of resignedMasternode) {
+      expect(masternode.state).toStrictEqual(MasternodeState.PRE_RESIGNED)
+      expect(masternode.resignTx).toStrictEqual(hex)
+    }
+  })
+
+  it('should throw an error with invalid masternode id', async () => {
+    const invalidMasternodeId = 'b3efcc1bf6cb77c465d7f5686a55f967e73b1a048a3716fdbffa523e22b66fbb'
+    const promise = client.masternode.resignMasternode(invalidMasternodeId)
+
+    await expect(promise).rejects.toThrow(RpcApiError)
+    await expect(promise).rejects.toThrow(`The masternode ${invalidMasternodeId} does not exist`)
+  })
+})
--- packages/jellyfish-api-core/src/category/masternode.ts
+++ packages/jellyfish-api-core/src/category/masternode.ts
@@ -105,6 +105,19 @@ export class Masternode {
   async getMasternode (masternodeId: string): Promise<MasternodeResult<MasternodeInfo>> {
     return await this.client.call('getmasternode', [masternodeId], 'number')
   }
+
+  /**
+   * Creates a transaction resigning a masternode.
+   *
+   * @param {string} masternodeId The masternode's id.
+   * @param {UTXO[]} [utxos] Array of specified utxos to spend.
+   * @param {string} [utxos.txid] The transaction id.
+   * @param {number} [utxos.vout] The output number.
+   * @return {Promise<string>} Resignation Transaction.
+   */
+  async resignMasternode (masternodeId: string, utxos?: UTXO[]): Promise<string> {
+    return await this.client.call('resignmasternode', [masternodeId, utxos], 'number')
+  }
 }
 
 export interface UTXO {
