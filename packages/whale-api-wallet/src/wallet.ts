fae128304d0f0b8da0925f2494b914202e8aaf69
Fuxing Loh <4266087+fuxingloh@users.noreply.github.com>
2021-07-01 12:31:36 +0800

    added withTransactionBuilder for WhaleWalletAccount to use Whale feeRate and prevout (#193)

--- packages/whale-api-wallet/src/wallet.ts
+++ packages/whale-api-wallet/src/wallet.ts
@@ -1,16 +1,30 @@
 import { WalletAccount, WalletAccountProvider, WalletHdNode } from '@defichain/jellyfish-wallet'
 import { WhaleApiClient } from '@defichain/whale-api-client'
 import { Network } from '@defichain/jellyfish-network'
+import { P2WPKHTransactionBuilder } from '@defichain/jellyfish-transaction-builder/dist'
+import { WhaleFeeRateProvider } from './feerate'
+import { WhalePrevoutProvider } from './prevout'
 
 export class WhaleWalletAccount extends WalletAccount {
-  constructor (public readonly client: WhaleApiClient, hdNode: WalletHdNode, network: Network) {
+  protected readonly feeRateProvider: WhaleFeeRateProvider
+  protected readonly prevoutProvider: WhalePrevoutProvider
+
+  constructor (public readonly client: WhaleApiClient, hdNode: WalletHdNode, network: Network, prevoutSize: number = 50) {
     super(hdNode, network)
+    this.feeRateProvider = new WhaleFeeRateProvider(client)
+    this.prevoutProvider = new WhalePrevoutProvider(this, prevoutSize)
   }
 
   async isActive (): Promise<boolean> {
     const address = await this.getAddress()
     const agg = await this.client.address.getAggregation(address)
     return agg !== undefined
+  }
+
+  withTransactionBuilder (): P2WPKHTransactionBuilder {
+    return new P2WPKHTransactionBuilder(this.feeRateProvider, this.prevoutProvider, {
+      get: (_) => this
+    })
   }
 }
 
