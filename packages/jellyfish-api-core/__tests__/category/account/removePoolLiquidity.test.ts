fc01c7aed65295693ad1c65df6183b1a0e33e1c2
Bella <izzycsy@gmail.com>
2021-07-06 02:48:30 +0800

    * Update tests

--- packages/jellyfish-api-core/__tests__/category/account/removePoolLiquidity.test.ts
+++ packages/jellyfish-api-core/__tests__/category/account/removePoolLiquidity.test.ts
@@ -1,6 +1,8 @@
 import { MasterNodeRegTestContainer } from '@defichain/testcontainers'
 import { ContainerAdapterClient } from '../../container_adapter_client'
 import { addPoolLiquidity, createPoolPair, createToken, getNewAddress, mintTokens } from '@defichain/testing'
+import BigNumber from 'bignumber.js'
+import { RpcApiError } from '@defichain/jellyfish-api-core'
 
 describe('Poolpair', () => {
   const container = new MasterNodeRegTestContainer()
@@ -17,106 +19,90 @@ describe('Poolpair', () => {
     await container.stop()
   })
 
-  it.only('should removeLiquidity', async () => {
-    const dogAddress = await getNewAddress(container)
-    const foxAddress = await getNewAddress(container)
-    // const poolPairAddress = await getNewAddress(container)
+  async function setup (symbolA: string, symbolB: string): Promise<string> {
+    const addressA = await getNewAddress(container)
+    const addressB = await getNewAddress(container)
     const poolLiquidityAddress = await getNewAddress(container)
 
-    await createToken(container, 'DOG')
-    await mintTokens(container, 'DOG', { address: dogAddress })
-
-    await createToken(container, 'FOX')
-    await mintTokens(container, 'FOX', { address: foxAddress })
-    const dogAccount = await container.call('getaccount', [dogAddress])
-    console.log('dogAccount: ', dogAccount)
-
-    await createPoolPair(container, 'DOG', 'FOX')
-    // const poolpairs = await container.call('listpoolpairs')
-    // console.log('poolpairs: ', poolpairs)
+    await createToken(container, symbolA)
+    await mintTokens(container, symbolA, { address: addressA })
+    await createToken(container, symbolB)
+    await mintTokens(container, symbolB, { address: addressB })
+    await createPoolPair(container, symbolA, symbolB)
     await addPoolLiquidity(container, {
-      tokenA: 'DOG',
-      amountA: 10,
-      tokenB: 'FOX',
-      amountB: 200,
+      tokenA: symbolA,
+      amountA: 100,
+      tokenB: symbolB,
+      amountB: 100,
       shareAddress: poolLiquidityAddress
     })
-    // totalLiquidity = sqrt(10 * 200) =
-    // const dogAccountAfter = await container.call('getaccount', [dogAddress])
-    // console.log('dogAccountAfter: ', dogAccountAfter)
 
-    const poolLiquidityAccount = await container.call('getaccount', [poolLiquidityAddress])
-    console.log('poolLiquidityAccountBefore: ', poolLiquidityAccount)
+    return poolLiquidityAddress
+  }
 
-    const poolPairBefore = await container.call('listpoolpairs')
-    console.log('poolPairBefore: ', poolPairBefore)
-    const totalLiquidityBefore = poolPairBefore['3'].totalLiquidity
+  it('should removeLiquidity', async () => {
+    const poolLiquidityAddress = await setup('DOG', 'FOX')
+
+    const poolPairBefore = (await container.call('getpoolpair', ['DOG-FOX']))['3']
+    const totalLiquidityBefore = new BigNumber(poolPairBefore.totalLiquidity)
+    const poolLiquidityAccountBefore = await container.call('getaccount', [poolLiquidityAddress]) // 44.72135954
+    expect(poolLiquidityAccountBefore.length).toStrictEqual(1)
+
+    const fee = new BigNumber('0.00001')
+    expect(poolLiquidityAccountBefore[0]).toStrictEqual(`${totalLiquidityBefore.minus(fee).toFixed(8)}@DOG-FOX`) // 44.72134954@DOG-FOX
+
     const hash = await client.poolpair.removePoolLiquidity(poolLiquidityAddress, '13@DOG-FOX')
-
     expect(typeof hash).toStrictEqual('string')
     expect(hash.length).toStrictEqual(64)
     await container.generate(1)
 
-    const poolPairAfter = await container.call('listpoolpairs')
-    console.log('poolPairAfter: ', poolPairAfter)
-    console.log('poolLiquidityAddress: ', poolLiquidityAddress)
+    const poolPairAfter = (await container.call('getpoolpair', ['DOG-FOX']))['3']
+    const totalLiquidityAfter = new BigNumber(poolPairAfter.totalLiquidity)
+    expect(totalLiquidityBefore.minus(totalLiquidityAfter).toString()).toStrictEqual('13')
 
     const poolLiquidityAccountAfter = await container.call('getaccount', [poolLiquidityAddress])
-    console.log('poolLiquidityAccountAfter: ', poolLiquidityAccountAfter)
+    expect(poolLiquidityAccountAfter.length).toStrictEqual(3)
 
-    const dogAccountAfter = await container.call('getaccount', [dogAddress])
-    const foxAccountAfter = await container.call('getaccount', [foxAddress])
-
-    console.log('dogAccountAfter: ', dogAccountAfter, foxAccountAfter)
-
-    const totalLiquidityAfter = poolPairAfter['3'].totalLiquidity
-    console.log('totalLiquidityAfter: ', totalLiquidityAfter)
-    console.log('totalLiquidityDiff: ', totalLiquidityBefore - totalLiquidityAfter)
-
-    expect(totalLiquidityBefore - totalLiquidityAfter).toStrictEqual(13)
+    const dogLiquidityDiff = new BigNumber(poolPairAfter.totalLiquidity)
+    const foxLiquidityDiff = new BigNumber(poolPairAfter.totalLiquidity)
+    expect(poolLiquidityAccountAfter[0]).toStrictEqual(`${dogLiquidityDiff.toFixed(8)}@DOG`)
+    expect(poolLiquidityAccountAfter[1]).toStrictEqual(`${foxLiquidityDiff.toFixed(8)}@FOX`)
+    expect(poolLiquidityAccountAfter[2]).toStrictEqual(`${totalLiquidityAfter.minus(fee).toFixed(8)}@DOG-FOX`)
   })
 
-  // it('should removeLiquidity with utxos', async () => {
-  //   const shareAddress = await container.call('getnewaddress')
-  //   const tokenAAddress = await container.call('getnewaddress', '10@DDFI')
-  //   const tokenBAddress = await container.call('getnewaddress', '200@DBTC')
-  //   const utxos = await container.call('listunspent')
-  //   await container.generate(1)
-  //
-  //   const inputs = utxos.map((utxo: any) => {
-  //     return {
-  //       txid: utxo.txid,
-  //       vout: utxo.vout
-  //     }
-  //   })
-  //
-  //   const hash = await client.poolpair.removePoolLiquidity({
-  //     [tokenAAddress]: '10@DOG',
-  //     [tokenBAddress]: '200@FOX'
-  //   }, shareAddress, { utxos: inputs })
-  //
-  //   expect(typeof hash).toStrictEqual('string')
-  //   expect(hash.length).toStrictEqual(64)
-  // })
+  it('should removeLiquidity with utxos', async () => {
+    const poolLiquidityAddress = await setup('APE', 'BEE')
+    await container.fundAddress(poolLiquidityAddress, 25)
+    const utxos = await container.call('listunspent')
+    const utxo = utxos.find((utxo: any) => utxo.address === poolLiquidityAddress)
 
-  // it('fail as utxos does not include an account owner', async () => {
-  //   const shareAddress = await container.call('getnewaddress')
-  //   const tokenAAddress = await container.call('getnewaddress')
-  //   const tokenBAddress = await container.call('getnewaddress')
-  //   const utxos = await container.call('listunspent')
-  //
-  //   const inputs = utxos.map((utxo: any) => {
-  //     return {
-  //       txid: utxo.txid,
-  //       vout: utxo.vout
-  //     }
-  //   })
-  //
-  //   const hash = await client.poolpair.removePoolLiquidity({
-  //     [tokenAAddress]: '10@DOG',
-  //     [tokenBAddress]: '200@FOX'
-  //   }, shareAddress, { utxos: inputs })
-  //
-  //   expect(typeof hash).rejects.toThrow('tx must have at least one input from account owner')
-  // })
+    const txid = await client.poolpair.removePoolLiquidity(
+      poolLiquidityAddress, '4@APE-BEE', { utxos: [{ txid: utxo.txid, vout: utxo.vout }] }
+    )
+    expect(typeof txid).toStrictEqual('string')
+    expect(txid.length).toStrictEqual(64)
+
+    await container.generate(1)
+
+    const rawtx = await container.call('getrawtransaction', [txid, true])
+
+    // the utxo input will be added into removeLiquidity tx vin
+    expect(rawtx.vin[0].txid).toStrictEqual(utxo.txid)
+  })
+
+  it('should removeLiquidity failed with utxos as tx input is not from account owner', async () => {
+    const poolLiquidityAddress = await setup('ELF', 'GOR')
+    const otherAddress = await getNewAddress(container)
+
+    await container.fundAddress(otherAddress, 25)
+
+    const utxos = await container.call('listunspent')
+    const utxo = utxos.find((utxo: any) => utxo.address === otherAddress)
+    const promise = client.poolpair.removePoolLiquidity(
+      poolLiquidityAddress, '4@ELF-GOR', { utxos: [{ txid: utxo.txid, vout: utxo.vout }] }
+    )
+
+    await expect(promise).rejects.toThrow(RpcApiError)
+    await expect(promise).rejects.toThrow('tx must have at least one input from account owner')
+  })
 })
