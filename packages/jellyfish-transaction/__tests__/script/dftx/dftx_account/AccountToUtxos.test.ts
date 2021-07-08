2f7605e2beb3bd07221fe3ec22427361d23ee0e5
jouzo <jdesclercs@gmail.com>
2021-07-08 09:54:10 +0200

    Refactor src/script/defi -> src/script/dftx and __tests__/script/defi -> __tests__/script/dftx

---
+++ packages/jellyfish-transaction/__tests__/script/dftx/dftx_account/AccountToUtxos.test.ts
@@ -0,0 +1,71 @@
+import BigNumber from 'bignumber.js'
+import { SmartBuffer } from 'smart-buffer'
+import { OP_DEFI_TX } from '../../../../src/script/dftx'
+import { OP_CODES } from '../../../../src'
+import { toBuffer, toOPCodes } from '../../../../src/script/_buffer'
+import { AccountToUtxos, CAccountToUtxos } from '../../../../src/script/dftx/dftx_account'
+
+it('should bi-directional buffer-object-buffer', () => {
+  const fixtures = [
+    '6a2b446654786217a914d6e3de1c51f22e580944bb6a1647f1d22f0159c78701000000007030e6550200000002',
+    '6a2b446654786217a914fad0d4ab78412ec38e7a0b118e51e147e947e02d870100000000f2664e750200000002',
+    '6a2b446654786217a91462f401bfbe944884f07b489eb97fd3b001d5303287010000000000a81dd50f00000002'
+  ]
+
+  fixtures.forEach(hex => {
+    const stack = toOPCodes(
+      SmartBuffer.fromBuffer(Buffer.from(hex, 'hex'))
+    )
+    const buffer = toBuffer(stack)
+    expect(buffer.toString('hex')).toStrictEqual(hex)
+    expect((stack[1] as OP_DEFI_TX).tx.type).toStrictEqual(0x62)
+  })
+})
+
+/**
+ * using accountToUtxos sample from
+ * https://explorer.defichain.io/#/DFI/mainnet/tx/8acd31c4ba08a31adefa0af5816e52ac5d61ad184a397bcc114590ecf2b08ed6
+ */
+const header = '6a2b4466547862' // OP_RETURN 44665478 62
+const data = '17a914d6e3de1c51f22e580944bb6a1647f1d22f0159c78701000000007030e6550200000002'
+const accountToUtxos: AccountToUtxos = {
+  from: {
+    stack: [
+      OP_CODES.OP_HASH160,
+      OP_CODES.OP_PUSHDATA_HEX_LE('d6e3de1c51f22e580944bb6a1647f1d22f0159c7'),
+      OP_CODES.OP_EQUAL
+    ]
+  },
+  balances: [
+    {
+      amount: new BigNumber('100.31083632'), token: 0
+    }
+  ],
+  mintingOutputsStart: 2
+}
+
+it('should craft dftx with OP_CODES._()', () => {
+  const stack = [
+    OP_CODES.OP_RETURN,
+    OP_CODES.OP_DEFI_TX_ACCOUNT_TO_UTXOS(accountToUtxos)
+  ]
+
+  const buffer = toBuffer(stack)
+  expect(buffer.toString('hex')).toStrictEqual(header + data)
+})
+
+describe('Composable', () => {
+  it('should compose from buffer to composable', () => {
+    const buffer = SmartBuffer.fromBuffer(Buffer.from(data, 'hex'))
+    const composable = new CAccountToUtxos(buffer)
+    expect(composable.toObject()).toStrictEqual(accountToUtxos)
+  })
+
+  it('should compose from composable to buffer', () => {
+    const composable = new CAccountToUtxos(accountToUtxos)
+    const buffer = new SmartBuffer()
+    composable.toBuffer(buffer)
+
+    expect(buffer.toBuffer().toString('hex')).toStrictEqual(data)
+  })
+})
---
+++ packages/jellyfish-transaction/__tests__/script/dftx/dftx_pool/PoolUpdatePair.test.ts
@@ -0,0 +1,82 @@
+import BigNumber from 'bignumber.js'
+import { SmartBuffer } from 'smart-buffer'
+import { OP_DEFI_TX } from '../../../../src/script/dftx'
+import { CPoolUpdatePair, PoolUpdatePair } from '../../../../src/script/dftx/dftx_pool'
+import { OP_CODES } from '../../../../src'
+import { toBuffer, toOPCodes } from '../../../../src/script/_buffer'
+
+it('should bi-directional buffer-object-buffer', () => {
+  const fixtures = [
+    // regtest fixtures
+    // height >= ClarkeQuayHeight (encoding includes customRewards)
+    // status, commission
+    '6a144466547875020000000100e1f505000000000000',
+    // ownerAddress
+    '6a2b44665478750200000000ffffffffffffffff17a91440d54b7a72387cb5c6d0125f2890eabf052f01908700',
+    // customRewards 2 tokens
+    '6a2c44665478750200000000ffffffffffffffff00020000000000e1f505000000000100000000a3e11100000000',
+    // customRewards 1 token
+    '6a2044665478750200000000ffffffffffffffff0001000000000008af2f00000000',
+    // all
+    '6a434466547875020000000120a107000000000017a914fd190704714762e6c30eb5b39071c1a52e6130ad87020000000000ca9a3b00000000010000000094357700000000',
+
+    // height < ClarkeQuayHeight (exclude customRewards from being encoded)
+    // all
+    '6a2a44665478750300000001c0cf6a000000000017a9141205616a55fbbdd91ec97e222fae709acc4fd2c887'
+  ]
+
+  fixtures.forEach(hex => {
+    const stack: any = toOPCodes(
+      SmartBuffer.fromBuffer(Buffer.from(hex, 'hex'))
+    )
+    const buffer = toBuffer(stack)
+    expect((stack[1] as OP_DEFI_TX).tx.type).toBe(0x75)
+    expect(buffer.toString('hex')).toBe(hex)
+  })
+})
+
+const header = '6a434466547875' // OP_RETURN, PUSH_DATA(44665478, 75)
+const data = '020000000120a107000000000017a914fd190704714762e6c30eb5b39071c1a52e6130ad87020000000000ca9a3b00000000010000000094357700000000'
+const poolUpdatePair: PoolUpdatePair = {
+  poolId: 2,
+  status: true,
+  commission: new BigNumber('0.005'),
+  ownerAddress: {
+    stack: [
+      OP_CODES.OP_HASH160,
+      OP_CODES.OP_PUSHDATA_HEX_LE('fd190704714762e6c30eb5b39071c1a52e6130ad'),
+      OP_CODES.OP_EQUAL
+    ]
+  },
+  customRewards: [
+    { token: 0, amount: new BigNumber('10') },
+    { token: 1, amount: new BigNumber('20') }
+  ]
+}
+
+it('should craft dftx with OP_CODES._()', () => {
+  const stack = [
+    OP_CODES.OP_RETURN,
+    OP_CODES.OP_DEFI_TX_POOL_UPDATE_PAIR(poolUpdatePair)
+  ]
+
+  const buffer = toBuffer(stack)
+  expect(buffer.toString('hex')).toBe(header + data)
+})
+
+describe('Composable', () => {
+  it('should compose from buffer to composable', () => {
+    const buffer = SmartBuffer.fromBuffer(Buffer.from(data, 'hex'))
+    const composable = new CPoolUpdatePair(buffer)
+
+    expect(composable.toObject()).toEqual(poolUpdatePair)
+  })
+
+  it('should compose from composable to buffer', () => {
+    const composable = new CPoolUpdatePair(poolUpdatePair)
+    const buffer = new SmartBuffer()
+    composable.toBuffer(buffer)
+
+    expect(buffer.toBuffer().toString('hex')).toEqual(data)
+  })
+})
