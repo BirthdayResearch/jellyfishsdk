43e3158e8eb2f942acc8af0a60679cfc9ca9701d
Ahmed Hilali <monstrobishi@gmail.com>
2021-07-09 17:59:23 +0800

    `jellyfish-wallet-classic` (#454)
    * Added classic wallet implementation
    
    * Remove bip from readme
    
    * Address codecov

---
+++ packages/jellyfish-wallet-classic/README.md
@@ -0,0 +1,6 @@
+[![npm](https://img.shields.io/npm/v/@defichain/jellyfish-wallet-classic)](https://www.npmjs.com/package/@defichain/jellyfish-wallet-classic/v/latest)
+[![npm@next](https://img.shields.io/npm/v/@defichain/jellyfish-wallet-classic/next)](https://www.npmjs.com/package/@defichain/jellyfish-wallet-classic/v/next)
+
+# @defichain/jellyfish-wallet-classic
+
+WalletClassic implements a simple, single elliptic pair wallet.
---
+++ packages/jellyfish-wallet-classic/src/classic.ts
@@ -0,0 +1,65 @@
+import { WalletEllipticPair } from '@defichain/jellyfish-wallet'
+import { EllipticPair } from '@defichain/jellyfish-crypto'
+import {
+  Transaction,
+  Vout,
+  TransactionSegWit
+} from '@defichain/jellyfish-transaction'
+import { SignInputOption, TransactionSigner } from '@defichain/jellyfish-transaction-signature'
+
+/**
+ * WalletClassic extends WalletEllipticPair with a simple classic implementation.
+ *
+ * Single elliptic pair wallet.
+ */
+export class WalletClassic implements WalletEllipticPair {
+  constructor (public readonly ellipticPair: EllipticPair) {
+  }
+
+  /**
+   * @return {Promise<Buffer>} compressed public key
+   */
+  async publicKey (): Promise<Buffer> {
+    return await this.ellipticPair.publicKey()
+  }
+
+  /**
+   * @return {Promise<Buffer>} privateKey
+   */
+  async privateKey (): Promise<Buffer> {
+    return await this.ellipticPair.privateKey()
+  }
+
+  /**
+   * @param {Buffer} hash to sign
+   * @return {Buffer} signature in DER format, SIGHASHTYPE not included
+   * @see https://tools.ietf.org/html/rfc6979
+   * @see https://github.com/bitcoin/bitcoin/pull/13666
+   */
+  async sign (hash: Buffer): Promise<Buffer> {
+    return await this.ellipticPair.sign(hash)
+  }
+
+  /**
+   * @param {Buffer} hash to verify with signature
+   * @param {Buffer} derSignature of the hash in encoded with DER, SIGHASHTYPE must not be included
+   * @return {boolean} validity of signature of the hash
+   */
+  async verify (hash: Buffer, derSignature: Buffer): Promise<boolean> {
+    return await this.ellipticPair.verify(hash, derSignature)
+  }
+
+  /**
+   * WalletClassic transaction signing.
+   *
+   * @param {Transaction} transaction to sign
+   * @param {Vout[]} prevouts of the transaction to fund this transaction
+   * @return {TransactionSegWit} a signed transaction
+   */
+  async signTx (transaction: Transaction, prevouts: Vout[]): Promise<TransactionSegWit> {
+    const inputs: SignInputOption[] = prevouts.map(prevout => {
+      return { prevout: prevout, ellipticPair: this }
+    })
+    return TransactionSigner.sign(transaction, inputs)
+  }
+}
