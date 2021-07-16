fb4cde0000fbef9990059f04179fdc85fe86ac65
Jouzo <jdesclercs@gmail.com>
2021-07-16 05:08:21 +0200

    Automatic Conversion of Integer/Fraction for MaxPrice in dftx_pool.ts (#469)
    * maxPrice as a single BigNumber with the first 8 bytes representing the integer and the last 8 bytes representing the fraction
    
    * testing PoolSwap with new interface
    
    * fix tests to use new PoolSwap interface with maxPrice as BigNumber
    
    * Handle too high precision in CPoolSwap, precision is limited to 8 decimals
    
    * Uses BigNumber gt method instead of > operator
    
    Co-authored-by: canonbrother <w.canonbrother@gmail.com>
    
    * Handle too high precision when reading from buffer
    
    * refactor CPoolSwap composers. Extract maxPriceAsBigNumber composer to buffer_composer.ts
    
    * test that poolswap fails or pass on a maxprice delta of 0.00000001
    
    * test to determine maxPrice accepted range: 0>= maxPrice <= 1200000000 and maxPrice precision
    
    * use bigNumber decimalPlaces method to handle too high precision
    
    * test ComposableBuffer.maxPriceAsBigNumber
    
    * revert check fraction fromBuffer
    
    * poolswap with 4.99999999 and 5.0 in two separate tests
    
    * fix comments format
    
    * uses token FISH for failing test 4.99999999
    
    * Target main
    
    Co-authored-by: canonbrother <w.canonbrother@gmail.com>

--- packages/jellyfish-transaction/__tests__/script/dftx/dftx_pool/PoolSwap.test.ts
+++ packages/jellyfish-transaction/__tests__/script/dftx/dftx_pool/PoolSwap.test.ts
@@ -7,15 +7,15 @@ import { CPoolSwap, PoolSwap } from '../../../../src/script/dftx/dftx_pool'
 
 it('should bi-directional buffer-object-buffer', () => {
   const fixtures = [
-    '6a4c4f446654787317a914c34ca9c54dc87e7e875b212ec6ba0704be3de587870000d6117e0300000017a914c34ca9c54dc87e7e875b212ec6ba0704be3de5878702ffffffffffffff7fffffffffffffff7f',
-    '6a4c4f446654787317a914635794bb3db157b6dac4dcb7467710a10f03c6ac8700809698000000000017a914582328ee9beebc3fb5844964302cc0fdbb27e04c8702ffffffffffffff7fffffffffffffff7f',
+    '6a4c4f446654787317a914c34ca9c54dc87e7e875b212ec6ba0704be3de587870000d6117e0300000017a914c34ca9c54dc87e7e875b212ec6ba0704be3de58787022fe28e7915000080ffcf430300000000',
+    '6a4c4f446654787317a914635794bb3db157b6dac4dcb7467710a10f03c6ac8700809698000000000017a914582328ee9beebc3fb5844964302cc0fdbb27e04c87022fe28e7915000080ffcf430300000000',
     '6a4c4f446654787317a9148646fc3d44c92e190c18db1cf08f430e421dba9887005671518a0000000017a9148646fc3d44c92e190c18db1cf08f430e421dba988707a0860100000000000000000000000000',
-    '6a4c4f446654787317a9140c1ff6de17f73dad607a2b03b5bd77b4e57327e58707d3e2a0470000000017a9140c1ff6de17f73dad607a2b03b5bd77b4e57327e58700ffffffffffffff7fffffffffffffff7f',
-    '6a4c4f446654787317a914094b5e971325dee0161beccc7a78592f95c27100870080397a120000000017a914094b5e971325dee0161beccc7a78592f95c271008702ffffffffffffff7fffffffffffffff7f',
+    '6a4c4f446654787317a9140c1ff6de17f73dad607a2b03b5bd77b4e57327e58707d3e2a0470000000017a9140c1ff6de17f73dad607a2b03b5bd77b4e57327e587002fe28e7915000080ffcf430300000000',
+    '6a4c4f446654787317a914094b5e971325dee0161beccc7a78592f95c27100870080397a120000000017a914094b5e971325dee0161beccc7a78592f95c2710087022fe28e7915000080ffcf430300000000',
     '6a4c4f446654787317a914f64714403dd1b872494851ca4ed8d5071af79ea6870087b4104d0200000017a914f64714403dd1b872494851ca4ed8d5071af79ea68707a0860100000000000000000000000000',
-    '6a4c5144665478731976a9140b7127e943eaa3f28536c3f046ddbdeb790f691e88ac0080889e2a0100000017a914b3a65aa3fd9c60860bebd231d71b5bc5749ff5be8702ffffffffffffff7fffffffffffffff7f',
-    '6a4c4f446654787317a9140164e31629342622b35bc134c8e4fe7c45c42e43870205fd0b030000000017a9140164e31629342622b35bc134c8e4fe7c45c42e438700ffffffffffffff7fffffffffffffff7f',
-    '6a4c4f446654787317a914963a12667d64728009567ad5eb26777e1673c479870000db7d2e0000000017a914963a12667d64728009567ad5eb26777e1673c4798702ffffffffffffff7fffffffffffffff7f'
+    '6a4c5144665478731976a9140b7127e943eaa3f28536c3f046ddbdeb790f691e88ac0080889e2a0100000017a914b3a65aa3fd9c60860bebd231d71b5bc5749ff5be87022fe28e7915000080ffcf430300000000',
+    '6a4c4f446654787317a9140164e31629342622b35bc134c8e4fe7c45c42e43870205fd0b030000000017a9140164e31629342622b35bc134c8e4fe7c45c42e4387002fe28e7915000080ffcf430300000000',
+    '6a4c4f446654787317a914963a12667d64728009567ad5eb26777e1673c479870000db7d2e0000000017a914963a12667d64728009567ad5eb26777e1673c47987022fe28e7915000080ffcf430300000000'
   ]
 
   fixtures.forEach(hex => {
@@ -29,7 +29,8 @@ it('should bi-directional buffer-object-buffer', () => {
 })
 
 const header = '6a4c4f4466547873' // OP_RETURN, PUSH_DATA(44665478, 73)
-const data = '17a914c34ca9c54dc87e7e875b212ec6ba0704be3de587870000d6117e0300000017a914c34ca9c54dc87e7e875b212ec6ba0704be3de5878702ffffffffffffff7fffffffffffffff7f'
+const data = '17a914c34ca9c54dc87e7e875b212ec6ba0704be3de587870000d6117e0300000017a914c34ca9c54dc87e7e875b212ec6ba0704be3de58787029a030000000000003088020200000000'
+
 const poolSwap: PoolSwap = {
   fromAmount: new BigNumber(150),
   fromScript: {
@@ -48,10 +49,7 @@ const poolSwap: PoolSwap = {
       OP_CODES.OP_EQUAL
     ]
   },
-  maxPrice: {
-    integer: new BigNumber('9223372036854775807'),
-    fraction: new BigNumber('9223372036854775807')
-  }
+  maxPrice: new BigNumber('922.33720368')
 }
 
 it('should craft dftx with OP_CODES._()', () => {
@@ -78,5 +76,118 @@ describe('Composable', () => {
     composable.toBuffer(buffer)
 
     expect(buffer.toBuffer().toString('hex')).toStrictEqual(data)
+  })
+
+  it('should bi-directional with two digits precision for 1.92', () => {
+    const poolSwapTwoDecimalPrecision = {
+      ...poolSwap,
+      maxPrice: new BigNumber('1.92')
+    }
+
+    const composable = new CPoolSwap(poolSwapTwoDecimalPrecision)
+    const buffer = new SmartBuffer()
+    composable.toBuffer(buffer)
+
+    const hex = buffer.toBuffer().toString('hex')
+
+    const buffer2 = SmartBuffer.fromBuffer(Buffer.from(hex, 'hex'))
+    const composable2 = new CPoolSwap(buffer2)
+
+    expect(composable2.toObject()).toStrictEqual(poolSwapTwoDecimalPrecision)
+  })
+
+  it('should bi-directional with seven digits precision for 0.00000003', () => {
+    const poolSwapEightnDecimalPrecision = {
+      ...poolSwap,
+      maxPrice: new BigNumber('0.00000003')
+    }
+
+    const composable = new CPoolSwap(poolSwapEightnDecimalPrecision)
+    const buffer = new SmartBuffer()
+    composable.toBuffer(buffer)
+
+    const hex = buffer.toBuffer().toString('hex') // "17a914c34ca9c54dc87e7e875b212ec6ba0704be3de587870000d6117e0300000017a914c34ca9c54dc87e7e875b212ec6ba0704be3de587870200000000000000000300000000000000"
+
+    const buffer2 = SmartBuffer.fromBuffer(Buffer.from(hex, 'hex'))
+    const composable2 = new CPoolSwap(buffer2)
+
+    expect(composable2.toObject()).toStrictEqual(poolSwapEightnDecimalPrecision)
+  })
+
+  it('should bi-directional with two digits precision for 2.0', () => {
+    const poolSwapFromDivision = {
+      ...poolSwap,
+      maxPrice: new BigNumber(100 / 50)
+    }
+
+    const composable = new CPoolSwap(poolSwapFromDivision)
+    const buffer = new SmartBuffer()
+    composable.toBuffer(buffer)
+
+    const hex = buffer.toBuffer().toString('hex')
+    const buffer2 = SmartBuffer.fromBuffer(Buffer.from(hex, 'hex'))
+    const composable2 = new CPoolSwap(buffer2)
+
+    expect(composable2.toObject()).toStrictEqual(poolSwapFromDivision)
+  })
+
+  it('should bi-directional with UInt64MAX', () => {
+    const poolSwapUint64MAX = {
+      ...poolSwap,
+      maxPrice: new BigNumber('18446744073709551615')
+    }
+
+    const composable = new CPoolSwap(poolSwapUint64MAX)
+    const buffer = new SmartBuffer()
+    composable.toBuffer(buffer)
+
+    const hex = buffer.toBuffer().toString('hex') // "17a914c34ca9c54dc87e7e875b212ec6ba0704be3de587870000d6117e0300000017a914c34ca9c54dc87e7e875b212ec6ba0704be3de5878702ffffffffffffffff0000000000000000"
+
+    const buffer2 = SmartBuffer.fromBuffer(Buffer.from(hex, 'hex'))
+    const composable2 = new CPoolSwap(buffer2)
+
+    expect(composable2.toObject()).toStrictEqual(poolSwapUint64MAX)
+  })
+
+  it('should bi-directional with UInt64MAX HIGH precision', () => {
+    const poolSwapUint64MAXHighPrecision = {
+      ...poolSwap,
+      maxPrice: new BigNumber('18446744073709551615.99999999')
+    }
+
+    const composable = new CPoolSwap(poolSwapUint64MAXHighPrecision)
+    const buffer = new SmartBuffer()
+    composable.toBuffer(buffer)
+
+    const hex = buffer.toBuffer().toString('hex') // "17a914c34ca9c54dc87e7e875b212ec6ba0704be3de587870000d6117e0300000017a914c34ca9c54dc87e7e875b212ec6ba0704be3de5878702ffffffffffffffffffe0f50500000000"
+
+    const buffer2 = SmartBuffer.fromBuffer(Buffer.from(hex, 'hex'))
+    const composable2 = new CPoolSwap(buffer2)
+
+    expect(composable2.toObject()).toStrictEqual(poolSwapUint64MAXHighPrecision)
+  })
+
+  it('should throw an error when more than 8 decimals', () => {
+    const poolSwapNineDecimalPrecision = {
+      ...poolSwap,
+      maxPrice: new BigNumber('1.999999999')
+    }
+
+    expect(() => {
+      const composable = new CPoolSwap(poolSwapNineDecimalPrecision)
+      const buffer = new SmartBuffer()
+      composable.toBuffer(buffer)
+    }).toThrow('Too many decimals to be correctly represented. Will lose precision with more than 8 decimals')
+  })
+
+  it('should throw an error when reading more than 8 decimals from buffer', () => {
+    const hex = '17a914c34ca9c54dc87e7e875b212ec6ba0704be3de587870000d6117e0300000017a914c34ca9c54dc87e7e875b212ec6ba0704be3de5878702ffffffffffffffffffe0f505ffffffff'
+
+    expect(() => {
+      const buffer = SmartBuffer.fromBuffer(Buffer.from(hex, 'hex'))
+      const composable = new CPoolSwap(buffer)
+
+      expect(composable.toObject()).toStrictEqual(poolSwap)
+    }).toThrow('Too many decimals read from buffer. Will lose precision with more than 8 decimals')
   })
 })
