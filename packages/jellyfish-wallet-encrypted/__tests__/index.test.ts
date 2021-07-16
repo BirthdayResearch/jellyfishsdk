b729a36b486d59427820e7cfed961d2ff959fdd5
ivan <ivan@zynesis.com>
2021-07-16 17:04:28 +0800

    passphrase only need when get pub/priv key, sign/verify

--- packages/jellyfish-wallet-encrypted/__tests__/index.test.ts
+++ packages/jellyfish-wallet-encrypted/__tests__/index.test.ts
@@ -44,13 +44,61 @@ describe('EncryptedMnemonicProvider', () => {
       }
     })
 
-    // tested deriveWithSeed() here
-    node = await provider.deriveWithSeed("44'/1129'/0'/0/0", seed)
+    node = await provider.derive("44'/1129'/0'/0/0")
 
-    const pubKey = await node.publicKey()
-    const privKey = await node.privateKey(passphrase)
+    const pubKey = await node.unlockPublicKey(passphrase)
+    const privKey = await node.unlockPrivateKey(passphrase)
     expect(pubKey.length).toStrictEqual(33)
     expect(privKey.length).toStrictEqual(32)
+  })
+
+  it('Derived MnemonicHdNode is encrypted, every default WalletHdNode method requires passphrase', async () => {
+    await expect(node.publicKey()).rejects.toThrow('Encrypted')
+    await expect(node.verify(Buffer.alloc(1), Buffer.alloc(1))).rejects.toThrow('Encrypted')
+    await expect(node.privateKey()).rejects.toThrow('Encrypted')
+    await expect(node.sign(Buffer.alloc(1))).rejects.toThrow('Encrypted')
+
+    const transaction: Transaction = {
+      version: 0x00000004,
+      lockTime: 0x00000000,
+      vin: [{
+        index: 0,
+        script: { stack: [] },
+        sequence: 4294967278,
+        txid: '9f96ade4b41d5433f4eda31e1738ec2b36f6e7d1420d94a6af99801a88f7f7ff'
+      }],
+      vout: [{
+        script: {
+          stack: [
+            OP_CODES.OP_0,
+            OP_CODES.OP_PUSHDATA(Buffer.from('1d0f172a0ecb48aee1be1f2687d2963ae33f71a1', 'hex'), 'little')
+          ]
+        },
+        value: new BigNumber('5.98'),
+        tokenId: 0x00
+      }]
+    }
+
+    const prevout: Vout = {
+      script: {
+        stack: [
+          OP_CODES.OP_0,
+          OP_CODES.OP_PUSHDATA(Buffer.from('1d0f172a0ecb48aee1be1f2687d2963ae33f71a1', 'hex'), 'little')
+        ]
+      },
+      value: new BigNumber('6'),
+      tokenId: 0x00
+    }
+
+    await expect(node.signTx(transaction, [{
+      ...prevout,
+      script: {
+        stack: [
+          OP_CODES.OP_0,
+          OP_CODES.OP_PUSHDATA(HASH160(await node.publicKey()), 'little')
+        ]
+      }
+    }])).rejects.toThrow('Encrypted')
   })
 
   it('EncryptedMnemonicProvider.load() - should be able to load/restore a provider instance', async () => {
@@ -70,53 +118,24 @@ describe('EncryptedMnemonicProvider', () => {
       }
     })
 
-    const pubKey = await node.publicKey()
-    const privKey = await node.privateKey(passphrase)
+    const pubKey = await node.unlockPublicKey(passphrase)
+    const privKey = await node.unlockPrivateKey(passphrase)
 
-    const loadedNode = await loaded.unlockAndDerive("44'/1129'/0'/0/0", passphrase)
-    const loadPubKey = await loadedNode.publicKey()
-    const loadPrivKey = await loadedNode.privateKey(passphrase)
+    const loadedNode = await loaded.derive("44'/1129'/0'/0/0")
+    const loadedPubKey = await loadedNode.unlockPublicKey(passphrase)
+    const loadedPrivKey = await loadedNode.unlockPrivateKey(passphrase)
 
-    expect(pubKey.toString('hex')).toStrictEqual(loadPubKey.toString('hex'))
-    expect(privKey.toString('hex')).toStrictEqual(loadPrivKey.toString('hex'))
-  })
-
-  it('deriveWithSeed() - invalid seed', async () => {
-    await expect(provider.deriveWithSeed("44'/1129'/0'/0/0", Buffer.from('e9873d79c6d87dc0fb6a5778633389f4e93213303da61f20bd67fc233aa33261'))).rejects
-      .toThrow('InvalidSeedHash')
-  })
-
-  it('unlockAndDerive() - invalid passphrase', async () => {
-    await expect(provider.unlockAndDerive("44'/1129'/0'/0/0", 'incorrect password')).rejects
-      .toThrow('InvalidPassphrase')
-  })
-
-  it('unlockAndDerive() - encrypted key missing', async () => {
-    // delete encrypted seed
-    await seedStore.setter(undefined)
-    await expect(provider.unlockAndDerive("44'/1129'/0'/0/0", 'incorrect password')).rejects
-      .toThrow('No encrypted seed found in storage')
-  })
-
-  it('unlockAndDerive() - should behave the same as deriveWithSeed() when valid passphrase provided', async () => {
-    const pubKey = await node.publicKey()
-    const privKey = await node.privateKey(passphrase)
-
-    const derivedUsingPassphrase = await provider.unlockAndDerive("44'/1129'/0'/0/0", passphrase)
-    const pubKey2 = await derivedUsingPassphrase.publicKey()
-    const privKey2 = await derivedUsingPassphrase.privateKey(passphrase)
-
-    expect(pubKey.toString('hex')).toStrictEqual(pubKey2.toString('hex'))
-    expect(privKey.toString('hex')).toStrictEqual(privKey2.toString('hex'))
+    expect(pubKey.toString('hex')).toStrictEqual(loadedPubKey.toString('hex'))
+    expect(privKey.toString('hex')).toStrictEqual(loadedPrivKey.toString('hex'))
   })
 
   it('Should be able to derive multiple node/elliptic pair and unlockable with same passphrase', async () => {
-    const pubKey = await node.publicKey()
-    const privKey = await node.privateKey(passphrase)
+    const pubKey = await node.unlockPublicKey(passphrase)
+    const privKey = await node.unlockPrivateKey(passphrase)
 
-    const node2 = await provider.unlockAndDerive("44'/1129'/1'/0/0", passphrase)
-    const pubKey2 = await node2.publicKey()
-    const privKey2 = await node2.privateKey(passphrase)
+    const node2 = await provider.derive("44'/1129'/1'/0/0")
+    const pubKey2 = await node2.unlockPublicKey(passphrase)
+    const privKey2 = await node2.unlockPrivateKey(passphrase)
     expect(pubKey2.length).toStrictEqual(33)
     expect(privKey2.length).toStrictEqual(32)
 
@@ -124,32 +143,18 @@ describe('EncryptedMnemonicProvider', () => {
     expect(privKey.toString('hex')).not.toStrictEqual(privKey2.toString('hex'))
   })
 
-  it('Should be able to unlock with valid passphrase', async () => {
-    await expect(node.unlock('something else')).rejects
-      .toThrow('InvalidPassphrase')
-
-    const unlocked = await node.unlock(passphrase)
-    expect(unlocked).toBeTruthy()
-    expect(await unlocked.publicKey()).toBeTruthy()
-  })
-
-  it('Should be able to unlock with valid passphrase', async () => {
-    await seedStore.setter(undefined)
-    await expect(node.unlock(passphrase)).rejects
-      .toThrow('No encrypted seed found in storage')
-  })
-
   it('Should be able to sign and verify', async () => {
     const hash = Buffer.from('e9071e75e25b8a1e298a72f0d2e9f4f95a0f5cdf86a533cda597eb402ed13b3a', 'hex')
-    const signature = await node.sign(passphrase, hash)
+    const signature = await node.unlockAndSign(passphrase, hash)
 
     expect(signature.length).toBeLessThanOrEqual(70)
     expect(signature.length).toBeGreaterThanOrEqual(67) // 0.00001 probability of being this length
 
-    expect(await node.verify(hash, signature)).toBeTruthy()
+    expect(await node.unlockAndVerify(passphrase, hash, signature)).toBeTruthy()
 
-    const node2 = await provider.unlockAndDerive("44'/1129'/1'/0/0", passphrase)
-    expect(await node2.verify(hash, signature)).toBeFalsy()
+    // meant to fail, differnt pubkey
+    const anotherNode = await provider.derive("44'/1129'/1'/0/0")
+    expect(await anotherNode.unlockAndVerify(passphrase, hash, signature)).toBeFalsy()
   })
 
   it('signTx()', async () => {
@@ -185,7 +190,7 @@ describe('EncryptedMnemonicProvider', () => {
       tokenId: 0x00
     }
 
-    const signed = await node.signTx(passphrase, transaction, [{
+    const signed = await node.unlockAndSignTx(passphrase, transaction, [{
       ...prevout,
       script: {
         stack: [
--- packages/jellyfish-wallet-encrypted/src/index.ts
+++ packages/jellyfish-wallet-encrypted/src/index.ts
@@ -1,38 +1,53 @@
-import { DERSignature, dSHA256 } from '@defichain/jellyfish-crypto'
+import { dSHA256 } from '@defichain/jellyfish-crypto'
 import { Bip32Options, MnemonicHdNode } from '@defichain/jellyfish-wallet-mnemonic'
 import { ScryptStorage } from './scrypt-storage'
-import secp256k1 from 'tiny-secp256k1'
 import * as bip32 from 'bip32'
 import { Transaction, Vout, TransactionSegWit } from '@defichain/jellyfish-transaction'
+import { WalletHdNode, WalletHdNodeProvider } from '@defichain/jellyfish-wallet'
 
 export * from './scryptsy'
 export * from './scrypt-storage'
 
-export class EncryptedMnemonicHdNode {
+export class EncryptedMnemonicHdNode implements WalletHdNode {
   constructor (
-    private readonly pubKey: Buffer,
     private readonly options: Bip32Options,
     private readonly path: string,
     private readonly scryptStorage: ScryptStorage
   ) {}
 
   /**
-   * @returns {Promise<Buffer>} 33 bytes public key
+   * @throws always throw error, this is encrypted
    */
   async publicKey (): Promise<Buffer> {
-    return this.pubKey
+    throw new Error('Encrypted')
   }
 
   /**
-   * To verify signature using public key from `this.publicKey()`
-   * @param {Buffer} hash original signed message of the `derSignature`
-   * @param {Buffer} derSignature result of a `WalletHdNode.sign()`
-   * @returns {Promise<boolean>} is signature valid
+   * @throws always throw error, this is encrypted
    */
   async verify (hash: Buffer, derSignature: Buffer): Promise<boolean> {
-    const signature = DERSignature.decode(derSignature)
-    // using lower logic, bip.Bip32Interface instantiation is very expensive (decrypt seed)
-    return secp256k1.verify(hash, this.pubKey, signature)
+    throw new Error('Encrypted')
+  }
+
+  /**
+   * @throws always throw error, this is encrypted
+   */
+  async privateKey (): Promise<Buffer> {
+    throw new Error('Encrypted')
+  }
+
+  /**
+   * @throws always throw error, this is encrypted
+   */
+  async sign (hash: Buffer): Promise<Buffer> {
+    throw new Error('Encrypted')
+  }
+
+  /**
+   * @throws always throw error, this is encrypted
+   */
+  async signTx (transaction: Transaction, prevouts: Vout[]): Promise<TransactionSegWit> {
+    throw new Error('Encrypted')
   }
 
   /**
@@ -40,7 +55,7 @@ export class EncryptedMnemonicHdNode {
    * @param {string} passphrase to unlock this.scryptStorage
    * @returns {Promise<bip32.BIP32Interface>}
    */
-  private async _unlock (passphrase: string): Promise<bip32.BIP32Interface> {
+  private async _decryptSeed (passphrase: string): Promise<bip32.BIP32Interface> {
     const seed = await this.scryptStorage.decrypt(passphrase)
     if (seed === null) {
       throw new Error('No encrypted seed found in storage')
@@ -56,8 +71,25 @@ export class EncryptedMnemonicHdNode {
    * @param {string} passphrase to unlock this.scryptStorage
    * @returns {Promise<MnemonicHdNode>}
    */
-  async unlock (passphrase: string): Promise<MnemonicHdNode> {
-    return new MnemonicHdNode(await this._unlock(passphrase), this.path)
+  private async _getHdNode (passphrase: string): Promise<MnemonicHdNode> {
+    return new MnemonicHdNode(await this._decryptSeed(passphrase), this.path)
+  }
+
+  /**
+   * @returns {Promise<Buffer>} 33 bytes public key
+   */
+  async unlockPublicKey (passphrase: string): Promise<Buffer> {
+    return await (await this._getHdNode(passphrase)).publicKey()
+  }
+
+  /**
+   * To verify signature using public key from `this.publicKey()`
+   * @param {Buffer} hash original signed message of the `derSignature`
+   * @param {Buffer} derSignature result of a `WalletHdNode.sign()`
+   * @returns {Promise<boolean>} is signature valid
+   */
+  async unlockAndVerify (passphrase: string, hash: Buffer, derSignature: Buffer): Promise<boolean> {
+    return await (await this._getHdNode(passphrase)).verify(hash, derSignature)
   }
 
   /**
@@ -66,8 +98,8 @@ export class EncryptedMnemonicHdNode {
    * @param {string} passphrase to unlock seed -> bip32interface -> private key
    * @returns {Buffer} 32 bytes private key
    */
-  async privateKey (passphrase: string): Promise<Buffer> {
-    return await (await this.unlock(passphrase)).privateKey()
+  async unlockPrivateKey (passphrase: string): Promise<Buffer> {
+    return await (await this._getHdNode(passphrase)).privateKey()
   }
 
   /**
@@ -76,9 +108,8 @@ export class EncryptedMnemonicHdNode {
    * @param {Buffer} hash message to sign
    * @returns {Buffer} signature
    */
-  async sign (passphrase: string, hash: Buffer): Promise<Buffer> {
-    const unlocked = (await this.unlock(passphrase))
-    return await unlocked.sign(hash)
+  async unlockAndSign (passphrase: string, hash: Buffer): Promise<Buffer> {
+    return await (await this._getHdNode(passphrase)).sign(hash)
   }
 
   /**
@@ -88,8 +119,8 @@ export class EncryptedMnemonicHdNode {
    * @param {Vout[]} prevouts
    * @returns {TransactionSegWit} signed transaction
    */
-  async signTx (passphrase: string, transaction: Transaction, prevouts: Vout[]): Promise<TransactionSegWit> {
-    return await (await this.unlock(passphrase)).signTx(transaction, prevouts)
+  async unlockAndSignTx (passphrase: string, transaction: Transaction, prevouts: Vout[]): Promise<TransactionSegWit> {
+    return await (await this._getHdNode(passphrase)).signTx(transaction, prevouts)
   }
 }
 
@@ -103,7 +134,7 @@ export interface CreateEncryptedMnemonicOptions extends LoadEncryptedMnemonicOpt
   seed: Buffer
 }
 
-export class EncryptedMnemonicProvider {
+export class EncryptedMnemonicProvider implements WalletHdNodeProvider<EncryptedMnemonicHdNode> {
   /**
    * @param {ScryptStorage} scryptStorage to store encrypted mnemonic seed
    * @param {Bip32Options} prefixOptions to reconstruct Bip32Interface when hdnode unlocked with passphrase
@@ -116,7 +147,8 @@ export class EncryptedMnemonicProvider {
   ) {}
 
   /**
-   * To create a provider which able to derive { @see EncryptedMnemonicHdNode } which has similar function as MnemonicHdNode
+   * To create a new provider which able to derive { @see EncryptedMnemonicHdNode } which has similar function as MnemonicHdNode
+   * with new passphrase
    *
    * @param {CreateEncryptedMnemonicOptions} encryptedMnemonicOptions
    * @param {ScryptStorage} encryptedMnemonicOptions.scryptStorage to store encrypted mnemonic seed
@@ -132,6 +164,13 @@ export class EncryptedMnemonicProvider {
     return new EncryptedMnemonicProvider(scryptStorage, options, seedHash)
   }
 
+  /**
+   * To instantiate an EncryptedMnemonicProvider from existing storage
+   * valid encryptedMnemonicOptions.passphrase expected to descrypt scrypted data successfully
+   *
+   * @param encryptedMnemonicOptions
+   * @returns {EncryptedMnemonicProvider}
+   */
   static async load (encryptedMnemonicOptions: LoadEncryptedMnemonicOptions): Promise<EncryptedMnemonicProvider> {
     const { passphrase, scryptStorage, options } = encryptedMnemonicOptions
     const seed = await scryptStorage.decrypt(passphrase)
@@ -142,36 +181,11 @@ export class EncryptedMnemonicProvider {
     return new EncryptedMnemonicProvider(scryptStorage, options, seedHash)
   }
 
-  /**
-   * @param {string} path Bip32Interface derivation path @example "44'/1129'/0'/0/0"
-   * @param {Buffer} seed the root seed, @see unlockAndDerive to derive by retrieving seed from encrypted storage
-   * @returns {EncryptedMnemonicHdNode}
-   */
-  async deriveWithSeed (path: string, seed: Buffer): Promise<EncryptedMnemonicHdNode> {
-    const root = bip32.fromSeed(seed)
-    const seedHash = dSHA256(seed).slice(0, 4)
-    if (Buffer.compare(seedHash, this.seedHash) !== 0) {
-      throw new Error('InvalidSeedHash')
-    }
+  derive (path: string): EncryptedMnemonicHdNode {
     return new EncryptedMnemonicHdNode(
-      root.derivePath(path).publicKey,
       this.options,
       path,
       this.scryptStorage
     )
-  }
-
-  /**
-   * To unlock (retrieve seed) and derive an `EncryptedMnemonicHdNode`
-   * @param {string} path Bip32Interface derivation path @example "44'/1129'/0'/0/0"
-   * @param {string} passphrase to unlock seed
-   * @returns {EncryptedMnemonicHdNode}
-   */
-  async unlockAndDerive (path: string, passphrase: string): Promise<EncryptedMnemonicHdNode> {
-    const seed = await this.scryptStorage.decrypt(passphrase)
-    if (seed === null) {
-      throw new Error('No encrypted seed found in storage')
-    }
-    return await this.deriveWithSeed(path, seed)
   }
 }
