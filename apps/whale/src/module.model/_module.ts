8f0d0a2d5e8fa6fe7a7e80698e42c7701158a814
Fuxing Loh <4266087+fuxingloh@users.noreply.github.com>
2021-07-28 15:23:14 +0800

    oracles scaffolding files for initial commit for easier reviewing experience (https://github.com/DeFiCh/whale/pull/276)
    * Index weightage
    
    * Improve code
    
    * Remove try-catch
    
    * Typo
    
    * Change code as per code review
    
    * Index price Feed
    
    * Improve code
    
    * Improve code
    
    * Minor fixes
    
    * Index price feed
    
    * Improve code
    
    * Minor refactoring
    
    * Minor
    
    * Index price data
    
    * Index aggregration price
    
    * Improve code
    
    * improve code
    
    * Improve code as per code review
    
    * Refactor code
    
    * Fix code to get latest oracleId
    
    * stable version
    
    * minor
    
    * Improve code
    
    * Improve code
    
    * Improve code
    
    * Improve test
    
    * Improve code as per code review
    
    * minor
    
    * Quick commit. Switch branch
    
    * Improve code
    
    * Stable version
    
    * Ensure rpc return same result
    
    * Refactor function name
    
    * Add 6th endpoint
    -> Able to query token and currency at a specific timestamp
    
    * Add 7th endpoint
    -> Able to query price percentage change for 2 timestamps
    
    * Improve test.
    Add last price updated time.
    
    * Improve test.
    Add another endpoint - Able to query all price for the interval of 2 timestamps.
    Continue refactoring code
    
    * Reduce the no of indexers from 4 to 3
    
    * Refactor code
    
    * Refactor code
    
    * Refactor price aggregration indexer
    
    * Improve indexers related code
    
    * Improve price data indexer code
    
    * Refactor mappers and indexers
    
    * Refactor test
    
    * Fix flanky test (Sometimes test pass sometimes fail)
    
    * Stable version
    
    * Fix flanky test
    
    * Revert "Fix flanky test"
    
    This reverts commit 8e2c8c4484a486b70c1a902bf2127893206cc629.
    
    * Fix typo and alignment
    
    * Remove price change percentage function
    
    * Add pagination to listTokenCurrencies function
    
    * Sort the result of getTokenCurrencies function
    
    * Add pagination to getTokenCurrencies function
    
    * Add pagination to getTokenCurrencies function
    
    * Add pagination feature to getPriceData
    
    * Add pagination feature to getPriceInterval function
    
    * Revert wrong commit
    
    * Refactor code
    
    * Apply changes for code review
    
    * Change from number to bignumber.
    Also, filter price with LIVE
    
    * Reduce indexers from 3 to 2
    
    * Fix lint errors
    
    * Attempt to fix flanky test
    
    * Attempt to fix flanky test
    
    * Attempt to fix flanky test
    
    * Attempt to fix flanky test
    
    * Attempt to fix flanky test
    
    * Revert "Attempt to fix flanky test"
    
    This reverts commit 69c9a0e6
    
    * Revert "Attempt to fix flanky test"
    
    This reverts commit c7b37231
    
    * Revert "Attempt to fix flanky test"
    
    This reverts commit 3548dd1d
    
    * Revert "Attempt to fix flanky test"
    
    This reverts commit fc583593
    
    * Revert "Attempt to fix flanky test"
    
    This reverts commit 51ab31f6
    
    * Attempt to fix flanky test
    by replacing
    await container.generate(1) with
    await waitForHeight
    
    * Revert "Attempt to fix flanky test by replacing await container.generate(1) with await waitForHeight"
    
    This reverts commit b90eedbd
    
    * Refactor code
    
    * Refactor code
    
    * Refactor code and don't set Oracle status to REMOVED unless tx type = REMOVEORACLE
    
    * Add comment
    
    * Refactored some parts
    
    * Fix tests
    
    * Fix more tests
    
    * Fix e2e test
    
    * Added invalidate tests
    
    * Describe should have own container reference
    
    * Rename timestamp1 to start and timestamp2 to end
    
    * Remove tokencurrency mapper and add OP_DEFI_TX_REMOVE_ORACLE to invalidate function
    
    * Revert "Remove tokencurrency mapper and add OP_DEFI_TX_REMOVE_ORACLE to invalidate function"
    
    This reverts commit d59db554
    
    * Get latest price for an oracle
    
    * Simplified aggregator
    
    * Change operator and fix test
    
    * Fix flanky test
    
    * revert to oracles scaffolding files first
    
    * added temp empty test
    
    Co-authored-by: jingyi2811 <jingyi2811@gmail.com>
    Co-authored-by: Ahmed Hilali <monstrobishi@gmail.com>

--- apps/whale/src/module.model/_module.ts
+++ apps/whale/src/module.model/_module.ts
@@ -1,4 +1,5 @@
 import { Global, Module } from '@nestjs/common'
+import { ModelProbeIndicator } from '@src/module.model/_model.probes'
 import { RawBlockMapper } from '@src/module.model/raw.block'
 import { BlockMapper } from '@src/module.model/block'
 import { ScriptActivityMapper } from '@src/module.model/script.activity'
@@ -7,7 +8,6 @@ import { ScriptUnspentMapper } from '@src/module.model/script.unspent'
 import { TransactionMapper } from '@src/module.model/transaction'
 import { TransactionVinMapper } from '@src/module.model/transaction.vin'
 import { TransactionVoutMapper } from '@src/module.model/transaction.vout'
-import { ModelProbeIndicator } from '@src/module.model/_model.probes'
 
 @Global()
 @Module({
