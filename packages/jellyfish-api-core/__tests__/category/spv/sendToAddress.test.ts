372de8029ff8f12716993f8eb143dda4c84b9519
jouzo <jdesclercs@gmail.com>
2021-07-29 16:43:17 +0200

    Use BigNumber for amount and feerate.

--- packages/jellyfish-api-core/__tests__/category/spv/sendToAddress.test.ts
+++ packages/jellyfish-api-core/__tests__/category/spv/sendToAddress.test.ts
@@ -1,7 +1,7 @@
 import { MasterNodeRegTestContainer } from '@defichain/testcontainers'
 import { RpcApiError } from '@defichain/jellyfish-api-core'
 import { ContainerAdapterClient } from '../../container_adapter_client'
-import { DEFAULT_FEE_RATE } from '../../../src/category/spv'
+import BigNumber from 'bignumber.js'
 
 describe('Spv', () => {
   const container = new MasterNodeRegTestContainer()
@@ -21,30 +21,31 @@ describe('Spv', () => {
 
   it('should sendToAddress', async () => {
     const otherAddress = 'bcrt1qfpnmx6jrn30yvscrw9spudj5aphyrc8es6epva'
-    const amount = 0.1
+    const amount = new BigNumber(0.1)
 
     const result = await client.spv.sendToAddress(otherAddress, amount)
     expect(typeof result.txid).toStrictEqual('string')
     expect(result.sendmessage).toStrictEqual('Success')
 
-    const balance = await container.call('spv_getbalance')
-    expect(balance).toStrictEqual(1 - amount - DEFAULT_FEE_RATE)
+    const balance = new BigNumber(await container.call('spv_getbalance'))
+    const expectedBalance = new BigNumber(1).minus(amount)
+    expect(balance.lte(expectedBalance)).toStrictEqual(true)
   })
 
   it('should not sendToAddress for invalid address', async () => {
-    const promise = client.spv.sendToAddress('XXXX', 1)
+    const promise = client.spv.sendToAddress('XXXX', new BigNumber(1))
     await expect(promise).rejects.toThrow(RpcApiError)
     await expect(promise).rejects.toThrow("RpcApiError: 'Invalid address', code: -5, method: spv_sendtoaddress")
   })
 
   it('should not sendToAddress with amount < 0', async () => {
-    const promise = client.spv.sendToAddress(await container.call('spv_getnewaddress'), -1)
+    const promise = client.spv.sendToAddress(await container.call('spv_getnewaddress'), new BigNumber(-1))
     await expect(promise).rejects.toThrow(RpcApiError)
     await expect(promise).rejects.toThrow("RpcApiError: 'Amount out of range', code: -3, method: spv_sendtoaddress")
   })
 
   it('should not sendToAddress with amount > 1200000000', async () => {
-    const promise = client.spv.sendToAddress(await container.call('spv_getnewaddress'), 1_200_000_000.1)
+    const promise = client.spv.sendToAddress(await container.call('spv_getnewaddress'), new BigNumber(1_200_000_000.1))
     await expect(promise).rejects.toThrow(RpcApiError)
     await expect(promise).rejects.toThrow("RpcApiError: 'Amount out of range', code: -3, method: spv_sendtoaddress")
   })
@@ -68,19 +69,20 @@ describe('Spv with custom feerate', () => {
 
   it('should sendToAddress with custom feerate', async () => {
     const otherAddress = 'bcrt1qfpnmx6jrn30yvscrw9spudj5aphyrc8es6epva'
-    const amount = 0.1
-    const feerate = 100000
+    const amount = new BigNumber(0.1)
+    const feerate = new BigNumber(1000)
 
-    const result = await client.spv.sendToAddress(otherAddress, 0.1, { feerate })
+    const result = await client.spv.sendToAddress(otherAddress, amount, { feerate })
     expect(typeof result.txid).toStrictEqual('string')
     expect(result.sendmessage).toStrictEqual('Success')
 
-    const balance = await container.call('spv_getbalance')
-    expect(balance).toBeLessThanOrEqual(1 - amount - feerate / 1_000_000_000)
+    const balance = new BigNumber(await container.call('spv_getbalance'))
+    const expectedBalance = new BigNumber(1).minus(amount)
+    expect(balance.lte(expectedBalance)).toStrictEqual(true)
   })
 
   it('should not sendToAddress with feerate below minimum acceptable amount', async () => {
-    const promise = client.spv.sendToAddress(await container.call('spv_getnewaddress'), 0.1, { feerate: 999 })
+    const promise = client.spv.sendToAddress(await container.call('spv_getnewaddress'), new BigNumber(0.1), { feerate: new BigNumber(999) })
     await expect(promise).rejects.toThrow(RpcApiError)
     await expect(promise).rejects.toThrow("RpcApiError: 'Fee size below minimum acceptable amount', code: -8, method: spv_sendtoaddress")
   })
@@ -100,7 +102,7 @@ describe('Spv with insufficient funds', () => {
   })
 
   it('should not sendToAddress with insufficient funds', async () => {
-    const promise = client.spv.sendToAddress(await container.call('spv_getnewaddress'), 1)
+    const promise = client.spv.sendToAddress(await container.call('spv_getnewaddress'), new BigNumber(1))
     await expect(promise).rejects.toThrow(RpcApiError)
     await expect(promise).rejects.toThrow('RpcApiError: \'Insufficient funds\', code: -6, method: spv_sendtoaddress')
   })
@@ -108,7 +110,7 @@ describe('Spv with insufficient funds', () => {
   it('should not sendToAddress with insufficient funds to cover fee', async () => {
     await container.call('spv_fundaddress', [await container.call('spv_getnewaddress')]) // Funds 1 BTC
 
-    const promise = client.spv.sendToAddress(await container.call('spv_getnewaddress'), 1) // Sends 1 BTC
+    const promise = client.spv.sendToAddress(await container.call('spv_getnewaddress'), new BigNumber(1)) // Sends 1 BTC
     await expect(promise).rejects.toThrow(RpcApiError)
     await expect(promise).rejects.toThrow('RpcApiError: \'Insufficient funds to cover fee\', code: -6, method: spv_sendtoaddress')
   })
--- packages/jellyfish-api-core/src/category/spv.ts
+++ packages/jellyfish-api-core/src/category/spv.ts
@@ -1,7 +1,7 @@
 import { ApiClient } from '../.'
+import BigNumber from 'bignumber.js'
 
-export const DEFAULT_FEE_RATE = 0.000012
-export const DEFAULT_FEE_RATE_IN_SATOSHIS = 1200
+export const DEFAULT_FEE_RATE_IN_SATOSHIS = new BigNumber('10000')
 
 /**
  * SPV RPCs for DeFi Blockchain
@@ -47,13 +47,13 @@ export class Spv {
    * Send a Bitcoin amount to a given address.
    *
    * @param {string} address Bitcoin address
-   * @param {number} amount Bitcoin amount
+   * @param {BigNumber} amount Bitcoin amount
    * @param {SendToAddressOptions} [options]
-   * @param {number} [options.feerate] Fee rate in satoshis per KB. Minimum is 1000. Defaults to 1200. See DEFAULT_FEE_RATE_IN_SATOSHIS
+   * @param {BigNumber} [options.feerate] Fee rate in satoshis per KB. Minimum is 1000. Defaults to 10000. See DEFAULT_FEE_RATE_IN_SATOSHIS
    * @return {Promise<SendMessageResult>}
    */
-  async sendToAddress (address: string, amount: number, options = { feerate: DEFAULT_FEE_RATE_IN_SATOSHIS }): Promise<SendMessageResult> {
-    return await this.client.call('spv_sendtoaddress', [address, amount, options.feerate], 'number')
+  async sendToAddress (address: string, amount: BigNumber, options = { feerate: DEFAULT_FEE_RATE_IN_SATOSHIS }): Promise<SendMessageResult> {
+    return await this.client.call('spv_sendtoaddress', [address, amount, options.feerate], 'bignumber')
   }
 }
 
@@ -71,7 +71,7 @@ export interface ReceivedByAddressInfo {
 }
 
 export interface SendToAddressOptions {
-  feerate: number
+  feerate: BigNumber
 }
 
 export interface SendMessageResult {
