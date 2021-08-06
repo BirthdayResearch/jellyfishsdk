a1ada0d277e4dbf97c2916ea198bf8f53cb25720
Fuxing Loh <4266087+fuxingloh@users.noreply.github.com>
2021-08-06 17:07:15 +0800

    Moved CachePrefix to global.cache.ts with the inclusion of `SEMAPHORE = -1` (#328)
    Added logging to ExceptionInterceptor

--- apps/whale-api/src/module.api/cache/defid.cache.ts
+++ apps/whale-api/src/module.api/cache/defid.cache.ts
@@ -2,13 +2,8 @@ import { CACHE_MANAGER, Inject, Injectable } from '@nestjs/common'
 import { Cache } from 'cache-manager'
 import { JsonRpcClient } from '@defichain/jellyfish-api-jsonrpc'
 import { TokenInfo } from '@defichain/jellyfish-api-core/dist/category/token'
-import { GlobalCache } from '@src/module.api/cache/global.cache'
+import { CachePrefix, GlobalCache } from '@src/module.api/cache/global.cache'
 import { PoolPairInfo } from '@defichain/jellyfish-api-core/dist/category/poolpair'
-
-export enum CachePrefix {
-  TOKEN_INFO,
-  POOL_PAIR_INFO
-}
 
 @Injectable()
 export class DeFiDCache extends GlobalCache {
@@ -28,26 +23,14 @@ export class DeFiDCache extends GlobalCache {
   }
 
   private async fetchTokenInfo (id: string): Promise<TokenInfo | undefined> {
-    try {
-      const result = await this.rpcClient.token.listTokens({
-        including_start: true,
-        limit: 1,
-        start: Number.parseInt(id)
-      }, true)
+    // You won't get not found error for this
+    const result = await this.rpcClient.token.listTokens({
+      including_start: true,
+      limit: 1,
+      start: Number.parseInt(id)
+    }, true)
 
-      const tokens = Object.values(result)
-      if (tokens[0] === undefined) {
-        return undefined
-      }
-      return tokens[0]
-    } catch (err) {
-      /* istanbul ignore else */
-      if (err?.payload?.message === 'Token not found') {
-        return undefined
-      } else {
-        throw err
-      }
-    }
+    return result[id]
   }
 
   async getPoolPairInfo (id: string): Promise<PoolPairInfo | undefined> {
