a03f6cdc1d8fdbb105729eddee0b9d52a6d1c3df
websterlcl <w.canonbrother@gmail.com>
2021-08-06 10:51:46 +0800

    add jsdoc
    typo

--- packages/jellyfish-transaction-builder/src/txn/txn_builder_masternode.ts
+++ packages/jellyfish-transaction-builder/src/txn/txn_builder_masternode.ts
@@ -3,6 +3,13 @@ import { OP_CODES, Script, TransactionSegWit, CreateMasterNode, ResignMasterNode
 import { P2WPKHTxnBuilder } from './txn_builder'
 
 export class TxnBuilderMasternode extends P2WPKHTxnBuilder {
+  /**
+   * Build create masternode transaction
+   *
+   * @param {CreateMasterNode} createMasternode transaction to create
+   * @param {Script} changeScript to send unspent to after deducting the (converted + fees)
+   * @return {Promise<TransactionSegWit>}
+   */
   async create (createMasternode: CreateMasterNode, changeScript: Script): Promise<TransactionSegWit> {
     return await this.createDeFiTx(
       OP_CODES.OP_DEFI_TX_CREATE_MASTER_NODE(createMasternode),
@@ -11,6 +18,13 @@ export class TxnBuilderMasternode extends P2WPKHTxnBuilder {
     )
   }
 
+  /**
+   * Build resign masternode transaction
+   *
+   * @param {ResignMasterNode} resignMasterNode transaction to create
+   * @param {Script} changeScript to send unspent to after deducting the (converted + fees)
+   * @return {Promise<TransactionSegWit>}
+   */
   async resign (resignMasterNode: ResignMasterNode, changeScript: Script): Promise<TransactionSegWit> {
     return await this.createDeFiTx(
       OP_CODES.OP_DEFI_TX_RESIGN_MASTER_NODE(resignMasterNode),
