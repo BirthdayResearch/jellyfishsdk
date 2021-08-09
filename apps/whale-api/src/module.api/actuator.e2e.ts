e0ce522864ad2f542c12748b3e87615532fb056f
Fuxing Loh <4266087+fuxingloh@users.noreply.github.com>
2021-08-09 17:54:12 +0800

    reconfigure probes to unhealthy if defid blocks are more than 4 headers behind (#331)
    * reconfigure probes for idb
    
    defid.readiness dead if defid blocks are more than 4 headers behind
    model.readiness dead synced blocks are more than 4 blocks behind
    
    * added probes healthy with peers testing
    
    * Update _model.probes.ts

--- apps/whale-api/src/module.api/actuator.e2e.ts
+++ apps/whale-api/src/module.api/actuator.e2e.ts
@@ -1,29 +1,133 @@
-import { MasterNodeRegTestContainer } from '@defichain/testcontainers'
+import { MasternodeGroup, MasterNodeRegTestContainer } from '@defichain/testcontainers'
 import { NestFastifyApplication } from '@nestjs/platform-fastify'
 import { createTestingApp, waitForIndexedHeight } from '@src/e2e.module'
 
-const container = new MasterNodeRegTestContainer()
-let app: NestFastifyApplication
+describe('no peers', () => {
+  const container = new MasterNodeRegTestContainer()
+  let app: NestFastifyApplication
 
-beforeAll(async () => {
-  await container.start()
-  await container.waitForReady()
-  app = await createTestingApp(container)
+  beforeAll(async () => {
+    await container.start()
+    app = await createTestingApp(container)
 
-  await container.generate(3)
-  await waitForIndexedHeight(app, 2)
+    await container.generate(3)
+    await waitForIndexedHeight(app, 2)
+  })
+
+  afterAll(async () => {
+    try {
+      await app.close()
+    } finally {
+      await container.stop()
+    }
+  })
+
+  describe('/_actuator/probes/liveness', () => {
+    it('should wait until liveness', async () => {
+      const res = await app.inject({
+        method: 'GET',
+        url: '/_actuator/probes/liveness'
+      })
+
+      expect(res.statusCode).toStrictEqual(200)
+      expect(res.json()).toStrictEqual({
+        details: {
+          defid: {
+            status: 'up'
+          },
+          model: {
+            status: 'up'
+          }
+        },
+        error: {},
+        info: {
+          defid: {
+            status: 'up'
+          },
+          model: {
+            status: 'up'
+          }
+        },
+        status: 'ok'
+      })
+    })
+  })
+
+  describe('/_actuator/probes/readiness', () => {
+    it('should wait until readiness, but never will be as it lacks connections', async () => {
+      const res = await app.inject({
+        method: 'GET',
+        url: '/_actuator/probes/readiness'
+      })
+
+      expect(res.statusCode).toStrictEqual(503)
+      expect(res.json()).toStrictEqual({
+        details: {
+          defid: {
+            blocks: expect.any(Number),
+            headers: expect.any(Number),
+            initialBlockDownload: expect.any(Boolean),
+            peers: 0,
+            status: 'down'
+          },
+          model: {
+            count: {
+              defid: expect.any(Number),
+              index: expect.any(Number)
+            },
+            status: 'up'
+          }
+        },
+        error: {
+          defid: {
+            blocks: expect.any(Number),
+            headers: expect.any(Number),
+            initialBlockDownload: expect.any(Boolean),
+            peers: 0,
+            status: 'down'
+          }
+        },
+        info: {
+          model: {
+            count: {
+              defid: expect.any(Number),
+              index: expect.any(Number)
+            },
+            status: 'up'
+          }
+        },
+        status: 'error'
+      })
+    })
+  })
 })
 
-afterAll(async () => {
-  try {
-    await app.close()
-  } finally {
-    await container.stop()
-  }
-})
+describe('with peers', () => {
+  const group = new MasternodeGroup([
+    new MasterNodeRegTestContainer(),
+    new MasterNodeRegTestContainer()
+  ])
+  const container = group.get(0)
+  let app: NestFastifyApplication
 
-describe('/_actuator/probes/liveness', () => {
-  it('should wait until liveness', async () => {
+  beforeAll(async () => {
+    await group.start()
+    app = await createTestingApp(container)
+
+    await container.generate(10)
+    await group.waitForSync()
+    await waitForIndexedHeight(app, 9)
+  })
+
+  afterAll(async () => {
+    try {
+      await app.close()
+    } finally {
+      await group.stop()
+    }
+  })
+
+  it('/_actuator/probes/liveness healthy', async () => {
     const res = await app.inject({
       method: 'GET',
       url: '/_actuator/probes/liveness'
@@ -51,26 +155,22 @@ describe('/_actuator/probes/liveness', () => {
       status: 'ok'
     })
   })
-})
 
-describe('/_actuator/probes/readiness', () => {
-  // TODO(fuxingloh): /_actuator/probes/readiness tests for it to be ready
-
-  it('should wait until readiness, but never will be as it lacks connections', async () => {
+  it('/_actuator/probes/readiness healthy', async () => {
     const res = await app.inject({
       method: 'GET',
       url: '/_actuator/probes/readiness'
     })
 
-    expect(res.statusCode).toStrictEqual(503)
+    expect(res.statusCode).toStrictEqual(200)
     expect(res.json()).toStrictEqual({
       details: {
         defid: {
           blocks: expect.any(Number),
           headers: expect.any(Number),
-          initialBlockDownload: expect.any(Boolean),
-          peers: 0,
-          status: 'down'
+          initialBlockDownload: false,
+          peers: 1,
+          status: 'up'
         },
         model: {
           count: {
@@ -80,16 +180,15 @@ describe('/_actuator/probes/readiness', () => {
           status: 'up'
         }
       },
-      error: {
+      error: {},
+      info: {
         defid: {
           blocks: expect.any(Number),
           headers: expect.any(Number),
-          initialBlockDownload: expect.any(Boolean),
-          peers: 0,
-          status: 'down'
-        }
-      },
-      info: {
+          initialBlockDownload: false,
+          peers: 1,
+          status: 'up'
+        },
         model: {
           count: {
             defid: expect.any(Number),
@@ -98,7 +197,7 @@ describe('/_actuator/probes/readiness', () => {
           status: 'up'
         }
       },
-      status: 'error'
+      status: 'ok'
     })
   })
 })
