f753b8a9ddaf02d0f1f59ce4a26f249793ba44ac
jingyi2811 <jingyi2811@gmail.com>
2021-08-10 13:11:38 +0800

    Improve comment

--- packages/jellyfish-api-core/__tests__/category/uzynTest.test.ts
+++ packages/jellyfish-api-core/__tests__/category/uzynTest.test.ts
@@ -40,7 +40,7 @@ describe('Oracle', () => {
 
   it('should test', async () => {
     const from = await container.call('getnewaddress')
-    await createToken(from, 'DBTC', 100) // mint 100 tokens and mine a block
+    await createToken(from, 'DBTC', 100) // create and mint 100 tokens. Mine blocks after every call
     await accountToAccount('DBTC', 100, from) // send the 100 token via accounttoaccount to another address and mine a block
     await client.wallet.sendToAddress(from, 10) // send DFI (UTXO via sendtoaddress)
     await container.generate(1) // mine a block
