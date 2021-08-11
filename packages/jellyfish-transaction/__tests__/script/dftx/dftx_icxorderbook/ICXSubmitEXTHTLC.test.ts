0c96f7607f8d215c4839cc26d99b497fce711866
surangap <surangatco@gmail.com>
2021-08-11 18:43:19 +0800

    Added dftx ICXSubmitEXTHTLC.

---
+++ packages/jellyfish-transaction/__tests__/script/dftx/dftx_icxorderbook/ICXSubmitEXTHTLC.test.ts
@@ -0,0 +1,64 @@
+import { SmartBuffer } from 'smart-buffer'
+import { OP_CODES } from '../../../../src/script'
+import { toBuffer, toOPCodes } from '../../../../src/script/_buffer'
+import { OP_DEFI_TX } from '../../../../src/script/dftx'
+import { CICXSubmitEXTHTLC, ICXSubmitEXTHTLC } from '../../../../src/script/dftx/dftx_icxorderbook'
+import { BigNumber } from '@defichain/jellyfish-json'
+
+it('should bi-directional buffer-object-buffer', () => {
+  const fixtures = [
+    '6a4c96446654783491470f554ca0816c2a11ed75a8e5939e915dbb62234ea51ac186ab2f8688e1db809698000000000020521a24e5418c971da262215bd30bd79f52611a63e038295b603f64fdc07f95223133734a51397742576838737369684855674161436d4e574a62424147354872394e21036494e7c9467c8c7ff3bf29e841907fb0fa24241866569944ea422479ec0e625218000000'
+  ]
+
+  fixtures.forEach(hex => {
+    const stack = toOPCodes(
+      SmartBuffer.fromBuffer(Buffer.from(hex, 'hex'))
+    )
+
+    const buffer = toBuffer(stack)
+    expect(buffer.toString('hex')).toBe(hex)
+    expect((stack[1] as OP_DEFI_TX).tx.type).toBe(0x34)
+  })
+})
+
+const header = '6a4c964466547834' // OP_RETURN(0x1a) OP_PUSHDATA1(0x4c) (length 150 = 0x96) CDfTx.SIGNATURE(0x44665478) CICXSubmitEXTHTLC.OP_CODE(0x34)
+// ICXSubmitEXTHTLC.offerTx[LE](0x91470f554ca0816c2a11ed75a8e5939e915dbb62234ea51ac186ab2f8688e1db) ICXSubmitEXTHTLC.amount(0x8096980000000000)
+// ICXSubmitEXTHTLC.hash[LE](0x20521a24e5418c971da262215bd30bd79f52611a63e038295b603f64fdc07f95) ICXSubmitEXTHTLC.htlcscriptAddress(0x223133734a51397742576838737369684855674161436d4e574a62424147354872394e)
+// ICXSubmitEXTHTLC.ownerPubkey(0x21036494e7c9467c8c7ff3bf29e841907fb0fa24241866569944ea422479ec0e6252) ICXSubmitEXTHTLC.timeout(0x18000000
+const data = '91470f554ca0816c2a11ed75a8e5939e915dbb62234ea51ac186ab2f8688e1db809698000000000020521a24e5418c971da262215bd30bd79f52611a63e038295b603f64fdc07f95223133734a51397742576838737369684855674161436d4e574a62424147354872394e21036494e7c9467c8c7ff3bf29e841907fb0fa24241866569944ea422479ec0e625218000000'
+
+const submitEXTHTLC: ICXSubmitEXTHTLC = {
+  offerTx: 'dbe188862fab86c11aa54e2362bb5d919e93e5a875ed112a6c81a04c550f4791',
+  amount: new BigNumber(0.10),
+  hash: '957fc0fd643f605b2938e0631a61529fd70bd35b2162a21d978c41e5241a5220',
+  htlcScriptAddress: '13sJQ9wBWh8ssihHUgAaCmNWJbBAG5Hr9N',
+  ownerPubkey: '036494e7c9467c8c7ff3bf29e841907fb0fa24241866569944ea422479ec0e6252',
+  timeout: 24
+}
+
+it('should craft dftx with OP_CODES._()', () => {
+  const stack = [
+    OP_CODES.OP_RETURN,
+    OP_CODES.OP_DEFI_TX_ICX_SUBMIT_EXT_HTLC(submitEXTHTLC)
+  ]
+
+  const buffer = toBuffer(stack)
+  expect(buffer.toString('hex')).toBe(header + data)
+})
+
+describe('Composable', () => {
+  it('should compose from buffer to composable', () => {
+    const buffer = SmartBuffer.fromBuffer(Buffer.from(data, 'hex'))
+    const composable = new CICXSubmitEXTHTLC(buffer)
+
+    expect(composable.toObject()).toEqual(submitEXTHTLC)
+  })
+
+  it('should compose from composable to buffer', () => {
+    const composable = new CICXSubmitEXTHTLC(submitEXTHTLC)
+    const buffer = new SmartBuffer()
+    composable.toBuffer(buffer)
+
+    expect(buffer.toBuffer().toString('hex')).toEqual(data)
+  })
+})
--- packages/jellyfish-transaction/src/script/dftx/dftx_icxorderbook.ts
+++ packages/jellyfish-transaction/src/script/dftx/dftx_icxorderbook.ts
@@ -2,6 +2,7 @@ import { BufferComposer, ComposableBuffer } from '../../buffer/buffer_composer'
 import BigNumber from 'bignumber.js'
 import { Script } from '../../tx'
 import { CScript } from '../../tx_composer'
+import { SmartBuffer } from 'smart-buffer'
 
 export enum ICXOrderType {
   /** type for DFI/BTC orders */
@@ -69,6 +70,50 @@ export class CICXSubmitDFCHTLC extends ComposableBuffer<ICXSubmitDFCHTLC> {
       ComposableBuffer.hexBEBufferLE(32, () => msg.offerTx, v => msg.offerTx = v),
       ComposableBuffer.satoshiAsBigNumber(() => msg.amount, v => msg.amount = v),
       ComposableBuffer.hexBEBufferLE(32, () => msg.hash, v => msg.hash = v),
+      ComposableBuffer.uInt32(() => msg.timeout, v => msg.timeout = v)
+    ]
+  }
+}
+
+/**
+ * ICXSubmitEXTHTLC DeFi transaction
+ */
+export interface ICXSubmitEXTHTLC {
+  offerTx: string // ----------| 32 byte, txid for which offer is this HTLC
+  amount: BigNumber // --------| 8 byte, amount that is put in HTLC
+  hash: string // -------------| 32 byte, hash for the hash lock part
+  htlcScriptAddress: string // -| 1 byte for len + len bytes, script address of external htlc
+  ownerPubkey: string // -------| 1 byte for len + len bytes, pubkey of the owner to which the funds are refunded if HTLC timeouts
+  timeout: number // ----------| 4 byte, timeout (absolute in block) for expiration of external htlc in external chain blocks
+}
+
+/**
+ * Composable ICXSubmitEXTHTLC, C stands for Composable.
+ * Immutable by design, bi-directional fromBuffer, toBuffer deep composer.
+ */
+export class CICXSubmitEXTHTLC extends ComposableBuffer<ICXSubmitEXTHTLC> {
+  static OP_CODE = 0x34
+  static OP_NAME = 'OP_DEFI_TX_ICX_SUBMIT_EXT_HTLC'
+
+  composers (msg: ICXSubmitEXTHTLC): BufferComposer[] {
+    return [
+      ComposableBuffer.hexBEBufferLE(32, () => msg.offerTx, v => msg.offerTx = v),
+      ComposableBuffer.satoshiAsBigNumber(() => msg.amount, v => msg.amount = v),
+      ComposableBuffer.hexBEBufferLE(32, () => msg.hash, v => msg.hash = v),
+      ComposableBuffer.varUIntUtf8BE(() => msg.htlcScriptAddress, v => msg.htlcScriptAddress = v),
+      { // NOTE(surangap): may be use optionalVarUIntHex when available or move this piece of code to buffer_composer.ts
+        fromBuffer: (buffer: SmartBuffer): void => {
+          const length = buffer.readUInt8()
+          const buff = Buffer.from(buffer.readBuffer(length))
+          msg.ownerPubkey = buff.toString('hex')
+        },
+        toBuffer: (buffer: SmartBuffer): void => {
+          const hex = msg.ownerPubkey
+          const buff: Buffer = Buffer.from(hex, 'hex')
+          buffer.writeUInt8(buff.length)
+          buffer.writeBuffer(buff)
+        }
+      },
       ComposableBuffer.uInt32(() => msg.timeout, v => msg.timeout = v)
     ]
   }
