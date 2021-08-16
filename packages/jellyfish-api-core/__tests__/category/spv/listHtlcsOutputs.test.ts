9112783cf0a5756d773352cc27443ab672249f76
jouzo <jdesclercs@gmail.com>
2021-08-16 13:15:26 +0200

    Adds test for multiple HTLC and specific HTLC address as filter + Refactor using Testing class from jellyfish-testing

--- packages/jellyfish-api-core/__tests__/category/spv/listHtlcsOutputs.test.ts
+++ packages/jellyfish-api-core/__tests__/category/spv/listHtlcsOutputs.test.ts
@@ -1,38 +1,37 @@
 import { MasterNodeRegTestContainer } from '@defichain/testcontainers'
 import { RpcApiError } from '@defichain/jellyfish-api-core'
-import { ContainerAdapterClient } from '../../container_adapter_client'
 import BigNumber from 'bignumber.js'
+import { Testing } from '@defichain/jellyfish-testing'
 
 describe('Spv', () => {
-  const container = new MasterNodeRegTestContainer()
-  const client = new ContainerAdapterClient(container)
+  const testing = Testing.create(new MasterNodeRegTestContainer())
 
   beforeAll(async () => {
-    await container.start()
-    await container.call('spv_fundaddress', [await container.call('spv_getnewaddress')]) // Funds 1 BTC
+    await testing.container.start()
+    await testing.container.spv.fundAddress(await testing.rpc.spv.getNewAddress()) // Funds 1 BTC
   })
 
   afterAll(async () => {
-    await container.stop()
+    await testing.container.stop()
   })
 
   it('should listHtlcOutputs', async () => {
-    const pubKeyA = await container.call('spv_getaddresspubkey', [await container.call('spv_getnewaddress')])
-    const pubKeyB = await container.call('spv_getaddresspubkey', [await container.call('spv_getnewaddress')])
+    const pubKeyA = await testing.rpc.spv.getAddressPubKey(await testing.rpc.spv.getNewAddress())
+    const pubKeyB = await testing.rpc.spv.getAddressPubKey(await testing.rpc.spv.getNewAddress())
     const timeout = 10
     const seed = 'aba5f7e9aecf6ec4372c8a1e49562680d066da4655ee8b4bb01640479fffeaa8'
     const seedhash = 'df95183883789f237977543885e1f82ddc045a3ba90c8f25b43a5b797a35d20e'
 
-    const htlc = await client.spv.createHtlc(pubKeyA, pubKeyB, { timeout: `${timeout}`, seedhash })
-    await container.generate(1)
-    const fund = await container.call('spv_sendtoaddress', [htlc.address, 0.1]) // Funds HTLC address
-    const claim = await client.spv.claimHtlc(
+    const htlc = await testing.rpc.spv.createHtlc(pubKeyA, pubKeyB, { timeout: `${timeout}`, seedhash })
+    await testing.container.generate(1)
+    const fund = await testing.rpc.spv.sendToAddress(htlc.address, new BigNumber(0.1)) // Funds HTLC address
+    const claim = await testing.rpc.spv.claimHtlc(
       htlc.address,
-      await container.call('spv_getnewaddress'),
+      await testing.rpc.spv.getNewAddress(),
       { seed }
     )
 
-    const list = await client.spv.listHtlcOutputs()
+    const list = await testing.rpc.spv.listHtlcOutputs()
     expect(list.length).toStrictEqual(1)
     expect(list[0]).toStrictEqual({
       txid: fund.txid,
@@ -47,30 +46,104 @@ describe('Spv', () => {
     })
   })
 
-  it('listHtlcOutputs should return empty list when called with a new address', async () => {
-    const pubKeyA = await container.call('spv_getaddresspubkey', [await container.call('spv_getnewaddress')])
-    const pubKeyB = await container.call('spv_getaddresspubkey', [await container.call('spv_getnewaddress')])
+  it('listHtlcOutputs should return empty list when called with a non HTLC address', async () => {
+    const pubKeyA = await testing.rpc.spv.getAddressPubKey(await testing.rpc.spv.getNewAddress())
+    const pubKeyB = await testing.rpc.spv.getAddressPubKey(await testing.rpc.spv.getNewAddress())
     const timeout = 10
     const seed = 'aba5f7e9aecf6ec4372c8a1e49562680d066da4655ee8b4bb01640479fffeaa8'
     const seedhash = 'df95183883789f237977543885e1f82ddc045a3ba90c8f25b43a5b797a35d20e'
 
-    const htlc = await client.spv.createHtlc(pubKeyA, pubKeyB, { timeout: `${timeout}`, seedhash })
-    await container.generate(1)
-    await container.call('spv_sendtoaddress', [htlc.address, 0.1]) // Funds HTLC address
-    await client.spv.claimHtlc(
+    const htlc = await testing.rpc.spv.createHtlc(pubKeyA, pubKeyB, { timeout: `${timeout}`, seedhash })
+    await testing.container.generate(1)
+    await testing.rpc.spv.sendToAddress(htlc.address, new BigNumber(0.1)) // Funds HTLC address
+    await testing.rpc.spv.claimHtlc(
       htlc.address,
-      await container.call('spv_getnewaddress'),
+      await testing.rpc.spv.getNewAddress(),
       { seed }
     )
 
-    const list = await client.spv.listHtlcOutputs(await container.call('spv_getnewaddress'))
+    const list = await testing.rpc.spv.listHtlcOutputs(await testing.rpc.spv.getNewAddress())
     expect(list.length).toStrictEqual(0)
   })
 
   it('should not listHtlcOutputs with invalid public address', async () => {
-    const promise = client.spv.listHtlcOutputs('XXXX')
+    const promise = testing.rpc.spv.listHtlcOutputs('XXXX')
 
     await expect(promise).rejects.toThrow(RpcApiError)
     await expect(promise).rejects.toThrow("RpcApiError: 'Invalid address', code: -5, method: spv_listhtlcoutputs")
+  })
+})
+
+describe('Spv with multiple HTLC', () => {
+  const testing = Testing.create(new MasterNodeRegTestContainer())
+
+  beforeAll(async () => {
+    await testing.container.start()
+    await testing.container.spv.fundAddress(await testing.rpc.spv.getNewAddress()) // Funds 1 BTC
+  })
+
+  afterAll(async () => {
+    await testing.container.stop()
+  })
+
+  it('should listHtlcOutputs with multiple HTLC', async () => {
+    const pubKeyA = await testing.rpc.spv.getAddressPubKey(await testing.rpc.spv.getNewAddress())
+    const pubKeyB = await testing.rpc.spv.getAddressPubKey(await testing.rpc.spv.getNewAddress())
+    const timeout = 10
+    const seedhash = 'df95183883789f237977543885e1f82ddc045a3ba90c8f25b43a5b797a35d20e'
+
+    const htlc = await testing.rpc.spv.createHtlc(pubKeyA, pubKeyB, { timeout: `${timeout}`, seedhash })
+    const htlc2 = await testing.rpc.spv.createHtlc(pubKeyA, pubKeyB, { timeout: `${timeout}`, seedhash }) // Creates a second HTLC
+    await testing.container.generate(1)
+    const fund = await testing.rpc.spv.sendToAddress(htlc.address, new BigNumber(0.1)) // Funds HTLC address
+    const fund2 = await testing.rpc.spv.sendToAddress(htlc2.address, new BigNumber(0.1)) // Funds second HTLC address
+
+    const list = await testing.rpc.spv.listHtlcOutputs()
+    expect(list.length).toStrictEqual(2)
+    expect(list[0]).toStrictEqual({
+      txid: fund.txid,
+      vout: expect.any(Number),
+      amount: new BigNumber(0.1),
+      address: htlc.address,
+      confirms: 1
+    })
+    expect(list[1]).toStrictEqual({
+      txid: fund2.txid,
+      vout: expect.any(Number),
+      amount: new BigNumber(0.1),
+      address: htlc2.address,
+      confirms: 1
+    })
+  })
+
+  it('should listHtlcOutputs with specific HTLC address as filter', async () => {
+    const pubKeyA = await testing.rpc.spv.getAddressPubKey(await testing.rpc.spv.getNewAddress())
+    const pubKeyB = await testing.rpc.spv.getAddressPubKey(await testing.rpc.spv.getNewAddress())
+    const timeout = 10
+    const seed = 'aba5f7e9aecf6ec4372c8a1e49562680d066da4655ee8b4bb01640479fffeaa8'
+    const seedhash = 'df95183883789f237977543885e1f82ddc045a3ba90c8f25b43a5b797a35d20e'
+
+    const htlc = await testing.rpc.spv.createHtlc(pubKeyA, pubKeyB, { timeout: `${timeout}`, seedhash })
+    await testing.container.generate(1)
+    const fund = await testing.rpc.spv.sendToAddress(htlc.address, new BigNumber(0.1)) // Funds HTLC address
+    const claim = await testing.rpc.spv.claimHtlc(
+      htlc.address,
+      await testing.rpc.spv.getNewAddress(),
+      { seed }
+    )
+
+    const list = await testing.rpc.spv.listHtlcOutputs(htlc.address)
+    expect(list.length).toStrictEqual(1)
+    expect(list[0]).toStrictEqual({
+      txid: fund.txid,
+      vout: expect.any(Number),
+      amount: new BigNumber(0.1),
+      address: htlc.address,
+      confirms: 1,
+      spent: {
+        txid: claim.txid,
+        confirms: 1
+      }
+    })
   })
 })
