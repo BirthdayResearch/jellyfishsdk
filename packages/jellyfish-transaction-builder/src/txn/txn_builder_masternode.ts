92c3ebc4ae14424ea69a85759af9ae54c8847fc5
websterlcl <w.canonbrother@gmail.com>
2021-08-18 11:12:53 +0800

    add network param to diff creation fee

--- packages/jellyfish-transaction-builder/src/txn/txn_builder_masternode.ts
+++ packages/jellyfish-transaction-builder/src/txn/txn_builder_masternode.ts
@@ -10,11 +10,12 @@ export class TxnBuilderMasternode extends P2WPKHTxnBuilder {
    * @param {Script} changeScript to send unspent to after deducting the (converted + fees)
    * @return {Promise<TransactionSegWit>}
    */
-  async create (createMasternode: CreateMasternode, changeScript: Script): Promise<TransactionSegWit> {
+  async create (createMasternode: CreateMasternode, changeScript: Script, network = 'regtest'): Promise<TransactionSegWit> {
+    const creationFee = network === 'regtest' ? new BigNumber('1') : new BigNumber('10')
     return await this.createDeFiTx(
       OP_CODES.OP_DEFI_TX_CREATE_MASTER_NODE(createMasternode),
       changeScript,
-      new BigNumber('1') // creation fee, regtest - 1, others - 10
+      creationFee
     )
   }
 
