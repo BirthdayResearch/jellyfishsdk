032657e4527cf3bed9be2054272a178251d756cb
jingyi2811 <jingyi2811@gmail.com>
2021-08-18 17:36:52 +0800

    Improve code

--- packages/jellyfish-api-core/__tests__/category/loan/setDefaultLoanScheme.test.ts
+++ packages/jellyfish-api-core/__tests__/category/loan/setDefaultLoanScheme.test.ts
@@ -1,46 +1,44 @@
-import { ContainerAdapterClient } from '../../container_adapter_client'
 import { LoanMasterNodeRegTestContainer } from './loan_container'
 import { UTXO } from '@defichain/jellyfish-api-core/category/loan'
 import BigNumber from 'bignumber.js'
+import { Testing } from '@defichain/jellyfish-testing'
 
 describe('Loan', () => {
   const container = new LoanMasterNodeRegTestContainer()
-  const client = new ContainerAdapterClient(container)
+  const testing = Testing.create(container)
 
   beforeAll(async () => {
-    await container.start()
-    await container.waitForReady()
-    await container.waitForWalletCoinbaseMaturity()
+    await testing.container.start()
+    await testing.container.waitForWalletCoinbaseMaturity()
 
-    await container.call('createloanscheme', [100, new BigNumber(1.5), 'scheme1'])
-    await container.generate(1)
+    await testing.container.call('createloanscheme', [100, new BigNumber(1.5), 'scheme1'])
+    await testing.generate(1)
 
-    await container.call('createloanscheme', [200, new BigNumber(2.5), 'scheme2'])
-    await container.generate(1)
-  })
-
-  afterAll(async () => {
-    await container.stop()
+    await testing.container.call('createloanscheme', [200, new BigNumber(2.5), 'scheme2'])
+    await testing.generate(1)
   })
 
   afterEach(async () => {
-    // NOTE(jingyi2811): Always set default scheme to scheme1
-    const data = await container.call('listloanschemes')
+    // Always set default scheme to scheme1
+    const data = await testing.container.call('listloanschemes')
     const record = data.find((d: { default: string }) => d.default)
     if (record.id !== 'scheme1') {
-      await client.loan.setDefaultLoanScheme('scheme1')
-      await container.generate(1)
+      await testing.rpc.loan.setDefaultLoanScheme('scheme1')
+      await testing.generate(1)
     }
   })
 
+  afterAll(async () => {
+    await testing.container.stop()
+  })
+
   it('should setDefaultLoanScheme', async () => {
-    const loanSchemeId = await client.loan.setDefaultLoanScheme('scheme2')
+    const loanSchemeId = await testing.rpc.loan.setDefaultLoanScheme('scheme2')
     expect(typeof loanSchemeId).toStrictEqual('string')
     expect(loanSchemeId.length).toStrictEqual(64)
+    await testing.generate(1)
 
-    await container.generate(1)
-
-    const result = await container.call('listloanschemes', [])
+    const result = await testing.container.call('listloanschemes', [])
     expect(result).toStrictEqual(
       [
         { id: 'scheme1', mincolratio: 100, interestrate: 1.5, default: false },
@@ -49,49 +47,58 @@ describe('Loan', () => {
     )
   })
 
-  it('should not setDefaultLoanScheme if the scheme is already a default', async () => {
-    const promise = client.loan.setDefaultLoanScheme('scheme1')
-    await expect(promise).rejects.toThrow('RpcApiError: \'Test DefaultLoanSchemeTx execution failed:\nLoan scheme with id scheme1 is already set as default\', code: -32600, method: setdefaultloanscheme')
-  })
-
-  it('should not setDefaultLoanScheme if id does not exists', async () => {
-    const promise = client.loan.setDefaultLoanScheme('scheme3')
-    await expect(promise).rejects.toThrow('RpcApiError: \'Test DefaultLoanSchemeTx execution failed:\nCannot find existing loan scheme with id scheme3\', code: -32600, method: setdefaultloanscheme')
+  it('should not setDefaultLoanScheme if id is an empty string', async () => {
+    const promise = testing.rpc.loan.setDefaultLoanScheme('')
+    await expect(promise).rejects.toThrow('RpcApiError: \'Test DefaultLoanSchemeTx execution failed:\nid cannot be empty or more than 8 chars long\', code: -32600, method: setdefaultloanscheme')
   })
 
   it('should not setDefaultLoanScheme if id is more than 8 chars long', async () => {
-    const promise = client.loan.setDefaultLoanScheme('123456789')
+    const promise = testing.rpc.loan.setDefaultLoanScheme('123456789')
     await expect(promise).rejects.toThrow('RpcApiError: \'Test DefaultLoanSchemeTx execution failed:\nid cannot be empty or more than 8 chars long\', code: -32600, method: setdefaultloanscheme')
   })
 
-  it('should not setDefaultLoanScheme if id is an empty string', async () => {
-    const promise = client.loan.setDefaultLoanScheme('')
-    await expect(promise).rejects.toThrow('RpcApiError: \'Test DefaultLoanSchemeTx execution failed:\nid cannot be empty or more than 8 chars long\', code: -32600, method: setdefaultloanscheme')
+  it('should not setDefaultLoanScheme if id does not exists', async () => {
+    const promise = testing.rpc.loan.setDefaultLoanScheme('scheme3')
+    await expect(promise).rejects.toThrow('RpcApiError: \'Test DefaultLoanSchemeTx execution failed:\nCannot find existing loan scheme with id scheme3\', code: -32600, method: setdefaultloanscheme')
   })
 
-  it('should not setDefaultLoanScheme if scheme is going to be deleted at future block', async () => {
-    await container.call('createloanscheme', [300, new BigNumber(3.5), 'scheme3'])
-    await container.generate(1)
+  it('should not setDefaultLoanScheme if the scheme is a default scheme', async () => {
+    const promise = testing.rpc.loan.setDefaultLoanScheme('scheme1')
+    await expect(promise).rejects.toThrow('RpcApiError: \'Test DefaultLoanSchemeTx execution failed:\nLoan scheme with id scheme1 is already set as default\', code: -32600, method: setdefaultloanscheme')
+  })
 
-    // NOTE(jingyi2811): Wait for block 100
-    await container.waitForBlockHeight(100)
+  it('should not setDefaultLoanScheme if the scheme is going to be deleted at future block', async () => {
+    await testing.container.call('createloanscheme', [300, new BigNumber(3.5), 'scheme3'])
+    await testing.generate(1)
 
-    // NOTE(jingyi2811): To delete at block 150
-    const loanSchemeId = await container.call('destroyloanscheme', ['scheme3', 150])
+    // Wait for block 110
+    await testing.container.waitForBlockHeight(110)
+
+    // To delete at block 120
+    const loanSchemeId = await testing.container.call('destroyloanscheme', ['scheme3', 120])
     expect(typeof loanSchemeId).toStrictEqual('string')
     expect(loanSchemeId.length).toStrictEqual(64)
-    await container.generate(1)
+    await testing.generate(1)
 
-    const promise = client.loan.setDefaultLoanScheme('scheme3')
-    await expect(promise).rejects.toThrow('RpcApiError: \'Test DefaultLoanSchemeTx execution failed:\nCannot set scheme3 as default, set to destroyed on block 150\', code: -32600, method: setdefaultloanscheme')
+    const promise = testing.rpc.loan.setDefaultLoanScheme('scheme3')
+    await expect(promise).rejects.toThrow('RpcApiError: \'Test DefaultLoanSchemeTx execution failed:\nCannot set scheme3 as default, set to destroyed on block 120\', code: -32600, method: setdefaultloanscheme')
 
-    // NOTE(jingyi2811): Delete at block 150
-    await container.waitForBlockHeight(150)
+    // Delete at block 120
+    await testing.container.waitForBlockHeight(120)
   })
 
   it('should setDefaultLoanScheme with utxos', async () => {
-    const address = await container.call('getnewaddress')
-    const utxos = await container.call('listunspent', [1, 9999999, [address], true])
+    const masternodes = await testing.container.call('listmasternodes')
+    let masternodeId = ''
+    for (const id in masternodes) {
+      const masternode = masternodes[id]
+      if (masternode.mintedBlocks > 0) { // Find masternode that mined at least one block
+        masternodeId = id
+        break
+      }
+    }
+
+    const utxos = await testing.container.call('listunspent', [1, 9999999, [masternodes[masternodeId].ownerAuthAddress]])
     const inputs: UTXO[] = utxos.map((utxo: UTXO) => {
       return {
         txid: utxo.txid,
@@ -99,14 +106,16 @@ describe('Loan', () => {
       }
     })
 
-    // NOTE(jingyi2811): Set default to scheme2
-    const loanSchemeId = await client.loan.setDefaultLoanScheme('scheme2', inputs)
+    const loanSchemeId = await testing.rpc.loan.setDefaultLoanScheme('scheme2', inputs)
     expect(typeof loanSchemeId).toStrictEqual('string')
     expect(loanSchemeId.length).toStrictEqual(64)
+    await testing.generate(1)
 
-    await container.generate(1)
+    const rawtx = await testing.container.call('getrawtransaction', [loanSchemeId, true])
+    expect(rawtx.vin[0].txid).toStrictEqual(utxos[0].txid)
+    expect(rawtx.vin[0].vout).toStrictEqual(utxos[0].vout)
 
-    const result = await container.call('listloanschemes', [])
+    const result = await testing.container.call('listloanschemes', [])
     expect(result).toStrictEqual(
       [
         { id: 'scheme1', mincolratio: 100, interestrate: 1.5, default: false },
@@ -115,9 +124,9 @@ describe('Loan', () => {
     )
   })
 
-  it('should not setDefaultLoanScheme with arbritary utxos', async () => {
-    const { txid, vout } = await container.fundAddress(await container.call('getnewaddress'), 10)
-    const promise = client.loan.setDefaultLoanScheme('scheme2', [{ txid, vout }])
+  it('should not setDefaultLoanScheme with arbitrary utxos', async () => {
+    const { txid, vout } = await testing.container.fundAddress(await testing.generateAddress(), 10)
+    const promise = testing.rpc.loan.setDefaultLoanScheme('scheme2', [{ txid, vout }])
     await expect(promise).rejects.toThrow('RpcApiError: \'Test DefaultLoanSchemeTx execution failed:\ntx not from foundation member!\', code: -32600, method: setdefaultloanscheme')
   })
 })
--- packages/jellyfish-api-core/src/category/loan.ts
+++ packages/jellyfish-api-core/src/category/loan.ts
@@ -1,7 +1,7 @@
 import { ApiClient } from '../.'
 
 /**
- * loan RPCs for DeFi Blockchain
+ * Loan RPCs for DeFi Blockchain
  */
 export class Loan {
   private readonly client: ApiClient
@@ -13,11 +13,11 @@ export class Loan {
   /**
    * Sets the default loan scheme.
    *
-   * @param {string} id
-   * @param {UTXO[]} [utxos = []]
-   * @param {string} [utxos.txid]
-   * @param {number} [utxos.vout]
-   * @return {Promise<string>} txid
+   * @param {string} id Unique identifier of the loan scheme, max 8 chars
+   * @param {UTXO[]} [utxos = []] Specific UTXOs to spend
+   * @param {string} [utxos.txid] Transaction Id
+   * @param {number} [utxos.vout] Output number
+   * @return {Promise<string>} Hex string of the transaction
    */
   async setDefaultLoanScheme (id: string, utxos: UTXO[] = []): Promise<string> {
     return await this.client.call('setdefaultloanscheme', [id, utxos], 'number')
