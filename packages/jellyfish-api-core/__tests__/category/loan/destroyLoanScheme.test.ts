687a4c454f8f77586f53efb5962895b3926234bc
jingyi2811 <jingyi2811@gmail.com>
2021-08-18 12:34:32 +0800

    Refactor code

--- packages/jellyfish-api-core/__tests__/category/loan/destroyLoanScheme.test.ts
+++ packages/jellyfish-api-core/__tests__/category/loan/destroyLoanScheme.test.ts
@@ -36,19 +36,23 @@ describe('Loan', () => {
     await testing.generate(1)
 
     // NOTE(jingyi2811): Before delete
-    let result = await testing.container.call('listloanschemes')
-    let data = result.filter((r: { id: string }) => r.id === 'scheme')
-    expect(data.length).toStrictEqual(1)
+    {
+      const data = await testing.container.call('listloanschemes')
+      const result = data.filter((d: { id: string }) => d.id === 'scheme')
+      expect(result.length).toStrictEqual(1)
+    }
 
     const loanSchemeId = await testing.rpc.loan.destroyLoanScheme('scheme')
-    await testing.generate(1)
     expect(typeof loanSchemeId).toStrictEqual('string')
     expect(loanSchemeId.length).toStrictEqual(64)
+    await testing.generate(1)
 
-    // NOTE(jingyi2811): after delete
-    data = await testing.container.call('listloanschemes')
-    result = data.filter((d: { id: string }) => d.id === 'scheme')
-    expect(result.length).toStrictEqual(0)
+    // NOTE(jingyi2811): After delete
+    {
+      const data = await testing.container.call('listloanschemes')
+      const result = data.filter((d: { id: string }) => d.id === 'scheme')
+      expect(result.length).toStrictEqual(0)
+    }
   })
 
   it('should not destroyLoanScheme if id is an empty string', async () => {
@@ -56,7 +60,7 @@ describe('Loan', () => {
     await expect(promise).rejects.toThrow('RpcApiError: \'Test DestroyLoanSchemeTx execution failed:\nid cannot be empty or more than 8 chars long\', code: -32600, method: destroyloanscheme')
   })
 
-  it('should not destroyLoanScheme if id is more than 8 chars', async () => {
+  it('should not destroyLoanScheme if id is more than 8 chars long', async () => {
     const promise = testing.rpc.loan.destroyLoanScheme('123456789')
     await expect(promise).rejects.toThrow('RpcApiError: \'Test DestroyLoanSchemeTx execution failed:\nid cannot be empty or more than 8 chars long\', code: -32600, method: destroyloanscheme')
   })
@@ -71,47 +75,52 @@ describe('Loan', () => {
     await expect(promise).rejects.toThrow('RpcApiError: \'Test DestroyLoanSchemeTx execution failed:\nCannot destroy default loan scheme, set new default first\', code: -32600, method: destroyloanscheme')
   })
 
-  it('should destroyLoanScheme at activateAfterBlock which is block 150', async () => {
+  it('should destroyLoanScheme at activateAfterBlock which is block 120', async () => {
     await testing.container.call('createloanscheme', [200, new BigNumber(2.5), 'scheme'])
     await testing.generate(1)
 
-    // NOTE(jingyi2811): Wait for block 100
-    await testing.container.waitForBlockHeight(100)
+    // NOTE(jingyi2811): Wait for block 110
+    await testing.container.waitForBlockHeight(110)
 
-    // NOTE(jingyi2811): To delete at block 150
-    const loanSchemeId = await testing.rpc.loan.destroyLoanScheme('scheme', 150)
+    // NOTE(jingyi2811): To delete at block 120
+    const loanSchemeId = await testing.rpc.loan.destroyLoanScheme('scheme', 120)
     expect(typeof loanSchemeId).toStrictEqual('string')
     expect(loanSchemeId.length).toStrictEqual(64)
     await testing.container.generate(1)
 
-    // NOTE(jingyi2811): shouldn't delete at block 101
-    let result = await testing.container.call('listloanschemes')
-    let data = result.filter((r: { id: string }) => r.id === 'scheme')
-    expect(data.length).toStrictEqual(1)
+    // NOTE(jingyi2811): Shouldn't delete at block 111
+    {
+      const data = await testing.container.call('listloanschemes')
+      const result = data.filter((d: { id: string }) => d.id === 'scheme')
+      expect(result.length).toStrictEqual(1)
+    }
 
-    await testing.container.waitForBlockHeight(150)
+    // NOTE(jingyi2811): Wait for block 120
+    await testing.container.waitForBlockHeight(120)
 
-    // NOTE(jingyi2811): should delete at block 150
-    result = await testing.container.call('listloanschemes')
-    data = result.filter((r: { id: string }) => r.id === 'scheme')
-    expect(data.length).toStrictEqual(0)
+    // NOTE(jingyi2811): Should delete at block 120
+    {
+      const data = await testing.container.call('listloanschemes')
+      const result = data.filter((d: { id: string }) => d.id === 'scheme')
+      expect(result.length).toStrictEqual(0)
+    }
   })
 
   it('should not destroyLoanScheme if activateAfterBlock is lesser than current height', async () => {
     await testing.container.call('createloanscheme', [200, new BigNumber(2.5), 'scheme'])
     await testing.generate(1)
 
-    // NOTE(jingyi2811): Wait for block 200
-    await testing.container.waitForBlockHeight(200)
+    // NOTE(jingyi2811): Wait for block 130
+    await testing.container.waitForBlockHeight(130)
 
-    // NOTE(jingyi2811): To delete at block 199, which should failed
-    const promise = testing.rpc.loan.destroyLoanScheme('scheme', 199)
+    // NOTE(jingyi2811): To delete at block 129, which should fail
+    const promise = testing.rpc.loan.destroyLoanScheme('scheme', 129)
     await expect(promise).rejects.toThrow('Destruction height below current block height, set future height\', code: -32600, method: destroyloanscheme')
   })
 
   it('should destroyLoanScheme with utxos', async () => {
     const address = await testing.generateAddress()
-    const utxos = await container.call('listunspent', [1, 9999999, [address], true])
+    const utxos = await testing.container.call('listunspent', [1, 9999999, [address], true])
     const inputs: UTXO[] = utxos.map((utxo: UTXO) => {
       return {
         txid: utxo.txid,
@@ -123,14 +132,13 @@ describe('Loan', () => {
     await testing.generate(1)
 
     const loanSchemeId = await testing.rpc.loan.destroyLoanScheme('scheme', undefined, { utxos: inputs })
-    await testing.generate(1)
-
     expect(typeof loanSchemeId).toStrictEqual('string')
     expect(loanSchemeId.length).toStrictEqual(64)
+    await testing.generate(1)
 
-    const result = await testing.container.call('listloanschemes')
-    const data = result.filter((r: { id: string }) => r.id === 'scheme')
-    expect(data.length).toStrictEqual(0)
+    const data = await testing.container.call('listloanschemes')
+    const result = data.filter((d: { id: string }) => d.id === 'scheme')
+    expect(result.length).toStrictEqual(0)
   })
 
   it('should not destroyLoanScheme with arbritary utxos', async () => {
--- packages/jellyfish-api-core/src/category/loan.ts
+++ packages/jellyfish-api-core/src/category/loan.ts
@@ -13,13 +13,13 @@ export class Loan {
   /**
    * Destroys a loan scheme.
    *
-   * @param {string} id
-   * @param {number} [activateAfterBlock]
+   * @param {string} id Unique identifier of the loan scheme
+   * @param {number} [activateAfterBlock] Block height at which new changes take effect
    * @param {DeleteLoanOptions} [options]
-   * @param {UTXO[]} [options.utxos = []]
-   * @param {string} [options.utxos.txid]
-   * @param {number} [options.utxos.vout]
-   * @return {Promise<string>}
+   * @param {UTXO[]} [options.utxos = []] Specific UTXOs to spend
+   * @param {string} [options.utxos.txid] Transaction Id
+   * @param {number} [options.utxos.vout] Output number
+   * @return {Promise<string>} hex string of the transaction
    */
   async destroyLoanScheme (id: string, activateAfterBlock?: number, options: DeleteLoanOptions = {}): Promise<string> {
     const { utxos = [] } = options
