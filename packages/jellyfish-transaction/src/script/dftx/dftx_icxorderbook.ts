7fcd05c5b84272561c96fde391e7dbe3186213d7
jouzo <jdesclercs@gmail.com>
2021-08-19 12:34:17 +0200

    restrict documentation receivePubkey to 0x00 | 0x21 | 0x41

--- packages/jellyfish-transaction/src/script/dftx/dftx_icxorderbook.ts
+++ packages/jellyfish-transaction/src/script/dftx/dftx_icxorderbook.ts
@@ -21,7 +21,7 @@ export interface ICXCreateOrder {
   orderType: number // -------------| 1 byte unsigned, 0x1 (INTERNAL) | 0x2 (EXTERNAL)
   tokenId: number // ---------------| VarUInt{1-9 bytes}
   ownerAddress: Script // ----------| n = VarUInt{1-9 bytes}, + n bytes
-  receivePubkey?: string // --------| n = VarUInt{1-9 bytes} (not optional, defaults to 0x00) + n bytes
+  receivePubkey?: string // --------| n = VarUInt{1-9 bytes}, 0x00 (default when undefined) | 0x21 (for COMPRESSED_PUBLIC_KEY_SIZE) | 0x41 (for PUBLIC_KEY_SIZE) + n bytes. See implementation at https://github.com/DeFiCh/ain/blob/ff53dcee23db2ffe0da9b147a0a53956f4e7ee31/src/pubkey.h#L57
   amountFrom: BigNumber // ---------| 8 bytes unsigned
   orderPrice: BigNumber // ---------| 8 bytes unsigned
   expiry: number // ----------------| 4 bytes unsigned
