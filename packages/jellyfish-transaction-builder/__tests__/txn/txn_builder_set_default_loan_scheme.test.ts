6dc9a9bbfd1d68d2cd9d57734ff493675431fce9
jingyi2811 <jingyi2811@gmail.com>
2021-08-19 12:20:19 +0800

    Minor

--- packages/jellyfish-transaction-builder/__tests__/txn/txn_builder_set_default_loan_scheme.test.ts
+++ packages/jellyfish-transaction-builder/__tests__/txn/txn_builder_set_default_loan_scheme.test.ts
@@ -5,119 +5,141 @@ import { fundEllipticPair, sendTransaction } from '../test.utils'
 import { WIF } from '@defichain/jellyfish-crypto'
 import BigNumber from 'bignumber.js'
 import { LoanMasterNodeRegTestContainer } from './loan_container'
+import { Testing } from '@defichain/jellyfish-testing'
 
 const container = new LoanMasterNodeRegTestContainer()
+const testing = Testing.create(container)
+
 let providers: MockProviders
 let builder: P2WPKHTransactionBuilder
 
 beforeAll(async () => {
-  await container.start()
-  await container.waitForWalletCoinbaseMaturity()
+  await testing.container.start()
+  await testing.container.waitForWalletCoinbaseMaturity()
 
-  providers = await getProviders(container)
+  providers = await getProviders(testing.container)
   providers.setEllipticPair(WIF.asEllipticPair(GenesisKeys[GenesisKeys.length - 1].owner.privKey))
   builder = new P2WPKHTransactionBuilder(providers.fee, providers.prevout, providers.elliptic)
 
-  // Prep 1000 DFI Token for testing
-  await container.waitForWalletBalanceGTE(1001)
+  // Default scheme
+  await testing.container.call('createloanscheme', [100, new BigNumber(1.5), 'scheme1'])
+  await testing.generate(1)
 
-  await container.call('createloanscheme', [100, new BigNumber(1.5), 'scheme1'])
-  await container.generate(1)
-
-  await container.call('createloanscheme', [200, new BigNumber(2.5), 'scheme2'])
-  await container.generate(1)
+  await testing.call('createloanscheme', [200, new BigNumber(2.5), 'scheme2'])
+  await testing.generate(1)
 })
 
 afterAll(async () => {
-  await container.stop()
+  await testing.container.stop()
 })
 
 beforeEach(async () => {
-  await container.waitForWalletBalanceGTE(11)
-
   // Fund 10 DFI UTXO
   await fundEllipticPair(container, providers.ellipticPair, 10)
   await providers.setupMocks() // required to move utxos
 })
 
 afterEach(async () => {
-  // NOTE(jingyi2811): Always set default scheme to scheme1
-  const data = await container.call('listloanschemes')
-  const record = data.find((d: { default: string }) => d.default)
-  if (record.id !== 'scheme1') {
-    await container.call('setdefaultloanscheme', ['scheme1'])
-    await container.generate(1)
+  const data = await testing.container.call('listloanschemes')
+  const result = data.filter((d: { default: boolean }) => !d.default)
+
+  for (let i = 0; i < result.length; i += 1) {
+    // Delete all schemes except default scheme
+    await testing.container.call('destroyloanscheme', [result[i].id])
+    await testing.generate(1)
   }
 })
 
-it('should setDefaultLoanScheme', async () => {
-  const script = await providers.elliptic.script()
-  const txn = await builder.loans.setDefaultLoanScheme({
-    identifier: 'scheme2'
-  }, script)
+describe('loan.setDefaultLoanScheme()', () => {
+  it('should setDefaultLoanScheme', async () => {
+    const script = await providers.elliptic.script()
+    const txn = await builder.loans.setDefaultLoanScheme({
+      identifier: 'scheme2'
+    }, script)
 
-  // Ensure the created txn is correct.
-  const outs = await sendTransaction(container, txn)
-  expect(outs[0].value).toStrictEqual(0)
-  expect(outs[1].value).toBeLessThan(10)
-  expect(outs[1].value).toBeGreaterThan(9.999)
-  expect(outs[1].scriptPubKey.addresses[0]).toStrictEqual(await providers.getAddress())
+    // Ensure the created txn is correct.
+    const outs = await sendTransaction(testing.container, txn)
+    expect(outs[0].value).toStrictEqual(0)
+    expect(outs[1].value).toBeLessThan(10)
+    expect(outs[1].value).toBeGreaterThan(9.999)
+    expect(outs[1].scriptPubKey.addresses[0]).toStrictEqual(await providers.getAddress())
 
-  // Ensure you don't send all your balance away during appoint oracle
-  const prevouts = await providers.prevout.all()
-  expect(prevouts.length).toStrictEqual(1)
-  expect(prevouts[0].value.toNumber()).toBeLessThan(10)
-  expect(prevouts[0].value.toNumber()).toBeGreaterThan(9.999)
+    // Ensure you don't send all your balance away during set default loan scheme
+    const prevouts = await providers.prevout.all()
+    expect(prevouts.length).toStrictEqual(1)
+    expect(prevouts[0].value.toNumber()).toBeLessThan(10)
+    expect(prevouts[0].value.toNumber()).toBeGreaterThan(9.999)
 
-  const result = await container.call('listloanschemes', [])
-  expect(result).toStrictEqual(
-    [
-      { id: 'scheme1', mincolratio: 100, interestrate: 1.5, default: false },
-      { id: 'scheme2', mincolratio: 200, interestrate: 2.5, default: true }
-    ]
-  )
-})
+    const result = await testing.container.call('listloanschemes', [])
+    expect(result).toStrictEqual(
+      [
+        { id: 'scheme1', mincolratio: 100, interestrate: 1.5, default: false },
+        { id: 'scheme2', mincolratio: 200, interestrate: 2.5, default: true }
+      ]
+    )
+  })
 
-it('should not setDefaultLoanScheme if the identifier is already a default', async () => {
-  const script = await providers.elliptic.script()
-  const txn = await builder.loans.setDefaultLoanScheme({
-    identifier: 'scheme1'
-  }, script)
+  it('should not setDefaultLoanScheme if identifier is an empty string', async () => {
+    const promise = testing.rpc.loan.setDefaultLoanScheme('')
+    await expect(promise).rejects.toThrow('RpcApiError: \'Test DefaultLoanSchemeTx execution failed:\nid cannot be empty or more than 8 chars long\', code: -32600, method: setdefaultloanscheme')
+  })
 
-  const promise = sendTransaction(container, txn)
-  await expect(promise).rejects.toThrow(DeFiDRpcError)
-  await expect(promise).rejects.toThrow('DefaultLoanSchemeTx: Loan scheme with id scheme1 is already set as default (code 16)\', code: -26')
-})
+  it('should not setDefaultLoanScheme if identifier is more than 8 chars long', async () => {
+    const script = await providers.elliptic.script()
+    const txn = await builder.loans.setDefaultLoanScheme({
+      identifier: '123456789'
+    }, script)
 
-it('should not setDefaultLoanScheme if identifier does not exists', async () => {
-  const script = await providers.elliptic.script()
-  const txn = await builder.loans.setDefaultLoanScheme({
-    identifier: 'scheme3'
-  }, script)
+    const promise = sendTransaction(container, txn)
+    await expect(promise).rejects.toThrow(DeFiDRpcError)
+    await expect(promise).rejects.toThrow('DefaultLoanSchemeTx: id cannot be empty or more than 8 chars long (code 16)\', code: -26')
+  })
 
-  const promise = sendTransaction(container, txn)
-  await expect(promise).rejects.toThrow(DeFiDRpcError)
-  await expect(promise).rejects.toThrow('DefaultLoanSchemeTx: Cannot find existing loan scheme with id scheme3 (code 16)\', code: -26')
-})
+  it('should not setDefaultLoanScheme if identifier does not exists', async () => {
+    const script = await providers.elliptic.script()
+    const txn = await builder.loans.setDefaultLoanScheme({
+      identifier: 'scheme3'
+    }, script)
 
-it('should not setDefaultLoanScheme if identifier is more than 8 chars long', async () => {
-  const script = await providers.elliptic.script()
-  const txn = await builder.loans.setDefaultLoanScheme({
-    identifier: '123456789'
-  }, script)
+    const promise = sendTransaction(container, txn)
+    await expect(promise).rejects.toThrow(DeFiDRpcError)
+    await expect(promise).rejects.toThrow('DefaultLoanSchemeTx: Cannot find existing loan scheme with id scheme3 (code 16)\', code: -26')
+  })
 
-  const promise = sendTransaction(container, txn)
-  await expect(promise).rejects.toThrow(DeFiDRpcError)
-  await expect(promise).rejects.toThrow('DefaultLoanSchemeTx: id cannot be empty or more than 8 chars long (code 16)\', code: -26')
-})
+  it('should not setDefaultLoanScheme if the identifier is a default scheme', async () => {
+    const script = await providers.elliptic.script()
+    const txn = await builder.loans.setDefaultLoanScheme({
+      identifier: 'scheme1'
+    }, script)
 
-it('should not setDefaultLoanScheme if identifier is an empty string', async () => {
-  const script = await providers.elliptic.script()
-  const txn = await builder.loans.setDefaultLoanScheme({
-    identifier: ''
-  }, script)
+    const promise = sendTransaction(container, txn)
+    await expect(promise).rejects.toThrow(DeFiDRpcError)
+    await expect(promise).rejects.toThrow('DefaultLoanSchemeTx: Loan scheme with id scheme1 is already set as default (code 16)\', code: -26')
+  })
 
-  const promise = sendTransaction(container, txn)
-  await expect(promise).rejects.toThrow(DeFiDRpcError)
-  await expect(promise).rejects.toThrow('DefaultLoanSchemeTx: id cannot be empty or more than 8 chars long (code 16)\', code: -26')
+  it('should not setDefaultLoanScheme if the scheme is going to be deleted at future block', async () => {
+    await testing.container.call('createloanscheme', [300, new BigNumber(3.5), 'scheme3'])
+    await testing.generate(1)
+
+    // Wait for block 110
+    await testing.container.waitForBlockHeight(110)
+
+    // To delete at block 120
+    const loanSchemeId = await testing.container.call('destroyloanscheme', ['scheme3', 120])
+    expect(typeof loanSchemeId).toStrictEqual('string')
+    expect(loanSchemeId.length).toStrictEqual(64)
+    await testing.generate(1)
+
+    const script = await providers.elliptic.script()
+    const txn = await builder.loans.setDefaultLoanScheme({
+      identifier: 'scheme3'
+    }, script)
+
+    // Ensure the created txn is correct.
+    const promise = sendTransaction(container, txn)
+    await expect(promise).rejects.toThrow('RpcApiError: \'Test DefaultLoanSchemeTx execution failed:\nCannot set scheme3 as default, set to destroyed on block 120\', code: -32600, method: setdefaultloanscheme')
+
+    // Delete at block 120
+    await testing.container.waitForBlockHeight(120)
+  })
 })
