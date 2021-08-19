0982bf25ae4f89162328eda00587ef112d68a390
jingyi2811 <jingyi2811@gmail.com>
2021-08-19 15:18:22 +0800

    Improve code

--- packages/jellyfish-api-core/__tests__/category/loan/updateLoanScheme.test.ts
+++ packages/jellyfish-api-core/__tests__/category/loan/updateLoanScheme.test.ts
@@ -1,59 +1,55 @@
-import { ContainerAdapterClient } from '../../container_adapter_client'
 import { LoanMasterNodeRegTestContainer } from './loan_container'
-import { UTXO } from '@defichain/jellyfish-api-core/category/loan'
 import BigNumber from 'bignumber.js'
+import { Testing } from '@defichain/jellyfish-testing'
+import { GenesisKeys } from '@defichain/testcontainers'
 
 describe('Loan', () => {
   const container = new LoanMasterNodeRegTestContainer()
-  const client = new ContainerAdapterClient(container)
+  const testing = Testing.create(container)
 
   beforeAll(async () => {
-    await container.start()
-    await container.waitForReady()
-    await container.waitForWalletCoinbaseMaturity()
+    await testing.container.start()
+    await testing.container.waitForWalletCoinbaseMaturity()
 
-    // NOTE(jingyi2811): default scheme
-    await container.call('createloanscheme', [100, new BigNumber(1.5), 'default'])
-    await container.generate(1)
+    // Default scheme
+    await testing.container.call('createloanscheme', [100, new BigNumber(1.5), 'default'])
+    await testing.generate(1)
   })
 
   beforeEach(async () => {
-    // NOTE(jingyi2811): Create scheme automatically in every test item
-    await container.call('createloanscheme', [200, new BigNumber(2.5), 'scheme'])
-    await container.generate(1)
+    // Create scheme automatically in every test item
+    await testing.container.call('createloanscheme', [200, new BigNumber(2.5), 'scheme1'])
+    await testing.generate(1)
   })
 
   afterEach(async () => {
-    const result = await container.call('listloanschemes')
-    const data = result.filter((r: { default: boolean }) => !r.default)
+    const data = await testing.container.call('listloanschemes')
+    const result = data.filter((d: { default: boolean }) => !d.default)
 
-    for (let i = 0; i < data.length; i += 1) {
-      // NOTE(jingyi2811): Delete all schemes except default scheme
-      await container.call('destroyloanscheme', [data[i].id])
-      await container.generate(1)
+    for (let i = 0; i < result.length; i += 1) {
+      // Delete all schemes except default scheme
+      await testing.container.call('destroyloanscheme', [result[i].id])
+      await testing.generate(1)
     }
   })
 
   afterAll(async () => {
-    await container.stop()
+    await testing.container.stop()
   })
 
   it('should updateLoanScheme', async () => {
-    const loanSchemeId = await client.loan.updateLoanScheme(300, new BigNumber(3.5), { id: 'scheme' })
-
+    const loanSchemeId = await testing.rpc.loan.updateLoanScheme(300, new BigNumber(3.5), { id: 'scheme2' })
     expect(typeof loanSchemeId).toStrictEqual('string')
     expect(loanSchemeId.length).toStrictEqual(64)
+    await testing.generate(1)
 
-    await container.generate(1)
-
-    const result = await container.call('listloanschemes')
-    const data = result.filter((r: { id: string }) => r.id === 'scheme')
-
-    expect(data.length).toStrictEqual(1)
-    expect(data[0]).toStrictEqual(
+    const data = await testing.container.call('listloanschemes')
+    const result = data.filter((d: { id: string }) => d.id === 'scheme2')
+    expect(result.length).toStrictEqual(1)
+    expect(result[0]).toStrictEqual(
       {
         default: false,
-        id: 'scheme',
+        id: 'scheme2',
         interestrate: 3.5,
         mincolratio: 300
       }
@@ -61,18 +57,23 @@ describe('Loan', () => {
   })
 
   it('should not updateLoanScheme if minConRatio is less than 100', async () => {
-    const promise = client.loan.updateLoanScheme(99, new BigNumber(4.5), { id: 'scheme' })
+    const promise = testing.rpc.loan.updateLoanScheme(99, new BigNumber(3.5), { id: 'scheme2' })
     await expect(promise).rejects.toThrow('RpcApiError: \'Test LoanSchemeTx execution failed:\nminimum collateral ratio cannot be less than 100\', code: -32600, method: updateloanscheme')
   })
 
-  it('should not updateLoanScheme if interestRate is less than 0', async () => {
-    const promise = client.loan.updateLoanScheme(400, new BigNumber(-1), { id: 'scheme' })
+  it('should not updateLoanScheme if interestRate is less than 0.01', async () => {
+    const promise = testing.rpc.loan.updateLoanScheme(400, new BigNumber(0.00999), { id: 'scheme2' })
     await expect(promise).rejects.toThrow('RpcApiError: \'Amount out of range\', code: -3, method: updateloanscheme')
   })
 
-  it('should not updateLoanScheme if scheme with same minConRatio and interestRate were created before', async () => {
-    const promise = client.loan.updateLoanScheme(100, new BigNumber(1.5), { id: 'scheme' })
+  it('should not updateLoanScheme if same minConRatio and interestRate were created before', async () => {
+    const promise = testing.rpc.loan.updateLoanScheme(100, new BigNumber(1.5), { id: 'scheme2' }) // Failed because its minColRatio and interestRate are same as default
     await expect(promise).rejects.toThrow('RpcApiError: \'Test LoanSchemeTx execution failed:\nLoan scheme default with same interestrate and mincolratio already exists\', code: -32600, method: updateloanscheme')
+  })
+
+  it('should not updateLoanScheme if id is not created before', async () => {
+    const promise = testing.updateLoanScheme(300, new BigNumber(3.5), { id: 'scheme3' })
+    await expect(promise).rejects.toThrow('RpcApiError: \'Test LoanSchemeTx execution failed:\nCannot find existing loan scheme with id scheme3\', code: -32600, method: updateloanscheme')
   })
 
   it('should not updateLoanScheme if id is an empty string', async () => {
@@ -85,14 +86,9 @@ describe('Loan', () => {
     await expect(promise).rejects.toThrow('RpcApiError: \'Test LoanSchemeTx execution failed:\nid cannot be empty or more than 8 chars long\', code: -32600, method: updateloanscheme')
   })
 
-  it('should not updateLoanScheme if id is not created before', async () => {
-    const promise = client.loan.updateLoanScheme(300, new BigNumber(3.5), { id: 'scheme2' })
-    await expect(promise).rejects.toThrow('RpcApiError: \'Test LoanSchemeTx execution failed:\nCannot find existing loan scheme with id scheme2\', code: -32600, method: updateloanscheme')
-  })
-
-  it('should updateLoanScheme at activateAfterBlock which is at block 150', async () => {
+  it('should updateLoanScheme at activateAfterBlock which is block 150', async () => {
     // NOTE(jingyi2811): Wait for block 100
-    await container.waitForBlockHeight(100)
+    await testing.container.waitForBlockHeight(100)
 
     // NOTE(jingyi2811): To update at block 150
     const loanSchemeId = await client.loan.updateLoanScheme(300, new BigNumber(3.5), { id: 'scheme', activateAfterBlock: 150 })
@@ -100,10 +96,10 @@ describe('Loan', () => {
     expect(typeof loanSchemeId).toStrictEqual('string')
     expect(loanSchemeId.length).toStrictEqual(64)
 
-    await container.generate(1)
+    await testing.generate(1)
 
     // NOTE(jingyi2811): shouldn't update at block 101
-    let result = await container.call('listloanschemes')
+    let result = await testing.container.call('listloanschemes')
     let data = result.filter((r: { id: string }) => r.id === 'scheme')
 
     expect(data.length).toStrictEqual(1)
@@ -117,10 +113,10 @@ describe('Loan', () => {
     )
 
     // NOTE(jingyi2811): Wait for block 150
-    await container.waitForBlockHeight(150)
+    await testing.container.waitForBlockHeight(150)
 
     // NOTE(jingyi2811): should update at block 150
-    result = await container.call('listloanschemes')
+    result = await testing.container.call('listloanschemes')
     data = result.filter((r: { id: string }) => r.id === 'scheme')
     expect(data.length).toStrictEqual(1)
     expect(data[0]).toStrictEqual(
@@ -135,7 +131,7 @@ describe('Loan', () => {
 
   it('should not updateLoanScheme if activateAfterBlock is less than current block', async () => {
     // NOTE(jingyi2811): Wait for block 200
-    await container.waitForBlockHeight(200)
+    await testing.container.waitForBlockHeight(200)
 
     // NOTE(jingyi2811): Attempt to updateLoanScheme on existing block
     const promise = client.loan.updateLoanScheme(300, new BigNumber(3.5), { id: 'scheme', activateAfterBlock: 100 })
@@ -144,13 +140,13 @@ describe('Loan', () => {
 
   it('should not updateLoanScheme if same minConRatio and interestRate pending loan scheme created before', async () => {
     // NOTE(jingyi2811): Wait for block 250
-    await container.waitForBlockHeight(250)
+    await testing.container.waitForBlockHeight(250)
 
     await client.loan.updateLoanScheme(300, new BigNumber(3.5), { id: 'scheme', activateAfterBlock: 300 })
-    await container.generate(1)
+    await testing.generate(1)
 
     // NOTE(jingyi2811): Create new scheme
-    await container.call('createloanscheme', [400, new BigNumber(4.5), 'scheme2'])
+    await testing.container.call('createloanscheme', [400, new BigNumber(4.5), 'scheme2'])
 
     // NOTE(jingyi2811): Updated same minColRatio and interestRate as the pending scheme
     const promise = client.loan.updateLoanScheme(300, new BigNumber(3.5), { id: 'scheme2' })
@@ -158,22 +154,15 @@ describe('Loan', () => {
   })
 
   it('should updateLoanScheme with utxos', async () => {
-    const address = await container.call('getnewaddress')
-    const utxos = await container.call('listunspent', [1, 9999999, [address], true])
-    const inputs: UTXO[] = utxos.map((utxo: UTXO) => {
-      return {
-        txid: utxo.txid,
-        vout: utxo.vout
-      }
-    })
+    const { txid, vout } = await testing.container.fundAddress(GenesisKeys[0].owner.address, 10)
 
-    const loanId = await client.loan.updateLoanScheme(300, new BigNumber(3.5), { id: 'scheme', utxos: inputs })
+    const loanId = await testing.updateLoanScheme(300, new BigNumber(3.5), { id: 'scheme', utxos: [{ txid, vout }] })
     expect(typeof loanId).toStrictEqual('string')
     expect(loanId.length).toStrictEqual(64)
 
-    await container.generate(1)
+    await testing.generate(1)
 
-    const result = await container.call('listloanschemes')
+    const result = await testing.container.call('listloanschemes')
     const data = result.filter((r: { id: string }) => r.id === 'scheme')
 
     expect(data.length).toStrictEqual(1)
@@ -183,7 +172,7 @@ describe('Loan', () => {
   })
 
   it('should not updateLoanScheme with arbritary utxos', async () => {
-    const { txid, vout } = await container.fundAddress(await container.call('getnewaddress'), 10)
+    const { txid, vout } = await testing.container.fundAddress(await testing.generateAddress(), 10)
     const promise = client.loan.updateLoanScheme(300, new BigNumber(3.5), { id: 'scheme', utxos: [{ txid, vout }] })
     await expect(promise).rejects.toThrow('RpcApiError: \'Test LoanSchemeTx execution failed:\ntx not from foundation member!\', code: -32600, method: updateloanscheme')
   })
