f334456da0ad886d4411600256290cde95eea340
jingyi2811 <jingyi2811@gmail.com>
2021-08-20 22:25:32 +0800

    Minor

--- packages/jellyfish-transaction-builder/src/index.ts
+++ packages/jellyfish-transaction-builder/src/index.ts
@@ -5,7 +5,7 @@ import { TxnBuilderAccount } from './txn/txn_builder_account'
 import { TxnBuilderOracles } from './txn/txn_builder_oracles'
 import { TxnBuilderLiqPool } from './txn/txn_builder_liq_pool'
 import { TxnBuilderMasternode } from './txn/txn_builder_masternode'
-import { TxnBuilderLoans } from '@defichain/jellyfish-transaction-builder/txn/txn_builder_loans'
+import { TxnBuilderLoans } from './txn/txn_builder_loans'
 
 export * from './provider'
 export * from './txn/txn_fee'
