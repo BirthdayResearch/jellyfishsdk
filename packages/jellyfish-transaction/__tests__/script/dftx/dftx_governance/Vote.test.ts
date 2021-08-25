65e84d688ec54a344c7f592f9a38454f53a07c86
jouzo <jdesclercs@gmail.com>
2021-08-25 09:56:09 +0200

    use toStrictEqual and document header + data

--- packages/jellyfish-transaction/__tests__/script/dftx/dftx_governance/Vote.test.ts
+++ packages/jellyfish-transaction/__tests__/script/dftx/dftx_governance/Vote.test.ts
@@ -15,14 +15,14 @@ it('should bi-directional buffer-object-buffer', () => {
     )
 
     const buffer = toBuffer(stack)
-    expect(buffer.toString('hex')).toBe(hex)
-    expect((stack[1] as OP_DEFI_TX).tx.type).toBe(0x56)
+    expect(buffer.toString('hex')).toStrictEqual(hex)
+    expect((stack[1] as OP_DEFI_TX).tx.type).toStrictEqual(0x56)
   })
 })
 
 describe('vote', () => {
-  const header = '6a464466547856' // OP_RETURN, PUSH_DATA(44665478, 56)
-  const data = '680aaa128c5f016b04f1a496ada226a99f5a18ead478a2ac6a665620b0987869b45fd5be75bde9dda6f11bb58e386a29834aa452413f3123f40acc6178026ce801'
+  const header = '6a464466547856' // OP_RETURN(0x6a) (length 70 = 0x46) CDfTx.SIGNATURE(0x44665478) Vote.OP_CODE(0x46)
+  const data = '680aaa128c5f016b04f1a496ada226a99f5a18ead478a2ac6a665620b0987869b45fd5be75bde9dda6f11bb58e386a29834aa452413f3123f40acc6178026ce801' // Vote.proposalId[BE](0x697898b02056666aaca278d4ea185a9fa926a2ad96a4f1046b015f8c12aa0a68) Vote.masternodeId[BE] (0xe86c027861cc0af423313f4152a44a83296a388eb51bf1a6dde9bd75bed55fb4) Vote.voteDecision(0x01)
   const Vote: Vote = {
     voteDecision: 0x01,
     proposalId: '697898b02056666aaca278d4ea185a9fa926a2ad96a4f1046b015f8c12aa0a68',
@@ -36,7 +36,7 @@ describe('vote', () => {
     ]
 
     const buffer = toBuffer(stack)
-    expect(buffer.toString('hex')).toBe(header + data)
+    expect(buffer.toString('hex')).toStrictEqual(header + data)
   })
 
   describe('Composable', () => {
@@ -44,7 +44,7 @@ describe('vote', () => {
       const buffer = SmartBuffer.fromBuffer(Buffer.from(data, 'hex'))
       const composable = new CVote(buffer)
 
-      expect(composable.toObject()).toEqual(Vote)
+      expect(composable.toObject()).toStrictEqual(Vote)
     })
 
     it('should compose from composable to buffer', () => {
@@ -52,7 +52,7 @@ describe('vote', () => {
       const buffer = new SmartBuffer()
       composable.toBuffer(buffer)
 
-      expect(buffer.toBuffer().toString('hex')).toEqual(data)
+      expect(buffer.toBuffer().toString('hex')).toStrictEqual(data)
     })
   })
 })
