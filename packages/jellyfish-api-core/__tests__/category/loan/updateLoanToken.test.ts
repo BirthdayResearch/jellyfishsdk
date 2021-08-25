bd7232ebe5475f7d5154223718e3cd16e5e0aa9e
jingyi2811 <jingyi2811@gmail.com>
2021-08-25 11:57:35 +0800

    Quick push, switch branch

--- packages/jellyfish-api-core/__tests__/category/loan/updateLoanToken.test.ts
+++ packages/jellyfish-api-core/__tests__/category/loan/updateLoanToken.test.ts
@@ -7,35 +7,47 @@ describe('Loan', () => {
   const container = new LoanMasterNodeRegTestContainer()
   const testing = Testing.create(container)
 
+  let priceFeedId: string
+  let loanTokenId: string
+
   beforeAll(async () => {
     await testing.container.start()
     await testing.container.waitForWalletCoinbaseMaturity()
-  })
 
-  it('should updateLoanToken', async () => {
-    const priceFeedId = await testing.container.call('appointoracle', [await container.getNewAddress('', 'legacy'), [{
-      token: 'AAPL',
-      currency: 'EUR'
+    priceFeedId = await testing.container.call('appointoracle', [await container.getNewAddress('', 'legacy'), [{
+      token: 'TOKEN',
+      currency: 'CURRENCY'
     }], 1])
     await testing.generate(1)
 
-    const loanTokenId = await testing.container.call('setloantoken', [
-      {
-        symbol: 'AAPL',
-        name: 'APPLE',
-        priceFeedId,
-        mintable: true,
-        interest: new BigNumber(0.01)
-      }, []])
-    await testing.generate(1)
-
-    await testing.rpc.loan.updateLoanToken({
-      token: 'AAPL',
-      symbol: 'AAPL',
-      name: 'APPLE',
+    loanTokenId = await testing.container.call('setloantoken', [{
+      symbol: 'TEST',
+      name: 'This is a test token',
       priceFeedId,
       mintable: true,
       interest: new BigNumber(0.01)
+    }, []])
+    await testing.generate(1)
+  })
+
+  afterEach(async () => {
+    await testing.rpc.loan.updateLoanToken({ // Always update to the existing record
+      token: 'TEST',
+      symbol: 'TEST',
+      name: 'This is a test token',
+      priceFeedId,
+      mintable: true,
+      interest: new BigNumber(0.01)
+    })
+    await testing.generate(1)
+  })
+
+  it('should updateLoanToken', async () => {
+    await testing.rpc.loan.updateLoanToken({
+      token: 'TEST',
+      symbol: 'TEST',
+      name: 'This is an ABC token',
+      priceFeedId
     })
     await testing.generate(1)
 
@@ -44,9 +56,9 @@ describe('Loan', () => {
       [loanTokenId]: {
         token: {
           1: {
-            symbol: 'AAPL',
-            symbolKey: 'AAPL',
-            name: 'APPLE',
+            symbol: 'TEST',
+            symbolKey: 'TEST',
+            name: 'This is an ABC token',
             decimal: 8,
             limit: 0,
             mintable: false,
@@ -64,112 +76,57 @@ describe('Loan', () => {
           }
         },
         priceFeedId,
-        interest: 0.01
+        interest: 0
       }
     })
   })
 
-  it('should updateLoanToken if symbol is more than 8 letters', async () => {
-    const priceFeedId = await testing.container.call('appointoracle', [await testing.generateAddress(), [{
-      token: 'TOKEN2',
-      currency: 'CURRENCY2'
-    }], 1])
-    await testing.generate(1)
-
-    const loanTokenId = await testing.container.call('setloantoken', [
-      {
-        symbol: 'TOKEN2',
-        name: 'TOKEN2',
-        priceFeedId,
-        mintable: true,
-        interest: new BigNumber(0.01)
-      }, []])
-    await testing.generate(1)
-
-    await testing.rpc.loan.updateLoanToken({
-      token: 'TOKEN2',
-      symbol: 'ABCDEFGHI',
-      name: 'TOKEN2',
-      priceFeedId,
-      mintable: true,
-      interest: new BigNumber(0.01)
-    })
-    await testing.generate(1)
-
-    const data = await testing.container.call('listloantokens', [])
-    const index = Object.keys(data).indexOf(loanTokenId) + 1
-    expect(data[loanTokenId].token[index].symbol).toStrictEqual('ABCDEFGH') // Only remain the first 8 letters
-  })
+  // it('should updateLoanToken if symbol is more than 8 letters', async () => {
+  //   await testing.rpc.loan.updateLoanToken({
+  //     token: 'TEST',
+  //     symbol: 'ABCDEFGH', // 9 letters
+  //     name: 'This is a test token',
+  //     priceFeedId
+  //   })
+  //   await testing.generate(1)
+  //
+  //   const data = await testing.container.call('listloantokens', [])
+  //   const index = Object.keys(data).indexOf(loanTokenId) + 1
+  //   expect(data[loanTokenId].token[index].symbol).toStrictEqual('ABCDEFGH') // Only remain the first 8 letters
+  // })
 
   it('should not updateLoanToken if token with same symbol was created before', async () => {
-    const priceFeedId1 = await testing.container.call('appointoracle', [await testing.generateAddress(), [{
-      token: 'TOKEN3',
-      currency: 'CURRENCY3'
+    const priceFeedId = await testing.container.call('appointoracle', [await testing.generateAddress(), [{
+      token: 'TOKEN1',
+      currency: 'CURRENCY1'
     }], 1])
     await testing.generate(1)
 
-    await testing.container.call('setloantoken', [
-      {
-        symbol: 'TOKEN3',
-        name: 'TOKEN3',
-        priceFeedId: priceFeedId1,
-        mintable: true,
-        interest: new BigNumber(0.01)
-      }, []])
-    await testing.generate(1)
-
-    const priceFeedId2 = await testing.container.call('appointoracle', [await testing.generateAddress(), [{
-      token: 'TOKEN4',
-      currency: 'CURRENCY4'
-    }], 1])
-    await testing.generate(1)
-
-    await testing.container.call('setloantoken', [
-      {
-        symbol: 'TOKEN4',
-        name: 'TOKEN4',
-        priceFeedId: priceFeedId2,
-        mintable: true,
-        interest: new BigNumber(0.01)
-      }, []])
+    await testing.container.call('setloantoken', [{
+      symbol: 'TOKEN1',
+      name: 'TOKEN1',
+      priceFeedId
+    }, []])
     await testing.generate(1)
 
     const promise = testing.rpc.loan.updateLoanToken({
-      token: 'TOKEN4',
-      symbol: 'TOKEN3',
-      name: 'TOKEN4',
-      priceFeedId: priceFeedId2,
+      token: 'TOKEN1',
+      symbol: 'TEST', // Symbol already exists
+      name: 'TOKEN1',
+      priceFeedId,
       mintable: true,
       interest: new BigNumber(0.01)
     })
 
-    await expect(promise).rejects.toThrow('RpcApiError: \'Test LoanUpdateLoanTokenTx execution failed:\ntoken with key \'TOKEN3\' already exists!\', code: -32600, method: updateloantoken')
+    await expect(promise).rejects.toThrow('RpcApiError: \'Test LoanUpdateLoanTokenTx execution failed:\ntoken with key \'TEST\' already exists!\', code: -32600, method: updateloantoken')
   })
 
   it('should updateLoanToken if name is more than 128 letters', async () => {
-    const priceFeedId = await testing.container.call('appointoracle', [await testing.generateAddress(), [{
-      token: 'TOKEN5',
-      currency: 'CURRENCY5'
-    }], 1])
-    await testing.generate(1)
-
-    const loanTokenId = await testing.container.call('setloantoken', [
-      {
-        symbol: 'TOKEN5',
-        name: 'TOKEN5',
-        priceFeedId,
-        mintable: true,
-        interest: new BigNumber(0.01)
-      }, []])
-    await testing.generate(1)
-
     await testing.rpc.loan.updateLoanToken({
-      token: 'TOKEN5',
-      symbol: 'TOKEN5',
+      token: 'TEST',
+      symbol: 'TOKEN',
       name: 'ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXY', // 129 letters
-      priceFeedId,
-      mintable: true,
-      interest: new BigNumber(0.01)
+      priceFeedId
     })
     await testing.generate(1)
 
@@ -180,61 +137,29 @@ describe('Loan', () => {
 
   it('should not updateLoanToken if token does not exist', async () => {
     const promise = testing.rpc.loan.updateLoanToken({
-      token: 'TOKEN6',
-      symbol: 'TOKEN6',
-      name: 'TOKEN6',
+      token: 'NOTEXISTSTOKEN',
+      symbol: 'NOTEXISTSTOKEN',
+      name: 'This token does not exists',
       priceFeedId: 'e40775f8bb396cd3d94429843453e66e68b1c7625d99b0b4c505ab004506697b',
       mintable: true,
       interest: new BigNumber(0.01)
     })
-    await expect(promise).rejects.toThrow('RpcApiError: \'Token TOKEN6 does not exist!\', code: -8, method: updateloantoken')
+    await expect(promise).rejects.toThrow('RpcApiError: \'Token NOTEXISTSTOKEN does not exist!\', code: -8, method: updateloantoken')
   })
 
-  it('should not updateLoanToken if priceFeed id is invalid', async () => {
-    const priceFeedId = await testing.container.call('appointoracle', [await testing.generateAddress(), [{
-      token: 'TOKEN7',
-      currency: 'CURRENCY7'
-    }], 1])
-    await testing.generate(1)
-
-    await testing.container.call('setloantoken', [
-      {
-        symbol: 'TOKEN7',
-        name: 'TOKEN7',
-        priceFeedId,
-        mintable: true,
-        interest: new BigNumber(0.01)
-      }, []])
-    await testing.generate(1)
-
-    const promise = testing.rpc.loan.updateLoanToken({
-      token: 'TOKEN7',
-      symbol: 'TOKEN7',
-      name: 'TOKEN7',
-      priceFeedId: 'e40775f8bb396cd3d94429843453e66e68b1c7625d99b0b4c505ab004506697b',
-      mintable: true,
-      interest: new BigNumber(0.01)
-    })
-    await expect(promise).rejects.toThrow('RpcApiError: \'Test LoanUpdateLoanTokenTx execution failed:\noracle (63b4c16d95d68ba65f1317cdcf79e03ef2a64b7e6479bda3801a24084c071693) does not exist!\', code: -32600, method: updateloantoken')
-  })
+  // it('should not updateLoanToken if priceFeed id is invalid', async () => {
+  //   const promise = testing.rpc.loan.updateLoanToken({
+  //     token: 'TEST',
+  //     symbol: 'TEST',
+  //     name: 'TEST',
+  //     priceFeedId: 'e40775f8bb396cd3d94429843453e66e68b1c7625d99b0b4c505ab004506697b',
+  //     mintable: true,
+  //     interest: new BigNumber(0.01)
+  //   })
+  //   await expect(promise).rejects.toThrow('RpcApiError: \'Test LoanUpdateLoanTokenTx execution failed:\noracle (63b4c16d95d68ba65f1317cdcf79e03ef2a64b7e6479bda3801a24084c071693) does not exist!\', code: -32600, method: updateloantoken')
+  // })
 
   it('should updateLoanToken if mintable is false', async () => {
-    const priceFeedId = await testing.container.call('appointoracle', [await testing.generateAddress(), [{
-      token: 'TOKEN8',
-      currency: 'CURRENCY8'
-    }], 1])
-    await testing.generate(1)
-
-    await testing.container.call('setloantoken', [
-      {
-        symbol: 'TOKEN8',
-        name: 'TOKEN8',
-        priceFeedId,
-        mintable: true,
-        interest: new BigNumber(0.01)
-      }, []])
-    await testing.generate(1)
-
     const loanTokenId = await testing.rpc.loan.updateLoanToken({
       token: 'TOKEN8',
       symbol: 'TOKEN8',
