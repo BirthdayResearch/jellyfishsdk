217a129411ac803012c0587585e432607ac041ca
Ahmed Hilali <monstrobishi@gmail.com>
2021-08-31 21:09:58 +0800

    Fix block minted issue (https://github.com/DeFiCh/whale/pull/374)
    * Fix block minted issue
    
    * Added test case
    
    * Added test specifically for block minted

--- apps/whale/src/module.indexer/model/block.minted.ts
+++ apps/whale/src/module.indexer/model/block.minted.ts
@@ -11,16 +11,20 @@ export class BlockMintedIndexer extends Indexer {
   }
 
   async index (block: RawBlock): Promise<void> {
-    const mn = await this.masternodeMapper.get(block.masternode)
-    if (mn !== undefined && block.masternode !== undefined) {
-      await this.masternodeMapper.put({ ...mn, mintedBlocks: mn.mintedBlocks + 1 })
+    if (block.masternode !== undefined) {
+      const mn = await this.masternodeMapper.get(block.masternode)
+      if (mn !== undefined) {
+        await this.masternodeMapper.put({ ...mn, mintedBlocks: mn.mintedBlocks + 1 })
+      }
     }
   }
 
   async invalidate (block: RawBlock): Promise<void> {
-    const mn = await this.masternodeMapper.get(block.masternode)
-    if (mn !== undefined && block.masternode !== undefined) {
-      await this.masternodeMapper.put({ ...mn, mintedBlocks: mn.mintedBlocks - 1 })
+    if (block.masternode !== undefined) {
+      const mn = await this.masternodeMapper.get(block.masternode)
+      if (mn !== undefined) {
+        await this.masternodeMapper.put({ ...mn, mintedBlocks: mn.mintedBlocks - 1 })
+      }
     }
   }
 }
