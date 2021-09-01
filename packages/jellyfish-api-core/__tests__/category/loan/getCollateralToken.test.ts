68e3b53a373ecf6c18e304d4e571f3553792c40d
jingyi2811 <jingyi2811@gmail.com>
2021-09-01 13:49:24 +0800

    Apply code changes as per code review

--- packages/jellyfish-api-core/__tests__/category/loan/getCollateralToken.test.ts
+++ packages/jellyfish-api-core/__tests__/category/loan/getCollateralToken.test.ts
@@ -36,7 +36,10 @@ describe('Loan', () => {
     }])
     await testing.generate(1)
 
-    const data = await testing.rpc.loan.getCollateralToken({ token: 'AAPL', height: 160 })
+    const data = await testing.rpc.loan.getCollateralToken({
+      token: 'AAPL',
+      height: 160
+    })
     expect(data).toStrictEqual({
       [collateralTokenId]: {
         token: 'AAPL',
@@ -45,79 +48,101 @@ describe('Loan', () => {
         activateAfterBlock: 160
       }
     })
-
-    // Update at block 160
-    await testing.container.waitForBlockHeight(160)
   })
 
   it('should not getCollateralToken if token does not exist', async () => {
-    // Wait for block 170
-    await testing.container.waitForBlockHeight(170)
-
     const promise = testing.rpc.loan.getCollateralToken({ token: 'TSLA', height: 170 })
     await expect(promise).rejects.toThrow('RpcApiError: \'Token  does not exist!\', code: -8, method: getcollateraltoken')
   })
+})
 
-  it('should getCollateralToken if height is set before current height', async () => {
-    // Wait for block 180
-    await testing.container.waitForBlockHeight(180)
+describe('Loan with height is set before current height', () => {
+  const container = new LoanMasterNodeRegTestContainer()
+  const testing = Testing.create(container)
 
-    await testing.token.create({ symbol: 'TSLA' })
+  beforeAll(async () => {
+    await testing.container.start()
+    await testing.container.waitForWalletCoinbaseMaturity()
+  })
+
+  afterAll(async () => {
+    await testing.container.stop()
+  })
+
+  it('should getCollateralToken', async () => {
+    // Wait for block 150
+    await testing.container.waitForBlockHeight(150)
+
+    await testing.token.create({ symbol: 'AAPL' })
     await testing.generate(1)
 
     const priceFeedId = await testing.container.call('appointoracle', [await testing.generateAddress(), [{
-      token: 'TSLA',
-      currency: 'USD'
+      token: 'AAPL',
+      currency: 'EUR'
     }], 1])
     await testing.generate(1)
 
     const collateralTokenId = await testing.container.call('setcollateraltoken', [{
-      token: 'TSLA',
+      token: 'AAPL',
       factor: new BigNumber(0.5),
       priceFeedId,
-      activateAfterBlock: 185
+      activateAfterBlock: 160
     }])
     await testing.generate(1)
 
-    const data = await testing.rpc.loan.getCollateralToken({ token: 'TSLA', height: 190 })
+    const data = await testing.rpc.loan.getCollateralToken({
+      token: 'AAPL',
+      height: 170
+    })
     expect(data).toStrictEqual({
       [collateralTokenId]: {
-        token: 'TSLA',
+        token: 'AAPL',
         factor: 0.5,
         priceFeedId,
-        activateAfterBlock: 185
+        activateAfterBlock: 160
       }
     })
+  })
+})
 
-    // Update at block 190
-    await testing.container.waitForBlockHeight(190)
+describe('Loan with height is set after current height', () => {
+  const container = new LoanMasterNodeRegTestContainer()
+  const testing = Testing.create(container)
+
+  beforeAll(async () => {
+    await testing.container.start()
+    await testing.container.waitForWalletCoinbaseMaturity()
   })
 
-  it('should not getCollateralToken if height is set after current height', async () => {
-    // Wait for block 200
-    await testing.container.waitForBlockHeight(200)
+  afterAll(async () => {
+    await testing.container.stop()
+  })
 
-    await testing.token.create({ symbol: 'MSFT' })
+  it('should getCollateralToken', async () => {
+    // Wait for block 150
+    await testing.container.waitForBlockHeight(150)
+
+    await testing.token.create({ symbol: 'AAPL' })
     await testing.generate(1)
 
     const priceFeedId = await testing.container.call('appointoracle', [await testing.generateAddress(), [{
-      token: 'MSFT',
-      currency: 'SGD'
+      token: 'AAPL',
+      currency: 'EUR'
     }], 1])
     await testing.generate(1)
 
     await testing.container.call('setcollateraltoken', [{
-      token: 'MSFT',
+      token: 'AAPL',
       factor: new BigNumber(0.5),
       priceFeedId,
-      activateAfterBlock: 210
+      activateAfterBlock: 160
     }])
     await testing.generate(1)
 
-    const data = await testing.rpc.loan.getCollateralToken({ token: 'MSFT', height: 205 })
+    const data = await testing.rpc.loan.getCollateralToken({
+      token: 'AAPL',
+      height: 159
+    })
     expect(data).toStrictEqual({})
-
-    // Update at block 210
-    await testing.container.waitForBlockHeight(210)
   })
 })
