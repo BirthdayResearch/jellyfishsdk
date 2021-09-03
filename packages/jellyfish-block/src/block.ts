b5b1c9f89c7ce5d21424c2b7fc4e4b6759769387
jouzo <jdesclercs@gmail.com>
2021-09-03 16:10:19 +0200

    small fix in comment

--- packages/jellyfish-block/src/block.ts
+++ packages/jellyfish-block/src/block.ts
@@ -4,7 +4,7 @@ import { CTransactionSegWit, TransactionSegWit } from '@defichain/jellyfish-tran
 
 export interface Block {
   blockHeader: BlockHeader // -------------| 124 bytes + signature (n = VarUInt{1-9 bytes} + n bytes)
-  transactions: TransactionSegWit[] // --| n = VarUInt{1-9 bytes}, + n transactions, list of transactions
+  transactions: TransactionSegWit[] // ----| n = VarUInt{1-9 bytes}, + n transactions, list of transactions
 }
 
 /**
@@ -12,10 +12,10 @@ export interface Block {
  * Immutable by design, bi-directional fromBuffer, toBuffer deep composer.
  */
 export class CBlock extends ComposableBuffer<Block> {
-  composers (bh: Block): BufferComposer[] {
+  composers (block: Block): BufferComposer[] {
     return [
-      ComposableBuffer.single<BlockHeader>(() => bh.blockHeader, v => bh.blockHeader = v, v => new CBlockHeader(v)),
-      ComposableBuffer.varUIntArray<TransactionSegWit>(() => bh.transactions, v => bh.transactions = v, v => new CTransactionSegWit(v))
+      ComposableBuffer.single<BlockHeader>(() => block.blockHeader, v => block.blockHeader = v, v => new CBlockHeader(v)),
+      ComposableBuffer.varUIntArray<TransactionSegWit>(() => block.transactions, v => block.transactions = v, v => new CTransactionSegWit(v))
     ]
   }
 }
