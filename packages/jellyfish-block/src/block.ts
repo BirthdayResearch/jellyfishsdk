c8d2286de2d5d57501d676f6b2284ca010fd513a
jouzo <jdesclercs@gmail.com>
2021-09-05 18:18:09 +0200

    transactions as Array<TransactionSegWit | Transaction>

--- packages/jellyfish-block/src/block.ts
+++ packages/jellyfish-block/src/block.ts
@@ -1,10 +1,16 @@
 import { BlockHeader, CBlockHeader } from './blockHeader'
 import { BufferComposer, ComposableBuffer } from '@defichain/jellyfish-buffer'
-import { CTransactionSegWit, TransactionSegWit } from '@defichain/jellyfish-transaction'
+import {
+  TransactionSegWit,
+  Transaction,
+  CTransaction,
+  CTransactionSegWit
+} from '@defichain/jellyfish-transaction'
+import { SmartBuffer } from 'smart-buffer'
 
 export interface Block {
   blockHeader: BlockHeader // -------------| 124 bytes + signature (n = VarUInt{1-9 bytes} + n bytes)
-  transactions: TransactionSegWit[] // ----| n = VarUInt{1-9 bytes}, + n transactions, list of transactions
+  transactions: Array<TransactionSegWit | Transaction> // ----| n = VarUInt{1-9 bytes}, + n transactions, list of transactions
 }
 
 /**
@@ -15,7 +21,28 @@ export class CBlock extends ComposableBuffer<Block> {
   composers (block: Block): BufferComposer[] {
     return [
       ComposableBuffer.single<BlockHeader>(() => block.blockHeader, v => block.blockHeader = v, v => new CBlockHeader(v)),
-      ComposableBuffer.varUIntArray<TransactionSegWit>(() => block.transactions, v => block.transactions = v, v => new CTransactionSegWit(v))
+      ComposableBuffer.varUIntArray<TransactionSegWit | Transaction>(() => block.transactions, v => block.transactions = v, v => {
+        /**
+         * Has to read the marker from buffer in order to determine wether it is a Transaction or TransactionSegWit.
+         */
+        if (v instanceof SmartBuffer) {
+          const readOffset = v.readOffset // Keep current offset in order to reset to this point once marker is read.
+          v.readUInt32LE() // Read 4 bytes from version and discard it.
+          const marker = v.readUInt8()
+          v.readOffset = readOffset
+          if (marker > 0x00) {
+            return new CTransaction(v)
+          } else {
+            return new CTransactionSegWit(v)
+          }
+        } else {
+          if ((v as TransactionSegWit).marker === 0x00) {
+            return new CTransactionSegWit(v as TransactionSegWit)
+          } else {
+            return new CTransaction(v)
+          }
+        }
+      })
     ]
   }
 }
