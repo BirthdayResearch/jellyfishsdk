21888852c39a515b8c46f857dc8e03dbd2531ded
Fuxing Loh <git@fuxing.dev>
2021-09-07 11:38:55 +0800

    increase waitForSync... timeout to 20 seconds

--- packages/testcontainers/src/chains/reg_test_container/container_group.ts
+++ packages/testcontainers/src/chains/reg_test_container/container_group.ts
@@ -74,7 +74,7 @@ export class ContainerGroup {
    * Wait for all container to receive the same txid in mempool
    *
    * @param {string} txid to wait for in mempool
-   * @param {number} [timeout=150000] in millis
+   * @param {number} [timeout=20000] in millis
    */
   async waitForMempoolSync (txid: string, timeout: number = 20000): Promise<void> {
     await waitForCondition(async () => {
@@ -88,9 +88,9 @@ export class ContainerGroup {
 
   /**
    * Wait for all container to sync up
-   * @param {number} [timeout=150000] in millis
+   * @param {number} [timeout=20000] in millis
    */
-  async waitForSync (timeout: number = 15000): Promise<void> {
+  async waitForSync (timeout: number = 20000): Promise<void> {
     await waitForCondition(async () => {
       const hashes = await Promise.all(Object.values(this.containers).map(async container => {
         return await container.getBestBlockHash()
