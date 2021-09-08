030cad046b7976d6fcfbef966cf5bb9f42b1235a
websterlcl <w.canonbrother@gmail.com>
2021-09-08 22:17:39 +0800

    add desc for generateanchorauths

--- packages/jellyfish-testing/src/anchor.ts
+++ packages/jellyfish-testing/src/anchor.ts
@@ -6,6 +6,17 @@ export class TestingGroupAnchor {
   ) {
   }
 
+  /**
+   * After every new block the anchor data creation team members will create
+   * and sign new anchor data.
+   * Anchor data is created every fifthteen blocks,
+   * the block height and hash chosen is then three hours further back into the chain
+   * and then more blocks until the every the fifthteen block frequency is matched again.
+   * https://github.com/DeFiCh/ain/wiki/What-is-an-anchor%3F#anchor-data-creation
+   * @param {number} numOfAuths
+   * @param {number} initOffsetHour
+   * @return {Promise<void>}
+   */
   async generateAnchorAuths (numOfAuths: number, initOffsetHour: number): Promise<void> {
     for (let i = 1; i < 3 + numOfAuths + 1; i += 1) {
       await this.testingGroup.exec(async testing => {
