1a0e064fc3555375cfa952b4543bcd229e145185
websterlcl <w.canonbrother@gmail.com>
2021-09-09 15:33:26 +0800

    applied tgroup on test

--- packages/jellyfish-api-core/__tests__/category/spv/listAnchorsUnRewarded.test.ts
+++ packages/jellyfish-api-core/__tests__/category/spv/listAnchorsUnRewarded.test.ts
@@ -1,60 +1,51 @@
 import { CreateAnchorResult } from '@defichain/jellyfish-api-core/category/spv'
-import { MasterNodeRegTestContainer, ContainerGroup, GenesisKeys } from '@defichain/testcontainers'
-import { ContainerAdapterClient } from '../../container_adapter_client'
+import { TestingGroup } from '@defichain/jellyfish-testing'
+import { GenesisKeys } from '@defichain/testcontainers'
 
 describe('Spv', () => {
-  const group = new ContainerGroup([
-    new MasterNodeRegTestContainer(GenesisKeys[0]),
-    new MasterNodeRegTestContainer(GenesisKeys[1]),
-    new MasterNodeRegTestContainer(GenesisKeys[2])
-  ])
-
-  const clients = [
-    new ContainerAdapterClient(group.get(0)),
-    new ContainerAdapterClient(group.get(1)),
-    new ContainerAdapterClient(group.get(2))
-  ]
+  const tGroup = TestingGroup.create(3)
 
   beforeAll(async () => {
-    await group.start()
-
+    await tGroup.start()
     await setup()
   })
 
   afterAll(async () => {
-    await group.stop()
+    await tGroup.stop()
   })
 
-  async function setMockTime (pastHour: number, futureHour = 0): Promise<void> {
-    const offset = Date.now() - (pastHour * 60 * 60 * 1000) + (futureHour * 60 * 60 * 1000)
-    for (let i = 0; i < clients.length; i += 1) {
-      await clients[i].misc.setMockTime(offset)
-    }
+  async function setMockTime (offsetHour: number): Promise<void> {
+    await tGroup.exec(async testing => {
+      await testing.misc.offsetTimeHourly(offsetHour)
+    })
   }
 
   async function setup (): Promise<void> {
-    const auths = await group.get(0).call('spv_listanchorauths')
-    expect(auths.length).toStrictEqual(0)
+    {
+      const auths = await tGroup.get(0).container.call('spv_listanchorauths')
+      expect(auths.length).toStrictEqual(0)
+    }
 
-    // time travel back 13 hours ago
-    await setMockTime(13)
+    // time travel back 12 hours ago
+    const initOffsetHour = -12
+    await setMockTime(initOffsetHour)
 
     // 15 as anchor frequency
     for (let i = 0; i < 15; i += 1) {
-      const container = group.get(i % clients.length)
+      const { container } = tGroup.get(i % tGroup.length())
       await container.generate(1)
-      await group.waitForSync()
+      await tGroup.waitForSync()
     }
 
     // check the auth and confirm anchor mn teams
-    await group.get(0).waitForAnchorTeams(clients.length)
+    await tGroup.get(0).container.waitForAnchorTeams(tGroup.length())
 
     // assertion for team
-    for (let i = 0; i < clients.length; i += 1) {
-      const container = group.get(i % clients.length)
+    for (let i = 0; i < tGroup.length(); i += 1) {
+      const { container } = tGroup.get(i % tGroup.length())
       const team = await container.call('getanchorteams')
-      expect(team.auth.length).toStrictEqual(clients.length)
-      expect(team.confirm.length).toStrictEqual(clients.length)
+      expect(team.auth.length).toStrictEqual(tGroup.length())
+      expect(team.confirm.length).toStrictEqual(tGroup.length())
       expect(team.auth.includes(GenesisKeys[0].operator.address))
       expect(team.auth.includes(GenesisKeys[1].operator.address))
       expect(team.auth.includes(GenesisKeys[2].operator.address))
@@ -63,30 +54,25 @@ describe('Spv', () => {
       expect(team.confirm.includes(GenesisKeys[2].operator.address))
     }
 
-    // generate anchor auths
-    for (let i = 1; i < 3 + 1; i += 1) {
-      await setMockTime(12, i)
-      await group.get(0).generate(15)
-      await group.waitForSync()
-    }
+    await tGroup.anchor.generateAnchorAuths(2, initOffsetHour)
 
-    await group.get(0).waitForAnchorAuths(clients.length)
+    await tGroup.get(0).container.waitForAnchorAuths(tGroup.length())
 
     // check each container should be quorum ready
-    for (let i = 0; i < clients.length; i += 1) {
-      const container = group.get(i % clients.length)
+    for (let i = 0; i < tGroup.length(); i += 1) {
+      const { container } = tGroup.get(i % tGroup.length())
       const auths = await container.call('spv_listanchorauths')
-      expect(auths.length).toStrictEqual(1)
-      expect(auths[0].signers).toStrictEqual(clients.length)
+      expect(auths.length).toStrictEqual(2)
+      expect(auths[0].signers).toStrictEqual(tGroup.length())
     }
 
-    await group.get(0).waitForBlockHeight(60)
-    await group.waitForSync()
+    await tGroup.get(0).container.waitForBlockHeight(60)
+    await tGroup.waitForSync()
   }
 
   async function createAnchor (): Promise<CreateAnchorResult> {
-    const rewardAddress = await clients[0].spv.getNewAddress()
-    return await clients[0].spv.createAnchor([{
+    const rewardAddress = await tGroup.get(0).rpc.spv.getNewAddress()
+    return await tGroup.get(0).rpc.spv.createAnchor([{
       txid: '11a276bb25585f6973a4dd68373cffff41dbcaddf12bbc1c2b489d1dc84564ee',
       vout: 2,
       amount: 15800,
@@ -99,28 +85,28 @@ describe('Spv', () => {
     const anchor2 = await createAnchor()
 
     // node1 does the same as node0
-    await group.get(1).call('spv_sendrawtx', [anchor1.txHex])
-    await group.get(1).call('spv_sendrawtx', [anchor2.txHex])
-    await group.get(1).generate(1)
-    await group.waitForSync()
+    await tGroup.get(1).container.call('spv_sendrawtx', [anchor1.txHex])
+    await tGroup.get(1).container.call('spv_sendrawtx', [anchor2.txHex])
+    await tGroup.get(1).generate(1)
+    await tGroup.waitForSync()
 
     {
-      const rewardConfs = await group.get(0).call('spv_listanchorrewardconfirms')
+      const rewardConfs = await tGroup.get(0).container.call('spv_listanchorrewardconfirms')
       expect(rewardConfs.length).toStrictEqual(0)
 
-      const unrewardeds = await clients[0].spv.listAnchorsUnrewarded()
+      const unrewardeds = await tGroup.get(0).rpc.spv.listAnchorsUnrewarded()
       expect(unrewardeds.length).toStrictEqual(0)
     }
 
     // 2 signers to confirm anchor reward
-    await group.get(0).call('spv_setlastheight', [6])
-    await group.get(1).call('spv_setlastheight', [6])
+    await tGroup.get(0).container.call('spv_setlastheight', [6])
+    await tGroup.get(1).container.call('spv_setlastheight', [6])
 
-    const anchors = await group.get(0).call('spv_listanchors')
+    const anchors = await tGroup.get(0).container.call('spv_listanchors')
     expect(anchors.length).toBeGreaterThan(0)
     const activeAnchor = anchors.find((anchor: any) => anchor.active === true)
 
-    const rewardConfs = await group.get(0).call('spv_listanchorrewardconfirms')
+    const rewardConfs = await tGroup.get(0).container.call('spv_listanchorrewardconfirms')
     expect(rewardConfs.length).toBeGreaterThan(0)
     const rewardConf = rewardConfs[0]
     expect(typeof rewardConf.btcTxHeight).toStrictEqual('number')
@@ -133,7 +119,7 @@ describe('Spv', () => {
     expect(typeof rewardConf.signers).toStrictEqual('number')
 
     {
-      const unrewardeds = await clients[0].spv.listAnchorsUnrewarded()
+      const unrewardeds = await tGroup.get(0).rpc.spv.listAnchorsUnrewarded()
       expect(unrewardeds.length).toBeGreaterThan(0)
       const unrewarded = unrewardeds[0]
       expect(typeof unrewarded.btcBlockHeight).toStrictEqual('number')
@@ -148,21 +134,21 @@ describe('Spv', () => {
       expect(typeof unrewarded.anchorCreationHeight).toStrictEqual('number')
     }
 
-    const commBalancesBefore = await group.get(0).call('listcommunitybalances')
+    const commBalancesBefore = await tGroup.get(0).container.call('listcommunitybalances')
 
-    await group.get(0).waitForAnchorRewardConfirms('regtest')
-    await group.get(0).generate(1)
+    await tGroup.get(0).container.waitForAnchorRewardConfirms('regtest')
+    await tGroup.get(0).generate(1)
 
-    const rewards = await clients[0].spv.listAnchorRewards()
+    const rewards = await tGroup.get(0).rpc.spv.listAnchorRewards()
     expect(rewards.length).toBeGreaterThan(0)
     expect(typeof rewards[0].AnchorTxHash).toStrictEqual('string')
     expect(typeof rewards[0].RewardTxHash).toStrictEqual('string')
 
-    const commBalancesAfter = await group.get(0).call('listcommunitybalances')
+    const commBalancesAfter = await tGroup.get(0).container.call('listcommunitybalances')
     expect(commBalancesBefore.AnchorReward > commBalancesAfter.AnchorReward).toStrictEqual(true)
 
     {
-      const unrewardeds = await clients[0].spv.listAnchorsUnrewarded()
+      const unrewardeds = await tGroup.get(0).rpc.spv.listAnchorsUnrewarded()
       expect(unrewardeds.length).toStrictEqual(0)
     }
   })
