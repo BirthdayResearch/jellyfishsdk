9d8250a8f6d946658f9c88d6adc683c22211adfd
Fuxing Loh <git@fuxing.dev>
2021-09-21 03:01:23 +0800

    fixed docs

--- packages/jellyfish-address/src/script.ts
+++ packages/jellyfish-address/src/script.ts
@@ -4,11 +4,12 @@ import { NetworkName } from '@defichain/jellyfish-network'
 import { fromScriptP2WPKH } from './p2wpkh'
 
 /**
- * Convert a script to address, this operation requires the network to be known as script is network agnostic.
+ * Convert a script to address, this operation requires the network to be known.
+ * A script is network agnostic while address is prefixed with network unique human readable part.
  *
  * @param {Script} script to convert into address
  * @param {NetworkName} network to prefix human readable part of the address
- * @return {string | undefined} address if is a recognizable
+ * @return {string | undefined} address if is a recognizable, undefined if fail to parse
  */
 export function fromScript (script: Script, network: NetworkName): string | undefined {
   const p2wpkh = fromScriptP2WPKH(script, network)
@@ -20,11 +21,12 @@ export function fromScript (script: Script, network: NetworkName): string | unde
 }
 
 /**
- * Convert a script to address, this operation requires the network to be known as script is network agnostic.
+ * Convert a script to address, this operation requires the network to be known.
+ * A script is network agnostic while address is prefixed with network unique human readable part.
  *
  * @param {string} hex of the script to convert into address
  * @param {NetworkName} network to prefix human readable part of the address
- * @return {string | undefined} address if is a recognizable
+ * @return {string | undefined} address if is a recognizable, undefined if fail to parse
  */
 export function fromScriptHex (hex: string, network: NetworkName): string | undefined {
   const buffer = Buffer.from(hex, 'hex')
@@ -32,11 +34,12 @@ export function fromScriptHex (hex: string, network: NetworkName): string | unde
 }
 
 /**
- * Convert a script to address, this operation requires the network to be known as script is network agnostic.
+ * Convert a script to address, this operation requires the network to be known.
+ * A script is network agnostic while address is prefixed with network unique human readable part.
  *
  * @param {Buffer} buffer of the script to convert into address
  * @param {NetworkName} network to prefix human readable part of the address
- * @return {string | undefined} address if is a recognizable
+ * @return {string | undefined} address if is a recognizable, undefined if fail to parse
  */
 export function fromScriptBuffer (buffer: Buffer, network: NetworkName): string | undefined {
   const script = new CScript(SmartBuffer.fromBuffer(buffer))
