bff708b8baf8ac5a53c980b094a02bea83892eb5
Fuxing Loh <git@fuxing.dev>
2021-09-21 16:14:05 +0800

    p2wsh wip

--- packages/jellyfish-address/src/p2wsh.ts
+++ packages/jellyfish-address/src/p2wsh.ts
@@ -1,6 +1,6 @@
 import { bech32 } from 'bech32'
 import { getNetwork, Network, NetworkName } from '@defichain/jellyfish-network'
-import { Script, OP_CODES, OP_PUSHDATA } from '@defichain/jellyfish-transaction'
+import { OP_CODES, OP_PUSHDATA, Script } from '@defichain/jellyfish-transaction'
 
 import { Bech32Address } from './bech32_address'
 import { Validator } from './address'
@@ -64,4 +64,23 @@ export class P2WSH extends Bech32Address {
     const utf8 = bech32.encode(network.bech32.hrp, includeVersion)
     return new P2WSH(network, utf8, hex, true)
   }
+}
+
+function isScriptP2WSH (script: Script): boolean {
+  return script.stack.length === 2 &&
+    script.stack[0].type === OP_CODES.OP_0.type &&
+    script.stack[1].type === 'OP_PUSHDATA' &&
+    (script.stack[1] as OP_PUSHDATA).length() === 32
+}
+
+export function fromScriptP2WPKH (script: Script, network: NetworkName): string | undefined {
+  if (!isScriptP2WSH(script)) {
+    return undefined
+  }
+
+  const hash = script.stack[1] as OP_PUSHDATA
+  const buffer = Buffer.from(hash.hex, 'hex')
+  const hrp = getNetwork(network).bech32.hrp
+  return undefined
+  // return Bech32.fromHash160(buffer, hrp, 0x00)
 }
