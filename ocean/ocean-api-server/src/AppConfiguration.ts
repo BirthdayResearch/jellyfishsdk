db1cbf7ca942a6afdfcda50278c1aef233eeab2f
Fuxing Loh <git@fuxing.dev>
2021-09-29 02:12:38 +0800

    initial server setup

---
+++ ocean/ocean-api-server/src/AppConfiguration.ts
@@ -0,0 +1,17 @@
+/**
+ * AppConfiguration declares a dictionary for a deeply configurable DeFi ocean setup.
+ * `process.env` resolves env variable at service initialization and allows setting of default.
+ * This configuration can be injected/replaced at runtime by overriding provider 'ConfigService' or
+ * replacing the config module.
+ */
+export const AppConfiguration = (): any => ({
+  isProd: process.env.NODE_ENV === 'production',
+  /**
+   * Allows you to override ocean endpoint version.
+   */
+  version: process.env.OCEAN_VERSION,
+  network: process.env.OCEAN_NETWORK,
+  defid: {
+    url: process.env.OCEAN_DEFID_URL
+  }
+})
