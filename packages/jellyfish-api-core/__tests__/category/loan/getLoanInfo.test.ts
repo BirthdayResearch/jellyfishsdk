8864ac57b25fb42ddfcf8d391c0aed25dd1c2595
ivan <ivan@zynesis.com>
2021-10-01 14:07:04 +0800

    fix no longer using dynamic oracle id

--- packages/jellyfish-api-core/__tests__/category/loan/getLoanInfo.test.ts
+++ packages/jellyfish-api-core/__tests__/category/loan/getLoanInfo.test.ts
@@ -6,7 +6,6 @@ import { GenesisKeys } from '@defichain/testcontainers'
 describe('Loan', () => {
   let container: LoanMasterNodeRegTestContainer
   let testing: Testing
-  let oracleId: string
 
   beforeEach(async () => {
     container = new LoanMasterNodeRegTestContainer()
@@ -17,10 +16,16 @@ describe('Loan', () => {
     await testing.token.create({ symbol: 'TSLA' })
     await testing.generate(1)
 
-    oracleId = await testing.container.call('appointoracle', [await testing.generateAddress(), [{
-      token: 'TSLA',
-      currency: 'USD'
-    }], 1])
+    await testing.container.call('appointoracle', [
+      await testing.generateAddress(),
+      [
+        {
+          token: 'TSLA',
+          currency: 'USD'
+        }
+      ],
+      1
+    ])
     await testing.generate(1)
   })
 
@@ -32,43 +37,44 @@ describe('Loan', () => {
     // Before setCollateralToken
     {
       const data = await testing.rpc.loan.getLoanInfo()
-      expect(data).toStrictEqual(
-        {
-          'Collateral tokens': {},
-          'Loan tokens': {},
-          'Loan schemes': [],
-          collateralValueUSD: new BigNumber(0),
-          loanValueUSD: new BigNumber(0)
-        }
-      )
+      expect(data).toStrictEqual({
+        'Collateral tokens': {},
+        'Loan tokens': {},
+        'Loan schemes': [],
+        collateralValueUSD: new BigNumber(0),
+        loanValueUSD: new BigNumber(0)
+      })
     }
 
-    const collateralTokenId = await testing.container.call('setcollateraltoken', [{
-      token: 'TSLA',
-      factor: new BigNumber(0.5),
-      priceFeedId: oracleId
-    }])
+    const collateralTokenId = await testing.container.call(
+      'setcollateraltoken',
+      [
+        {
+          token: 'TSLA',
+          factor: new BigNumber(0.5),
+          priceFeedId: 'TSLA/USD'
+        }
+      ]
+    )
     await testing.generate(1)
 
     // After setCollateralToken
     {
       const data = await testing.rpc.loan.getLoanInfo()
-      expect(data).toStrictEqual(
-        {
-          'Collateral tokens': {
-            [collateralTokenId]: {
-              activateAfterBlock: new BigNumber(104),
-              factor: new BigNumber(0.5),
-              priceFeedId: oracleId,
-              token: 'TSLA'
-            }
-          },
-          'Loan tokens': {},
-          'Loan schemes': [],
-          collateralValueUSD: new BigNumber(0),
-          loanValueUSD: new BigNumber(0)
-        }
-      )
+      expect(data).toStrictEqual({
+        'Collateral tokens': {
+          [collateralTokenId]: {
+            activateAfterBlock: new BigNumber(104),
+            factor: new BigNumber(0.5),
+            priceFeedId: 'TSLA/USD',
+            token: 'TSLA'
+          }
+        },
+        'Loan tokens': {},
+        'Loan schemes': [],
+        collateralValueUSD: new BigNumber(0),
+        loanValueUSD: new BigNumber(0)
+      })
     }
   })
 
@@ -76,45 +82,52 @@ describe('Loan', () => {
     // Before createLoanScheme
     {
       const data = await testing.rpc.loan.getLoanInfo()
-      expect(data).toStrictEqual(
-        {
-          'Collateral tokens': {},
-          'Loan tokens': {},
-          'Loan schemes': [],
-          collateralValueUSD: new BigNumber(0),
-          loanValueUSD: new BigNumber(0)
-        }
-      )
+      expect(data).toStrictEqual({
+        'Collateral tokens': {},
+        'Loan tokens': {},
+        'Loan schemes': [],
+        collateralValueUSD: new BigNumber(0),
+        loanValueUSD: new BigNumber(0)
+      })
     }
 
-    await testing.container.call('createloanscheme', [100, new BigNumber(1.5), 'default'])
+    await testing.container.call('createloanscheme', [
+      100,
+      new BigNumber(1.5),
+      'default'
+    ])
     await testing.container.generate(1)
 
-    await testing.container.call('createloanscheme', [200, new BigNumber(2.5), 'scheme'])
+    await testing.container.call('createloanscheme', [
+      200,
+      new BigNumber(2.5),
+      'scheme'
+    ])
     await testing.container.generate(1)
 
     // After createLoanScheme
     {
       const data = await testing.rpc.loan.getLoanInfo()
-      expect(data).toStrictEqual(
-        {
-          'Collateral tokens': {},
-          'Loan tokens': {},
-          'Loan schemes': [{
+      expect(data).toStrictEqual({
+        'Collateral tokens': {},
+        'Loan tokens': {},
+        'Loan schemes': [
+          {
             id: 'default',
             mincolratio: new BigNumber(100),
             interestrate: new BigNumber(1.5),
             default: true
-          }, {
+          },
+          {
             id: 'scheme',
             mincolratio: new BigNumber(200),
             interestrate: new BigNumber(2.5),
             default: false
-          }],
-          collateralValueUSD: new BigNumber(0),
-          loanValueUSD: new BigNumber(0)
-        }
-      )
+          }
+        ],
+        collateralValueUSD: new BigNumber(0),
+        loanValueUSD: new BigNumber(0)
+      })
     }
   })
 
@@ -122,70 +135,75 @@ describe('Loan', () => {
     // Before setLoanToken
     {
       const data = await testing.rpc.loan.getLoanInfo()
-      expect(data).toStrictEqual(
-        {
-          'Collateral tokens': {},
-          'Loan tokens': {},
-          'Loan schemes': [],
-          collateralValueUSD: new BigNumber(0),
-          loanValueUSD: new BigNumber(0)
-        }
-      )
+      expect(data).toStrictEqual({
+        'Collateral tokens': {},
+        'Loan tokens': {},
+        'Loan schemes': [],
+        collateralValueUSD: new BigNumber(0),
+        loanValueUSD: new BigNumber(0)
+      })
     }
 
     // setLoanToken with new token
-    const anotherTokenOracleId = await testing.container.call('appointoracle', [await testing.generateAddress(), [{
-      token: 'APPL',
-      currency: 'USD'
-    }], 1])
+    await testing.container.call('appointoracle', [
+      await testing.generateAddress(),
+      [
+        {
+          token: 'APPL',
+          currency: 'USD'
+        }
+      ],
+      1
+    ])
     await testing.generate(1)
-    const loanTokenId = await testing.container.call('setloantoken', [{
-      symbol: 'APPL',
-      name: 'APPLE',
-      priceFeedId: anotherTokenOracleId,
-      interest: new BigNumber(0.01)
-    }])
+    const loanTokenId = await testing.container.call('setloantoken', [
+      {
+        symbol: 'APPL',
+        name: 'APPLE',
+        priceFeedId: 'APPL/USD',
+        interest: new BigNumber(0.01)
+      }
+    ])
     await testing.generate(1)
     const height = new BigNumber(await testing.container.getBlockCount())
 
     // After setLoanToken
     {
       const data = await testing.rpc.loan.getLoanInfo()
-      expect(data).toStrictEqual(
-        {
-          'Collateral tokens': {},
-          'Loan tokens': {
-            [loanTokenId]: {
-              token: {
-                2: {
-                  collateralAddress: GenesisKeys[0].owner.address,
-                  creationHeight: height,
-                  creationTx: loanTokenId,
-                  decimal: new BigNumber(8),
-                  destructionHeight: new BigNumber(-1),
-                  destructionTx: '0000000000000000000000000000000000000000000000000000000000000000',
-                  finalized: false,
-                  isDAT: true,
-                  isLPS: false,
-                  isLoanToken: true,
-                  limit: new BigNumber(0),
-                  mintable: true,
-                  minted: new BigNumber(0),
-                  name: 'APPLE',
-                  symbol: 'APPL',
-                  symbolKey: 'APPL',
-                  tradeable: true
-                }
-              },
-              priceFeedId: anotherTokenOracleId,
-              interest: new BigNumber(0.01)
-            }
-          },
-          'Loan schemes': [],
-          collateralValueUSD: new BigNumber(0),
-          loanValueUSD: new BigNumber(0)
-        }
-      )
+      expect(data).toStrictEqual({
+        'Collateral tokens': {},
+        'Loan tokens': {
+          [loanTokenId]: {
+            token: {
+              2: {
+                collateralAddress: GenesisKeys[0].owner.address,
+                creationHeight: height,
+                creationTx: loanTokenId,
+                decimal: new BigNumber(8),
+                destructionHeight: new BigNumber(-1),
+                destructionTx:
+                  '0000000000000000000000000000000000000000000000000000000000000000',
+                finalized: false,
+                isDAT: true,
+                isLPS: false,
+                isLoanToken: true,
+                limit: new BigNumber(0),
+                mintable: true,
+                minted: new BigNumber(0),
+                name: 'APPLE',
+                symbol: 'APPL',
+                symbolKey: 'APPL',
+                tradeable: true
+              }
+            },
+            priceFeedId: 'APPL/USD',
+            interest: new BigNumber(0.01)
+          }
+        },
+        'Loan schemes': [],
+        collateralValueUSD: new BigNumber(0),
+        loanValueUSD: new BigNumber(0)
+      })
     }
   })
 })
