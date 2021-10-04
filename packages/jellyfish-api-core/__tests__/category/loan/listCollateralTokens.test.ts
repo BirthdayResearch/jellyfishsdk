884e7b804e443d1fec885b505ac70e49e7b6deb9
jingyi2811 <jingyi2811@gmail.com>
2021-10-04 15:25:33 +0800

    Apply code changes as per code review

--- packages/jellyfish-api-core/__tests__/category/loan/listCollateralTokens.test.ts
+++ packages/jellyfish-api-core/__tests__/category/loan/listCollateralTokens.test.ts
@@ -2,7 +2,7 @@ import { LoanMasterNodeRegTestContainer } from './loan_container'
 import BigNumber from 'bignumber.js'
 import { Testing } from '@defichain/jellyfish-testing'
 
-describe('Loan listCollateralTokens with empty param or all param', () => {
+describe('Loan listCollateralTokens with empty param, all param or (height + all params)', () => {
   const container = new LoanMasterNodeRegTestContainer()
   const testing = Testing.create(container)
 
@@ -67,8 +67,10 @@ describe('Loan listCollateralTokens with empty param or all param', () => {
 
     {
       // List all collateral tokens
-      const data = await testing.rpc.loan.listCollateralTokens({ all: true })
-      expect(data).toStrictEqual({
+      const data1 = await testing.rpc.loan.listCollateralTokens({ all: true })
+      const data2 = await testing.rpc.loan.listCollateralTokens({ height: 50, all: true }) // if height and all combination used, only all is considered.
+      expect(data1).toStrictEqual(data2)
+      expect(data1).toStrictEqual({
         [collateralTokenId1]: {
           activateAfterBlock: new BigNumber(blockCount),
           factor: new BigNumber(0.5),
@@ -86,7 +88,7 @@ describe('Loan listCollateralTokens with empty param or all param', () => {
   })
 })
 
-describe('Loan listCollateralTokens with height', () => {
+describe('Loan listCollateralTokens with height only', () => {
   const container = new LoanMasterNodeRegTestContainer()
   const testing = Testing.create(container)
 
@@ -103,13 +105,25 @@ describe('Loan listCollateralTokens with height', () => {
     await testing.token.create({ symbol: 'AAPL' })
     await testing.generate(1)
 
+    await testing.token.create({ symbol: 'TSLA' })
+    await testing.generate(1)
+
     await testing.container.call('appointoracle', [await testing.generateAddress(), [{
       token: 'AAPL',
       currency: 'USD'
     }], 1])
     await testing.generate(1)
 
-    const collateralTokenId = await testing.container.call('setcollateraltoken', [{
+    await testing.container.call('appointoracle', [await testing.generateAddress(), [{
+      token: 'TSLA',
+      currency: 'USD'
+    }], 1])
+    await testing.generate(1)
+
+    // Wait for block 110
+    await testing.container.waitForBlockHeight(110)
+
+    const collateralTokenId1 = await testing.container.call('setcollateraltoken', [{
       token: 'AAPL',
       factor: new BigNumber(0.5),
       priceFeedId: 'AAPL/USD'
@@ -117,15 +131,41 @@ describe('Loan listCollateralTokens with height', () => {
     await testing.generate(1)
     const blockCount = await testing.container.getBlockCount()
 
+    const collateralTokenId2 = await testing.container.call('setcollateraltoken', [{
+      token: 'TSLA',
+      factor: new BigNumber(1.0),
+      priceFeedId: 'TSLA/USD',
+      activateAfterBlock: 130
+    }])
+    await testing.generate(1)
+
     // Only remain AAPL which has already been activated
     {
       const data = await testing.rpc.loan.listCollateralTokens({ height: blockCount })
       expect(data).toStrictEqual({
-        [collateralTokenId]: {
+        [collateralTokenId1]: {
           token: 'AAPL',
           factor: new BigNumber(0.5),
           priceFeedId: 'AAPL/USD',
           activateAfterBlock: new BigNumber(blockCount)
+        }
+      })
+    }
+
+    {
+      const data = await testing.rpc.loan.listCollateralTokens({ height: 130 })
+      expect(data).toStrictEqual({
+        [collateralTokenId1]: {
+          token: 'AAPL',
+          factor: new BigNumber(0.5),
+          priceFeedId: 'AAPL/USD',
+          activateAfterBlock: new BigNumber(blockCount)
+        },
+        [collateralTokenId2]: {
+          token: 'TSLA',
+          factor: new BigNumber(1.0),
+          priceFeedId: 'TSLA/USD',
+          activateAfterBlock: new BigNumber(130)
         }
       })
     }
--- packages/jellyfish-api-core/__tests__/category/loan/loan_container.ts
+++ packages/jellyfish-api-core/__tests__/category/loan/loan_container.ts
@@ -2,7 +2,7 @@ import { MasterNodeRegTestContainer, StartOptions, GenesisKeys, MasterNodeKey } 
 
 export class LoanMasterNodeRegTestContainer extends MasterNodeRegTestContainer {
   constructor (masternodeKey: MasterNodeKey = GenesisKeys[0]) {
-    super(masternodeKey, 'defi/defichain:HEAD-39c3669')
+    super(masternodeKey, 'defi/defichain:HEAD-59c8ad7')
   }
 
   protected getCmd (opts: StartOptions): string[] {
