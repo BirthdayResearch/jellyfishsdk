ffea85fb02495c6fe808e2ff402c42f3b6aa6df0
Suraj Auwal <siradjiawoual@gmail.com>
2021-10-06 09:29:04 +0100

    /stats emission (#355)
    * Rough emission calculation
    
    * Changed dfi values to big number
    
    * Added reduction test
    
    * Improved code
    
    * Improved test based on code reviews
    
    * Additional reduction test
    
    * Improvement based on code reviews
    
    * Added changes from code reviews
    
    * Refactored test file
    
    * Compressed and improved code
    
    * Refactored test
    
    * simplify stats emission logic and testing (#440)
    
    * simplified and refactor emission code
    no point testing duplicated code logic
    
    * fixed and added tests
    
    * fixed and added tests
    
    * fixed missing }
    
    * fixed linting issues
    
    Co-authored-by: Fuxing Loh <4266087+fuxingloh@users.noreply.github.com>
    Co-authored-by: Fuxing Loh <git@fuxing.dev>

--- packages/whale-api-client/__tests__/api/stats.test.ts
+++ packages/whale-api-client/__tests__/api/stats.test.ts
@@ -4,66 +4,67 @@ import { StubService } from '../stub.service'
 import { WhaleApiClient } from '../../src'
 import { addPoolLiquidity, createPoolPair, createToken, getNewAddress, mintTokens } from '@defichain/testing'
 
-let container: MasterNodeRegTestContainer
-let service: StubService
-let client: WhaleApiClient
-
-beforeAll(async () => {
-  container = new MasterNodeRegTestContainer()
-  service = new StubService(container)
-  client = new StubWhaleApiClient(service)
-
-  await container.start()
-  await container.waitForWalletCoinbaseMaturity()
-  await service.start()
-
-  await createToken(container, 'A')
-  await mintTokens(container, 'A')
-  await createToken(container, 'B')
-  await mintTokens(container, 'B')
-
-  await createPoolPair(container, 'A', 'DFI')
-  await addPoolLiquidity(container, {
-    tokenA: 'A',
-    amountA: 100,
-    tokenB: 'DFI',
-    amountB: 200,
-    shareAddress: await getNewAddress(container)
-  })
-  await createPoolPair(container, 'B', 'DFI')
-  await addPoolLiquidity(container, {
-    tokenA: 'B',
-    amountA: 50,
-    tokenB: 'DFI',
-    amountB: 200,
-    shareAddress: await getNewAddress(container)
-  })
-  await createToken(container, 'USDT')
-  await createPoolPair(container, 'USDT', 'DFI')
-  await mintTokens(container, 'USDT')
-  await addPoolLiquidity(container, {
-    tokenA: 'USDT',
-    amountA: 1000,
-    tokenB: 'DFI',
-    amountB: 431.51288,
-    shareAddress: await getNewAddress(container)
-  })
-  const height = await container.getBlockCount()
-  await container.generate(1)
-  await service.waitForIndexedHeight(height)
-})
-
-afterAll(async () => {
-  try {
-    await service.stop()
-  } finally {
-    await container.stop()
-  }
-})
-
 describe('stats', () => {
-  it('should get', async () => {
+  let container: MasterNodeRegTestContainer
+  let service: StubService
+  let client: WhaleApiClient
+
+  beforeAll(async () => {
+    container = new MasterNodeRegTestContainer()
+    service = new StubService(container)
+    client = new StubWhaleApiClient(service)
+
+    await container.start()
+    await container.waitForWalletCoinbaseMaturity()
+    await service.start()
+
+    await createToken(container, 'A')
+    await mintTokens(container, 'A')
+    await createToken(container, 'B')
+    await mintTokens(container, 'B')
+
+    await createPoolPair(container, 'A', 'DFI')
+    await addPoolLiquidity(container, {
+      tokenA: 'A',
+      amountA: 100,
+      tokenB: 'DFI',
+      amountB: 200,
+      shareAddress: await getNewAddress(container)
+    })
+    await createPoolPair(container, 'B', 'DFI')
+    await addPoolLiquidity(container, {
+      tokenA: 'B',
+      amountA: 50,
+      tokenB: 'DFI',
+      amountB: 200,
+      shareAddress: await getNewAddress(container)
+    })
+    await createToken(container, 'USDT')
+    await createPoolPair(container, 'USDT', 'DFI')
+    await mintTokens(container, 'USDT')
+    await addPoolLiquidity(container, {
+      tokenA: 'USDT',
+      amountA: 1000,
+      tokenB: 'DFI',
+      amountB: 431.51288,
+      shareAddress: await getNewAddress(container)
+    })
+    const height = await container.getBlockCount()
+    await container.generate(1)
+    await service.waitForIndexedHeight(height)
+  })
+
+  afterAll(async () => {
+    try {
+      await service.stop()
+    } finally {
+      await container.stop()
+    }
+  })
+
+  it('should get stat data', async () => {
     const data = await client.stats.get()
+
     expect(data).toStrictEqual({
       count: { blocks: 117, prices: 0, tokens: 7, masternodes: 8 },
       burned: { address: 0, emission: 7014.88, fee: 3, total: 7017.88 },
@@ -77,6 +78,14 @@ describe('stats', () => {
             weeks: 0
           }
         ]
+      },
+      emission: {
+        total: 405.04,
+        anchor: 0.081008,
+        dex: 103.08268,
+        community: 19.887464,
+        masternode: 134.999832,
+        burned: 146.989016
       },
       blockchain: {
         difficulty: expect.any(Number)
--- packages/whale-api-client/src/api/stats.ts
+++ packages/whale-api-client/src/api/stats.ts
@@ -41,6 +41,14 @@ export interface StatsData {
   masternodes: {
     locked: Array<{weeks: number, tvl: number, count: number}>
   }
+  emission: {
+    total: number
+    masternode: number
+    dex: number
+    community: number
+    anchor: number
+    burned: number
+  }
   blockchain: {
     difficulty: number
   }
