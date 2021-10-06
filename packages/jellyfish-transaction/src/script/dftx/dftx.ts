241c4080f3eb0a2c0fbf69bfd6626ccf06f6b6d4
surangap <surangatco@gmail.com>
2021-10-06 18:44:36 +0800

    Added TakeLoan dftx.

--- packages/jellyfish-transaction/src/script/dftx/dftx.ts
+++ packages/jellyfish-transaction/src/script/dftx/dftx.ts
@@ -87,7 +87,9 @@ import {
   CCreateVault,
   CreateVault,
   DepositToVault,
-  CDepositToVault
+  CDepositToVault,
+  CTakeLoan,
+  TakeLoan
 } from './dftx_loans'
 
 /**
@@ -242,6 +244,8 @@ export class CDfTx extends ComposableBuffer<DfTx<any>> {
         return compose<CreateVault>(CCreateVault.OP_NAME, d => new CCreateVault(d))
       case CDepositToVault.OP_CODE:
         return compose<DepositToVault>(CDepositToVault.OP_NAME, d => new CDepositToVault(d))
+      case CTakeLoan.OP_CODE:
+        return compose<TakeLoan>(CTakeLoan.OP_NAME, d => new CTakeLoan(d))
       default:
         return compose<DeFiOpUnmapped>(CDeFiOpUnmapped.OP_NAME, d => new CDeFiOpUnmapped(d))
     }
