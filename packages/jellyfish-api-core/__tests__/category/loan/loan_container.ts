0f081217c34ea2a2d258be85770b2860bc32486d
jingyi2811 <jingyi2811@gmail.com>
2021-10-08 19:40:05 +0800

    Use back previous docker image

--- packages/jellyfish-api-core/__tests__/category/loan/loan_container.ts
+++ packages/jellyfish-api-core/__tests__/category/loan/loan_container.ts
@@ -2,7 +2,7 @@ import { MasterNodeRegTestContainer, StartOptions, GenesisKeys, MasterNodeKey } 
 
 export class LoanMasterNodeRegTestContainer extends MasterNodeRegTestContainer {
   constructor (masternodeKey: MasterNodeKey = GenesisKeys[0]) {
-    super(undefined, 'defi/defichain:HEAD-333f43e')
+    super(masternodeKey, 'defi/defichain:epic-loans-b1981e5')
   }
 
   protected getCmd (opts: StartOptions): string[] {
