c892c2cc4039fea6168445706e58261c497907d0
jingyi2811 <jingyi2811@gmail.com>
2021-10-08 17:28:31 +0800

    Apply code changes as per code review

--- packages/jellyfish-api-core/__tests__/category/loan/createVault.test.ts
+++ packages/jellyfish-api-core/__tests__/category/loan/createVault.test.ts
@@ -56,6 +56,29 @@ describe('Loan createVault', () => {
     })
   })
 
+  it('should createVault with default scheme if CreateVault.loanSchemeId is not given', async () => {
+    const ownerAddress = await testing.generateAddress()
+    const vaultId = await testing.rpc.loan.createVault({
+      ownerAddress: ownerAddress
+    })
+
+    expect(typeof vaultId).toStrictEqual('string')
+    expect(vaultId.length).toStrictEqual(64)
+    await testing.generate(1)
+
+    const data = await testing.rpc.call('getvault', [vaultId], 'bignumber')
+    expect(data).toStrictEqual({
+      loanSchemeId: 'default', // Get default loan scheme
+      ownerAddress: ownerAddress,
+      isUnderLiquidation: false,
+      collateralAmounts: [],
+      loanAmount: [],
+      collateralValue: expect.any(BigNumber),
+      loanValue: expect.any(BigNumber),
+      currentRatio: expect.any(BigNumber)
+    })
+  })
+
   it('should not createVault if ownerAddress is invalid', async () => {
     const promise = testing.rpc.loan.createVault({
       ownerAddress: '1234',
@@ -110,6 +133,28 @@ describe('Loan createVault', () => {
   //
   //   await expect(promise).rejects.toThrow('RpcApiError: \'Test VaultTx execution failed:\ntx must have at least one input from token owner\', code: -32600, method: createvault')
   // })
+
+  describe('Loan createVault when no default scheme and CreateVault.loanSchemeId is not given', () => {
+    const container = new LoanMasterNodeRegTestContainer()
+    const testing = Testing.create(container)
+
+    beforeAll(async () => {
+      await testing.container.start()
+      await testing.container.waitForWalletCoinbaseMaturity()
+    })
+
+    afterAll(async () => {
+      await testing.container.stop()
+    })
+
+    it('should not createVault when no default scheme and CreateVault.loanSchemeId is not given', async () => {
+      const promise = testing.rpc.loan.createVault({
+        ownerAddress: await testing.generateAddress()
+      })
+
+      await expect(promise).rejects.toThrow('RpcApiError: \'Test VaultTx execution failed:\nThere is not default loan scheme\', code: -32600, method: createvault')
+    })
+  })
 })
 
 describe('Loan createVault with scheme set to be destroyed', () => {
