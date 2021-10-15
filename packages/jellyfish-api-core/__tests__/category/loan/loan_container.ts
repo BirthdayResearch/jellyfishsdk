d03be167e96650eeaf400c7b5e36dfe3c13795b1
Ivan <ivan@zynesis.com>
2021-10-15 12:49:11 +0800

    enhance test case (withdraw balance to third party address still waiting for defid fix)

--- packages/jellyfish-api-core/__tests__/category/loan/loan_container.ts
+++ packages/jellyfish-api-core/__tests__/category/loan/loan_container.ts
@@ -2,7 +2,8 @@ import { MasterNodeRegTestContainer, StartOptions, GenesisKeys, MasterNodeKey } 
 
 export class LoanMasterNodeRegTestContainer extends MasterNodeRegTestContainer {
   constructor (masternodeKey: MasterNodeKey = GenesisKeys[0]) {
-    super(masternodeKey, 'defi/defichain:HEAD-90c0220')
+    // super(masternodeKey, 'defi/defichain:HEAD-90c0220') // original
+    super(masternodeKey, 'defi/defichain:HEAD-c4235c3') // wait for bug fix branch to be merged
   }
 
   protected getCmd (opts: StartOptions): string[] {
