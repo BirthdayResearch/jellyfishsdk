23a070517b71df3006c9be283e9d8a71c5ddd6ca
jingyi2811 <jingyi2811@gmail.com>
2021-10-18 14:48:24 +0800

    Update to the latest docker image

--- packages/jellyfish-api-core/__tests__/category/loan/loan_container.ts
+++ packages/jellyfish-api-core/__tests__/category/loan/loan_container.ts
@@ -2,7 +2,7 @@ import { MasterNodeRegTestContainer, StartOptions, GenesisKeys, MasterNodeKey } 
 
 export class LoanMasterNodeRegTestContainer extends MasterNodeRegTestContainer {
   constructor (masternodeKey: MasterNodeKey = GenesisKeys[0]) {
-    super(masternodeKey, 'defi/defichain:HEAD-90c0220')
+    super(masternodeKey, 'defi/defichain:HEAD-6c0af27')
   }
 
   protected getCmd (opts: StartOptions): string[] {
