7753b6210916318b7f1926f12a72c0644bc8eaa7
jingyi2811 <jingyi2811@gmail.com>
2021-10-26 19:47:58 +0800

    Add loan scheme get and list endpoints (#429)
    * Add scaffolding code for loan
    
    * Add loan schemes endpoints
    
    * Improve test
    
    * Improve test
    
    * Improve test
    
    * Minor
    
    * Apply changes as per code review
    
    * Update to the latest docker image
    
    * removed loan container
    
    * rename to just Loan Controller
    
    * updated test
    
    * rename to just Loan
    
    * top level rename of loan
    
    * various fixes and conformation to implementation style
    
    * rename rename typo
    
    Co-authored-by: Fuxing Loh <git@fuxing.dev>

--- packages/whale-api-client/src/api/loan.ts
+++ packages/whale-api-client/src/api/loan.ts
@@ -1,6 +1,34 @@
 import { WhaleApiClient } from '../whale.api.client'
+import { ApiPagedResponse } from '../whale.api.response'
 
 export class Loan {
   constructor (private readonly client: WhaleApiClient) {
   }
+
+  /**
+   * Paginate query loan schemes.
+   *
+   * @param {number} size of scheme to query
+   * @param {string} next set of schemes
+   * @return {Promise<ApiPagedResponse<LoanScheme>>}
+   */
+  async listScheme (size: number = 30, next?: string): Promise<ApiPagedResponse<LoanScheme>> {
+    return await this.client.requestList('GET', 'loans/schemes', size, next)
+  }
+
+  /**
+   * Get information about a scheme with given scheme id.
+   *
+   * @param {string} id scheme id to get
+   * @return {Promise<LoanScheme>}
+   */
+  async getScheme (id: string): Promise<LoanScheme> {
+    return await this.client.requestData('GET', `loans/schemes/${id}`)
+  }
+}
+
+export interface LoanScheme {
+  id: string
+  minColRatio: string
+  interestRate: string
 }
