07b7c41b7437bf4361f5f46fa5cf15c6a172b182
Fuxing Loh <git@fuxing.dev>
2021-11-08 21:52:07 +0800

    bump to use revert getgov behavior

--- packages/testcontainers/src/containers/DeFiDContainer.ts
+++ packages/testcontainers/src/containers/DeFiDContainer.ts
@@ -29,7 +29,7 @@ export abstract class DeFiDContainer extends DockerContainer {
     if (process?.env?.DEFICHAIN_DOCKER_IMAGE !== undefined) {
       return process.env.DEFICHAIN_DOCKER_IMAGE
     }
-    return 'defi/defichain:2.0.0'
+    return 'defi/defichain:HEAD-65cf498'
   }
 
   public static readonly DefaultStartOptions = {
