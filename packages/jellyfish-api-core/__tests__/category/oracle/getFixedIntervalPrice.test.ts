bb97dd5a32800171f2a1596c7a718ed4e6dc203e
websterlcl <w.canonbrother@gmail.com>
2021-11-08 13:06:51 +0800

    move multi oracle to another scope

--- packages/jellyfish-api-core/__tests__/category/oracle/getFixedIntervalPrice.test.ts
+++ packages/jellyfish-api-core/__tests__/category/oracle/getFixedIntervalPrice.test.ts
@@ -3,16 +3,56 @@ import BigNumber from 'bignumber.js'
 import { Testing } from '@defichain/jellyfish-testing'
 import { RpcApiError } from '@defichain/jellyfish-api-core'
 
-describe('Oracle', () => {
-  const container = new LoanMasterNodeRegTestContainer()
-  const testing = Testing.create(container)
-  let oracleId: string
-  let timestamp: number
-  const priceFeeds = [
-    { token: 'DFI', currency: 'USD' },
-    { token: 'UBER', currency: 'USD' }
-  ]
+const container = new LoanMasterNodeRegTestContainer()
+const testing = Testing.create(container)
+let oracleId: string
+let timestamp: number
+const priceFeeds = [
+  { token: 'DFI', currency: 'USD' },
+  { token: 'UBER', currency: 'USD' }
+]
 
+async function setup (): Promise<void> {
+  // token setup
+  const aliceColAddr = await testing.generateAddress()
+  await testing.token.dfi({ address: aliceColAddr, amount: 100000 })
+  await testing.generate(1)
+
+  // oracle setup
+  const addr = await testing.generateAddress()
+  oracleId = await testing.rpc.oracle.appointOracle(addr, priceFeeds, { weightage: 1 })
+  await testing.generate(1)
+
+  timestamp = Math.floor(new Date().getTime() / 1000)
+  await testing.rpc.oracle.setOracleData(
+    oracleId,
+    timestamp,
+    {
+      prices: [
+        { tokenAmount: '1@DFI', currency: 'USD' },
+        { tokenAmount: '8@UBER', currency: 'USD' }
+      ]
+    }
+  )
+  await testing.generate(1)
+
+  // setCollateralToken DFI
+  await testing.rpc.loan.setCollateralToken({
+    token: 'DFI',
+    factor: new BigNumber(1),
+    fixedIntervalPriceId: 'DFI/USD'
+  })
+  await testing.generate(1)
+
+  // setLoanToken UBER
+  await testing.rpc.loan.setLoanToken({
+    symbol: 'UBER',
+    fixedIntervalPriceId: 'UBER/USD'
+  })
+  await testing.generate(1)
+}
+
+describe('Oracle', () => {
   beforeAll(async () => {
     await testing.container.start()
     await testing.container.waitForWalletCoinbaseMaturity()
@@ -22,46 +62,6 @@ describe('Oracle', () => {
   afterAll(async () => {
     await testing.container.stop()
   })
-
-  async function setup (): Promise<void> {
-    // token setup
-    const aliceColAddr = await testing.generateAddress()
-    await testing.token.dfi({ address: aliceColAddr, amount: 100000 })
-    await testing.generate(1)
-
-    // oracle setup
-    const addr = await testing.generateAddress()
-    oracleId = await testing.rpc.oracle.appointOracle(addr, priceFeeds, { weightage: 1 })
-    await testing.generate(1)
-
-    timestamp = Math.floor(new Date().getTime() / 1000)
-    await testing.rpc.oracle.setOracleData(
-      oracleId,
-      timestamp,
-      {
-        prices: [
-          { tokenAmount: '1@DFI', currency: 'USD' },
-          { tokenAmount: '8@UBER', currency: 'USD' }
-        ]
-      }
-    )
-    await testing.generate(1)
-
-    // setCollateralToken DFI
-    await testing.rpc.loan.setCollateralToken({
-      token: 'DFI',
-      factor: new BigNumber(1),
-      fixedIntervalPriceId: 'DFI/USD'
-    })
-    await testing.generate(1)
-
-    // setLoanToken UBER
-    await testing.rpc.loan.setLoanToken({
-      symbol: 'UBER',
-      fixedIntervalPriceId: 'UBER/USD'
-    })
-    await testing.generate(1)
-  }
 
   it('should getFixedIntervalPrices', async () => {
     {
@@ -137,6 +137,30 @@ describe('Oracle', () => {
         isLive: false
       })
     }
+  })
+
+  it('should not getFixedIntervalPrice as empty id', async () => {
+    const promise = testing.rpc.oracle.getFixedIntervalPrice('')
+    await expect(promise).rejects.toThrow(RpcApiError)
+    await expect(promise).rejects.toThrow('Invalid parameters, argument "fixedIntervalPriceId" must be non-null')
+  })
+
+  it('should not getFixedIntervalPrice as non-existence price id', async () => {
+    const promise = testing.rpc.oracle.getFixedIntervalPrice('DURIAN/USD')
+    await expect(promise).rejects.toThrow(RpcApiError)
+    await expect(promise).rejects.toThrow('fixedIntervalPrice with id <DURIAN/USD> not found')
+  })
+})
+
+describe('Multi Oracles', () => {
+  beforeAll(async () => {
+    await testing.container.start()
+    await testing.container.waitForWalletCoinbaseMaturity()
+    await setup()
+  })
+
+  afterAll(async () => {
+    await testing.container.stop()
   })
 
   it('should getFixedIntervalPrices with several oracles', async () => {
@@ -222,17 +246,5 @@ describe('Oracle', () => {
         isLive: false
       })
     }
-  })
-
-  it('should not getFixedIntervalPrice as empty id', async () => {
-    const promise = testing.rpc.oracle.getFixedIntervalPrice('')
-    await expect(promise).rejects.toThrow(RpcApiError)
-    await expect(promise).rejects.toThrow('Invalid parameters, argument "fixedIntervalPriceId" must be non-null')
-  })
-
-  it('should not getFixedIntervalPrice as non-existence price id', async () => {
-    const promise = testing.rpc.oracle.getFixedIntervalPrice('DURIAN/USD')
-    await expect(promise).rejects.toThrow(RpcApiError)
-    await expect(promise).rejects.toThrow('fixedIntervalPrice with id <DURIAN/USD> not found')
   })
 })
