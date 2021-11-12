576b4e4a0993c9eaaa9161131b5bc70badb0d1fb
Ivan Lee <44192990+ivan-zynesis@users.noreply.github.com>
2021-11-12 16:19:10 +0800

    RPC: `setGovHeight` and `listGovs` (#789)
    * add rpc, test, doc
    
    * add invalid activation height test
    
    * rename rpc func name
    
    * leave setGovHeight min height check to consensus
    
    * add extra test for both setGov and setGovHeight
    
    * fix test
    
    * add suggested test cases
    
    * fix missing arg
    
    * fix test and add listGovs new rpc
    
    * Update packages/jellyfish-api-core/__tests__/category/masternode/listGovs.test.ts
    
    Co-authored-by: surangap <surangatco@gmail.com>
    
    * Update docs/node/RPC Category/11-masternode.md
    
    Co-authored-by: jingyi2811 <jingyi2811@gmail.com>
    
    Co-authored-by: surangap <surangatco@gmail.com>
    Co-authored-by: jingyi2811 <jingyi2811@gmail.com>

--- packages/jellyfish-api-core/__tests__/category/masternode/setGov.test.ts
+++ packages/jellyfish-api-core/__tests__/category/masternode/setGov.test.ts
@@ -1,5 +1,5 @@
 import { RpcApiError } from '@defichain/jellyfish-api-core'
-import { MasterNodeRegTestContainer } from '@defichain/testcontainers'
+import { GenesisKeys, MasterNodeRegTestContainer } from '@defichain/testcontainers'
 import { createPoolPair, createToken } from '@defichain/testing'
 import { ContainerAdapterClient } from '../../container_adapter_client'
 
@@ -11,13 +11,7 @@ describe('Masternode', () => {
     await container.start()
     await container.waitForReady()
     await container.waitForWalletCoinbaseMaturity()
-  })
 
-  afterAll(async () => {
-    await container.stop()
-  })
-
-  it('should setGov LP_SPLITS', async () => {
     const govBefore = await client.masternode.getGov('LP_SPLITS')
     expect(Object.keys(govBefore.LP_SPLITS).length).toStrictEqual(0)
 
@@ -25,13 +19,48 @@ describe('Masternode', () => {
     await createToken(container, 'DOG')
     await createPoolPair(container, 'CAT', 'DFI')
     await createPoolPair(container, 'DOG', 'DFI')
+  })
 
+  afterAll(async () => {
+    await container.stop()
+  })
+
+  it('should setGov LP_SPLITS', async () => {
     await client.masternode.setGov({ LP_SPLITS: { 3: 0.2, 4: 0.8 } })
     await container.generate(1)
 
     const govAfter = await client.masternode.getGov('LP_SPLITS')
     expect(govAfter.LP_SPLITS['3'].toString()).toStrictEqual('0.2')
     expect(govAfter.LP_SPLITS['4'].toString()).toStrictEqual('0.8')
+  })
+
+  it('should setGov with specific utxos', async () => {
+    const utxo = await container.fundAddress(GenesisKeys[0].owner.address, 10)
+
+    { // before utxo spent
+      const utxos = await container.call('listunspent')
+      const found = utxos.find((u: any) => u.txid === utxo.txid && u.vout === utxo.vout)
+      expect(found).not.toStrictEqual(undefined)
+    }
+
+    await client.masternode.setGov({ LP_SPLITS: { 3: 0.3, 4: 0.7 } }, [utxo])
+    await container.generate(1)
+
+    const govAfter = await client.masternode.getGov('LP_SPLITS')
+    expect(govAfter.LP_SPLITS['3'].toString()).toStrictEqual('0.3')
+    expect(govAfter.LP_SPLITS['4'].toString()).toStrictEqual('0.7')
+
+    { // after utxo spent
+      const utxos = await container.call('listunspent')
+      const found = utxos.find((u: any) => u.txid === utxo.txid && u.vout === utxo.vout)
+      expect(found).toStrictEqual(undefined)
+    }
+  })
+
+  it('should fail if GovVar key is not registered', async () => {
+    const promise = client.masternode.setGov({ INVALID: 'value' })
+    await expect(promise).rejects.toThrow(RpcApiError)
+    await expect(promise).rejects.toThrow('Variable INVALID not registered')
   })
 
   it('should be failed to setGov LP_REWARD as manually set after Eunos hard fork is not allowed', async () => {
