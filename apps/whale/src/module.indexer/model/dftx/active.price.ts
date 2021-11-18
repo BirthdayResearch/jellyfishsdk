904b47137b2ba5dd38283d8615fb2ef1e8d36a72
Ahmed Hilali <monstrobishi@gmail.com>
2021-11-19 00:27:14 +0800

    Refactor `active-price` indexer + new tests (https://github.com/DeFiCh/whale/pull/576)
    * Added isLive false test
    
    * Add additional timestamp check to active price
    
    * Update tests
    
    * New active price implementation
    
    * Fix tests

--- apps/whale/src/module.indexer/model/dftx/active.price.ts
+++ apps/whale/src/module.indexer/model/dftx/active.price.ts
@@ -1,15 +1,18 @@
 import { NetworkName } from '@defichain/jellyfish-network'
+import { CSetLoanToken, SetLoanToken } from '@defichain/jellyfish-transaction'
 import { Inject, Injectable } from '@nestjs/common'
-import { Indexer, RawBlock } from '@src/module.indexer/model/_abstract'
+import { RawBlock } from '@src/module.indexer/model/_abstract'
 import { OraclePriceActive, OraclePriceActiveMapper } from '@src/module.model/oracle.price.active'
 import { OraclePriceAggregated, OraclePriceAggregatedMapper } from '@src/module.model/oracle.price.aggregated'
-import { PriceTickerMapper } from '@src/module.model/price.ticker'
+import { PriceTickerMapper, PriceTicker } from '@src/module.model/price.ticker'
 import { HexEncoder } from '@src/module.model/_hex.encoder'
-import { PriceTicker } from '@whale-api-client/api/prices'
 import BigNumber from 'bignumber.js'
+import { DfTxIndexer, DfTxTransaction } from './_abstract'
 
 @Injectable()
-export class ActivePriceIndexer extends Indexer {
+export class ActivePriceIndexer extends DfTxIndexer<SetLoanToken> {
+  OP_CODE: number = CSetLoanToken.OP_CODE
+
   BLOCK_INTERVAL: number
   DEVIATION_THRESHOLD: number = 0.3
   MINIMUM_LIVE_ORACLES: number = 2
@@ -24,35 +27,48 @@ export class ActivePriceIndexer extends Indexer {
     this.BLOCK_INTERVAL = network === 'regtest' ? 6 : 120
   }
 
-  async index (block: RawBlock): Promise<void> {
-    if (block.height % this.BLOCK_INTERVAL !== 0) {
+  async index (block: RawBlock, txns: Array<DfTxTransaction<SetLoanToken>>): Promise<void> {
+    if (block.height % this.BLOCK_INTERVAL === 0) {
+      await this.performActivePriceTickForAll(block)
+      // return early since we updated all the price ticks already
       return
     }
 
+    // only update for specific setLoanToken tx (to match ain implementation)
+    for (const { dftx: { data } } of txns) {
+      await this.performActivePriceTick(block, `${data.currencyPair.token}-${data.currencyPair.currency}`)
+    }
+  }
+
+  async performActivePriceTickForAll (block: RawBlock): Promise<void> {
     const tickers: PriceTicker[] = await this.priceTickerMapper.query(Number.MAX_SAFE_INTEGER)
     for (const ticker of tickers) {
-      const aggregatedPrices = await this.aggregatedMapper.query(ticker.id, 1)
-      if (aggregatedPrices.length < 1) {
-        continue
-      }
-
-      const previousPrices = await this.activePriceMapper.query(ticker.id, 1)
-      await this.activePriceMapper.put(this.mapActivePrice(block, ticker, aggregatedPrices[0], previousPrices[0]))
+      await this.performActivePriceTick(block, ticker.id)
     }
+  }
+
+  async performActivePriceTick (block: RawBlock, tickerId: string): Promise<void> {
+    const aggregatedPrices = await this.aggregatedMapper.query(tickerId, 1)
+    if (aggregatedPrices.length < 1) {
+      return
+    }
+
+    const previousPrices = await this.activePriceMapper.query(tickerId, 1)
+    await this.activePriceMapper.put(this.mapActivePrice(block, tickerId, aggregatedPrices[0], previousPrices[0]))
   }
 
   private mapActivePrice (
     block: RawBlock,
-    ticker: PriceTicker,
+    tickerId: string,
     aggregatedPrice: OraclePriceAggregated,
     previousActive?: OraclePriceActive
   ): OraclePriceActive {
-    const nextPrice = this.isAggregateValid(aggregatedPrice.aggregated) ? aggregatedPrice.aggregated : undefined
+    const nextPrice = this.isAggregateValid(aggregatedPrice, block) ? aggregatedPrice.aggregated : undefined
     const activePrice = previousActive?.next !== undefined ? previousActive.next : previousActive?.active
 
     return {
-      id: `${ticker.id}-${block.height}`,
-      key: ticker.id,
+      id: `${tickerId}-${block.height}`,
+      key: tickerId,
       isLive: this.isLive(activePrice, nextPrice),
       block: { hash: block.hash, height: block.height, medianTime: block.mediantime, time: block.time },
       active: activePrice,
@@ -61,7 +77,8 @@ export class ActivePriceIndexer extends Indexer {
     }
   }
 
-  private isLive (active: OraclePriceActive['active'], next: OraclePriceActive['next']): boolean {
+  private isLive (active: OraclePriceActive['active'],
+    next: OraclePriceActive['next']): boolean {
     if (active === undefined || next === undefined) {
       return false
     }
@@ -84,22 +101,28 @@ export class ActivePriceIndexer extends Indexer {
     return true
   }
 
-  private isAggregateValid (aggregate: OraclePriceActive['next']): boolean {
-    if (aggregate?.oracles === undefined) {
+  private isAggregateValid (aggregate: OraclePriceAggregated, block: RawBlock): boolean {
+    // The last aggregated price (i.e. setOracleData), was more than an hour ago,
+    // therefore it's invalid
+    if (Math.abs(aggregate.block.time - block.time) >= 3600) {
       return false
     }
 
-    if (aggregate?.oracles.active < this.MINIMUM_LIVE_ORACLES) {
+    if (aggregate.aggregated.oracles === undefined) {
       return false
     }
 
-    if (aggregate?.weightage <= 0) {
+    if (aggregate.aggregated.oracles.active < this.MINIMUM_LIVE_ORACLES) {
+      return false
+    }
+
+    if (aggregate.aggregated.weightage <= 0) {
       return false
     }
     return true
   }
 
-  async invalidate (block: RawBlock): Promise<void> {
+  async invalidate (block: RawBlock, _: Array<DfTxTransaction<SetLoanToken>>): Promise<void> {
     if (block.height % this.BLOCK_INTERVAL !== 0) {
       return
     }
