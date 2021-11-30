440aaac3252c89fa3b5e6c98279d8b2f0330e5ce
chee-chyuan <angchee@live.com.my>
2021-11-30 18:28:38 +0800

    added test case for getmasternodeblocks with depth parameter (not working atm)

--- packages/jellyfish-api-core/__tests__/category/masternode/getMasternodeBlocks.test.ts
+++ packages/jellyfish-api-core/__tests__/category/masternode/getMasternodeBlocks.test.ts
@@ -34,4 +34,26 @@ describe('Masternode', () => {
     const masternodeBlock = await client.masternode.getMasternodeBlocks(identifier)
     expect(Object.keys(masternodeBlock).length).toEqual(mintedBlockNumber)
   })
+
+  it('should show only list of blocks for masternode according to the depth', async () => {
+    const address = await container.getNewAddress('', 'legacy')
+    const id = await container.call('createmasternode', [address])
+    await container.generate(1)
+
+    await container.setDeFiConf(['masternode_operator=' + address])
+    await container.restart()
+
+    await container.generate(20)
+    const mintedBlockNumber = 20
+    await container.generate(mintedBlockNumber, address)
+
+    const identifier: MasternodeBlock = {
+      id,
+      ownerAuthAddress: address
+    }
+    const depth = 1
+    const masternodeBlock = await client.masternode.getMasternodeBlocks(identifier, depth)
+    console.log(masternodeBlock)
+    expect(Object.keys(masternodeBlock).length).toEqual(depth)
+  })
 })
