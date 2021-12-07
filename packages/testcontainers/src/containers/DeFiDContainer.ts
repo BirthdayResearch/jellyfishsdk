126f60d9463cdaebb478d622115248905ab44938
Fuxing Loh <git@fuxing.dev>
2021-12-08 03:03:07 +0800

    test defi/defichain:HEAD-d632b1a

--- packages/testcontainers/src/containers/DeFiDContainer.ts
+++ packages/testcontainers/src/containers/DeFiDContainer.ts
@@ -29,7 +29,7 @@ export abstract class DeFiDContainer extends DockerContainer {
     if (process?.env?.DEFICHAIN_DOCKER_IMAGE !== undefined) {
       return process.env.DEFICHAIN_DOCKER_IMAGE
     }
-    return 'defi/defichain:2.2.1'
+    return 'defi/defichain:HEAD-d632b1a'
   }
 
   public static readonly DefaultStartOptions = {
