366bb5a02146141092ae7ffee6a2fe9d07a4fb40
chee-chyuan <angchee@live.com.my>
2021-12-10 15:13:43 +0800

    change method to mint loan token in compositeSwap.test.ts

--- packages/jellyfish-api-core/__tests__/category/poolpair/compositeSwap.test.ts
+++ packages/jellyfish-api-core/__tests__/category/poolpair/compositeSwap.test.ts
@@ -7,10 +7,16 @@ import { GenesisKeys } from '@defichain/testcontainers'
 import { UTXO } from 'packages/jellyfish-api-core/src/category/wallet'
 import BigNumber from 'bignumber.js'
 
+function now (): number {
+  return Math.floor(new Date().getTime() / 1000)
+}
+
 describe('compositeSwap', () => {
   const container = new LoanMasterNodeRegTestContainer()
   const testing = Testing.create(container)
   const client = new ContainerAdapterClient(container)
+  let loanVaultId: string
+  let loanMinterAddr: string
 
   beforeAll(async () => {
     await testing.container.start()
@@ -18,22 +24,175 @@ describe('compositeSwap', () => {
     await testing.token.dfi({ amount: 30000 })
     await testing.generate(1)
 
-    await testing.token.create({ symbol: 'CAT' })
+    // setup oracle
+    const priceFeeds = [
+      { token: 'DFI', currency: 'USD' },
+      { token: 'BTC', currency: 'USD' },
+      { token: 'TSLA', currency: 'USD' },
+      { token: 'ABC', currency: 'USD' },
+      { token: 'CAT', currency: 'USD' },
+      { token: 'DOG', currency: 'USD' },
+      { token: 'PATHA', currency: 'USD' },
+      { token: 'PATHB', currency: 'USD' },
+      { token: 'XYZ', currency: 'USD' },
+      { token: 'ELF', currency: 'USD' },
+      { token: 'ORC', currency: 'USD' },
+      { token: 'UNDY', currency: 'USD' },
+      { token: 'HUMAN', currency: 'USD' },
+      { token: 'EMPTY', currency: 'USD' }
+    ]
+
+    const oraclePriceData = {
+      prices: [
+        { tokenAmount: '1@DFI', currency: 'USD' },
+        { tokenAmount: '0.1@CAT', currency: 'USD' },
+        { tokenAmount: '0.1@DOG', currency: 'USD' },
+        { tokenAmount: '0.1@TSLA', currency: 'USD' },
+        { tokenAmount: '0.1@ABC', currency: 'USD' },
+        { tokenAmount: '0.1@PATHA', currency: 'USD' },
+        { tokenAmount: '0.1@PATHB', currency: 'USD' },
+        { tokenAmount: '0.1@XYZ', currency: 'USD' },
+        { tokenAmount: '0.1@ELF', currency: 'USD' },
+        { tokenAmount: '0.1@ORC', currency: 'USD' },
+        { tokenAmount: '0.1@UNDY', currency: 'USD' },
+        { tokenAmount: '0.1@HUMAN', currency: 'USD' },
+        { tokenAmount: '0.1@EMPTY', currency: 'USD' }
+      ]
+    }
+
+    const oracleAddr = await testing.generateAddress()
+    const oracleId = await testing.rpc.oracle.appointOracle(oracleAddr, priceFeeds, { weightage: 1 })
+    await testing.generate(1)
+
+    await testing.rpc.oracle.setOracleData(oracleId, now(), oraclePriceData)
+
+    // set up loan vaults for loan tokens
+    loanMinterAddr = await testing.generateAddress()
+    const utxos = await testing.rpc.wallet.listUnspent()
+    const inputs = utxos.map((utxo: { txid: string, vout: number }) => {
+      return {
+        txid: utxo.txid,
+        vout: utxo.vout
+      }
+    })
+
+    await testing.rpc.account.utxosToAccount({ [loanMinterAddr]: '10000000@DFI' }, inputs)
+    await testing.generate(1)
+
+    // create loan and vault for minting of loan tokens
+    const loanTokenSchemeId = 'minter'
+    await testing.rpc.loan.createLoanScheme({
+      minColRatio: 100,
+      interestRate: new BigNumber(0.01),
+      id: loanTokenSchemeId
+    })
+
+    await testing.generate(1)
+    const loanTokenVaultAddr = await testing.generateAddress()
+    loanVaultId = await testing.rpc.loan.createVault({
+      ownerAddress: loanTokenVaultAddr,
+      loanSchemeId: loanTokenSchemeId
+    })
+
+    await testing.generate(1)
+
+    // setup collateral token and loan token
+    await testing.rpc.loan.setCollateralToken({
+      token: 'DFI',
+      factor: new BigNumber(1),
+      fixedIntervalPriceId: 'DFI/USD'
+    })
+
+    await testing.container.generate(1)
+
+    await testing.rpc.loan.setLoanToken({
+      symbol: 'TSLA',
+      fixedIntervalPriceId: 'TSLA/USD'
+    })
+    await testing.rpc.loan.setLoanToken({
+      symbol: 'CAT',
+      fixedIntervalPriceId: 'CAT/USD'
+    })
+    await testing.rpc.loan.setLoanToken({
+      symbol: 'DOG',
+      fixedIntervalPriceId: 'CAT/USD'
+    })
+    await testing.rpc.loan.setLoanToken({
+      symbol: 'ABC',
+      fixedIntervalPriceId: 'ABC/USD'
+    })
+    await testing.rpc.loan.setLoanToken({
+      symbol: 'PATHA',
+      fixedIntervalPriceId: 'PATHA/USD'
+    })
+    await testing.rpc.loan.setLoanToken({
+      symbol: 'PATHB',
+      fixedIntervalPriceId: 'PATHB/USD'
+    })
+    await testing.rpc.loan.setLoanToken({
+      symbol: 'XYZ',
+      fixedIntervalPriceId: 'XYZ/USD'
+    })
+    await testing.rpc.loan.setLoanToken({
+      symbol: 'ELF',
+      fixedIntervalPriceId: 'ELF/USD'
+    })
+    await testing.container.generate(1)
+
+    await testing.rpc.loan.setLoanToken({
+      symbol: 'ORC',
+      fixedIntervalPriceId: 'ORC/USD'
+    })
+    await testing.container.generate(1)
+    await testing.rpc.loan.setLoanToken({
+      symbol: 'UNDY',
+      fixedIntervalPriceId: 'UNDY/USD'
+    })
+    await testing.container.generate(1)
+    await testing.rpc.loan.setLoanToken({
+      symbol: 'HUMAN',
+      fixedIntervalPriceId: 'HUMAN/USD'
+    })
+    await testing.container.generate(1)
+    await testing.rpc.loan.setLoanToken({
+      symbol: 'EMPTY',
+      fixedIntervalPriceId: 'EMPTY/USD'
+    })
+
+    await testing.container.generate(1)
+
+    // take loan for each loan tokens that we need
+    await testing.rpc.loan.depositToVault({
+      vaultId: loanVaultId,
+      from: loanMinterAddr,
+      amount: '10000000@DFI'
+    })
+
+    await testing.container.generate(1)
+
+    await testing.rpc.loan.takeLoan({
+      vaultId: loanVaultId,
+      amounts: '30000@CAT',
+      to: loanMinterAddr
+    })
+
     await container.generate(1)
     await testing.poolpair.create({ tokenA: 'CAT', tokenB: 'DFI' })
-    await container.generate(1)
-    await testing.token.mint({ symbol: 'CAT', amount: 30000 })
     await container.generate(1)
     await testing.poolpair.add({
       a: { symbol: 'CAT', amount: 25000 },
       b: { symbol: 'DFI', amount: 10000 }
     })
 
-    await testing.token.create({ symbol: 'DOG' })
     await container.generate(1)
     await testing.poolpair.create({ tokenA: 'DOG', tokenB: 'DFI' })
     await container.generate(1)
-    await testing.token.mint({ symbol: 'DOG', amount: 30000 })
+
+    await testing.rpc.loan.takeLoan({
+      vaultId: loanVaultId,
+      amounts: '30000@DOG',
+      to: loanMinterAddr
+    })
     await container.generate(1)
     await testing.poolpair.add({
       a: { symbol: 'DOG', amount: 28000 },
@@ -41,22 +200,35 @@ describe('compositeSwap', () => {
     })
     await container.generate(1)
 
-    // consensus should pick the path with better rate
-    await testing.token.create({ symbol: 'ABC' })
-    await testing.token.create({ symbol: 'PATHA' })
-    await testing.token.create({ symbol: 'PATHB' })
-    await testing.token.create({ symbol: 'XYZ' })
-    await container.generate(1)
-
     await testing.poolpair.create({ tokenA: 'ABC', tokenB: 'PATHA' })
     await testing.poolpair.create({ tokenA: 'XYZ', tokenB: 'PATHA' })
     await testing.poolpair.create({ tokenA: 'ABC', tokenB: 'PATHB' })
     await testing.poolpair.create({ tokenA: 'XYZ', tokenB: 'PATHB' })
     await container.generate(1)
-    await testing.token.mint({ symbol: 'ABC', amount: 10000 })
-    await testing.token.mint({ symbol: 'PATHA', amount: 10000 })
-    await testing.token.mint({ symbol: 'PATHB', amount: 10000 })
-    await testing.token.mint({ symbol: 'XYZ', amount: 10000 })
+
+    await testing.rpc.loan.takeLoan({
+      vaultId: loanVaultId,
+      amounts: '10000@ABC',
+      to: loanMinterAddr
+    })
+
+    await testing.rpc.loan.takeLoan({
+      vaultId: loanVaultId,
+      amounts: '10000@PATHA',
+      to: loanMinterAddr
+    })
+
+    await testing.rpc.loan.takeLoan({
+      vaultId: loanVaultId,
+      amounts: '10000@PATHB',
+      to: loanMinterAddr
+    })
+
+    await testing.rpc.loan.takeLoan({
+      vaultId: loanVaultId,
+      amounts: '10000@XYZ',
+      to: loanMinterAddr
+    })
     await container.generate(1)
 
     // PATH A: expensive
@@ -81,21 +253,34 @@ describe('compositeSwap', () => {
     await container.generate(1)
 
     // longer conversion path (3 swaps)
-    await testing.token.create({ symbol: 'ELF' })
-    await testing.token.create({ symbol: 'ORC' })
-    await testing.token.create({ symbol: 'UNDY' })
-    await testing.token.create({ symbol: 'HUMAN' })
-    await container.generate(1)
-
     await testing.poolpair.create({ tokenA: 'ELF', tokenB: 'ORC' })
     await testing.poolpair.create({ tokenA: 'ORC', tokenB: 'UNDY' })
     await testing.poolpair.create({ tokenA: 'UNDY', tokenB: 'HUMAN' })
     await container.generate(1)
 
-    await testing.token.mint({ symbol: 'ELF', amount: 160000 })
-    await testing.token.mint({ symbol: 'ORC', amount: 160000 })
-    await testing.token.mint({ symbol: 'UNDY', amount: 160000 })
-    await testing.token.mint({ symbol: 'HUMAN', amount: 160000 })
+    await testing.rpc.loan.takeLoan({
+      vaultId: loanVaultId,
+      amounts: '160000@ELF',
+      to: loanMinterAddr
+    })
+
+    await testing.rpc.loan.takeLoan({
+      vaultId: loanVaultId,
+      amounts: '160000@ORC',
+      to: loanMinterAddr
+    })
+
+    await testing.rpc.loan.takeLoan({
+      vaultId: loanVaultId,
+      amounts: '160000@UNDY',
+      to: loanMinterAddr
+    })
+
+    await testing.rpc.loan.takeLoan({
+      vaultId: loanVaultId,
+      amounts: '160000@HUMAN',
+      to: loanMinterAddr
+    })
     await container.generate(1)
 
     // large pool volume to minimize slippage effect on swap
@@ -399,12 +584,15 @@ describe('compositeSwap', () => {
   it('should not compositeSwap - Cannot find usable pool pair', async () => {
     const fromAddress = await testing.generateAddress()
 
-    await testing.token.create({ symbol: 'TSLA' })
     await testing.token.create({ symbol: 'APPL' })
     await container.generate(1)
     await testing.poolpair.create({ tokenA: 'TSLA', tokenB: 'DFI' })
     await container.generate(1)
-    await testing.token.mint({ symbol: 'TSLA', amount: 100 })
+    await testing.rpc.loan.takeLoan({
+      vaultId: loanVaultId,
+      amounts: '100@TSLA',
+      to: loanMinterAddr
+    })
     await container.generate(1)
 
     const metadata: poolpair.PoolSwapMetadata = {
@@ -443,11 +631,13 @@ describe('compositeSwap', () => {
   it('should not compositeSwap - lack of liquidity', async () => {
     const fromAddress = await testing.generateAddress()
 
-    await testing.token.create({ symbol: 'EMPTY' })
-    await container.generate(1)
     await testing.poolpair.create({ tokenA: 'EMPTY', tokenB: 'DFI' })
     await container.generate(1)
-    await testing.token.mint({ symbol: 'EMPTY', amount: 100 })
+    await testing.rpc.loan.takeLoan({
+      vaultId: loanVaultId,
+      amounts: '100@EMPTY',
+      to: loanMinterAddr
+    })
     await container.generate(1)
 
     const metadata: poolpair.PoolSwapMetadata = {
