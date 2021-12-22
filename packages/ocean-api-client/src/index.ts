bff30be03602e32e7ccceb29da41bd3910bfb09e
Fuxing Loh <git@fuxing.dev>
2021-12-22 14:45:20 +0800

    feat(ocean-api): add ocean-api-client core

---
+++ packages/ocean-api-client/src/index.ts
@@ -0,0 +1,24 @@
+export * from './errors/ApiException'
+export * from './errors/ApiValidationException'
+export * from './errors/ClientException'
+export * from './errors/TimeoutException'
+
+export * from './ApiResponse'
+export * from './OceanApiClient'
+
+// TODO(fuxingloh):
+//  export * from './api/Fee'
+//  import { OceanApiClient } from '../OceanApiClient'
+//
+//  export class Fee {
+//    constructor (private readonly client: OceanApiClient) {
+//    }
+//
+//    /**
+//     * @param {number} confirmationTarget in blocks till fee get confirmed
+//     * @return {Promise<number>} fee rate per KB
+//     */
+//    async estimate (confirmationTarget: number = 10): Promise<number> {
+//      return await this.client.requestData('GET', `fee/estimate?confirmationTarget=${confirmationTarget}`)
+//    }
+//  }
