0caef8566ef5b38ed91a981bc5a7c7882220fdbf
Fuxing Loh <4266087+fuxingloh@users.noreply.github.com>
2021-12-28 12:11:51 +0800

    Update DeFiDContainer.ts

--- packages/testcontainers/src/containers/DeFiDContainer.ts
+++ packages/testcontainers/src/containers/DeFiDContainer.ts
@@ -29,7 +29,7 @@ export abstract class DeFiDContainer extends DockerContainer {
     if (process?.env?.DEFICHAIN_DOCKER_IMAGE !== undefined) {
       return process.env.DEFICHAIN_DOCKER_IMAGE
     }
-    return 'defi/defichain:2.3.1'
+    return 'defi/defichain:2.3.2'
   }
 
   public static readonly DefaultStartOptions = {
