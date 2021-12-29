057a57fb931814709e3bbefaf3b292f4ff5467f0
surangap <surangatco@gmail.com>
2021-12-29 14:43:09 +0800

    Moved alternative mining test for getmasternodeblocks to a separate scope for isolatation.

--- packages/jellyfish-api-core/__tests__/category/masternode/getMasternodeBlocks.test.ts
+++ packages/jellyfish-api-core/__tests__/category/masternode/getMasternodeBlocks.test.ts
@@ -6,7 +6,6 @@ import { RegTestFoundationKeys } from '@defichain/jellyfish-network'
 describe('Masternode', () => {
   const tGroup = TestingGroup.create(2, i => new MasterNodeRegTestContainer(RegTestFoundationKeys[i]))
   const masterNodeProvider1 = tGroup.get(0)
-  const masterNodeProvider2 = tGroup.get(1)
 
   beforeAll(async () => {
     await tGroup.start()
@@ -67,52 +66,6 @@ describe('Masternode', () => {
     expect(Object.keys(masternodeBlock).length).toEqual(depth)
   })
 
-  it('should mint blocks by different masternode correctly and the depth is correct', async () => {
-    // check both MN1, MN2 on the same tip
-    expect(await masterNodeProvider1.rpc.blockchain.getBestBlockHash()).toStrictEqual(await masterNodeProvider2.rpc.blockchain.getBestBlockHash())
-
-    // MN identifiers - use owner address
-    const MN1Identifier: MasternodeBlock = {
-      ownerAddress: RegTestFoundationKeys[0].owner.address
-    }
-    const MN2Identifier: MasternodeBlock = {
-      ownerAddress: RegTestFoundationKeys[1].owner.address
-    }
-
-    // get minted blocks so far.
-    const MN1AlreadyMinedBlocks = await masterNodeProvider1.rpc.masternode.getMasternodeBlocks(MN1Identifier)
-    const MN2AlreadyMinedBlocks = await masterNodeProvider2.rpc.masternode.getMasternodeBlocks(MN2Identifier)
-
-    // mine blocks alternatively
-    const numberOfBlockPerMasterNode = 10
-    for (let i = 0; i < numberOfBlockPerMasterNode; i++) {
-      await masterNodeProvider1.container.generate(1)
-      await tGroup.waitForSync()
-      await masterNodeProvider2.container.generate(1)
-      await tGroup.waitForSync()
-    }
-
-    // check both MN1, MN2 on the same tip
-    expect(await masterNodeProvider1.rpc.blockchain.getBestBlockHash()).toStrictEqual(await masterNodeProvider2.rpc.blockchain.getBestBlockHash())
-
-    const MN1MinedBlocks = await masterNodeProvider1.rpc.masternode.getMasternodeBlocks(MN1Identifier)
-    const MN2MinedBlocks = await masterNodeProvider2.rpc.masternode.getMasternodeBlocks(MN2Identifier)
-
-    expect(Object.keys(MN1MinedBlocks).length).toEqual(Object.keys(MN1AlreadyMinedBlocks).length + numberOfBlockPerMasterNode)
-    expect(Object.keys(MN2MinedBlocks).length).toEqual(Object.keys(MN2AlreadyMinedBlocks).length + numberOfBlockPerMasterNode)
-
-    // Query with depth given
-    {
-      const depth = 4
-      const MN1MinedBlocks = await masterNodeProvider1.rpc.masternode.getMasternodeBlocks(MN1Identifier, depth)
-      const MN2MinedBlocks = await masterNodeProvider2.rpc.masternode.getMasternodeBlocks(MN2Identifier, depth)
-
-      // should return last two blocks mined by each MN, within a depth of 4 from the tip.
-      expect(Object.keys(MN1MinedBlocks).length).toEqual(depth / 2)
-      expect(Object.keys(MN2MinedBlocks).length).toEqual(depth / 2)
-    }
-  })
-
   it('should fail if no identifier information is provided', async () => {
     const identifier: MasternodeBlock = {}
     const promise = masterNodeProvider1.rpc.masternode.getMasternodeBlocks(identifier)
@@ -162,5 +115,68 @@ describe('Masternode', () => {
 
     promise = masterNodeProvider1.rpc.masternode.getMasternodeBlocks(identifier)
     await expect(promise).rejects.toThrow('Invalid P2PKH address')
+  })
+})
+
+describe('Masternode', () => {
+  const tGroup = TestingGroup.create(2, i => new MasterNodeRegTestContainer(RegTestFoundationKeys[i]))
+  const masterNodeProvider1 = tGroup.get(0)
+  const masterNodeProvider2 = tGroup.get(1)
+
+  beforeAll(async () => {
+    await tGroup.start()
+    await masterNodeProvider1.container.waitForWalletCoinbaseMaturity()
+    await tGroup.waitForSync()
+  })
+
+  afterAll(async () => {
+    await tGroup.stop()
+  })
+
+  it('should mint blocks by different masternode correctly and the depth is correct', async () => {
+    await tGroup.waitForSync()
+    // check both MN1, MN2 on the same tip
+    expect(await masterNodeProvider1.rpc.blockchain.getBestBlockHash()).toStrictEqual(await masterNodeProvider2.rpc.blockchain.getBestBlockHash())
+
+    // MN identifiers - use owner address
+    const MN1Identifier: MasternodeBlock = {
+      ownerAddress: RegTestFoundationKeys[0].owner.address
+    }
+    const MN2Identifier: MasternodeBlock = {
+      ownerAddress: RegTestFoundationKeys[1].owner.address
+    }
+
+    // get minted blocks so far.
+    const MN1AlreadyMinedBlocks = await masterNodeProvider1.rpc.masternode.getMasternodeBlocks(MN1Identifier)
+    const MN2AlreadyMinedBlocks = await masterNodeProvider2.rpc.masternode.getMasternodeBlocks(MN2Identifier)
+
+    // mine blocks alternatively
+    const numberOfBlockPerMasterNode = 10
+    for (let i = 0; i < numberOfBlockPerMasterNode; i++) {
+      await masterNodeProvider1.container.generate(1)
+      await tGroup.waitForSync()
+      await masterNodeProvider2.container.generate(1)
+      await tGroup.waitForSync()
+    }
+
+    // check both MN1, MN2 on the same tip
+    expect(await masterNodeProvider1.rpc.blockchain.getBestBlockHash()).toStrictEqual(await masterNodeProvider2.rpc.blockchain.getBestBlockHash())
+
+    const MN1MinedBlocks = await masterNodeProvider1.rpc.masternode.getMasternodeBlocks(MN1Identifier)
+    const MN2MinedBlocks = await masterNodeProvider2.rpc.masternode.getMasternodeBlocks(MN2Identifier)
+
+    expect(Object.keys(MN1MinedBlocks).length).toEqual(Object.keys(MN1AlreadyMinedBlocks).length + numberOfBlockPerMasterNode)
+    expect(Object.keys(MN2MinedBlocks).length).toEqual(Object.keys(MN2AlreadyMinedBlocks).length + numberOfBlockPerMasterNode)
+
+    // Query with depth given
+    {
+      const depth = 4
+      const MN1MinedBlocks = await masterNodeProvider1.rpc.masternode.getMasternodeBlocks(MN1Identifier, depth)
+      const MN2MinedBlocks = await masterNodeProvider2.rpc.masternode.getMasternodeBlocks(MN2Identifier, depth)
+
+      // should return last two blocks mined by each MN, within a depth of 4 from the tip.
+      expect(Object.keys(MN1MinedBlocks).length).toEqual(depth / 2)
+      expect(Object.keys(MN2MinedBlocks).length).toEqual(depth / 2)
+    }
   })
 })
