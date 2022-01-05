5a80e937f9f8988d6eafe61f5b38e51a200fe47a
monstrobishi <monstrobishi@gmail.com>
2022-01-05 11:23:09 +0000

    Add annotations to page query

--- apps/ocean-api/src/_core/api.query.ts
+++ apps/ocean-api/src/_core/api.query.ts
@@ -1,5 +1,6 @@
 import { IsInt, IsOptional, IsString, Min } from 'class-validator'
 import { Transform, Type } from 'class-transformer'
+import { ArgsType, Field, Int } from '@nestjs/graphql'
 
 /**
  * Pagination query with
@@ -8,15 +9,20 @@ import { Transform, Type } from 'class-transformer'
  *
  * This provides a singular consistent query pattern for DeFi Whale.
  */
+
+@ArgsType()
 export class PaginationQuery {
   @IsOptional()
   @IsInt()
   @Transform(({ value }) => value > 200 ? 200 : value)
   @Min(1)
   @Type(() => Number)
+  @Field((type) => Int)
   size: number = 30
 
   @IsOptional()
   @IsString()
+
+  @Field((type) => String)
   next?: string
 }
