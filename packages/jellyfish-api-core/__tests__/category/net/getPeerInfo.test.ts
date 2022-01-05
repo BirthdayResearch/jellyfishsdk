44eb27bd2f7115dfddda856d0f1a2551bbf09481
Kodemon <dev@kodemon.net>
2022-01-05 03:12:06 +0000

    update unit tests
     - Account for undefined values
     - Perform key=>value pair checking in 1 schema test instead of 2 (is sort of redundant).

--- packages/jellyfish-api-core/__tests__/category/net/getPeerInfo.test.ts
+++ packages/jellyfish-api-core/__tests__/category/net/getPeerInfo.test.ts
@@ -21,40 +21,44 @@ function createNetworkTests (name: string, container: RegTestContainer | MasterN
       await expect(client.net.getPeerInfo()).resolves.toHaveLength(2)
     })
 
-    it('should return expected schema', async () => {
+    it('should return valid schema', async () => {
       for (const peer of await client.net.getPeerInfo()) {
         expect(typeof peer.id).toStrictEqual('number')
-        expect(typeof peer.addr).toStrictEqual('string')
-        expect(typeof peer.addrbind).toStrictEqual('string')
+
+        expectValidAddress(peer.addr)
+
+        expectUndefinedOrValidAddress(peer.addrbind)
+        expectUndefinedOrValidAddress(peer.addrlocal)
+
         expect(typeof peer.services).toStrictEqual('string')
+        expect(peer.services).toHaveLength(16)
+
         expect(typeof peer.relaytxes).toStrictEqual('boolean')
         expect(typeof peer.lastsend).toStrictEqual('number')
         expect(typeof peer.lastrecv).toStrictEqual('number')
+        expect(typeof peer.bytessent).toStrictEqual('number')
+        expect(typeof peer.bytesrecv).toStrictEqual('number')
         expect(typeof peer.conntime).toStrictEqual('number')
         expect(typeof peer.timeoffset).toStrictEqual('number')
+
+        expectUndefinedOrEquals('number', peer.pingtime)
+        expectUndefinedOrEquals('number', peer.minping)
+        expectUndefinedOrEquals('number', peer.pingwait)
+
         expect(typeof peer.version).toStrictEqual('number')
         expect(typeof peer.subver).toStrictEqual('string')
         expect(typeof peer.inbound).toStrictEqual('boolean')
         expect(typeof peer.addnode).toStrictEqual('boolean')
         expect(typeof peer.startingheight).toStrictEqual('number')
-        expect(typeof peer.banscore).toStrictEqual('number')
-        expect(typeof peer.synced_headers).toStrictEqual('number')
-        expect(typeof peer.synced_blocks).toStrictEqual('number')
+
+        expectUndefinedOrEquals('number', peer.banscore)
+        expectUndefinedOrEquals('number', peer.synced_headers)
+        expectUndefinedOrEquals('number', peer.synced_blocks)
+
         expect(typeof peer.inflight).toStrictEqual('object')
         expect(typeof peer.whitelisted).toStrictEqual('boolean')
         expect(typeof peer.permissions).toStrictEqual('object')
         expect(typeof peer.minfeefilter).toStrictEqual('number')
-        expect(typeof peer.bytessent_per_msg).toStrictEqual('object')
-        expect(typeof peer.bytesrecv_per_msg).toStrictEqual('object')
-      }
-    })
-
-    it('should return valid values', async () => {
-      for (const peer of await client.net.getPeerInfo()) {
-        expectValidAddress(peer.addr)
-        expectValidAddress(peer.addrbind)
-
-        expect(peer.services).toHaveLength(16)
 
         expectValidBytesPerMessage(peer.bytessent_per_msg)
         expectValidBytesPerMessage(peer.bytesrecv_per_msg)
@@ -63,12 +67,25 @@ function createNetworkTests (name: string, container: RegTestContainer | MasterN
   })
 }
 
+function expectUndefinedOrEquals (expected: any, actual?: any): void {
+  if (actual !== undefined) {
+    expect(typeof actual).toStrictEqual(expected)
+  }
+}
+
+function expectUndefinedOrValidAddress (address?: string): void {
+  if (address !== undefined) {
+    expectValidAddress(address)
+  }
+}
+
 function expectValidAddress (address: string): void {
-  return expect(/^(?:[0-9]{1,3}\.){3}[0-9]{1,3}(:[0-9]{1,5})?$/.test(address)).toBeTruthy()
+  expect(typeof address).toStrictEqual('string')
+  expect(/^(?:[0-9]{1,3}\.){3}[0-9]{1,3}(:[0-9]{1,5})?$/.test(address)).toBeTruthy()
 }
 
 function expectValidBytesPerMessage (obj: Record<string, number>): void {
   for (const key in obj) {
-    expect(obj[key]).toBeGreaterThanOrEqual(0)
+    expect(typeof obj[key]).toStrictEqual('number')
   }
 }
