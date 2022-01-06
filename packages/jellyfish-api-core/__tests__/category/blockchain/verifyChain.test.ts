a8dd276f2b3a58cd307c96b67a1fd2ca01695789
Kodemon <dev@kodemon.net>
2022-01-06 05:34:55 +0000

    add additional test cases
    Try to account for each possible level and test for behavior in combination with the amount of blocks being checked.

--- packages/jellyfish-api-core/__tests__/category/blockchain/verifyChain.test.ts
+++ packages/jellyfish-api-core/__tests__/category/blockchain/verifyChain.test.ts
@@ -7,14 +7,35 @@ describe('verifyChain', () => {
 
   beforeAll(async () => {
     await container.start()
-    await container.waitForBlockHeight(3)
+    await container.waitForBlockHeight(10)
   })
 
   afterAll(async () => {
     await container.stop()
   })
 
-  it('should return true for a valid chain', async () => {
+  it('should return true when asking for all blocks for each check level', async () => {
     await expect(client.blockchain.verifyChain()).resolves.toBeTruthy()
+    await expect(client.blockchain.verifyChain(0)).resolves.toBeTruthy()
+    await expect(client.blockchain.verifyChain(1)).resolves.toBeTruthy()
+    await expect(client.blockchain.verifyChain(2)).resolves.toBeTruthy()
+    await expect(client.blockchain.verifyChain(3)).resolves.toBeTruthy()
+    await expect(client.blockchain.verifyChain(4)).resolves.toBeTruthy()
+  })
+
+  it('should return true when asking for 1 block for each check level', async () => {
+    await expect(client.blockchain.verifyChain(0, 1)).resolves.toBeTruthy()
+    await expect(client.blockchain.verifyChain(1, 1)).resolves.toBeTruthy()
+    await expect(client.blockchain.verifyChain(2, 1)).resolves.toBeTruthy()
+    await expect(client.blockchain.verifyChain(3, 1)).resolves.toBeTruthy()
+    await expect(client.blockchain.verifyChain(4, 1)).resolves.toBeTruthy()
+  })
+
+  it('should return true when asking for 12 blocks for each check level', async () => {
+    await expect(client.blockchain.verifyChain(0, 12)).resolves.toBeTruthy()
+    await expect(client.blockchain.verifyChain(1, 12)).resolves.toBeTruthy()
+    await expect(client.blockchain.verifyChain(2, 12)).resolves.toBeTruthy()
+    await expect(client.blockchain.verifyChain(3, 12)).resolves.toBeTruthy()
+    await expect(client.blockchain.verifyChain(4, 12)).resolves.toBeTruthy()
   })
 })
