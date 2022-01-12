0037db79a08f7e4a80660c62676ab011fd697716
Chanaka Sameera <kchanakasameera@gmail.com>
2022-01-12 12:33:46 +0530

    Review update

--- packages/jellyfish-api-core/__tests__/category/masternode/isAppliedCustomTransaction.test.ts
+++ packages/jellyfish-api-core/__tests__/category/masternode/isAppliedCustomTransaction.test.ts
@@ -3,20 +3,18 @@ import { MasterNodeRegTestContainer } from '@defichain/testcontainers'
 
 describe('Masternode', () => {
   const testing = Testing.create(new MasterNodeRegTestContainer())
-  const container = testing.container
-  const client = testing.rpc
 
   beforeAll(async () => {
-    await container.start()
-    await container.waitForWalletCoinbaseMaturity()
+    await testing.container.start()
+    await testing.container.waitForWalletCoinbaseMaturity()
   })
 
   afterAll(async () => {
-    await container.stop()
+    await testing.container.stop()
   })
 
   it('should be passed while using valid id and height', async () => {
-    const goldAddress = await container.getNewAddress('', 'legacy')
+    const goldAddress = await testing.container.getNewAddress('', 'legacy')
     const goldMetadata = {
       symbol: 'GOLD',
       name: 'shiny gold',
@@ -25,11 +23,11 @@ describe('Masternode', () => {
       tradeable: true,
       collateralAddress: goldAddress
     }
-    const goldTokenId = await client.token.createToken(goldMetadata)
-    await container.generate(1)
-    const goldHeight = await client.blockchain.getBlockCount()
+    const goldTokenId = await testing.rpc.token.createToken(goldMetadata)
+    await testing.container.generate(1)
+    const goldHeight = await testing.rpc.blockchain.getBlockCount()
 
-    const silverAddress = await container.getNewAddress('', 'legacy')
+    const silverAddress = await testing.container.getNewAddress('', 'legacy')
     const silverMetadata = {
       symbol: 'SILVER',
       name: 'just silver',
@@ -38,11 +36,11 @@ describe('Masternode', () => {
       tradeable: true,
       collateralAddress: silverAddress
     }
-    const silverTokenId = await client.token.createToken(silverMetadata)
-    await container.generate(1)
-    const silverHeight = await client.blockchain.getBlockCount()
+    const silverTokenId = await testing.rpc.token.createToken(silverMetadata)
+    await testing.container.generate(1)
+    const silverHeight = await testing.rpc.blockchain.getBlockCount()
 
-    const copperAddress = await container.getNewAddress('', 'legacy')
+    const copperAddress = await testing.container.getNewAddress('', 'legacy')
     const copperMetadata = {
       symbol: 'COPPER',
       name: 'just copper',
@@ -51,22 +49,22 @@ describe('Masternode', () => {
       tradeable: true,
       collateralAddress: copperAddress
     }
-    const copperTokenId = await client.token.createToken(copperMetadata)
-    await container.generate(1)
-    const copperHeight = await client.blockchain.getBlockCount()
+    const copperTokenId = await testing.rpc.token.createToken(copperMetadata)
+    await testing.container.generate(1)
+    const copperHeight = await testing.rpc.blockchain.getBlockCount()
 
-    const goldResult = await client.masternode.isAppliedCustomTransaction(goldTokenId, goldHeight)
+    const goldResult = await testing.rpc.masternode.isAppliedCustomTransaction(goldTokenId, goldHeight)
     expect(goldResult).toStrictEqual(true)
 
-    const silverResult = await client.masternode.isAppliedCustomTransaction(silverTokenId, silverHeight)
+    const silverResult = await testing.rpc.masternode.isAppliedCustomTransaction(silverTokenId, silverHeight)
     expect(silverResult).toStrictEqual(true)
 
-    const copperResult = await client.masternode.isAppliedCustomTransaction(copperTokenId, copperHeight)
+    const copperResult = await testing.rpc.masternode.isAppliedCustomTransaction(copperTokenId, copperHeight)
     expect(copperResult).toStrictEqual(true)
   })
 
   it('should be failed while using invalid height', async () => {
-    const brassAddress = await container.getNewAddress('', 'legacy')
+    const brassAddress = await testing.container.getNewAddress('', 'legacy')
     const brassMetadata = {
       symbol: 'BRASS',
       name: 'shiny brass',
@@ -75,23 +73,23 @@ describe('Masternode', () => {
       tradeable: true,
       collateralAddress: brassAddress
     }
-    const brassTokenId = await client.token.createToken(brassMetadata)
-    await container.generate(1)
-    const brassHeight = await client.blockchain.getBlockCount()
+    const brassTokenId = await testing.rpc.token.createToken(brassMetadata)
+    await testing.container.generate(1)
+    const brassHeight = await testing.rpc.blockchain.getBlockCount()
 
-    const brassResult = await client.masternode.isAppliedCustomTransaction(brassTokenId, brassHeight + 1)
+    const brassResult = await testing.rpc.masternode.isAppliedCustomTransaction(brassTokenId, brassHeight + 1)
     expect(brassResult).toStrictEqual(false)
   })
 
   it('should be failed while using invalid id', async () => {
-    const blockHeight = await client.blockchain.getBlockCount()
+    const blockHeight = await testing.rpc.blockchain.getBlockCount()
 
-    const result = await client.masternode.isAppliedCustomTransaction('b2bb09ffe9f9b292f13d23bafa1225ef26d0b9906da7af194c5738b63839b235', blockHeight)
+    const result = await testing.rpc.masternode.isAppliedCustomTransaction('b2bb09ffe9f9b292f13d23bafa1225ef26d0b9906da7af194c5738b63839b235', blockHeight)
     expect(result).toStrictEqual(false)
 
     // Hex hash id with 63 chars
     try {
-      await client.masternode.isAppliedCustomTransaction('2bb09ffe9f9b292f13d23bafa1225ef26d0b9906da7af194c5738b63839b235', blockHeight)
+      await testing.rpc.masternode.isAppliedCustomTransaction('2bb09ffe9f9b292f13d23bafa1225ef26d0b9906da7af194c5738b63839b235', blockHeight)
       throw new Error('It should not reach here')
     } catch (error: any) {
       expect(error.message).toContain('must be of length 64')
@@ -99,7 +97,7 @@ describe('Masternode', () => {
 
     // Invalid hash id with a non hex char
     try {
-      await client.masternode.isAppliedCustomTransaction('b2bb09ffe9f9b292f13d23bafa1225ef26d0b9906da7af194c5738b63839b23z', blockHeight)
+      await testing.rpc.masternode.isAppliedCustomTransaction('b2bb09ffe9f9b292f13d23bafa1225ef26d0b9906da7af194c5738b63839b23z', blockHeight)
       throw new Error('It should not reach here')
     } catch (error: any) {
       expect(error.message).toContain('must be hexadecimal string')
