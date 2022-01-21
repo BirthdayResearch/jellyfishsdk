89d3ff4ebeac75955107320a05f2e09588875d39
websterlcl <w.canonbrother@gmail.com>
2022-01-21 15:32:31 +0800

    added rpcctrl test

---
+++ apps/playground-api/__tests__/controllers/RpcController.test.ts
@@ -0,0 +1,22 @@
+import { PlaygroundApiTesting } from '../../testing/PlaygroundApiTesting'
+
+const apiTesting = PlaygroundApiTesting.create()
+
+beforeAll(async () => {
+  await apiTesting.start()
+  await apiTesting.container.waitForBlockHeight(100)
+})
+
+afterAll(async () => {
+  await apiTesting.stop()
+})
+
+it('should be able call rpc getblockcount', async () => {
+  const res: any = await apiTesting.client.rpc.call('getblockcount', [], 'number')
+  expect(res.value).toBeGreaterThanOrEqual(100)
+})
+
+it('should be blacklisted', async () => {
+  const promise = apiTesting.client.rpc.call('importprivkey', ['0'.repeat(52)], 'number')
+  await expect(promise).rejects.toThrow('RPC method is blacklisted')
+})
