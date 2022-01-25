94097c21b6a3baf3898c9052baf4022b8b0ef0f0
chee-chyuan <angchee@live.com.my>
2022-01-25 09:27:56 +0800

    update image

--- packages/testcontainers/src/containers/DeFiDContainer.ts
+++ packages/testcontainers/src/containers/DeFiDContainer.ts
@@ -35,7 +35,7 @@ export abstract class DeFiDContainer extends DockerContainer {
     if (process?.env?.DEFICHAIN_DOCKER_IMAGE !== undefined) {
       return process.env.DEFICHAIN_DOCKER_IMAGE
     }
-    return 'defi/defichain:HEAD-3d68224'
+    return 'defi/defichain:master-4456bd5'
   }
 
   public static readonly DefaultStartOptions = {
