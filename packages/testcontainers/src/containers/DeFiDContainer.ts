bbe853a49938814d4a1939d68fbef558c8456c1b
chee-chyuan <angchee@live.com.my>
2022-01-27 17:56:02 +0800

    chage image

--- packages/testcontainers/src/containers/DeFiDContainer.ts
+++ packages/testcontainers/src/containers/DeFiDContainer.ts
@@ -29,7 +29,7 @@ export abstract class DeFiDContainer extends DockerContainer {
     if (process?.env?.DEFICHAIN_DOCKER_IMAGE !== undefined) {
       return process.env.DEFICHAIN_DOCKER_IMAGE
     }
-    return 'defi/defichain:master-a6537c0'
+    return 'defi/defichain:HEAD-87ace32'
   }
 
   public static readonly DefaultStartOptions = {
