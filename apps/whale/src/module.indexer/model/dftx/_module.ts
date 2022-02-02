776c121cf3f28e18daa21beb20b3408bdf01d61c
Ivan Lee <44192990+ivan-zynesis@users.noreply.github.com>
2022-02-03 00:14:50 +0800

    Drop unused/unexposed database and indexer (https://github.com/DeFiCh/whale/pull/690)
    * quick save
    
    * drop all token mapper
    
    * remove ununsed indexer import

--- apps/whale/src/module.indexer/model/dftx/_module.ts
+++ apps/whale/src/module.indexer/model/dftx/_module.ts
@@ -6,12 +6,8 @@ import { SetOracleDataIndexer } from '@src/module.indexer/model/dftx/set.oracle.
 import { SetOracleDataIntervalIndexer } from '@src/module.indexer/model/dftx/set.oracle.data.interval'
 import { CreateMasternodeIndexer } from '@src/module.indexer/model/dftx/create.masternode'
 import { ResignMasternodeIndexer } from '@src/module.indexer/model/dftx/resign.masternode'
-import { CreateTokenIndexer } from '@src/module.indexer/model/dftx/create.token'
-import { CreatePoolPairIndexer } from '@src/module.indexer/model/dftx/create.poolpair'
-import { UpdatePoolPairIndexer } from '@src/module.indexer/model/dftx/update.poolpair'
 import { NetworkName } from '@defichain/jellyfish-network'
 import { ConfigService } from '@nestjs/config'
-import { SetLoanTokenIndexer } from './set.loan.token'
 import { ActivePriceIndexer } from './active.price'
 import { PlaceAuctionBidIndexer } from './place.auction.bid'
 
@@ -23,10 +19,6 @@ const indexers = [
   CreateMasternodeIndexer,
   ResignMasternodeIndexer,
   SetOracleDataIntervalIndexer,
-  CreateTokenIndexer,
-  CreatePoolPairIndexer,
-  UpdatePoolPairIndexer,
-  SetLoanTokenIndexer,
   ActivePriceIndexer,
   PlaceAuctionBidIndexer
 ]
