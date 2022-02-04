565dbccc7513f9f8b455fd3d22e24cfb73da7564
Joel-David Wong <git@joeldavid.dev>
2022-02-04 17:25:47 +0800

    WIP: validateAddress

--- apps/legacy-api/src/controllers/MiscController.ts
+++ apps/legacy-api/src/controllers/MiscController.ts
@@ -1,6 +1,7 @@
 import { Controller, Get, Query } from '@nestjs/common'
 import { WhaleApiClient } from '@defichain/whale-api-client'
 import { StatsData } from '@defichain/whale-api-client/dist/api/stats'
+import { fromAddress } from '@defichain/jellyfish-address'
 
 // For current Stats api endpoints: https://github.com/cakedefi/defi-stats-api/blob/ef46b74cc929003eb72fc39942049efc8681bf66/src/config/v1/v1.controller.ts
 
@@ -8,7 +9,7 @@ import { StatsData } from '@defichain/whale-api-client/dist/api/stats'
 export class MiscController {
   @Get('getblockcount')
   async getToken (
-    @Query('network') network: string = 'mainnet'
+    @Query('network') network: 'mainnet' | 'testnet' | 'regtest' = 'mainnet'
   ): Promise<{ [key: string]: Number }> {
     const api = new WhaleApiClient({
       version: 'v0',
@@ -19,6 +20,17 @@ export class MiscController {
     const data: StatsData = await api.stats.get()
     return {
       data: data.count.blocks
+    }
+  }
+
+  @Get('validateAddress')
+  async validateAddress (
+    @Query('network') network: 'mainnet' | 'testnet' | 'regtest' = 'mainnet',
+    @Query('address') address: string
+  ): Promise<{ [key: string]: Number }> {
+    console.log(fromAddress(address, network))
+    return {
+      data: 0
     }
   }
 }
