5f7cfffd7afc2c038ad941f6b9fb4165eb160eb5
Joel-David Wong <git@joeldavid.dev>
2022-02-04 17:22:54 +0800

    Added legacy-api with tests

---
+++ apps/legacy-api/src/controllers/MiscController.ts
@@ -0,0 +1,24 @@
+import { Controller, Get, Query } from '@nestjs/common'
+import { WhaleApiClient } from '@defichain/whale-api-client'
+import { StatsData } from '@defichain/whale-api-client/dist/api/stats'
+
+// For current Stats api endpoints: https://github.com/cakedefi/defi-stats-api/blob/ef46b74cc929003eb72fc39942049efc8681bf66/src/config/v1/v1.controller.ts
+
+@Controller('v1')
+export class MiscController {
+  @Get('getblockcount')
+  async getToken (
+    @Query('network') network: string = 'mainnet'
+  ): Promise<{ [key: string]: Number }> {
+    const api = new WhaleApiClient({
+      version: 'v0',
+      network: network,
+      url: 'https://ocean.defichain.com'
+    })
+
+    const data: StatsData = await api.stats.get()
+    return {
+      data: data.count.blocks
+    }
+  }
+}
---
+++ apps/legacy-api/src/controllers/TokenController.ts
@@ -0,0 +1,87 @@
+import { Controller, Get, Query } from '@nestjs/common'
+import { WhaleApiClient } from '@defichain/whale-api-client'
+import { TokenData } from '@defichain/whale-api-client/dist/api/tokens'
+
+// For current Stats api endpoints: https://github.com/cakedefi/defi-stats-api/blob/ef46b74cc929003eb72fc39942049efc8681bf66/src/config/v1/v1.controller.ts
+
+@Controller('v1')
+export class TokenController {
+  @Get('gettoken')
+  async getToken (
+    @Query('network') network: string = 'mainnet',
+    @Query('id') tokenId: string
+  ): Promise<{ [key: string]: LegacyTokenData }> {
+    const api = new WhaleApiClient({
+      version: 'v0',
+      network: network,
+      url: 'https://ocean.defichain.com'
+    })
+
+    const data = await api.tokens.get(tokenId)
+    return {
+      [data.id]: reformatTokenData(data)
+    }
+  }
+
+  @Get('listtokens')
+  async listTokens (
+    @Query('network') network: string = 'mainnet',
+    @Query('id') tokenId: string
+  ): Promise<{ [key: string]: LegacyTokenData }> {
+    const api = new WhaleApiClient({
+      version: 'v0',
+      network: network,
+      url: 'https://ocean.defichain.com'
+    })
+
+    const data: TokenData[] = await api.tokens.list(200)
+
+    const results: { [key: string]: LegacyTokenData } = {}
+    data.forEach(token => {
+      results[token.id] = reformatTokenData(token)
+    })
+    return results
+  }
+}
+
+interface LegacyTokenData {
+  'symbol': string
+  'symbolKey': string
+  'name': string
+  'decimal': number
+  'limit': number
+  'mintable': boolean
+  'tradeable': boolean
+  'isDAT': boolean
+  'isLPS': boolean
+  'finalized': boolean
+  'isLoanToken': boolean
+  'minted': number
+  'creationTx': string
+  'creationHeight': number
+  'destructionTx': string
+  'destructionHeight': number
+  'collateralAddress': string | undefined
+}
+
+function reformatTokenData (data: TokenData): LegacyTokenData {
+  return {
+    symbol: data.symbol,
+    symbolKey: data.symbolKey,
+    name: data.name,
+    decimal: data.decimal,
+    limit: Number(data.limit),
+    mintable: data.mintable,
+    tradeable: data.tradeable,
+    isDAT: data.isDAT,
+    isLPS: data.isLPS,
+    finalized: data.finalized,
+    isLoanToken: data.isLoanToken,
+    minted: Number(data.minted),
+    creationTx: data.creation.tx,
+    creationHeight: data.creation.height,
+    destructionTx: data.destruction.tx,
+    destructionHeight: data.destruction.height,
+    collateralAddress: data.collateralAddress
+  }
+}
