ab41055dfd8ea4a589e86d521b594d7fc9806020
Chanaka Sameera <kchanakasameera@gmail.com>
2022-02-11 10:36:47 +0530

    Updated test case to use jellyfish-testing

--- packages/jellyfish-api-core/__tests__/category/account/getAccountHistory.test.ts
+++ packages/jellyfish-api-core/__tests__/category/account/getAccountHistory.test.ts
@@ -1,5 +1,5 @@
 import { MasterNodeRegTestContainer } from '@defichain/testcontainers'
-import { ContainerAdapterClient } from '../../container_adapter_client'
+import { Testing } from '@defichain/jellyfish-testing'
 import waitForExpect from 'wait-for-expect'
 import { RpcApiError } from '@defichain/jellyfish-api-core'
 
@@ -22,31 +22,30 @@ function createTokenForContainer (container: MasterNodeRegTestContainer) {
 }
 
 describe('Account', () => {
-  const container = new MasterNodeRegTestContainer()
-  const client = new ContainerAdapterClient(container)
-  const createToken = createTokenForContainer(container)
+  const testing = Testing.create(new MasterNodeRegTestContainer())
+  const createToken = createTokenForContainer(testing.container)
 
   beforeAll(async () => {
-    await container.start()
-    await container.waitForWalletCoinbaseMaturity()
-    await container.waitForWalletBalanceGTE(100)
-    await createToken(await container.getNewAddress(), 'DBTC', 200)
+    await testing.container.start()
+    await testing.container.waitForWalletCoinbaseMaturity()
+    await testing.container.waitForWalletBalanceGTE(100)
+    await createToken(await testing.container.getNewAddress(), 'DBTC', 200)
   })
 
   afterAll(async () => {
-    await container.stop()
+    await testing.container.stop()
   })
 
   it('should getAccountHistory', async () => {
     await waitForExpect(async () => {
-      const accountHistories = await client.account.listAccountHistory()
+      const accountHistories = await testing.rpc.account.listAccountHistory()
       expect(accountHistories.length).toBeGreaterThan(0)
     })
 
-    const accountHistories = await client.account.listAccountHistory()
+    const accountHistories = await testing.rpc.account.listAccountHistory()
     const referenceHistory = accountHistories[0]
 
-    const history = await client.account.getAccountHistory(referenceHistory.owner, referenceHistory.blockHeight, referenceHistory.txn)
+    const history = await testing.rpc.account.getAccountHistory(referenceHistory.owner, referenceHistory.blockHeight, referenceHistory.txn)
     expect(history.owner).toStrictEqual(referenceHistory.owner)
     expect(history.blockHeight).toStrictEqual(referenceHistory.blockHeight)
     expect(history.txn).toStrictEqual(referenceHistory.txn)
@@ -56,30 +55,30 @@ describe('Account', () => {
 
   it('should not getAccountHistory', async () => {
     await waitForExpect(async () => {
-      const accountHistories = await client.account.listAccountHistory()
+      const accountHistories = await testing.rpc.account.listAccountHistory()
       expect(accountHistories.length).toBeGreaterThan(0)
     })
 
-    const accountHistories = await client.account.listAccountHistory()
+    const accountHistories = await testing.rpc.account.listAccountHistory()
     const referenceHistory = accountHistories[0]
 
-    let history = await client.account.getAccountHistory(referenceHistory.owner, referenceHistory.blockHeight - 1, referenceHistory.txn)
+    let history = await testing.rpc.account.getAccountHistory(referenceHistory.owner, referenceHistory.blockHeight - 1, referenceHistory.txn)
     expect(history.owner).toBeUndefined()
 
-    history = await client.account.getAccountHistory(referenceHistory.owner, referenceHistory.blockHeight, referenceHistory.txn - 1)
+    history = await testing.rpc.account.getAccountHistory(referenceHistory.owner, referenceHistory.blockHeight, referenceHistory.txn - 1)
     expect(history.owner).toBeUndefined()
 
-    history = await client.account.getAccountHistory('mrLrCt3kMFhhfYeT8euQw5ERwyvFg9K21j', referenceHistory.blockHeight, referenceHistory.txn - 1)
+    history = await testing.rpc.account.getAccountHistory('mrLrCt3kMFhhfYeT8euQw5ERwyvFg9K21j', referenceHistory.blockHeight, referenceHistory.txn - 1)
     expect(history.owner).toBeUndefined()
 
-    const historyPromise = client.account.getAccountHistory('mrLrCt3kMFhhfYeT8euQw5ERw', referenceHistory.blockHeight, referenceHistory.txn - 1)
+    const historyPromise = testing.rpc.account.getAccountHistory('mrLrCt3kMFhhfYeT8euQw5ERw', referenceHistory.blockHeight, referenceHistory.txn - 1)
     await expect(historyPromise).rejects.toThrow(RpcApiError)
     await expect(historyPromise).rejects.toThrow('does not refer to any valid address')
 
-    history = await client.account.getAccountHistory(referenceHistory.owner, -103, referenceHistory.txn)
+    history = await testing.rpc.account.getAccountHistory(referenceHistory.owner, -103, referenceHistory.txn)
     expect(history.owner).toBeUndefined()
 
-    history = await client.account.getAccountHistory(referenceHistory.owner, referenceHistory.blockHeight, -1)
+    history = await testing.rpc.account.getAccountHistory(referenceHistory.owner, referenceHistory.blockHeight, -1)
     expect(history.owner).toBeUndefined()
   })
 })
