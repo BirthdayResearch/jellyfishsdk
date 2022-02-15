32b8de413521ca9cb4e0b31737d587a9205f4f27
elihuansen <elihuansen@gmail.com>
2022-02-15 08:56:16 +0800

    perf(jellyfish-api-core): WhaleApiClient caching

--- apps/legacy-api/src/modules/ControllerModule.ts
+++ apps/legacy-api/src/modules/ControllerModule.ts
@@ -2,6 +2,7 @@ import { CacheModule, Module } from '@nestjs/common'
 import { TokenController } from '../controllers/TokenController'
 import { PoolPairController } from '../controllers/PoolPairController'
 import { MiscController } from '../controllers/MiscController'
+import { WhaleApiClientProvider } from '../providers/WhaleApiClientProvider'
 
 /**
  * Exposed ApiModule for public interfacing
@@ -14,6 +15,9 @@ import { MiscController } from '../controllers/MiscController'
     TokenController,
     PoolPairController,
     MiscController
+  ],
+  providers: [
+    WhaleApiClientProvider
   ]
 })
 export class ControllerModule {
