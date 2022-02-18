e2d0002157240dba7df453af0d7148f1337b8101
Ivan Lee <44192990+ivan-zynesis@users.noreply.github.com>
2022-02-18 14:28:46 +0800

    feat(`rich-list-api`): initialization (#1080)
    * first commit, initialize new app with some spec narrative
    
    * add missing build config
    
    * remove raw tx controller

---
+++ apps/rich-list-api/src/modules/ControllerModule.ts
@@ -0,0 +1,43 @@
+import { CacheModule, Module } from '@nestjs/common'
+import { ActuatorController } from '../controllers/ActuatorController'
+import { APP_FILTER, APP_INTERCEPTOR, APP_PIPE } from '@nestjs/core'
+import { GlobalValidationPipe } from '../controllers/filters/GlobalValidationPipe'
+import { ResponseInterceptor } from '../controllers/filters/ResponseInterceptor'
+import { ErrorFilter } from '../controllers/filters/ErrorFilter'
+import { ConfigService } from '@nestjs/config'
+import { NetworkName } from '@defichain/jellyfish-network'
+
+/**
+ * Exposed ApiModule for public interfacing
+ */
+@Module({
+  imports: [
+    CacheModule.register()
+  ],
+  controllers: [
+    ActuatorController
+  ],
+  providers: [
+    {
+      provide: APP_PIPE,
+      useClass: GlobalValidationPipe
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: ResponseInterceptor
+    },
+    {
+      provide: APP_FILTER,
+      useClass: ErrorFilter
+    },
+    {
+      provide: 'NETWORK_NAME',
+      useFactory: (configService: ConfigService): NetworkName => {
+        return configService.get<string>('network') as NetworkName
+      },
+      inject: [ConfigService]
+    }
+  ]
+})
+export class ControllerModule {
+}
---
+++ apps/rich-list-api/src/modules/RootModule.ts
@@ -0,0 +1,53 @@
+import * as Joi from 'joi'
+import { Module } from '@nestjs/common'
+import { ConfigModule, ConfigService } from '@nestjs/config'
+import { ControllerModule } from './ControllerModule'
+import { BlockchainCppModule } from './BlockchainCppModule'
+import { ActuatorModule } from './ActuatorModule'
+import { TypeOrmModule } from '@nestjs/typeorm'
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      cache: true,
+      validationSchema: ENV_VALIDATION_SCHEMA()
+    }),
+    ActuatorModule,
+    TypeOrmModule.forRootAsync({
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('POSTGRES_HOST'),
+        port: configService.get<number>('POSTGRES_PORT'),
+        username: configService.get<string>('POSTGRES_USER'),
+        password: configService.get<string>('POSTGRES_PASS'),
+        database: configService.get<string>('POSTGRES_DB'),
+        migrations: [/* TODO(@ivan-zynesis): create table if not exists */],
+        entities: [/* TODO */],
+        synchronize: false
+      }),
+      inject: [ConfigService]
+    }),
+    // QueueModule,
+    BlockchainCppModule,
+    ControllerModule
+  ]
+})
+export class RootModule {
+}
+
+function ENV_VALIDATION_SCHEMA (): any {
+  return Joi.object({
+    NODE_ENV: Joi.string().valid('production', 'test').default('test'),
+    PORT: Joi.number().default(3000),
+    API_VERSION: Joi.string().regex(/^v[0-9]+(\.[0-9]+)?$/).default('v1'),
+    API_NETWORK: Joi.string().valid('regtest', 'testnet', 'mainnet').default('regtest'),
+    PLAYGROUND_ENABLE: Joi.boolean(),
+
+    POSTGRES_HOST: Joi.string().ip(),
+    POSTGRES_PORT: Joi.number().default(5432),
+    POSTGRES_USER: Joi.string(),
+    POSTGRES_PASS: Joi.string(),
+    POSTGRES_DB: Joi.string()
+  })
+}
