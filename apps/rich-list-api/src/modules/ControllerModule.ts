5046a09a54247a3de120c7f777dce4556e92389b
ivan-zynesis <ivan@zynesis.com>
2022-02-18 11:47:07 +0800

    first commit, initialize new app with some spec narrative

---
+++ apps/rich-list-api/src/modules/ControllerModule.ts
@@ -0,0 +1,45 @@
+import { CacheModule, Module } from '@nestjs/common'
+import { ActuatorController } from '../controllers/ActuatorController'
+import { APP_FILTER, APP_INTERCEPTOR, APP_PIPE } from '@nestjs/core'
+import { GlobalValidationPipe } from '../controllers/filters/GlobalValidationPipe'
+import { ResponseInterceptor } from '../controllers/filters/ResponseInterceptor'
+import { ErrorFilter } from '../controllers/filters/ErrorFilter'
+import { ConfigService } from '@nestjs/config'
+import { NetworkName } from '@defichain/jellyfish-network'
+import { RawTxController } from '../controllers/RawTxController'
+
+/**
+ * Exposed ApiModule for public interfacing
+ */
+@Module({
+  imports: [
+    CacheModule.register()
+  ],
+  controllers: [
+    ActuatorController,
+    RawTxController
+  ],
+  providers: [
+    {
+      provide: APP_PIPE,
+      useClass: GlobalValidationPipe
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: ResponseInterceptor
+    },
+    {
+      provide: APP_FILTER,
+      useClass: ErrorFilter
+    },
+    {
+      provide: 'NETWORK_NAME',
+      useFactory: (configService: ConfigService): NetworkName => {
+        return configService.get<string>('network') as NetworkName
+      },
+      inject: [ConfigService]
+    }
+  ]
+})
+export class ControllerModule {
+}
