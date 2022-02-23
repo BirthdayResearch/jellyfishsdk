09964402230eed45cd28544988dd584e34beb0e5
ivan-zynesis <ivan@zynesis.com>
2022-02-23 15:28:48 +0800

    simplify syntax, fix dumb mistake

--- packages/rich-list-core/src/RichListCore.ts
+++ packages/rich-list-core/src/RichListCore.ts
@@ -1,5 +1,5 @@
 import { ApiClient, BigNumber, blockchain as defid } from '@defichain/jellyfish-api-core'
-import { QueueClient } from './lib/Queue'
+import { QueueClient, Queue } from './lib/Queue'
 import { PersistentStorage } from './lib/PersistentStorage'
 import { AddressParser } from './controller/AddressParser'
 import { NetworkName } from '@defichain/jellyfish-network'
@@ -20,7 +20,8 @@ export class RichListCore {
     private readonly apiClient: ApiClient,
     private readonly existingRichList: PersistentStorage<number, RichListItem[]>,
     private readonly queueClient: QueueClient<string>,
-    private readonly crawledBlocks: PersistentLinkedList<BlockDroppedOutAddresses>
+    private readonly crawledBlocks: PersistentLinkedList<string>,
+    private readonly droppedFromRichList: PersistentLinkedList<string[]>
   ) {
     this.addressParser = new AddressParser(apiClient, network)
   }
@@ -47,26 +48,27 @@ export class RichListCore {
     }
 
     const lastHashed = await this.crawledBlocks.getLast()
-    if (lastHashed !== undefined && lastHashed.hash !== nextBlock.previousblockhash) {
+    if (lastHashed !== undefined && lastHashed !== nextBlock.previousblockhash) {
       // TODO(@ivan-zynesis): invalidate()
     } else {
-      const queue = await this.queueClient.createQueueIfNotExist(this._queueName(), 'LIFO')
-
-      const addresses: string[] = []
-      for (const tx of nextBlock.tx) {
-        const addresses = await this.addressParser.parse(tx)
-        for (const a of addresses) {
-          addresses.push(a)
-          await queue.push(a)
-        }
+      const queue = await this._addressQueue()
+      const _addresses = await this._getAddresses(nextBlock)
+      for (const a of _addresses) {
+        await queue.push(a)
       }
-      await this.crawledBlocks.append({
-        hash: nextBlock.hash,
-        richListDroppedOut: addresses
-      })
+      await this.crawledBlocks.append(nextBlock.hash)
     }
 
     return await this._catchUp()
+  }
+
+  private async _getAddresses (block: defid.Block<defid.Transaction>): Promise<string[]> {
+    const _addresses = []
+    for (const tx of block.tx) {
+      const addresses = await this.addressParser.parse(tx)
+      _addresses.push(...addresses)
+    }
+    return _addresses
   }
 
   private async _getBlock (height: number): Promise<defid.Block<defid.Transaction> | undefined> {
@@ -136,7 +138,7 @@ export class RichListCore {
   }
 
   private async _getActiveAddressBalances (tokens: number[], queuedAddressLimit: number): Promise<ActiveAddressAccountAmount> {
-    const queue = await this.queueClient.createQueueIfNotExist(this._queueName(), 'LIFO')
+    const queue = await this._addressQueue()
     const addresses = await queue.receive(queuedAddressLimit)
 
     const balances: { [key: string]: AccountAmount } = {}
@@ -166,9 +168,8 @@ export class RichListCore {
   private _queueName (): string {
     return 'RichListCore_ACTIVE_ADDRESSES'
   }
-}
 
-export interface BlockDroppedOutAddresses {
-  hash: string
-  richListDroppedOut: string[]
+  private async _addressQueue (): Promise<Queue<string>> {
+    return await this.queueClient.createQueueIfNotExist('RichListCore_ACTIVE_ADDRESSES', 'LIFO')
+  }
 }
