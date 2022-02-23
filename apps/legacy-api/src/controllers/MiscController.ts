a9651af0bc306a2e7e4e4a0e79df51659aa00593
elihuansen <elihuansen@gmail.com>
2022-02-23 15:14:42 +0800

    feat(apps/legacy-api): Legacy API - validateaddress and getgov
    - Added NotFoundException that mimics current behaviour

--- apps/legacy-api/src/controllers/MiscController.ts
+++ apps/legacy-api/src/controllers/MiscController.ts
@@ -1,11 +1,13 @@
-import { Controller, Get, Query } from '@nestjs/common'
+import { Controller, Get, NotFoundException, Query } from '@nestjs/common'
 import { StatsData } from '@defichain/whale-api-client/dist/api/stats'
 import { WhaleApiClientProvider } from '../providers/WhaleApiClientProvider'
 import { NetworkValidationPipe, SupportedNetwork } from '../pipes/NetworkValidationPipe'
+import { ValidateAddressResult } from '@defichain/jellyfish-api-core/src/category/wallet'
 
 @Controller('v1')
 export class MiscController {
-  constructor (private readonly whaleApiClientProvider: WhaleApiClientProvider) {}
+  constructor (private readonly whaleApiClientProvider: WhaleApiClientProvider) {
+  }
 
   @Get('getblockcount')
   async getToken (
@@ -17,5 +19,42 @@ export class MiscController {
     return {
       data: data.count.blocks
     }
+  }
+
+  @Get('validateaddress')
+  async validateAddress (
+    @Query('network', NetworkValidationPipe) network: SupportedNetwork = 'mainnet',
+    @Query('address') address: string
+  ): Promise<{ data: Partial<ValidateAddressResult> }> {
+    const api = this.whaleApiClientProvider.getClient(network)
+    const rpcResult = await api.rpc.call<ValidateAddressResult>('validateaddress', [address], 'number')
+
+    const response: Partial<ValidateAddressResult> = { isvalid: rpcResult.isvalid }
+    if (rpcResult.isvalid) {
+      response.isvalid = rpcResult.isvalid
+      response.address = rpcResult.address
+      response.scriptPubKey = rpcResult.scriptPubKey
+      response.isscript = rpcResult.isscript
+      response.iswitness = rpcResult.iswitness
+    }
+
+    return { data: response }
+  }
+
+  @Get('getgov')
+  async getGov (
+    @Query('network', NetworkValidationPipe) network: SupportedNetwork = 'mainnet',
+    @Query('name') name: 'LP_DAILY_DFI_REWARD' | 'LP_SPLITS'
+  ): Promise<Record<string, any>> {
+    const api = this.whaleApiClientProvider.getClient(network)
+
+    if (name !== 'LP_DAILY_DFI_REWARD' && name !== 'LP_SPLITS') {
+      throw new NotFoundException()
+    }
+
+    const rpcResult = await api.rpc.call<Record<string, any>>('getgov', [name], 'bignumber')
+
+    // e.g. { "LP_DAILY_DFI_REWARD": "123" }
+    return { [name]: rpcResult[name] }
   }
 }
