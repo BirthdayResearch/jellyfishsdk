bb98451eecfc75f2145f56a5eb026700bf4bd97f
Chanaka Sameera <kchanakasameera@gmail.com>
2022-02-25 21:47:05 +0530

    CWalletTx.nIndex is used as txn instead of entry.vout

--- packages/testcontainers/src/containers/DeFiDContainer.ts
+++ packages/testcontainers/src/containers/DeFiDContainer.ts
@@ -35,7 +35,7 @@ export abstract class DeFiDContainer extends DockerContainer {
     if (process?.env?.DEFICHAIN_DOCKER_IMAGE !== undefined) {
       return process.env.DEFICHAIN_DOCKER_IMAGE
     }
-    return 'defi/defichain:HEAD-cb91c82'
+    return 'defi/defichain:HEAD-c3174ce'
   }
 
   public static readonly DefaultStartOptions = {
