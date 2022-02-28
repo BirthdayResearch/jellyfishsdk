71ee1179b89b62a33c4f837d41b6c53109e0b93e
Kodemon <dev@kodemon.net>
2022-02-28 15:30:48 +0900

    add report script
    Since the build works in a matrix I have yet to find a clean solution to a concatenated output that can be used in subsequent job to produce a nice formatted message.
    
    This solution simply adds a script that creates QOL tags in the same job rather than relying on previous jobs.

---
+++ .github/scripts/release-report.js
@@ -0,0 +1,25 @@
+/**
+ * Release Report
+ * 
+ * Generate a release message in PRs reporting the success of a new build
+ * along with some quality of life links.
+ * 
+ */
+
+ const apps = process.env.APPS.split(",")
+
+ module.exports = ({ context }) => {
+   const links = getContainerLinks(context)
+   return `
+   Build preview for jellyfish apps is ready!
+             
+   Built with commit ${ context.sha }
+ 
+   ${links.join("\n")}
+   `
+ }
+ 
+ function getContainerLinks({ payload: { number } }) {
+   return apps.map(app => `https://ghcr.io/defich/${app}:pr-${number}`)
+ }
+ 
