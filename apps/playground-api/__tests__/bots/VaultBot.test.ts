6415ec1a07cc99d2f8d273885d1ac9f5fc43de65
websterlcl <w.canonbrother@gmail.com>
2022-03-01 23:49:59 +0800

    fix vaultbot.test.ts

--- apps/playground-api/__tests__/bots/VaultBot.test.ts
+++ apps/playground-api/__tests__/bots/VaultBot.test.ts
@@ -1,8 +1,6 @@
 import { PlaygroundApiTesting } from '../../testing/PlaygroundApiTesting'
 import waitForExpect from 'wait-for-expect'
 import { PlaygroundProbeIndicator } from '../../src/PlaygroundIndicator'
-import { Vault } from '@defichain/jellyfish-api-core/src/category/loan'
-import { PoolPairsResult } from '@defichain/jellyfish-api-core/src/category/poolpair'
 
 const testing = PlaygroundApiTesting.create()
 
@@ -20,49 +18,52 @@ afterAll(async () => {
 
 it('should have a vault', async () => {
   await waitForExpect(async () => {
-    const vaults: Vault[] = await testing.client.rpc.call('listvaults', [], 'number')
+    const vaults = await testing.container.call('listvaults')
     expect(vaults.length).toBeGreaterThan(0)
   }, 90000)
 })
 
 it('should have liquidity in DUSD-DFI pool', async () => {
   await waitForExpect(async () => {
-    const pairsResult: PoolPairsResult = await testing.client.rpc.call('getpoolpair', ['DUSD-DFI'], 'bignumber')
-    expect(Object.values(pairsResult)[0].reserveA.gt(0)).toBeTruthy()
-    expect(Object.values(pairsResult)[0].reserveB.gt(0)).toBeTruthy()
+    const pairsResult = await testing.container.call('getpoolpair', ['DUSD-DFI'])
+    const poolId = Object.keys(pairsResult)[0]
+    expect(pairsResult[poolId].reserveA > 0).toBeTruthy()
+    expect(pairsResult[poolId].reserveB > 0).toBeTruthy()
   }, 90000)
 })
 
 it('should have liquidity in TU10-DUSD pool', async () => {
   await waitForExpect(async () => {
-    const pairsResult: PoolPairsResult = await testing.client.rpc.call('getpoolpair', ['TU10-DUSD'], 'bignumber')
-    expect(Object.values(pairsResult)[0].reserveA.gt(0)).toBeTruthy()
-    expect(Object.values(pairsResult)[0].reserveB.gt(0)).toBeTruthy()
+    const pairsResult = await testing.container.call('getpoolpair', ['TU10-DUSD'])
+    const poolId = Object.keys(pairsResult)[0]
+    expect(pairsResult[poolId].reserveA > 0).toBeTruthy()
+    expect(pairsResult[poolId].reserveB > 0).toBeTruthy()
   }, 90000)
 })
 
 it('should have liquidity in TD10-DUSD pool', async () => {
   await waitForExpect(async () => {
-    const pairsResult: PoolPairsResult = await testing.client.rpc.call('getpoolpair', ['TD10-DUSD'], 'bignumber')
-    expect(Object.values(pairsResult)[0].reserveA.gt(0)).toBeTruthy()
-    expect(Object.values(pairsResult)[0].reserveB.gt(0)).toBeTruthy()
+    const pairsResult = await testing.container.call('getpoolpair', ['TD10-DUSD'])
+    const poolId = Object.keys(pairsResult)[0]
+    expect(pairsResult[poolId].reserveA > 0).toBeTruthy()
+    expect(pairsResult[poolId].reserveB > 0).toBeTruthy()
   }, 90000)
 })
 
 it('should have liquidity in TS25-DUSD pool', async () => {
   await waitForExpect(async () => {
-    const pairsResult: PoolPairsResult = await testing.client.rpc.call('getpoolpair', ['TS25-DUSD'], 'bignumber')
-
-    expect(Object.values(pairsResult)[0].reserveA.gt(0)).toBeTruthy()
-    expect(Object.values(pairsResult)[0].reserveB.gt(0)).toBeTruthy()
+    const pairsResult = await testing.container.call('getpoolpair', ['TS25-DUSD'])
+    const poolId = Object.keys(pairsResult)[0]
+    expect(pairsResult[poolId].reserveA > 0).toBeTruthy()
+    expect(pairsResult[poolId].reserveB > 0).toBeTruthy()
   }, 90000)
 })
 
 it('should have liquidity in TR50-DUSD pool', async () => {
   await waitForExpect(async () => {
-    const pairsResult: PoolPairsResult = await testing.client.rpc.call('getpoolpair', ['TR50-DUSD'], 'bignumber')
-
-    expect(Object.values(pairsResult)[0].reserveA.gt(0)).toBeTruthy()
-    expect(Object.values(pairsResult)[0].reserveB.gt(0)).toBeTruthy()
+    const pairsResult = await testing.container.call('getpoolpair', ['TR50-DUSD'])
+    const poolId = Object.keys(pairsResult)[0]
+    expect(pairsResult[poolId].reserveA > 0).toBeTruthy()
+    expect(pairsResult[poolId].reserveB > 0).toBeTruthy()
   }, 90000)
 })
