6f2731ad66f993711daaa1071507550f09b4ae44
websterlcl <w.canonbrother@gmail.com>
2022-03-02 15:12:24 +0800

    refactor BlockchainCppModule to be as independent to load a config from diff location

--- apps/libs/actuator/src/index.ts
+++ apps/libs/actuator/src/index.ts
@@ -1,3 +1,3 @@
 export * from './ActuatorModule'
 export * from './ActuatorController'
-export * from './BlockchainCppModule'
+export * from './BlockchainCppIndicator'
--- apps/ocean-api/src/modules/RootModule.ts
+++ apps/ocean-api/src/modules/RootModule.ts
@@ -2,7 +2,8 @@ import * as Joi from 'joi'
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 import { ControllerModule } from './ControllerModule'
-import { ActuatorModule, BlockchainCppModule } from '@defichain-apps/libs/actuator'
+import { BlockchainCppModule } from './BlockchainCppModule'
+import { ActuatorModule } from '@defichain-apps/libs/actuator'
 import { PlaygroundModule } from './PlaygroundModule'
 
 @Module({
