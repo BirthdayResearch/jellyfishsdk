0cf23e149d1cd97e45d89d6d6042214b95b54de6
chen <23054115+cwkang1998@users.noreply.github.com>
2022-03-03 13:57:53 +0800

    refactor(rich-list-core): change param name to be better suited

--- packages/rich-list-core/src/controller/AddressParser/dftx/utxosToAccount.ts
+++ packages/rich-list-core/src/controller/AddressParser/dftx/utxosToAccount.ts
@@ -5,8 +5,8 @@ import { DfTxAddressParser } from './_abstract'
 export class UtxosToAccountParser extends DfTxAddressParser<UtxosToAccount> {
   OP_CODE: number = CUtxosToAccount.OP_CODE
 
-  extract (accountToAccount: DfTx<UtxosToAccount>): string[] {
-    const addresses = accountToAccount.data.to
+  extract (utxosToAccount: DfTx<UtxosToAccount>): string[] {
+    const addresses = utxosToAccount.data.to
       .map((toScriptBal) => fromScript(toScriptBal.script, this.network)?.address as string)
     return addresses
   }
