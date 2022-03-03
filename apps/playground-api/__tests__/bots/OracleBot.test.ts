9b11b0c209daddfe2ca0f10122e1f4071b1aefb7
canonbrother <w.canonbrother@gmail.com>
2022-03-03 17:02:43 +0800

    chore(apps/pg-api): playground migration (#1140)
    * mig pg into apps monorepo, follow the design
    
    * fix pg module
    
    * rename bot -> bots, setup -> setups
    
    * fix tsconfig
    
    * rename oracle.bot -> OracleBot, same apply as bot.ts and vault.bot.ts, add OracleBot.ts
    
    * add pg mod test and vault bot test
    
    * uncomment walletctrl test
    
    * use regtestfoundationkeys
    
    * fix vaultbot.test.ts
    
    * use container.call instead of client.rpc.call
    
    * import HealthIndicatorRes from @nestjs, remove from actuator
    
    * move libs/actuator/src/filters to libs/filters
    
    * rm log
    
    * add tsconfig.lib.json for filters
    
    * refactor - do BlockchainCppIndicator as lib, do BlockchainCppModule on its particular apps
    
    * fix blockchaincppmodule on rist-list-api
    
    * add BlockchainCppModule on richlist
    
    * add blockchaincpp and pg-api on nest-cli, fix import on pg-api/blockchaincppmodule
    
    * rm old blockchaincppmodule of ocean-api and rist-list-api
    
    * revert tsconfig
    
    * optimize apps/tsconfig
    
    revert tsconfig.build
    
    * fix pg config
    
    * revert tsconfig.build changes

---
+++ apps/playground-api/__tests__/bots/OracleBot.test.ts
@@ -0,0 +1,64 @@
+import { PlaygroundApiTesting } from '../../testing/PlaygroundApiTesting'
+import waitForExpect from 'wait-for-expect'
+import { OracleBot } from '../../src/bots/OracleBot'
+import { OracleData } from '@defichain/jellyfish-api-core/src/category/oracle'
+
+const testing = PlaygroundApiTesting.create()
+
+beforeAll(async () => {
+  await testing.start()
+})
+
+afterAll(async () => {
+  await testing.stop()
+})
+
+describe('oracle bot', () => {
+  it('should change oracle price', async () => {
+    const setupOracle = testing.app.get(OracleBot)
+    const oracleId = setupOracle.oracleIds[0]
+
+    await waitForExpect(async () => {
+      const oracleData: OracleData = await testing.container.call('getoracledata', [oracleId])
+
+      const CU10 = oracleData.tokenPrices.find(x => x.token === 'CU10')
+      expect(CU10?.amount).toBeDefined()
+
+      const TU10 = oracleData.tokenPrices.find(x => x.token === 'TU10')
+      expect(TU10?.amount).toBeDefined()
+
+      const CD10 = oracleData.tokenPrices.find(x => x.token === 'CD10')
+      expect(CD10?.amount).toBeDefined()
+
+      const TD10 = oracleData.tokenPrices.find(x => x.token === 'TD10')
+      expect(TD10?.amount).toBeDefined()
+
+      const CS25 = oracleData.tokenPrices.find(x => x.token === 'CS25')
+      expect(CS25?.amount).toBeDefined()
+
+      const TS25 = oracleData.tokenPrices.find(x => x.token === 'TS25')
+      expect(TS25?.amount).toBeDefined()
+
+      const CR50 = oracleData.tokenPrices.find(x => x.token === 'CR50')
+      expect(CR50?.amount).toBeDefined()
+
+      const TR50 = oracleData.tokenPrices.find(x => x.token === 'TR50')
+      expect(TR50?.amount).toBeDefined()
+
+      const DFI = oracleData.tokenPrices.find(x => x.token === 'DFI')
+      expect(DFI?.amount).toBeDefined()
+
+      const BTC = oracleData.tokenPrices.find(x => x.token === 'BTC')
+      expect(BTC?.amount).toBeDefined()
+
+      const ETH = oracleData.tokenPrices.find(x => x.token === 'ETH')
+      expect(ETH?.amount).toBeDefined()
+
+      const USDC = oracleData.tokenPrices.find(x => x.token === 'USDC')
+      expect(USDC?.amount).toBeDefined()
+
+      const USDT = oracleData.tokenPrices.find(x => x.token === 'USDT')
+      expect(USDT?.amount).toBeDefined()
+    }, 200000)
+  })
+})
---
+++ apps/playground-api/src/bots/OracleBot.ts
@@ -0,0 +1,178 @@
+import { Injectable } from '@nestjs/common'
+import { BigNumber } from '@defichain/jellyfish-json'
+import { ApiClient } from '@defichain/jellyfish-api-core'
+import { Interval } from '@nestjs/schedule'
+
+enum PriceDirection {
+  UP_ABSOLUTE, // Always going up (absolute value)
+  DOWN_ABSOLUTE, // Always going down (absolute value)
+  UP_PERCENTAGE, // Always going up (percentage value)
+  DOWN_PERCENTAGE, // Always going down (percentage value)
+  RANDOM, // Randomly goes up or down (minimum $1)
+  STABLE // Fixed value
+}
+
+type PriceDirectionFunction = (price: BigNumber, priceChange: BigNumber) => BigNumber
+
+const PriceDirectionFunctions: Record<PriceDirection, PriceDirectionFunction> = {
+  [PriceDirection.UP_ABSOLUTE]: (price: BigNumber, priceChange: BigNumber) => {
+    return price.plus(priceChange)
+  },
+  [PriceDirection.DOWN_ABSOLUTE]: (price: BigNumber, priceChange: BigNumber) => {
+    return price.minus(priceChange)
+  },
+  [PriceDirection.UP_PERCENTAGE]: (price: BigNumber, priceChange: BigNumber) => {
+    return price.plus(price.times(priceChange))
+  },
+  [PriceDirection.DOWN_PERCENTAGE]: (price: BigNumber, priceChange: BigNumber) => {
+    return price.minus(price.times(priceChange))
+  },
+  [PriceDirection.RANDOM]: (price: BigNumber, priceChange: BigNumber) => {
+    const change = price.times(priceChange)
+    if (Math.random() > 0.5 || price.lt(1)) {
+      return price.plus(change)
+    }
+    return price.minus(change)
+  },
+  [PriceDirection.STABLE]: (price: BigNumber, priceChange: BigNumber) => {
+    return price
+  }
+}
+
+interface SimulatedOracleFeed {
+  token: string
+  amount: BigNumber
+  change: BigNumber
+  direction: PriceDirection
+}
+
+@Injectable()
+export class OracleBot {
+  private feeds: SimulatedOracleFeed[] = [
+    {
+      token: 'CU10',
+      amount: new BigNumber(10),
+      change: new BigNumber(0.0001),
+      direction: PriceDirection.UP_PERCENTAGE
+    },
+    {
+      token: 'TU10',
+      amount: new BigNumber(10),
+      change: new BigNumber(0.0001),
+      direction: PriceDirection.UP_PERCENTAGE
+    },
+    {
+      token: 'CD10',
+      amount: new BigNumber(1000000000),
+      change: new BigNumber(0.0001),
+      direction: PriceDirection.DOWN_PERCENTAGE
+    },
+    {
+      token: 'TD10',
+      amount: new BigNumber(1000000000),
+      change: new BigNumber(0.0001),
+      direction: PriceDirection.DOWN_PERCENTAGE
+    },
+    {
+      token: 'CS25',
+      amount: new BigNumber(25),
+      change: new BigNumber(0),
+      direction: PriceDirection.STABLE
+    },
+    {
+      token: 'TS25',
+      amount: new BigNumber(25),
+      change: new BigNumber(0),
+      direction: PriceDirection.STABLE
+    },
+    {
+      token: 'CR50',
+      amount: new BigNumber(1000),
+      change: new BigNumber(0.33),
+      direction: PriceDirection.RANDOM
+    },
+    {
+      token: 'TR50',
+      amount: new BigNumber(1000),
+      change: new BigNumber(0.33),
+      direction: PriceDirection.RANDOM
+    },
+    {
+      token: 'DFI',
+      amount: new BigNumber(100),
+      change: new BigNumber(0),
+      direction: PriceDirection.STABLE
+    },
+    {
+      token: 'BTC',
+      amount: new BigNumber(50),
+      change: new BigNumber(0),
+      direction: PriceDirection.STABLE
+    },
+    {
+      token: 'ETH',
+      amount: new BigNumber(10),
+      change: new BigNumber(0),
+      direction: PriceDirection.STABLE
+    },
+    {
+      token: 'USDC',
+      amount: new BigNumber(1),
+      change: new BigNumber(0),
+      direction: PriceDirection.STABLE
+    },
+    {
+      token: 'USDT',
+      amount: new BigNumber(0.99),
+      change: new BigNumber(0),
+      direction: PriceDirection.STABLE
+    },
+    {
+      token: 'ZERO',
+      amount: new BigNumber(0.99),
+      change: new BigNumber(0),
+      direction: PriceDirection.STABLE
+    },
+    {
+      token: 'OFF',
+      amount: new BigNumber(1),
+      change: new BigNumber(0),
+      direction: PriceDirection.STABLE
+    }
+  ]
+
+  public oracleIds: string[] = []
+
+  constructor (readonly client: ApiClient) {
+  }
+
+  @Interval(6000)
+  async run (): Promise<void> {
+    for (const oracleId of this.oracleIds) {
+      await this.publish(oracleId)
+    }
+
+    this.feeds = this.feeds.map(value => {
+      const func = PriceDirectionFunctions[value.direction]
+      return {
+        ...value,
+        amount: func(value.amount, value.change)
+      }
+    })
+  }
+
+  async publish (oracleId: string): Promise<void> {
+    const time = Math.floor(Date.now() / 1000)
+
+    await this.client.oracle.setOracleData(oracleId, time, {
+      prices: this.feeds
+        .filter(value => {
+          return value.amount.gt(new BigNumber(0.00000001)) && value.amount.lt(new BigNumber(1_200_000_000))
+        })
+        .map(v => ({
+          tokenAmount: `${v.amount.toFixed(8)}@${v.token}`,
+          currency: 'USD'
+        }))
+    })
+  }
+}
