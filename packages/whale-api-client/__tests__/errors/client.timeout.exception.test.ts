375af8db9cdc0a3f094f29ceb0b2e15a09201b97
Kodemon <dev@kodemon.net>
2022-03-04 14:18:22 +0900

    split up packages

--- packages/whale-api-client/__tests__/errors/client.timeout.exception.test.ts
+++
@@ -1,7 +0,0 @@
-import { WhaleClientTimeoutException } from '../../src'
-
-it('WhaleClientTimeoutException should be structured as', () => {
-  const exception = new WhaleClientTimeoutException(15000)
-
-  expect(exception.message).toStrictEqual('request aborted due to timeout of 15000 ms')
-})
--- packages/whale-api-wallet/src/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './feerate'
-export * from './prevout'
-export * from './wallet'
