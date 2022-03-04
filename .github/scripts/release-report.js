dcde0ac1cd86b8c753d60f71cadffef970d5164d
Fuxing Loh <git@fuxing.dev>
2022-03-04 12:01:51 +0800

    format release-report.js since it's not indent shifted

--- .github/scripts/release-report.js
+++ .github/scripts/release-report.js
@@ -3,22 +3,20 @@
  *
  * Generate a release message in PRs reporting the success of a new build
  * along with some quality of life links.
- *
  */
-
-const apps = process.env.APPS.split(",")
-
 module.exports = ({ context }) => {
   const links = getContainerLinks(context)
   return `
-  Build preview for jellyfish apps is ready!
-            
-  Built with commit ${ context.sha }
 
-  ${links.join("\n")}
-  `
+Build preview for jellyfish apps is ready!
+          
+Built with commit ${context.sha}
+
+${links.join('\n')}
+`
 }
 
-function getContainerLinks({ payload: { number } }) {
+function getContainerLinks ({ payload: { number } }) {
+  const apps = process.env.APPS.split(',')
   return apps.map(app => `ghcr.io/defich/${app}:pr-${number}`)
 }
