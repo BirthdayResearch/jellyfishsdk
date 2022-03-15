c6721b90d0c34687e2c043bae8ae04ea0fb667f8
chen <23054115+cwkang1998@users.noreply.github.com>
2022-03-15 20:38:14 +0800

    chore(rich-list-api): complete tests for `RichListController`

--- apps/rich-list-api/__test__/controllers/RichListController.test.ts
+++ apps/rich-list-api/__test__/controllers/RichListController.test.ts
@@ -1,22 +1,24 @@
-import { JsonRpcClient } from '@defichain/jellyfish-api-jsonrpc'
 import { MasterNodeRegTestContainer } from '@defichain/testcontainers'
 import { RichListController } from '../../src/controllers/RichList'
-import { RichListCoreTest } from '@defichain/rich-list-core/test/RichListCoreTest'
+import { RichListCoreTest, waitForCatchingUp } from '@defichain/rich-list-core/test/RichListCoreTest'
+import { RichListCore } from '@defichain/rich-list-core'
+import { Testing } from '@defichain/jellyfish-testing'
 
 describe('RichListController', () => {
-  const container = new MasterNodeRegTestContainer()
+  const testing = Testing.create(new MasterNodeRegTestContainer())
   let richListController!: RichListController
+  let core: RichListCore
 
   beforeAll(async () => {
-    await container.start()
-    const rpc = new JsonRpcClient(await container.getCachedRpcUrl())
-    const core = RichListCoreTest(rpc)
+    await testing.container.start()
+    await testing.container.waitForWalletCoinbaseMaturity()
+    core = RichListCoreTest(testing.rpc)
     richListController = new RichListController(core)
     await core.start()
   })
 
   afterAll(async () => {
-    await container.stop()
+    await testing.container.stop()
   })
 
   it('should reject invalid number as token id', async () => {
@@ -28,7 +30,71 @@ describe('RichListController', () => {
   })
 
   it('should retrieve rich list', async () => {
-    await richListController.get(-1)
-    // TODO: more comprehensive e2e
+    await waitForCatchingUp(core)
+    await core.calculateNext()
+    const expected = await core.get('-1')
+    const actual = await richListController.get(-1)
+
+    expect(actual.length).toStrictEqual(expected.length)
+    expect(actual).toStrictEqual(expected)
+  })
+
+  it('should retrieve utxo rich list', async () => {
+    await waitForCatchingUp(core)
+    await core.calculateNext()
+    const expected = await core.get('-1')
+    const actual = await richListController.get(-1)
+
+    expect(actual.length).toStrictEqual(expected.length)
+    expect(actual).toStrictEqual(expected)
+  })
+  it('should retrieve newly added rich addresses', async () => {
+    await waitForCatchingUp(core)
+    await core.calculateNext()
+    const expected = await core.get('-1')
+    let actual = await richListController.get(-1)
+    expect(actual.length).toStrictEqual(expected.length)
+    expect(actual).toStrictEqual(expected)
+
+    const newRich = await testing.container.getNewAddress()
+    await testing.container.fundAddress(newRich, 100)
+    await core.start()
+    await waitForCatchingUp(core)
+    await core.calculateNext()
+
+    actual = await richListController.get(-1)
+    expect(actual).toContainEqual({
+      address: newRich,
+      amount: expect.any(Number)
+    })
+  })
+
+  it('should retrieve token rich list', async () => {
+    const newRich = await testing.container.getNewAddress()
+    await testing.container.fundAddress(newRich, 100)
+    await testing.rpc.token.createToken({
+      symbol: 'RLST',
+      name: 'RichListToken',
+      isDAT: true,
+      mintable: true,
+      tradeable: true,
+      collateralAddress: newRich
+    })
+    await testing.container.generate(1)
+    await testing.rpc.token.mintTokens('10000@RLST')
+    await testing.container.generate(1)
+    await core.start()
+    await waitForCatchingUp(core)
+    await core.calculateNext()
+
+    const expected = await core.get('1')
+    const actual = await richListController.get(1)
+
+    expect(actual.length).toStrictEqual(expected.length)
+    expect(actual).toStrictEqual(expected)
+    expect(actual).toContainEqual({
+      address: newRich,
+      amount: 10000
+    })
   })
 })
