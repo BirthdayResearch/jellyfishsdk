8ffd4288abae636a05fbbafa8d231ad15cc6ee27
Joel-David Wong <git@joeldavid.dev>
2022-03-27 20:32:25 +0800

    init status-api with blockchain status endpoint

---
+++ apps/status-api/__test__/controllers/BlockchainController.test.ts
@@ -0,0 +1,24 @@
+import { StatusApiTesting } from '../../testing/StatusApiTesting'
+
+describe('BlockchainController', () => {
+  const apiTesting = StatusApiTesting.create()
+
+  beforeAll(async () => {
+    await apiTesting.start()
+  })
+
+  afterAll(async () => {
+    await apiTesting.stop()
+  })
+
+  it('/blockchain/status - MainNet', async () => {
+    const res = await apiTesting.app.inject({
+      method: 'GET',
+      url: '/blockchain/status'
+    })
+
+    console.log(res.body)
+
+    expect(res.statusCode).toStrictEqual(200)
+  })
+})
---
+++ apps/status-api/src/pipes/NetworkValidationPipe.ts
@@ -0,0 +1,33 @@
+import {
+  ArgumentMetadata,
+  HttpException,
+  Injectable,
+  PipeTransform
+} from '@nestjs/common'
+
+export type SupportedNetwork = 'mainnet' | 'testnet'
+
+@Injectable()
+export class NetworkValidationPipe implements PipeTransform {
+  private static readonly VALID_NETWORKS: Set<undefined | SupportedNetwork> = new Set([
+    undefined, // defaults to 'mainnet'
+    'mainnet',
+    'testnet'
+  ])
+
+  transform (value: any, metadata: ArgumentMetadata): any {
+    if (NetworkValidationPipe.VALID_NETWORKS.has(value)) {
+      return value
+    }
+    throw new InvalidNetworkException()
+  }
+}
+
+export class InvalidNetworkException extends HttpException {
+  constructor () {
+    super({
+      statusCode: 404,
+      message: 'Not Found'
+    }, 404)
+  }
+}
