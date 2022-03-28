f510f141ddc74a4c91cd9818dee2abb0155c3f7f
chen <23054115+cwkang1998@users.noreply.github.com>
2022-03-29 04:18:46 +0800

    chore(rich-list-core): add more tests for RichListCore

--- packages/rich-list-core/__tests__/RichListCore.test.ts
+++ packages/rich-list-core/__tests__/RichListCore.test.ts
@@ -1,5 +1,6 @@
 import { BigNumber } from '@defichain/jellyfish-api-core'
 import { JsonRpcClient } from '@defichain/jellyfish-api-jsonrpc'
+import { Testing } from '@defichain/jellyfish-testing'
 import { MasterNodeRegTestContainer } from '@defichain/testcontainers'
 import { RichListCore } from '../src/RichListCore'
 import { RichListCoreTest, waitForCatchingUp } from '../src/test/RichListCoreTest'
@@ -21,21 +22,21 @@ const EXPECTED_RICH_LIST_ADDRESSES = [
 ]
 
 describe('RichListCore', () => {
-  let container!: MasterNodeRegTestContainer
+  let testing!: Testing
   let apiClient!: JsonRpcClient
   let richListCore!: RichListCore
 
   beforeEach(async () => {
-    container = new MasterNodeRegTestContainer()
-    await container.start()
-    await container.waitForWalletCoinbaseMaturity()
+    testing = Testing.create(new MasterNodeRegTestContainer())
+    await testing.container.start()
+    await testing.container.waitForWalletCoinbaseMaturity()
 
-    apiClient = new JsonRpcClient(await container.getCachedRpcUrl())
+    apiClient = new JsonRpcClient(await testing.container.getCachedRpcUrl())
     richListCore = RichListCoreTest(apiClient)
   })
 
   afterEach(async () => {
-    await container.stop()
+    await testing.container.stop()
   })
 
   describe('startOrResume() - should start crawl blocks and push active addresses', () => {
@@ -46,6 +47,25 @@ describe('RichListCore', () => {
       const queue = await richListCore.queueClient.createQueueIfNotExist('RichListCore_ACTIVE_ADDRESSES', 'LIFO')
       const queuedItem = await queue.receive(Number.MAX_SAFE_INTEGER)
       expect(queuedItem).toStrictEqual(EXPECTED_RICH_LIST_ADDRESSES)
+    })
+
+    it('should extract new addresses from transactions and push into queue', async () => {
+      richListCore.start()
+      await waitForCatchingUp(richListCore)
+      const queue = await richListCore.queueClient.createQueueIfNotExist('RichListCore_ACTIVE_ADDRESSES', 'LIFO')
+      let queuedItem = await queue.receive(Number.MAX_SAFE_INTEGER)
+      expect(queuedItem).toStrictEqual(EXPECTED_RICH_LIST_ADDRESSES)
+      const newAddr = await testing.generateAddress()
+      await testing.rpc.account.utxosToAccount({ [newAddr]: '100@0' })
+      await testing.generate(1)
+      await testing.rpc.account.accountToUtxos(newAddr, { [newAddr]: '25@0' })
+      await testing.generate(1)
+
+      richListCore.start()
+      await waitForCatchingUp(richListCore)
+      expect(queuedItem).not.toContain(newAddr)
+      queuedItem = await queue.receive(Number.MAX_SAFE_INTEGER)
+      expect(queuedItem).toContain(newAddr)
     })
   })
 
@@ -71,6 +91,39 @@ describe('RichListCore', () => {
         const next = richList[i + 1]
         expect(EXPECTED_RICH_LIST_ADDRESSES).toContain(current.address)
         expect(EXPECTED_RICH_LIST_ADDRESSES).toContain(next.address)
+        expect(current.amount).toBeGreaterThanOrEqual(next.amount)
+      }
+    })
+
+    it('should update rich list with newly added address', async () => {
+      richListCore.start()
+      await waitForCatchingUp(richListCore)
+      await richListCore.calculateNext()
+
+      const before = await richListCore.get('-1')
+      expect(before.length).toStrictEqual(EXPECTED_RICH_LIST_ADDRESSES.length)
+
+      for (let i = 0; i < before.length - 1; i++) {
+        const current = before[i]
+        const next = before[i + 1]
+        expect(EXPECTED_RICH_LIST_ADDRESSES).toContain(current.address)
+        expect(EXPECTED_RICH_LIST_ADDRESSES).toContain(next.address)
+        expect(current.amount).toBeGreaterThanOrEqual(next.amount)
+      }
+
+      const newAddr = await testing.generateAddress()
+      await testing.rpc.account.utxosToAccount({ [newAddr]: '100@0' })
+      await testing.generate(1)
+      await testing.rpc.account.accountToUtxos(newAddr, { [newAddr]: '25@0' })
+      await testing.generate(1)
+      richListCore.start()
+      await waitForCatchingUp(richListCore)
+      await richListCore.calculateNext()
+      const after = await richListCore.get('-1')
+      expect(after).toContainEqual({ address: newAddr, amount: expect.any(Number) })
+      for (let i = 0; i < after.length - 1; i++) {
+        const current = after[i]
+        const next = after[i + 1]
         expect(current.amount).toBeGreaterThanOrEqual(next.amount)
       }
     })
