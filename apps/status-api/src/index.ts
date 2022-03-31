e9c701f7f1c3413784d603583f4ccd583209c8b6
Nichelle <nichelle@cakedefi.com>
2022-03-31 17:04:23 +0800

    Setup server for status-api

---
+++ apps/status-api/src/index.ts
@@ -0,0 +1,47 @@
+import { RootModule } from './modules/RootModule'
+import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify'
+import { NestFactory } from '@nestjs/core'
+import { ConfigService } from '@nestjs/config'
+import { RootServer } from '@defichain-apps/libs/rootserver'
+
+export class StatusApiServer extends RootServer {
+  app?: NestFastifyApplication
+
+  async create (): Promise<NestFastifyApplication> {
+    const adapter = new FastifyAdapter({ logger: true })
+    return await NestFactory.create<NestFastifyApplication>(RootModule, adapter)
+  }
+
+  async configure (app: NestFastifyApplication): Promise<void> {
+    app.enableCors({
+      origin: '*',
+      methods: ['GET'],
+      allowedHeaders: ['Content-Type'],
+      maxAge: 60 * 24 * 7
+    })
+  }
+
+  async init (app: NestFastifyApplication, config: ConfigService): Promise<void> {
+    const port = config.get<number>('PORT', 3000)
+    await app.listen(port, '0.0.0.0')
+  }
+
+  async start (): Promise<void> {
+    this.app = await this.create()
+    const config = this.app.get(ConfigService)
+
+    await this.configure(this.app)
+    await this.init(this.app, config)
+  }
+
+  async stop (): Promise<void> {
+    await this.app?.close()
+  }
+}
+
+/**
+ * Bootstrap RootModule and start server
+ */
+if (require.main === module) {
+  void new StatusApiServer().start()
+}
---
+++ apps/status-api/src/modules/WhaleApiModule.ts
@@ -0,0 +1,69 @@
+import { Global, Injectable, Module } from '@nestjs/common'
+import { HealthIndicatorResult } from '@nestjs/terminus'
+import { ActuatorProbes, ProbeIndicator } from '@defichain-apps/libs/actuator'
+import { BlockchainInfo } from '@defichain/jellyfish-api-core/src/category/blockchain'
+import { WhaleApiClient } from '@defichain/whale-api-client'
+import { ConfigService } from '@nestjs/config'
+
+@Injectable()
+export class WhaleApiProbeIndicator extends ProbeIndicator {
+  constructor (private readonly client: WhaleApiClient) {
+    super()
+  }
+
+  async liveness (): Promise<HealthIndicatorResult> {
+    try {
+      await this.client.stats.get()
+      return this.withAlive('whale')
+    } catch (ignored) {
+      return this.withDead('whale', 'could not connect to mainnet')
+    }
+  }
+
+  async readiness (): Promise<HealthIndicatorResult> {
+    let info
+    try {
+      info = await this.client.rpc.call<BlockchainInfo>('getblockchaininfo', [], 'number')
+    } catch (ignored) {
+      return this.withDead('whale', 'could not get blockchain info from mainnet')
+    }
+
+    const details = {
+      initialBlockDownload: info.initialblockdownload,
+      blocks: info.blocks,
+      headers: info.headers
+    }
+    return this.withAlive('whale', details)
+  }
+}
+
+@Global()
+@Module({
+  providers: [
+    WhaleApiProbeIndicator,
+    {
+      provide: WhaleApiClient,
+      useFactory: (configService: ConfigService): WhaleApiClient => {
+        return new WhaleApiClient({
+          version: 'v0',
+          network: configService.get<string>('network'),
+          url: 'https://ocean.defichain.com'
+        })
+      },
+      inject: [ConfigService]
+    }
+  ],
+  exports: [
+  ]
+})
+export class WhaleApiModule {
+  constructor (
+    private readonly probes: ActuatorProbes,
+    private readonly whaleApiClientProbeIndicator: WhaleApiProbeIndicator
+  ) {
+  }
+
+  async onApplicationBootstrap (): Promise<void> {
+    this.probes.add(this.whaleApiClientProbeIndicator)
+  }
+}
