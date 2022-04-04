7f1b992ea7da9c276cd126e4e6c55f632f70f5a2
Eli <elicodesforfun@gmail.com>
2022-04-05 01:01:45 +0800

    fix(sanity-testing): Point defid url to correct bridge with regtest credentials

--- packages/testcontainers/src/containers/SanityContainer/WhaleSanityContainer.ts
+++ packages/testcontainers/src/containers/SanityContainer/WhaleSanityContainer.ts
@@ -2,21 +2,25 @@ import { MasterNodeRegTestContainer } from '../RegTestContainer/Masternode'
 import { SanityContainer } from '.'
 
 export class WhaleSanityContainer extends SanityContainer {
-  constructor (master: MasterNodeRegTestContainer = new MasterNodeRegTestContainer(), tag: string = 'local') {
+  constructor (
+    readonly master: MasterNodeRegTestContainer = new MasterNodeRegTestContainer(),
+    tag: string = 'local'
+  ) {
     super(master, 'whale', tag)
   }
 
   public async start (): Promise<void> {
     await super.start()
 
-    const ip = await this.blockchain.getIp('bridge')
+    const hostRegTestIp = 'host.docker.internal' // TODO(eli-lim): Works on linux?
+    const hostRegTestPort = await this.master.getPort('19554/tcp')
 
     this.container = await this.docker.createContainer({
       name: this.generateName(),
       Image: this.image,
       Tty: true,
       Env: [
-        `WHALE_DEFID_URL=http://whale-rpcuser:whale-rpcpassword@${ip}:19554`,
+        `WHALE_DEFID_URL=http://testcontainers-user:testcontainers-password@${hostRegTestIp}:${hostRegTestPort}`,
         'WHALE_NETWORK=regtest',
         'WHALE_DATABASE_PROVIDER=memory'
       ],
