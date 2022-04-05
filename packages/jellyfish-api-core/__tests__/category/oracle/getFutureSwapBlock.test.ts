5ae46c81156f3f01da9daeabc55bf7856f3e9926
jingyi2811 <jingyi2811@gmail.com>
2022-04-05 20:20:08 +0800

    Refactor test

--- packages/jellyfish-api-core/__tests__/category/oracle/getFutureSwapBlock.test.ts
+++ packages/jellyfish-api-core/__tests__/category/oracle/getFutureSwapBlock.test.ts
@@ -42,67 +42,99 @@ describe('Oracle getFutureSwapBlock', () => {
       amount: 1
     })
     await testing.generate(1)
-  }
 
-  beforeAll(async () => {
-    await testing.container.start()
-    await testing.container.waitForWalletCoinbaseMaturity()
-    await setup()
-  })
-
-  afterAll(async () => {
-    await testing.container.stop()
-  })
-
-  it('Should getFutureSwapBlock', async () => {
     const tslaAddress = await testing.generateAddress()
 
     await testing.rpc.account.accountToAccount(collateralAddress, {
       [tslaAddress]: '1@TSLA'
     })
     await testing.generate(1)
+  }
 
-    // Before reward_pct and block_period Gov attributes are set,
-    // future swap block should be equal to zero
-    {
+  beforeEach(async () => {
+    await testing.container.start()
+    await testing.container.waitForWalletCoinbaseMaturity()
+    await setup()
+  })
+
+  afterEach(async () => {
+    await testing.container.stop()
+  })
+
+  describe('Should getFutureSwapBlock with number > 0', () => {
+    it('Should getFutureSwapBlock If GOV attributes: reward_pct, block_period and active values are set', async () => {
+      // Add GOV attributes
+      await testing.rpc.masternode.setGov({
+        ATTRIBUTES: {
+          'v0/params/dfip2203/reward_pct': futureRewardPercentage,
+          'v0/params/dfip2203/block_period': futureInterval.toString()
+        }
+      })
+      await testing.generate(1)
+
+      await testing.rpc.masternode.setGov({
+        ATTRIBUTES: {
+          'v0/params/dfip2203/active': 'true'
+        }
+      })
+      await testing.generate(1)
+
+      // Call getfutureswapblock
+      {
+        const futureSwapBlock = await testing.container.call('getfutureswapblock')
+        const currentBlockCount = await testing.rpc.blockchain.getBlockCount()
+        expect(currentBlockCount).toStrictEqual(108)
+        expect(futureSwapBlock).toStrictEqual(125) // 108 + 25 - (108 % 25) = 133 - 8 = 125
+      }
+
+      // Generate 25 more blocks
+      await testing.generate(25)
+
+      // Call getfutureswapblock again after 25 blocks
+      {
+        const futureSwapBlock = await testing.container.call('getfutureswapblock')
+        const currentBlockCount = await testing.rpc.blockchain.getBlockCount()
+        expect(currentBlockCount).toStrictEqual(133)
+        expect(futureSwapBlock).toStrictEqual(150) // 133 + 25 - (133 % 25) = 158 - 8 = 150
+      }
+    })
+  })
+
+  describe('Should getFutureSwapBlock with number = 0', () => {
+    it('Should getFutureSwapBlock if no GOV attribute is set', async () => {
       const futureSwapBlock = await testing.rpc.oracle.getFutureSwapBlock()
       expect(futureSwapBlock).toStrictEqual(0)
-    }
+    })
 
-    // Add reward_pct and block_period GOV attributes
-    await testing.rpc.masternode.setGov({ ATTRIBUTES: { 'v0/params/dfip2203/reward_pct': futureRewardPercentage, 'v0/params/dfip2203/block_period': futureInterval.toString() } })
-    await testing.generate(1)
+    it('Should getFutureSwapBlock if GOV attributes: active is set to false', async () => {
+      {
+        // Only active is set to false
+        await testing.rpc.masternode.setGov({ ATTRIBUTES: { 'v0/params/dfip2203/active': 'false' } })
+        await testing.generate(1)
 
-    await testing.rpc.masternode.setGov({ ATTRIBUTES: { 'v0/params/dfip2203/active': 'true' } })
-    await testing.generate(1)
+        const futureSwapBlock = await testing.rpc.oracle.getFutureSwapBlock()
+        expect(futureSwapBlock).toStrictEqual(0)
+      }
 
-    // Call getfutureswapblock
-    {
-      const futureSwapBlock = await testing.container.call('getfutureswapblock')
-      const currentBlockCount = await testing.rpc.blockchain.getBlockCount()
-      expect(currentBlockCount).toStrictEqual(108)
-      expect(futureSwapBlock).toStrictEqual(125) // 108 + 25 - (108 % 25) = 133 - 8 = 125
-    }
+      {
+        // If reward_pct and block_period value are set
+        await testing.rpc.masternode.setGov({ ATTRIBUTES: { 'v0/params/dfip2203/reward_pct': futureRewardPercentage, 'v0/params/dfip2203/block_period': futureInterval.toString() } })
+        await testing.generate(1)
 
-    // Generate 25 more blocks
-    await testing.generate(25)
+        const futureSwapBlock = await testing.rpc.oracle.getFutureSwapBlock()
+        expect(futureSwapBlock).toStrictEqual(0)
+      }
+    })
 
-    // Call getfutureswapblock again after 25 blocks
-    {
-      const futureSwapBlock = await testing.container.call('getfutureswapblock')
-      const currentBlockCount = await testing.rpc.blockchain.getBlockCount()
-      expect(currentBlockCount).toStrictEqual(133)
-      expect(futureSwapBlock).toStrictEqual(150) // 133 + 25 - (133 % 25) = 158 - 8 = 150
-    }
+    it('Should getFutureSwapBlock if GOV attributes: active is set to true but reward_pct and block_period are not set', async () => {
+      {
+        // Only active is set to false
+        await testing.rpc.masternode.setGov({ ATTRIBUTES: { 'v0/params/dfip2203/active': 'true' } })
+        await testing.generate(1)
 
-    // If active is set to false, regardless reward_pct and block_period Gov attributes are set,
-    // future swap block should be equal to zero
-    await testing.rpc.masternode.setGov({ ATTRIBUTES: { 'v0/params/dfip2203/active': 'false' } })
-    await testing.generate(1)
-
-    {
-      const futureSwapBlock = await testing.container.call('getfutureswapblock')
-      expect(futureSwapBlock).toStrictEqual(0)
-    }
+        const futureSwapBlock = await testing.rpc.oracle.getFutureSwapBlock()
+        expect(futureSwapBlock).toStrictEqual(0)
+      }
+    })
   })
 })
