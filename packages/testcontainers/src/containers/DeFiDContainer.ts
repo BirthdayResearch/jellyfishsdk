6181ce9a7868f9760980dc63b07531221484eedf
jingyi2811 <jingyi2811@gmail.com>
2022-04-05 19:21:56 +0800

    Stable version

--- packages/testcontainers/src/containers/DeFiDContainer.ts
+++ packages/testcontainers/src/containers/DeFiDContainer.ts
@@ -35,7 +35,7 @@ export abstract class DeFiDContainer extends DockerContainer {
     if (process?.env?.DEFICHAIN_DOCKER_IMAGE !== undefined) {
       return process.env.DEFICHAIN_DOCKER_IMAGE
     }
-    return 'defi/defichain:master-2a236bb79'
+    return 'defi/defichain:master-57c099c1a'
   }
 
   public static readonly DefaultStartOptions = {
