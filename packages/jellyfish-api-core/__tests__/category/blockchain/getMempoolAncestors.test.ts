0732eac2cedbcf709bbdd9c2b8a488759e357f77
justin <justinwong8991@gmail.com>
2022-04-05 18:07:43 +0800

    merged changes and added tests for error and no ancestors

--- packages/jellyfish-api-core/__tests__/category/blockchain/getMempoolAncestors.test.ts
+++ packages/jellyfish-api-core/__tests__/category/blockchain/getMempoolAncestors.test.ts
@@ -10,18 +10,27 @@ describe('getMempoolAncestors', () => {
 
   beforeAll(async () => {
     await container.start()
-    await container.waitForReady()
     await container.waitForWalletCoinbaseMaturity()
   })
 
   afterAll(async () => {
     await container.stop()
   })
-  it('should getMempoolAncestors and return JSON object if verbose is true', async () => {
+
+  it('should return no ancestor ids for tx without ancestors', async () => {
+    await waitForExpect(async () => {
+      const txId = await client.wallet.sendToAddress('mwsZw8nF7pKxWH8eoKL9tPxTpaFkz7QeLU', 0.0003)
+      const mempoolEntry: string[] = await client.blockchain.getMempoolAncestors(txId, false)
+      expect(mempoolEntry.length).toStrictEqual(0)
+    }, 10000)
+  })
+
+  it('should return JSON object if verbose is true', async () => {
     await waitForExpect(async () => {
       const txId = await client.wallet.sendToAddress('mwsZw8nF7pKxWH8eoKL9tPxTpaFkz7QeLU', 0.0003)
       const mempoolEntry: MempoolTx = await client.blockchain.getMempoolAncestors(txId, true)
       const entryKey = Object.keys(mempoolEntry)[0]
+      expect(Object.keys(mempoolEntry).length).toBeGreaterThan(0)
       expect(mempoolEntry[entryKey]).toStrictEqual({
         fees: expect.any(Object),
         vsize: expect.any(BigNumber),
@@ -44,11 +53,18 @@ describe('getMempoolAncestors', () => {
     }, 10000)
   })
 
-  it('should getMempoolAncestors and return array of txids if verbose is false', async () => {
+  it('should return array of txids if verbose is false', async () => {
     await waitForExpect(async () => {
       const txId = await client.wallet.sendToAddress('mwsZw8nF7pKxWH8eoKL9tPxTpaFkz7QeLU', 0.0003)
       const mempoolEntry: string[] = await client.blockchain.getMempoolAncestors(txId, false)
       expect(mempoolEntry.length).toBeGreaterThan(0)
+    }, 10000)
+  })
+
+  it('should return error if tx not in mempool', async () => {
+    await waitForExpect(async () => {
+      const txId = '9fb9c46b1d12dae8a4a35558f7ef4b047df3b444b1ead61d334e4f187f5f58b7'
+      await expect(client.blockchain.getMempoolAncestors(txId, false)).rejects.toThrow('RpcApiError: \'Transaction not in mempool\', code: -5, method: getmempoolancestors')
     }, 10000)
   })
 })
