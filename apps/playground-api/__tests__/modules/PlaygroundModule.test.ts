557563c72de450607a86acf70c32624e0aaaaa55
websterlcl <w.canonbrother@gmail.com>
2022-04-15 11:28:32 +0800

    inspect next minus current

--- apps/playground-api/__tests__/modules/PlaygroundModule.test.ts
+++ apps/playground-api/__tests__/modules/PlaygroundModule.test.ts
@@ -226,6 +226,7 @@ it('should have gov set', async () => {
 
   const current = await testing.container.getBlockCount()
   const next = await testing.container.call('getfutureswapblock', [])
+  expect(next - current).toBeLessThanOrEqual(10)
   await testing.generate(next - current)
 
   {
