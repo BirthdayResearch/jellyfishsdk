17743a899ce80605c1f3191f44be724d9ae26480
JustinWong98 <justinwong8991@gmail.com>
2022-04-18 17:36:06 +0800

    basic RPC implementation with dockerode supporting up to 2 parameters

--- apps/blockchain-cpp/src/commands/rpc.ts
+++ apps/blockchain-cpp/src/commands/rpc.ts
@@ -1,5 +1,11 @@
 import { Command } from '@oclif/core'
+import { exec } from 'child_process'
 
+interface argsType {
+  method: string
+  firstParam: string | undefined
+  secondParam: string | undefined
+}
 export class RPCMethod extends Command {
   static description = 'Run an RPC command to the blockchain'
 
@@ -16,6 +22,20 @@ export class RPCMethod extends Command {
   ]
 
   async run (): Promise<void> {
-    // const {args} = await this.parse(RPCMethod)
+    const { args }: {args: argsType} = await this.parse(RPCMethod)
+    // TODO: Refactor these two statements below - this is just to handle undefined cases explicitly for eslint
+    if (args.firstParam === undefined) {
+      args.firstParam = ''
+    }
+    if (args.secondParam === undefined) {
+      args.secondParam = ''
+    }
+    const script = exec(`docker exec -i 'defi-cli-container' defi-cli ${args.method} ${args.firstParam} ${args.secondParam}`)
+    script.stdout!.on('data', data => {
+      console.log(data.toString())
+    })
+    script.stderr!.on('data', data => {
+      console.log(data.toString())
+    })
   }
 }
--- apps/blockchain-cpp/src/commands/start.ts
+++ apps/blockchain-cpp/src/commands/start.ts
@@ -1,12 +1,13 @@
 import { Command } from '@oclif/core'
 import Dockerode from 'Dockerode'
 
-// TODO: OUTPUT ONLY STREAM AND FIX CALLBACK ERROR HANDLING
+// TODO: OUTPUT ONLY STREAM
 export class Start extends Command {
   static description = 'Start a DeFiChain Node with or without linking a local snapshot repository'
 
   static args = [
-    { name: 'snapshot', description: 'Absolute Path to local snapshot to use and upload into docker', required: false }
+    { name: 'snapshot', description: 'Absolute Path to local snapshot to use and upload into docker', required: false },
+    { name: 'attach', description: 'if true - attaches terminal to docker container', required: false }
   ]
 
   async run (): Promise<void> {
@@ -17,12 +18,13 @@ export class Start extends Command {
 
     const startOptions = {
       Image: 'defi/defichain:2.6.0',
+      name: 'defi-cli-container',
       AttachStdin: false,
-      AttachStdout: true,
-      AttachStderr: true,
+      AttachStdout: false,
+      AttachStderr: false,
       Tty: false,
-      OpenStdin: true,
-      StdinOnce: true,
+      OpenStdin: false,
+      StdinOnce: false,
       HostConfig: {}
     }
 
@@ -35,26 +37,24 @@ export class Start extends Command {
           `${snapshot}:/data`
         ]
       }
-      // TODO: allow user to get into the docker container CLI in seperate command
-      // const container = docker.getContainer('defisnapshot_defi-blockchain')
-      // container.attach({stream:true, stdout:true, stderr:true}, function handler(err, stream) {
-      //   container.modem.demuxStream(stream, process.stdout, process.stderr);
-      // })
     }
     docker.createContainer(startOptions).then(
       (container) => {
         if (container !== undefined) {
           container.start()
             .then(() => {
-              console.log('The local node has successfully booted up! :)')
-              console.log('Type defi-cli --help to see the full list of commands.')
-              console.log('Happy hacking!')
               // TODO: Allow streaming of Docker Container output while keeping input in our CLI
-              container.attach({ stream: true, stdout: true, stderr: true })
-                .then((stream) => {
-                  container.modem.demuxStream(stream, process.stdout, process.stderr)
-                })
-                .catch((err) => console.log(err))
+              if (args.attach === 'true') {
+                container.attach({ stream: true, stdout: true, stderr: true, stdin: true })
+                  .then((stream) => {
+                    stream.pipe(process.stdout)
+                  })
+                  .catch((err) => console.log(err))
+              } else {
+                console.log('The local node has successfully booted up! :)')
+                console.log('Type defi-cli --help to see the full list of commands.')
+                console.log('Happy hacking!')
+              }
             })
             .catch((err) => console.log(err))
         } else {
@@ -62,26 +62,5 @@ export class Start extends Command {
         }
       }
     ).catch((err) => console.log(err))
-    // docker.createContainer(startOptions, ((err:string, container) => {
-    //       if (container !== undefined) {
-    //         container.start(function(err: string): void {
-    //           if (err) {
-    //             console.log (err)
-    //           }
-    //           else {
-    //             console.log('The local node has successfully booted up! :)')
-    //             console.log('Type defi-cli --help to see the full list of commands.')
-    //             console.log('Happy hacking!')
-    //             // TODO: Allow streaming of Docker Container output while keeping input in our CLI
-    //             container.attach({stream: true, stdout: true, stderr: true}, function (err, stream) {
-    //               container.modem.demuxStream(stream, process.stdout, process.stderr);
-    //             });
-    //           }
-    //         })
-    //       }
-    //       else {
-    //         console.log("ERR: Container is undefined. Please try again.")
-    //       }
-    //   }))
   }
 }
