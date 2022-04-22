da302e44eca51a0aa8af07ae72e4e2cf72dae192
chen <23054115+cwkang1998@users.noreply.github.com>
2022-04-22 17:05:54 +0800

    chore(jellyfish-api-core): refactor use of `TestingGroup`

--- packages/jellyfish-api-core/__tests__/category/vault/estimateVault.test.ts
+++ packages/jellyfish-api-core/__tests__/category/vault/estimateVault.test.ts
@@ -1,38 +1,38 @@
 import { MasterNodeRegTestContainer } from '@defichain/testcontainers'
 import BigNumber from 'bignumber.js'
-import { TestingGroup } from '@defichain/jellyfish-testing'
+import { Testing } from '@defichain/jellyfish-testing'
 import { RpcApiError } from '@defichain/jellyfish-api-core'
 import { TokenInfo } from '@defichain/jellyfish-api-core/dist/category/token'
 
 describe('Vault estimateVault', () => {
-  const tGroup = TestingGroup.create(1, i => new MasterNodeRegTestContainer())
+  const testing = Testing.create(new MasterNodeRegTestContainer())
 
-  async function setup (tGroup: TestingGroup): Promise<void> {
+  async function setup (testing: Testing): Promise<void> {
     // token setup
-    const collateralAddress = await tGroup.get(0).container.getNewAddress()
-    await tGroup.get(0).token.dfi({ address: collateralAddress, amount: 30000 })
-    await tGroup.get(0).generate(1)
+    const collateralAddress = await testing.container.getNewAddress()
+    await testing.token.dfi({ address: collateralAddress, amount: 30000 })
+    await testing.generate(1)
     // Setup collateral token
-    await tGroup.get(0).token.create({ symbol: 'BTC', collateralAddress })
-    await tGroup.get(0).generate(1)
-    await tGroup.get(0).token.mint({ symbol: 'BTC', amount: 20000 })
-    await tGroup.get(0).generate(1)
+    await testing.token.create({ symbol: 'BTC', collateralAddress })
+    await testing.generate(1)
+    await testing.token.mint({ symbol: 'BTC', amount: 20000 })
+    await testing.generate(1)
     // Setup non collateral token
-    await tGroup.get(0).token.create({ symbol: 'DOGE', collateralAddress })
-    await tGroup.get(0).generate(1)
+    await testing.token.create({ symbol: 'DOGE', collateralAddress })
+    await testing.generate(1)
 
     // oracle setup
-    const addr = await tGroup.get(0).generateAddress()
+    const addr = await testing.generateAddress()
     const priceFeeds = [
       { token: 'DFI', currency: 'USD' },
       { token: 'BTC', currency: 'USD' },
       { token: 'TSLA', currency: 'USD' },
       { token: 'MSFT', currency: 'USD' }
     ]
-    const oracleId = await tGroup.get(0).rpc.oracle.appointOracle(addr, priceFeeds, { weightage: 1 })
-    await tGroup.get(0).generate(1)
+    const oracleId = await testing.rpc.oracle.appointOracle(addr, priceFeeds, { weightage: 1 })
+    await testing.generate(1)
     const oracleTickTimestamp = Math.floor(new Date().getTime() / 1000)
-    await tGroup.get(0).rpc.oracle.setOracleData(oracleId, oracleTickTimestamp, {
+    await testing.rpc.oracle.setOracleData(oracleId, oracleTickTimestamp, {
       prices: [
         { tokenAmount: '1@DFI', currency: 'USD' },
         { tokenAmount: '10000@BTC', currency: 'USD' },
@@ -40,74 +40,73 @@ describe('Vault estimateVault', () => {
         { tokenAmount: '5@MSFT', currency: 'USD' }
       ]
     })
-    await tGroup.get(0).generate(1)
+    await testing.generate(1)
 
     // collateral token
-    await tGroup.get(0).rpc.loan.setCollateralToken({
+    await testing.rpc.loan.setCollateralToken({
       token: 'DFI',
       factor: new BigNumber(1),
       fixedIntervalPriceId: 'DFI/USD'
     })
-    await tGroup.get(0).generate(1)
+    await testing.generate(1)
 
     // collateral token
-    await tGroup.get(0).rpc.loan.setCollateralToken({
+    await testing.rpc.loan.setCollateralToken({
       token: 'BTC',
       factor: new BigNumber(0.5),
       fixedIntervalPriceId: 'BTC/USD'
     })
-    await tGroup.get(0).generate(1)
+    await testing.generate(1)
 
     // loan token
-    await tGroup.get(0).rpc.loan.setLoanToken({
+    await testing.rpc.loan.setLoanToken({
       symbol: 'TSLA',
       fixedIntervalPriceId: 'TSLA/USD'
     })
-    await tGroup.get(0).generate(1)
-    await tGroup.get(0).rpc.loan.setLoanToken({
+    await testing.generate(1)
+    await testing.rpc.loan.setLoanToken({
       symbol: 'MSFT',
       fixedIntervalPriceId: 'MSFT/USD'
     })
-    await tGroup.get(0).generate(1)
+    await testing.generate(1)
 
-    await tGroup.waitForSync()
-    await tGroup.get(0).container.waitForPriceValid('TSLA/USD')
-    await tGroup.get(0).container.waitForPriceValid('MSFT/USD')
+    await testing.container.waitForPriceValid('TSLA/USD')
+    await testing.container.waitForPriceValid('MSFT/USD')
   }
 
   beforeAll(async () => {
-    await tGroup.start()
-    await tGroup.get(0).container.waitForWalletCoinbaseMaturity()
-    await setup(tGroup)
+    await testing.container.start()
+    await testing.container.waitForWalletCoinbaseMaturity()
+    await setup(testing)
   })
 
   afterAll(async () => {
-    await tGroup.stop()
+    await testing.container.stop()
   })
 
   it('should fail if collateralAmounts array contains non collateral token', async () => {
-    const tokenInfo: Record<string, TokenInfo> = await tGroup.get(0).container.call('gettoken', ['DOGE'])
-    const promise = tGroup.get(0).rpc.vault.estimateVault(['5000@DOGE'], ['100@TSLA'])
+    const tokenInfo: Record<string, TokenInfo> = await testing.container.call('gettoken', ['DOGE'])
+    const promise = testing.rpc.vault.estimateVault(['5000@DOGE'], ['100@TSLA'])
     await expect(promise).rejects.toThrow(RpcApiError)
     await expect(promise).rejects.toThrow(`Token with id (${Object.keys(tokenInfo)[0]}) is not a valid collateral!`)
   })
 
   it('should fail if collateralAmounts array contains non collateral token (loan token)', async () => {
-    const tokenInfo: Record<string, TokenInfo> = await tGroup.get(0).container.call('gettoken', ['MSFT'])
-    const promise = tGroup.get(0).rpc.vault.estimateVault(['500@MSFT'], ['100@TSLA'])
+    const tokenInfo: Record<string, TokenInfo> = await testing.container.call('gettoken', ['MSFT'])
+    const promise = testing.rpc.vault.estimateVault(['500@MSFT'], ['100@TSLA'])
     await expect(promise).rejects.toThrow(RpcApiError)
     await expect(promise).rejects.toThrow(`Token with id (${Object.keys(tokenInfo)[0]}) is not a valid collateral!`)
   })
 
   it('should fail if loanAmounts array contains non loan token', async () => {
-    const tokenInfo: Record<string, TokenInfo> = await tGroup.get(0).container.call('gettoken', ['BTC'])
-    const promise = tGroup.get(0).rpc.vault.estimateVault(['10000@DFI'], ['1@BTC'])
+    const tokenInfo: Record<string, TokenInfo> = await testing.container.call('gettoken', ['BTC'])
+    const promise = testing.rpc.vault.estimateVault(['10000@DFI'], ['1@BTC'])
     await expect(promise).rejects.toThrow(RpcApiError)
     await expect(promise).rejects.toThrow(`Token with id (${Object.keys(tokenInfo)[0]}) is not a loan token!`)
   })
 
   it('should estimateVault for empty values', async () => {
-    const estimation = await tGroup.get(0).rpc.vault.estimateVault([], [])
+    const estimation = await testing.rpc.vault.estimateVault([], [])
     expect(estimation.collateralValue).toStrictEqual(new BigNumber('0'))
     expect(estimation.loanValue).toStrictEqual(new BigNumber('0'))
     expect(estimation.informativeRatio).toStrictEqual(new BigNumber('-1'))
@@ -115,7 +114,7 @@ describe('Vault estimateVault', () => {
   })
 
   it('should estimateVault (overcollateralized)', async () => {
-    const estimation = await tGroup.get(0).rpc.vault.estimateVault(['10000@DFI', '1@BTC'], ['1000@MSFT'])
+    const estimation = await testing.rpc.vault.estimateVault(['10000@DFI', '1@BTC'], ['1000@MSFT'])
     expect(estimation.collateralValue).toStrictEqual(new BigNumber('15000')) // DFI is 1 to 1 USD, BTC is 10k USD with 0.5 factor making it 5000
     expect(estimation.loanValue).toStrictEqual(new BigNumber('5000')) // TSLA price/USD + MSFT price/USD = 1000 * 5
     expect(estimation.informativeRatio).toStrictEqual(new BigNumber('300')) // informativeRatio = collateralValue / loanValue = 15000 / 5000 * 100
@@ -123,7 +122,7 @@ describe('Vault estimateVault', () => {
   })
 
   it('should estimateVault (undercollateralized)', async () => {
-    const estimation = await tGroup.get(0).rpc.vault.estimateVault(['5000@DFI', '1@BTC'], ['5000@TSLA', '1000@MSFT'])
+    const estimation = await testing.rpc.vault.estimateVault(['5000@DFI', '1@BTC'], ['5000@TSLA', '1000@MSFT'])
     expect(estimation.collateralValue).toStrictEqual(new BigNumber('10000')) // DFI is 1 to 1 USD, BTC is 10k USD with 0.5 factor making it 5000
     expect(estimation.loanValue).toStrictEqual(new BigNumber('15000')) // TSLA price/USD + MSFT price/USD = 5000 * 2 + 1000 * 5
     expect(estimation.informativeRatio).toStrictEqual(new BigNumber('66.66666666')) // informativeRatio = collateralValue / loanValue = 10000 / 15000 * 100
@@ -131,7 +130,7 @@ describe('Vault estimateVault', () => {
   })
 
   it('should estimateVault with collateral only', async () => {
-    const estimation = await tGroup.get(0).rpc.vault.estimateVault(['5000@DFI', '1@BTC'], [])
+    const estimation = await testing.rpc.vault.estimateVault(['5000@DFI', '1@BTC'], [])
     expect(estimation.collateralValue).toStrictEqual(new BigNumber('10000')) // DFI is 1 to 1 USD, BTC is 10k USD with 0.5 factor making it 5000
     expect(estimation.loanValue).toStrictEqual(new BigNumber('0'))
     expect(estimation.informativeRatio).toStrictEqual(new BigNumber('-1')) // informativeRatio = collateralValue / loanValue = 10000 / 0 * 100
@@ -139,7 +138,7 @@ describe('Vault estimateVault', () => {
   })
 
   it('should estimateVault with loan only', async () => {
-    const estimation = await tGroup.get(0).rpc.vault.estimateVault([], ['5000@TSLA', '1000@MSFT'])
+    const estimation = await testing.rpc.vault.estimateVault([], ['5000@TSLA', '1000@MSFT'])
     expect(estimation.collateralValue).toStrictEqual(new BigNumber('0'))
     expect(estimation.loanValue).toStrictEqual(new BigNumber('15000')) // TSLA price/USD + MSFT price/USD = 5000 * 2 + 1000 * 5
     expect(estimation.informativeRatio).toStrictEqual(new BigNumber('0')) // informativeRatio = collateralValue / loanValue = 0 / 15000 * 100
