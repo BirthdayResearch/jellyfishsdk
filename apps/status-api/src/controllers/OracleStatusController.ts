fe890304b55c1f0fa6d31bf4accea87be5cc67d4
Nichelle <nichelle@cakedefi.com>
2022-04-25 21:10:57 +0800

    Refactor check for any ticker data for better readability

--- apps/status-api/src/controllers/OracleStatusController.ts
+++ apps/status-api/src/controllers/OracleStatusController.ts
@@ -34,15 +34,19 @@ export class OracleStatusController {
       const token = priceFeed.token
       const currency = priceFeed.currency
       const oraclePriceFeed = await this.client.oracles.getPriceFeed(id, token, currency, 1)
-      if (oraclePriceFeed.length !== 0) { // check if ticker is not returning any data, else move on to the next ticker
-        const nowEpoch = Date.now()
-        const latestPublishedTime = oraclePriceFeed[0].block.medianTime * 1000
-        const timeDiff = nowEpoch - latestPublishedTime
 
-        // check if ticker last published price within 60 min, else move on to next ticker
-        if (timeDiff <= (60 * 60 * 1000)) { // increasing to 60 min as there are occurrences where publishing txn did not go through (~3 times)
-          return { status: 'operational' }
-        }
+      // move on to the next ticker if oraclePriceFeed does not return any data
+      if (oraclePriceFeed.length === 0) {
+        continue
+      }
+
+      const nowEpoch = Date.now()
+      const latestPublishedTime = oraclePriceFeed[0].block.medianTime * 1000
+      const timeDiff = nowEpoch - latestPublishedTime
+
+      // check if ticker last published price within 60 min, else move on to next ticker
+      if (timeDiff <= (60 * 60 * 1000)) { // increasing to 60 min as there are occurrences where publishing txn did not go through (~3 times)
+        return { status: 'operational' }
       }
     }
     return { status: 'outage' } // return as outage if all tickers from this oracle does not fulfill above conditions
