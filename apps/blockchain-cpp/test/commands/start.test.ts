3a355713f9e166e9d3d42b03d5b6a02e68f89a34
JustinWong98 <justinwong8991@gmail.com>
2022-04-25 22:15:45 +0800

    two tests for start - debugging needed

--- apps/blockchain-cpp/test/commands/start.test.ts
+++ apps/blockchain-cpp/test/commands/start.test.ts
@@ -1,17 +1,31 @@
 import { Start } from '../../src/commands/start'
-import { exec } from 'child_process'
+import Dockerode from 'dockerode'
 // NOTE: you have to use the CLI to run tests - using IntelliJ (i.e. clicking it) to run it seems to pass in unwanted args
 const consoleSpy = jest.spyOn(console, 'log').mockImplementation()
+// TODO: Use .env for test purposes
+// const snapshotLocation: string = (process.env.SNAPSHOT as string)
+
 describe('start', () => {
+  const docker = new Dockerode()
   beforeEach(() => {
     consoleSpy.mockClear()
   })
-  afterEach(() => {
-    exec('docker rm $(docker stop defi-cli-container)')
-  })
+  // TODO: Figure out why this doesn't work
+  // afterEach(async () => {
+  //   const container = docker.getContainer('defi-cli-container')
+  //   await container.stop()
+  //   await container.remove()
+  // })
   it('should start container in detached mode', async () => {
     await Start.run([])
     // TODO: test directly if docker container starts?
+    expect(console.log).toHaveBeenLastCalledWith('Happy hacking!')
+    const container = docker.getContainer('defi-cli-container')
+    await container.stop()
+    await container.remove()
+  })
+  it('should start container with valid snapshot', async () => {
+    await Start.run(['/Users/justinwong/Documents/snapshot'])
     expect(console.log).toHaveBeenLastCalledWith('Happy hacking!')
   })
 })
