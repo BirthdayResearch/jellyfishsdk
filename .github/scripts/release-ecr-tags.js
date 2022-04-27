8b4af10de3a17aa263fd069db66c089cb3668894
Nichelle <nichelle@cakedefi.com>
2022-04-27 14:24:15 +0800

    Push image to ECR from GitHub Actions

---
+++ .github/scripts/release-ecr-tags.js
@@ -0,0 +1,21 @@
+/**
+ * Release Tags
+ *
+ * Creating release tag based on each release version for AWS ECR Public
+ *
+ */
+
+module.exports = ({ context }) => {
+  if (context.eventName === 'release') {
+    return getReleaseTag(context)
+  }
+  throw new Error('Release Violation: Could not determine the required release tags.')
+}
+
+function getReleaseTag(domain, app, context) {
+  const semver = context.payload.release.tag_name.replace('v', '')
+  if (semver.match(/^[0-9]+\.[0-9]+\.[0-9]+$/) === false) {
+    throw new Error(`Release Violation: Provided version '${semver}' is not valid semver.`)
+  }
+  return semver
+}
