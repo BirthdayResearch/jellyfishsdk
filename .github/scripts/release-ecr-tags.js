2aee9a08a638a475047ef43fb9d5b711f281a577
Nichelle <nichelle@cakedefi.com>
2022-05-04 09:32:07 +0800

    Fix release tag for ecr

--- .github/scripts/release-ecr-tags.js
+++ .github/scripts/release-ecr-tags.js
@@ -12,9 +12,9 @@ module.exports = ({ context }) => {
   throw new Error('Release Violation: Could not determine the required release tags.')
 }
 
-function getReleaseTag(domain, app, context) {
-  const semver = context.payload.release.tag_name.replace('v', '')
-  if (semver.match(/^[0-9]+\.[0-9]+\.[0-9]+$/) === false) {
+function getReleaseTag(context) {
+  const semver = context.payload.release.tag_name
+  if (semver.match(/^v[0-9]+\.[0-9]+\.[0-9]+$/) === null) {
     throw new Error(`Release Violation: Provided version '${semver}' is not valid semver.`)
   }
   return semver
