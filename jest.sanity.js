41197309c17ff9a6710572357b4e9f2454860564
Eli <elicodesforfun@gmail.com>
2022-05-18 14:36:06 +0800

    chore(sanity-testing): increase sanity tests timeout to 5 mins

--- jest.sanity.js
+++ jest.sanity.js
@@ -4,5 +4,6 @@ module.exports = {
   ...config,
   testRegex: '((\\.|/)(sanity))\\.ts$',
   testPathIgnorePatterns: [],
-  globalSetup: './jest.sanity.setup.js'
+  globalSetup: './jest.sanity.setup.js',
+  testTimeout: 300000
 }
