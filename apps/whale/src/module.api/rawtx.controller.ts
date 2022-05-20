5e0fa32a250d18a0136aeb301a354e31668e4fee
Eli <elicodesforfun@gmail.com>
2022-05-20 14:58:20 +0800

    chore(whale-migration): revert minor changes

--- apps/whale/src/module.api/rawtx.controller.ts
+++ apps/whale/src/module.api/rawtx.controller.ts
@@ -11,8 +11,8 @@ import {
   OP_CODES,
   OP_DEFI_TX
 } from '@defichain/jellyfish-transaction'
-import { RpcApiError } from '@defichain/jellyfish-api-core'
 import { DeFiDCache } from './cache/defid.cache'
+import { RpcApiError } from '@defichain/jellyfish-api-core'
 
 class RawTxDto {
   @IsNotEmpty()
