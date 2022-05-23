6d96a0aeba167663ab7eef8678b84b19747d8152
Nichelle <nichelle@cakedefi.com>
2022-05-23 22:10:41 +0800

    Test image for new configurations

--- .github/scripts/release-ecr-tags.js
+++ .github/scripts/release-ecr-tags.js
@@ -6,6 +6,10 @@
  */
 
 module.exports = ({ context }) => {
+  if (context.eventName === 'pull_request'){
+    return '1.0.0'
+  }
+
   if (context.eventName === 'release') {
     return getReleaseTag(context)
   }
