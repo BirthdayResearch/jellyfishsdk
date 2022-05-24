e965ad13d3baea4a2123deec6a0eaa7cd2303e54
jingyi2811 <jingyi2811@gmail.com>
2022-05-24 16:20:48 +0800

    Update docker image

--- packages/testcontainers/src/containers/DeFiDContainer.ts
+++ packages/testcontainers/src/containers/DeFiDContainer.ts
@@ -35,7 +35,7 @@ export abstract class DeFiDContainer extends DockerContainer {
     if (process?.env?.DEFICHAIN_DOCKER_IMAGE !== undefined) {
       return process.env.DEFICHAIN_DOCKER_IMAGE
     }
-    return 'defi/defichain:master-507dcc90b'
+    return 'defi/defichain:HEAD-8bb48a108'
   }
 
   public static readonly DefaultStartOptions = {
