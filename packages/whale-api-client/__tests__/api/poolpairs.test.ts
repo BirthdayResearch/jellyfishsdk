8f9afb4a601b563e592c8cd27b6a095aa81ee8b0
canonbrother <w.canonbrother@gmail.com>
2022-06-14 11:48:28 +0800

    fix poolpair.test.ts

--- packages/whale-api-client/__tests__/api/poolpairs.test.ts
+++ packages/whale-api-client/__tests__/api/poolpairs.test.ts
@@ -779,8 +779,17 @@ describe('poolswap aggregated', () => {
         bucket: expect.any(Number),
         id: expect.any(String),
         key: '10-86400'
+      },
+      {
+        aggregated: {
+          amounts: {},
+          usd: 0
+        },
+        block: expect.any(Object),
+        bucket: expect.any(Number),
+        id: expect.any(String),
+        key: '10-86400'
       }
-
     ])
 
     const hourAggregated: ApiPagedResponse<PoolSwapAggregatedData> = await client.poolpairs.listPoolSwapAggregates('10', PoolSwapAggregatedInterval.ONE_HOUR, 3)
