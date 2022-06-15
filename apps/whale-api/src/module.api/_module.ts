bf53fcb57d3bc19c7ffbdba3d4db03e6524725b5
Yu Siang <39639836+chanyusiang@users.noreply.github.com>
2022-06-15 21:05:40 +0800

    feat(migrate-getsubgraphswaps): Migrated getsubgraphswaps logic (#1514)
    * feat(migrate-getsubgraphswaps): Migrated getsubgraphswaps logic from legacy-api to whale-api
    
    * chore(whale-api/legacy): add tests
    
    * chore(whale-api/legacy): code cleanup
    
    * remove console.log
    
    * remove unused imports
    
    * skip prod-dependent getsubgraphswaps tests
    
    * remove trailing forward slash in url
    
    * add network query param to request
    
    * change to use simple cache
    
    * add default value for pagination.limit and default to empty array for undefined history.amounts
    
    * bypass response interceptor
    
    * limit to 20 and disable cache
    
    * move logic into LegacySubgraphService
    
    * make injectable and reorg code
    
    * restore caching and change SupportedNetwork to NetworkName instead
    
    * remove unused cache from legacy-api
    
    * rename to LegacyCache and change it to use an indepedent underlying cache
    
    * set default to 20
    
    Co-authored-by: Eli <elicodesforfun@gmail.com>

--- apps/whale-api/src/module.api/_module.ts
+++ apps/whale-api/src/module.api/_module.ts
@@ -9,7 +9,7 @@ import { PoolPairController } from './poolpair.controller'
 import { PoolPairService } from './poolpair.service'
 import { MasternodeService } from './masternode.service'
 import { DeFiDCache } from './cache/defid.cache'
-import { SemaphoreCache } from '@defichain-apps/libs/caches'
+import { SemaphoreCache, LegacyCache } from '@defichain-apps/libs/caches'
 import { ExceptionInterceptor } from './interceptors/exception.interceptor'
 import { ResponseInterceptor } from './interceptors/response.interceptor'
 import { TokenController } from './token.controller'
@@ -31,6 +31,8 @@ import { LoanController } from './loan.controller'
 import { LoanVaultService } from './loan.vault.service'
 import { PoolSwapPathFindingService } from './poolswap.pathfinding.service'
 import { PoolPairPricesService } from './poolpair.prices.service'
+import { LegacyController } from './legacy.controller'
+import { LegacySubgraphService } from './legacy.subgraph.service'
 
 /**
  * Exposed ApiModule for public interfacing
@@ -51,7 +53,8 @@ import { PoolPairPricesService } from './poolpair.prices.service'
     StatsController,
     FeeController,
     RawtxController,
-    LoanController
+    LoanController,
+    LegacyController
   ],
   providers: [
     {
@@ -76,6 +79,7 @@ import { PoolPairPricesService } from './poolpair.prices.service'
     },
     DeFiDCache,
     SemaphoreCache,
+    LegacyCache,
     PoolPairService,
     PoolSwapPathFindingService,
     PoolPairPricesService,
@@ -92,7 +96,8 @@ import { PoolPairPricesService } from './poolpair.prices.service'
         }
       },
       inject: [ConfigService]
-    }
+    },
+    LegacySubgraphService
   ]
 })
 export class ApiModule {
