22c43f9dd981071926c7bc89fb05566bcf4339b0
Chanaka Sameera <kchanakasameera@gmail.com>
2022-06-17 08:30:43 +0530

    Test ain#1348

--- packages/testcontainers/src/containers/DeFiDContainer.ts
+++ packages/testcontainers/src/containers/DeFiDContainer.ts
@@ -35,7 +35,7 @@ export abstract class DeFiDContainer extends DockerContainer {
     if (process?.env?.DEFICHAIN_DOCKER_IMAGE !== undefined) {
       return process.env.DEFICHAIN_DOCKER_IMAGE
     }
-    return 'defi/defichain:HEAD-85f3701e3'
+    return 'defi/defichain:HEAD-f10c47513'
   }
 
   public static readonly DefaultStartOptions = {
