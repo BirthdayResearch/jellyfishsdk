9158478a2b06b0c1816e3f0d90722e0a065856dd
Chanaka Sameera <kchanakasameera@gmail.com>
2022-06-22 09:59:49 +0530

    Update the ain image

--- packages/testcontainers/src/containers/DeFiDContainer.ts
+++ packages/testcontainers/src/containers/DeFiDContainer.ts
@@ -35,7 +35,7 @@ export abstract class DeFiDContainer extends DockerContainer {
     if (process?.env?.DEFICHAIN_DOCKER_IMAGE !== undefined) {
       return process.env.DEFICHAIN_DOCKER_IMAGE
     }
-    return 'defi/defichain:master-27e9965ef'
+    return 'defi/defichain:HEAD-181332b77'
   }
 
   public static readonly DefaultStartOptions = {
