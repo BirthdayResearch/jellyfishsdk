08b9cd96de05722e68af18c0ae79fca58e5c2c09
Fuxing Loh <git@fuxing.dev>
2022-06-27 21:43:32 +0800

    fixed dftx_balance.ts

--- packages/jellyfish-transaction/src/script/dftx/dftx_balance.ts
+++ packages/jellyfish-transaction/src/script/dftx/dftx_balance.ts
@@ -3,17 +3,22 @@ import { BufferComposer, ComposableBuffer } from '@defichain/jellyfish-buffer'
 import { Script } from '../../tx'
 import { CScript } from '../../tx_composer'
 
-export interface TokenBalance {
+/**
+ * Known as "struct CBalances" in cpp.
+ */
+export interface TokenBalanceUInt32 {
   token: number // ---------------------| 4 bytes unsigned
   amount: BigNumber // -----------------| 8 bytes unsigned
 }
 
 /**
+ * Known as "struct CBalances" in cpp.
+ *
  * Composable TokenBalance, C stands for Composable.
  * Immutable by design, bi-directional fromBuffer, toBuffer deep composer.
  */
-export class CTokenBalance extends ComposableBuffer<TokenBalance> {
-  composers (tb: TokenBalance): BufferComposer[] {
+export class CTokenBalance extends ComposableBuffer<TokenBalanceUInt32> {
+  composers (tb: TokenBalanceUInt32): BufferComposer[] {
     return [
       ComposableBuffer.uInt32(() => tb.token, v => tb.token = v),
       ComposableBuffer.satoshiAsBigNumber(() => tb.amount, v => tb.amount = v)
@@ -23,7 +28,7 @@ export class CTokenBalance extends ComposableBuffer<TokenBalance> {
 
 export interface ScriptBalances {
   script: Script // --------------------| n = VarUInt{1-9 bytes}, + n bytes
-  balances: TokenBalance[] // ----------| c = VarUInt{1-9 bytes}, + c x TokenBalance
+  balances: TokenBalanceUInt32[] // ----------| c = VarUInt{1-9 bytes}, + c x TokenBalance
 }
 
 /**
@@ -40,21 +45,23 @@ export class CScriptBalances extends ComposableBuffer<ScriptBalances> {
 }
 
 /**
- * TokenBalanceVarInt
+ * Known as "struct CTokenAmount" in cpp.
  */
 export interface TokenBalanceVarInt {
-  token: number // ---------------------| VarUInt{1-9 bytes}
+  token: number // ---------------------| VarInt{MSB-b128}
   amount: BigNumber // -----------------| 8 bytes unsigned
 }
 
 /**
+ * Known as "struct CTokenAmount" in cpp.
+ *
  * Composable TokenBalanceVarInt, C stands for Composable.
  * Immutable by design, bi-directional fromBuffer, toBuffer deep composer.
  */
 export class CTokenBalanceVarInt extends ComposableBuffer<TokenBalanceVarInt> {
   composers (tb: TokenBalanceVarInt): BufferComposer[] {
     return [
-      ComposableBuffer.compactSize(() => tb.token, v => tb.token = v),
+      ComposableBuffer.varInt(() => tb.token, v => tb.token = v),
       ComposableBuffer.satoshiAsBigNumber(() => tb.amount, v => tb.amount = v)
     ]
   }
