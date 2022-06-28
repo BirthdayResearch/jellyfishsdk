4003877b398e848822997c5082af922ebee465ab
canonbrother <w.canonbrother@gmail.com>
2022-06-28 23:45:08 +0800

    merge incoming change - use 2.9.0

--- packages/testcontainers/src/containers/DeFiDContainer.ts
+++ packages/testcontainers/src/containers/DeFiDContainer.ts
@@ -35,7 +35,7 @@ export abstract class DeFiDContainer extends DockerContainer {
     if (process?.env?.DEFICHAIN_DOCKER_IMAGE !== undefined) {
       return process.env.DEFICHAIN_DOCKER_IMAGE
     }
-    return 'defi/defichain:master-1a616920f'
+    return 'defi/defichain:2.9.0'
   }
 
   public static readonly DefaultStartOptions = {
