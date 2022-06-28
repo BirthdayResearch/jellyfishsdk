b58ceded1518369e27da9a1edcf7ca928393ea58
canonbrother <w.canonbrother@gmail.com>
2022-06-28 17:03:29 +0800

    master-1a616920f

--- packages/testcontainers/src/containers/DeFiDContainer.ts
+++ packages/testcontainers/src/containers/DeFiDContainer.ts
@@ -35,7 +35,7 @@ export abstract class DeFiDContainer extends DockerContainer {
     if (process?.env?.DEFICHAIN_DOCKER_IMAGE !== undefined) {
       return process.env.DEFICHAIN_DOCKER_IMAGE
     }
-    return 'defi/defichain:master-27e9965ef'
+    return 'defi/defichain:master-1a616920f'
   }
 
   public static readonly DefaultStartOptions = {
