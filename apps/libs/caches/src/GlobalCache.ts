4688dc68cc98969ad8015c461ba3a7170cc3c01a
pierregee <pierre.gabaldon@gmail.com>
2022-07-06 13:35:09 +0800

    remove defid cache enum

--- apps/libs/caches/src/GlobalCache.ts
+++ apps/libs/caches/src/GlobalCache.ts
@@ -11,8 +11,7 @@ export enum CachePrefix {
   TOKEN_INFO_SYMBOL = 2,
   LOAN_SCHEME_INFO = 3,
   POOL_PAIRS = 4,
-  ALL_TOKEN_INFO = 5,
-  DEX_FEES = 6
+  ALL_TOKEN_INFO = 5
 }
 
 export class GlobalCache {
