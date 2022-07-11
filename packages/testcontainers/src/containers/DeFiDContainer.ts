8fccf28c7ae4ff6e94304a4a0ad6a63ad95ad725
Chanaka Sameera <kchanakasameera@gmail.com>
2022-07-11 22:32:47 +0200

    Added latest ain#1342 image

--- packages/testcontainers/src/containers/DeFiDContainer.ts
+++ packages/testcontainers/src/containers/DeFiDContainer.ts
@@ -35,7 +35,7 @@ export abstract class DeFiDContainer extends DockerContainer {
     if (process?.env?.DEFICHAIN_DOCKER_IMAGE !== undefined) {
       return process.env.DEFICHAIN_DOCKER_IMAGE
     }
-    return 'defi/defichain:HEAD-78944b877'
+    return 'defi/defichain:HEAD-7a791bccd'
   }
 
   public static readonly DefaultStartOptions = {
