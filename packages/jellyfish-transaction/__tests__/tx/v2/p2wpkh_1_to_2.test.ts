44fce0b469aaff91ba8cab6790bdca7dd3ef1a73
Fuxing Loh <git@fuxing.dev>
2022-08-14 18:21:39 +0800

    instead create @defichain/bitcoinjs, updated tests in jellyfish-transaction to import the original bitcoinjs-lib

--- packages/jellyfish-transaction/__tests__/tx/v2/p2wpkh_1_to_2.test.ts
+++ packages/jellyfish-transaction/__tests__/tx/v2/p2wpkh_1_to_2.test.ts
@@ -4,31 +4,35 @@ import { MasterNodeRegTestContainer } from '@defichain/testcontainers'
 import { Testing } from '@defichain/jellyfish-testing'
 import { getNetworkBitcoinJsLib } from '@defichain/jellyfish-network'
 import { WIF, Elliptic } from '@defichain/jellyfish-crypto'
-import { getECPair, BitcoinJsLib } from '@defichain/bitcoinjs'
+import * as bitcoin from 'bitcoinjs-lib'
+import { ECPairAPI, ECPairFactory } from 'ecpair'
+import * as ecc from 'tiny-secp256k1'
 
 const unsigned = '0200000001e28bf7657f36b80bcfc7854a25ff0b2e0adbaffb95870b8387a637fd1e26d0970000000000ffffffff02804a5d05000000001600144ab4391ce5a732e36139e72d79a28e01b7b080348085b50d0000000016001425a544c073cbca4e88d59f95ccd52e584c7e6a8200000000'
 const signed = '02000000000101e28bf7657f36b80bcfc7854a25ff0b2e0adbaffb95870b8387a637fd1e26d0970000000000ffffffff02804a5d05000000001600144ab4391ce5a732e36139e72d79a28e01b7b080348085b50d0000000016001425a544c073cbca4e88d59f95ccd52e584c7e6a820247304402203649fd61d1dd402969bdc8c74c704ab7ea3951916744901b764809f208da0c7b022030db92126984acda41dc8379c100e4e997e6f6face2b16b486037dd8cc662c31012103c1f7238aa1d97af163018b76afc000f378698da9537cf6ad7dc902643a3dd5d100000000'
 
-it('bi-directional unsigned buffer', () => {
-  const fromBuffer = SmartBuffer.fromBuffer(Buffer.from(unsigned, 'hex'))
-  const tx = new CTransaction(fromBuffer)
+describe('encoding', () => {
+  it('bi-directional unsigned buffer', () => {
+    const fromBuffer = SmartBuffer.fromBuffer(Buffer.from(unsigned, 'hex'))
+    const tx = new CTransaction(fromBuffer)
 
-  const toBuffer = new SmartBuffer()
-  tx.toBuffer(toBuffer)
-  expect(toBuffer.toBuffer().toString('hex')).toStrictEqual(unsigned)
+    const toBuffer = new SmartBuffer()
+    tx.toBuffer(toBuffer)
+    expect(toBuffer.toBuffer().toString('hex')).toStrictEqual(unsigned)
+  })
+
+  it('bi-directional signed buffer', () => {
+    const fromBuffer = SmartBuffer.fromBuffer(Buffer.from(signed, 'hex'))
+    const tx = new CTransactionSegWit(fromBuffer)
+
+    const toBuffer = new SmartBuffer()
+    tx.toBuffer(toBuffer)
+
+    expect(toBuffer.toBuffer().toString('hex')).toStrictEqual(signed)
+  })
 })
 
-it('bi-directional signed buffer', () => {
-  const fromBuffer = SmartBuffer.fromBuffer(Buffer.from(signed, 'hex'))
-  const tx = new CTransactionSegWit(fromBuffer)
-
-  const toBuffer = new SmartBuffer()
-  tx.toBuffer(toBuffer)
-
-  expect(toBuffer.toBuffer().toString('hex')).toStrictEqual(signed)
-})
-
-describe('e2e', () => { // submit to testcontainer
+describe('e2e submit to testcontainers', () => {
   const container = new MasterNodeRegTestContainer()
   const testing = Testing.create(container)
 
@@ -41,6 +45,7 @@ describe('e2e', () => { // submit to testcontainer
     await testing.container.stop()
   })
 
+  const ECPair: ECPairAPI = ECPairFactory(ecc)
   const RegTest = getNetworkBitcoinJsLib('regtest')
   const keySet = {
     bech32: 'bcrt1qykj5fsrne09yazx4n72ue4fwtpx8u65zac9zhn',
@@ -67,14 +72,17 @@ describe('e2e', () => { // submit to testcontainer
     expect(unspent[0].spendable).toStrictEqual(true)
     expect(unspent[0].solvable).toStrictEqual(true)
 
-    const { address, output } = BitcoinJsLib.getP2WPKH(pubKey, RegTest)
+    const { address, output } = bitcoin.payments.p2wpkh({
+      pubkey: pubKey,
+      network: RegTest
+    })
     expect(address).toStrictEqual(keySet.bech32)
 
-    const keyPair = getECPair(privKey, {
+    const keyPair = ECPair.fromPrivateKey(privKey, {
       network: RegTest
     })
 
-    const psbt = BitcoinJsLib.psbt(RegTest)
+    const psbt = new bitcoin.Psbt({ network: RegTest })
       .addInput({
         hash: txid,
         index: vout,
