2411b4a3b9acf220da233e1c529e664129941e22
Fuxing Loh <git@fuxing.dev>
2022-08-31 10:57:03 +0800

    set docker tag to 2.10.0

--- packages/testcontainers/src/containers/DeFiDContainer.ts
+++ packages/testcontainers/src/containers/DeFiDContainer.ts
@@ -35,7 +35,7 @@ export abstract class DeFiDContainer extends DockerContainer {
     if (process?.env?.DEFICHAIN_DOCKER_IMAGE !== undefined) {
       return process.env.DEFICHAIN_DOCKER_IMAGE
     }
-    return 'defi/defichain:HEAD-df29a86b9'
+    return 'defi/defichain:2.10.0'
   }
 
   public static readonly DefaultStartOptions = {
