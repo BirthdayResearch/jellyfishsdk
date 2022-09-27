715c841861d54c559552229b65aa65979107fecc
Delphine <delphinekoh@hotmail.com>
2022-09-27 14:35:55 +0800

    separate tests describe block  based on scenario

--- packages/jellyfish-api-core/__tests__/category/blockchain/getMempoolAncestors.test.ts
+++ packages/jellyfish-api-core/__tests__/category/blockchain/getMempoolAncestors.test.ts
@@ -2,11 +2,9 @@ import { MasterNodeRegTestContainer } from '@defichain/testcontainers'
 import { Testing } from '@defichain/jellyfish-testing'
 import BigNumber from 'bignumber.js'
 
-describe('getMempoolAncestors', () => {
+describe('transactions without ancestors', () => {
   const container = new MasterNodeRegTestContainer()
   const testing = Testing.create(container)
-  const address = 'mwsZw8nF7pKxWH8eoKL9tPxTpaFkz7QeLU'
-  const amount = 0.003
 
   beforeAll(async () => {
     await testing.container.start()
@@ -36,58 +34,67 @@ describe('getMempoolAncestors', () => {
       .rejects
       .toThrow('RpcApiError: \'parameter 1 must be of length 64 (not 17, for \'invalidtxidstring\')\', code: -8, method: getmempoolancestors')
   })
+})
 
-  describe('transactions with ancestors', () => {
-    beforeAll(async () => {
-      for (let i = 0; i < 3; i++) {
-        await testing.rpc.wallet.sendToAddress(address, amount)
-      }
-    })
+describe('transactions with ancestors', () => {
+  const container = new MasterNodeRegTestContainer()
+  const testing = Testing.create(container)
 
-    it('should return JSON object if verbose is true', async () => {
-      const txId = await testing.rpc.wallet.sendToAddress(address, amount)
-      const mempoolAncestors = await testing.rpc.blockchain.getMempoolAncestors(txId, true)
-      const keys = Object.keys(mempoolAncestors)
-      expect(keys.length).toBeGreaterThan(0)
-      for (const key of keys) {
-        expect(mempoolAncestors[key]).toStrictEqual({
-          fees: expect.any(Object),
-          vsize: expect.any(BigNumber),
-          weight: expect.any(BigNumber),
-          fee: expect.any(BigNumber),
-          modifiedfee: expect.any(BigNumber),
-          time: expect.any(BigNumber),
-          height: expect.any(BigNumber),
-          descendantcount: expect.any(BigNumber),
-          descendantsize: expect.any(BigNumber),
-          descendantfees: expect.any(BigNumber),
-          ancestorcount: expect.any(BigNumber),
-          ancestorsize: expect.any(BigNumber),
-          ancestorfees: expect.any(BigNumber),
-          wtxid: expect.any(String),
-          depends: expect.any(Array),
-          spentby: expect.any(Array),
-          'bip125-replaceable': expect.any(Boolean)
-        })
-      }
-    })
+  beforeAll(async () => {
+    await testing.container.start()
+    await testing.container.waitForWalletCoinbaseMaturity()
+    for (let i = 0; i < 3; i++) {
+      await testing.rpc.wallet.sendToAddress('mwsZw8nF7pKxWH8eoKL9tPxTpaFkz7QeLU', 0.003)
+    }
+  })
 
-    it('should return array of transaction ids if verbose is false', async () => {
-      const txId = await testing.rpc.wallet.sendToAddress(address, amount)
-      const mempoolAncestors = await testing.rpc.blockchain.getMempoolAncestors(txId, false)
-      expect(mempoolAncestors.length).toBeGreaterThan(0)
-      for (const ancestorId of mempoolAncestors) {
-        expect(ancestorId).toStrictEqual(expect.stringMatching(/^[0-9a-f]{64}$/))
-      }
-    })
+  afterAll(async () => {
+    await testing.container.stop()
+  })
 
-    it('should return array of transaction ids if verbose is undefined', async () => {
-      const txId = await testing.rpc.wallet.sendToAddress(address, amount)
-      const mempoolAncestors = await testing.rpc.blockchain.getMempoolAncestors(txId)
-      expect(mempoolAncestors.length).toBeGreaterThan(0)
-      for (const ancestorId of mempoolAncestors) {
-        expect(ancestorId).toStrictEqual(expect.stringMatching(/^[0-9a-f]{64}$/))
-      }
-    })
+  it('should return JSON object if verbose is true', async () => {
+    const txId = await testing.rpc.wallet.sendToAddress('mwsZw8nF7pKxWH8eoKL9tPxTpaFkz7QeLU', 0.003)
+    const mempoolAncestors = await testing.rpc.blockchain.getMempoolAncestors(txId, true)
+    const keys = Object.keys(mempoolAncestors)
+    expect(keys.length).toBeGreaterThan(0)
+    for (const key of keys) {
+      expect(mempoolAncestors[key]).toStrictEqual({
+        fees: expect.any(Object),
+        vsize: expect.any(BigNumber),
+        weight: expect.any(BigNumber),
+        fee: expect.any(BigNumber),
+        modifiedfee: expect.any(BigNumber),
+        time: expect.any(BigNumber),
+        height: expect.any(BigNumber),
+        descendantcount: expect.any(BigNumber),
+        descendantsize: expect.any(BigNumber),
+        descendantfees: expect.any(BigNumber),
+        ancestorcount: expect.any(BigNumber),
+        ancestorsize: expect.any(BigNumber),
+        ancestorfees: expect.any(BigNumber),
+        wtxid: expect.any(String),
+        depends: expect.any(Array),
+        spentby: expect.any(Array),
+        'bip125-replaceable': expect.any(Boolean)
+      })
+    }
+  })
+
+  it('should return array of transaction ids if verbose is false', async () => {
+    const txId = await testing.rpc.wallet.sendToAddress('mwsZw8nF7pKxWH8eoKL9tPxTpaFkz7QeLU', 0.003)
+    const mempoolAncestors = await testing.rpc.blockchain.getMempoolAncestors(txId, false)
+    expect(mempoolAncestors.length).toBeGreaterThan(0)
+    for (const ancestorId of mempoolAncestors) {
+      expect(ancestorId).toStrictEqual(expect.stringMatching(/^[0-9a-f]{64}$/))
+    }
+  })
+
+  it('should return array of transaction ids if verbose is undefined', async () => {
+    const txId = await testing.rpc.wallet.sendToAddress('mwsZw8nF7pKxWH8eoKL9tPxTpaFkz7QeLU', 0.003)
+    const mempoolAncestors = await testing.rpc.blockchain.getMempoolAncestors(txId)
+    expect(mempoolAncestors.length).toBeGreaterThan(0)
+    for (const ancestorId of mempoolAncestors) {
+      expect(ancestorId).toStrictEqual(expect.stringMatching(/^[0-9a-f]{64}$/))
+    }
   })
 })
