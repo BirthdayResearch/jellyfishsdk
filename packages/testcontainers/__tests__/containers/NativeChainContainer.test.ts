7fc45d00e9066574324d39704e7d6273543bbf64
Isaac Yong <isaac@cakedefi.com>
2022-10-05 15:50:06 +0800

    Add basic tests

--- packages/testcontainers/__tests__/containers/NativeChainContainer.test.ts
+++ packages/testcontainers/__tests__/containers/NativeChainContainer.test.ts
@@ -1,7 +1,29 @@
 import { Network } from 'testcontainers'
 import { NativeChainContainer, StartedNativeChainContainer } from '../../src/containers/NativeChainContainer'
 
-describe('container error handling', () => {
+describe('nativechain mainnet', () => {
+  let container: StartedNativeChainContainer
+
+  beforeAll(async () => {
+    const startedNetwork = await new Network().start()
+    container = await new NativeChainContainer()
+      .withNetworkMode((startedNetwork).getName())
+      .withBlockchainNetwork('mainnet')
+      .withStartupTimeout(60_000)
+      .start()
+  })
+
+  afterAll(async () => {
+    await container.stop()
+  })
+
+  it('should be able to getmininginfo and chain should be main', async () => {
+    const { chain } = await container.getMiningInfo()
+    expect(chain).toStrictEqual('main')
+  })
+})
+
+describe('nativechain testnet', () => {
   let container: StartedNativeChainContainer
 
   beforeAll(async () => {
@@ -20,5 +42,74 @@ describe('container error handling', () => {
   it('should be able to getmininginfo and chain should be test', async () => {
     const { chain } = await container.getMiningInfo()
     expect(chain).toStrictEqual('test')
+  })
+})
+
+describe('native testnet: override docker image', () => {
+  let container: StartedNativeChainContainer
+
+  beforeAll(async () => {
+    const startedNetwork = await new Network().start()
+    container = await new NativeChainContainer('defi/defichain:1.6.4')
+      .withNetworkMode((startedNetwork).getName())
+      .withBlockchainNetwork('testnet')
+      .withStartupTimeout(60_000)
+      .start()
+  })
+
+  afterAll(async () => {
+    await container.stop()
+  })
+
+  it('should be able to getmininginfo and chain should be test', async () => {
+    const { chain } = await container.getMiningInfo()
+    expect(chain).toStrictEqual('test')
+  })
+})
+
+describe('nativechain regtest', () => {
+  let container: StartedNativeChainContainer
+
+  beforeAll(async () => {
+    const startedNetwork = await new Network().start()
+    container = await new NativeChainContainer()
+      .withNetworkMode((startedNetwork).getName())
+      .withBlockchainNetwork('regtest')
+      .withStartupTimeout(60_000)
+      .start()
+  })
+
+  afterAll(async () => {
+    await container.stop()
+  })
+
+  it('should be able to getmininginfo and chain should be regtest', async () => {
+    const { chain } = await container.getMiningInfo()
+    expect(chain).toStrictEqual('regtest')
+  })
+})
+
+describe('nativechain fluency', () => {
+  let container: StartedNativeChainContainer
+
+  const containerName = 'fluent-name'
+
+  beforeAll(async () => {
+    const startedNetwork = await new Network().start()
+    container = await new NativeChainContainer()
+      .withNetworkMode((startedNetwork).getName())
+      .withBlockchainNetwork('regtest')
+      .withName(containerName)
+      .withStartupTimeout(60_000)
+      .start()
+  })
+
+  afterAll(async () => {
+    await container.stop()
+  })
+
+  it('should return custom container name', async () => {
+    const name = container.getName()
+    expect(name).toStrictEqual(`/${containerName}`)
   })
 })
