ac3b176175881eada25102600e890284c21a795e
Fuxing Loh <git@fuxing.dev>
2022-10-19 21:14:44 +0800

    use defi/defichain:HEAD-49fba65ce

--- packages/testcontainers/src/containers/DeFiDContainer.ts
+++ packages/testcontainers/src/containers/DeFiDContainer.ts
@@ -35,7 +35,7 @@ export abstract class DeFiDContainer extends DockerContainer {
     if (process?.env?.DEFICHAIN_DOCKER_IMAGE !== undefined) {
       return process.env.DEFICHAIN_DOCKER_IMAGE
     }
-    return 'defi/defichain:HEAD-aaaec97e8'
+    return 'defi/defichain:HEAD-49fba65ce'
   }
 
   public static readonly DefaultStartOptions = {
