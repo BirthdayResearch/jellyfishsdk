a4dd16fb77a2958cf96752a5ad470209a838765b
Shoham Chakraborty <shhmchk@gmail.com>
2022-10-27 15:37:15 +0800

    bump container tag

--- packages/testcontainers/src/containers/DeFiDContainer.ts
+++ packages/testcontainers/src/containers/DeFiDContainer.ts
@@ -35,7 +35,7 @@ export abstract class DeFiDContainer extends DockerContainer {
     if (process?.env?.DEFICHAIN_DOCKER_IMAGE !== undefined) {
       return process.env.DEFICHAIN_DOCKER_IMAGE
     }
-    return 'defi/defichain:epic-grandcentral-79a24035e'
+    return 'defi/defichain:epic-grandcentral-bfb0eedb7'
   }
 
   public static readonly DefaultStartOptions = {
