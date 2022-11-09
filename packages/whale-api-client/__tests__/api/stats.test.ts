2d18bd66a3449a49f750362ba6f0601a24098818
pierregee <pierre.gabaldon@gmail.com>
2022-11-09 15:39:16 +0800

    adjust stats tvl e2e

--- packages/whale-api-client/__tests__/api/stats.test.ts
+++ packages/whale-api-client/__tests__/api/stats.test.ts
@@ -93,10 +93,10 @@ describe('stats', () => {
         total: 7327.58
       },
       tvl: {
-        dex: 5853.942343505482,
+        dex: 5853.942327903219,
         masternodes: 185.39423435054823,
         loan: 0,
-        total: 6039.336577856031
+        total: 6039.336562253768
       },
       price: {
         usd: 2.317427929381853,
@@ -473,10 +473,10 @@ describe('loan - stats', () => {
       },
       burned: expect.any(Object),
       tvl: {
-        dex: 6000,
+        dex: 4000,
         loan: 20000,
         masternodes: 40,
-        total: 26040
+        total: 24040
       },
       price: {
         usd: 0.5,
