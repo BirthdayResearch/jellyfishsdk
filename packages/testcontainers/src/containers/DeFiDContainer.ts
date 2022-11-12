9412fafc972a3625f92b96f07da6ae42970d7117
Shoham Chakraborty <shhmchk@gmail.com>
2022-11-13 00:39:10 +0800

    update container

--- packages/testcontainers/src/containers/DeFiDContainer.ts
+++ packages/testcontainers/src/containers/DeFiDContainer.ts
@@ -35,7 +35,7 @@ export abstract class DeFiDContainer extends DockerContainer {
     if (process?.env?.DEFICHAIN_DOCKER_IMAGE !== undefined) {
       return process.env.DEFICHAIN_DOCKER_IMAGE
     }
-    return 'defi/defichain:epic-grandcentral-bfb0eedb7'
+    return 'defi/defichain:master-23c76b3b9'
   }
 
   public static readonly DefaultStartOptions = {
