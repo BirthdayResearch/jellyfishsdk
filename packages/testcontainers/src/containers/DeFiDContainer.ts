1e7755a17231d25a392d03370a634f59905a9a2c
Shoham Chakraborty <shhmchk@gmail.com>
2022-11-17 16:36:38 +0800

    Add new consortium attribute tests, bump docker image tag

--- packages/testcontainers/src/containers/DeFiDContainer.ts
+++ packages/testcontainers/src/containers/DeFiDContainer.ts
@@ -35,7 +35,7 @@ export abstract class DeFiDContainer extends DockerContainer {
     if (process?.env?.DEFICHAIN_DOCKER_IMAGE !== undefined) {
       return process.env.DEFICHAIN_DOCKER_IMAGE
     }
-    return 'defi/defichain:HEAD-453b1c948'
+    return 'defi/defichain:master-7ce2d1721'
   }
 
   public static readonly DefaultStartOptions = {
