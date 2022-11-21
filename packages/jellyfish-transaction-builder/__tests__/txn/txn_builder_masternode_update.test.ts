e4a9e878a697d59b1551d3a91cf416ead4050663
Mark Tan <marktanrj@gmail.com>
2022-11-22 03:19:04 +0800

    Add tests for updating operator and reward address

--- packages/jellyfish-transaction-builder/__tests__/txn/txn_builder_masternode_update.test.ts
+++ packages/jellyfish-transaction-builder/__tests__/txn/txn_builder_masternode_update.test.ts
@@ -39,21 +39,31 @@ describe('UpdateMasternode', () => {
     const balance = await container.call('getbalance')
     expect(balance >= 2).toBeTruthy()
 
+    // enable updating
+    await jsonRpc.masternode.setGov({
+      ATTRIBUTES: {
+        'v0/params/feature/mn-setowneraddress': 'true',
+        'v0/params/feature/mn-setoperatoraddress': 'true',
+        'v0/params/feature/mn-setrewardaddress': 'true'
+      }
+    })
+    await container.generate(1)
+
     const pubKey = await providers.ellipticPair.publicKey()
     const collateralAddress = Bech32.fromPubKey(pubKey, 'bcrt')
     const txid = await jsonRpc.masternode.createMasternode(collateralAddress)
-    await container.generate(30)
+    await container.generate(20)
 
-    const address2 = await container.getNewAddress('', 'bech32')
-    const addressDest2: P2WPKH = P2WPKH.fromAddress(RegTest, address2, P2WPKH)
-    const addressDestHex2 = addressDest2.pubKeyHash
+    const address = await container.getNewAddress('', 'bech32')
+    const addressDest: P2WPKH = P2WPKH.fromAddress(RegTest, address, P2WPKH)
+    const addressDestHex = addressDest.pubKeyHash
 
     const updateMasternode: UpdateMasternode = {
       nodeId: txid,
       updates: [
         {
           updateType: 0x02,
-          address: { addressType: 0x01, addressPubKeyHash: addressDestHex2 }
+          address: { addressType: 0x04, addressPubKeyHash: addressDestHex }
         }
       ]
     }
@@ -61,25 +71,77 @@ describe('UpdateMasternode', () => {
     const script = await providers.elliptic.script()
     const txn: TransactionSegWit = await builder.masternode.update(updateMasternode, script)
     const encoded: string = OP_CODES.OP_DEFI_TX_UPDATE_MASTER_NODE(updateMasternode).asBuffer().toString('hex')
-    const expectedRedeemScript = `6d${encoded}`
+    const expectedRedeemScript = `6a${encoded}`
 
     const outs = await sendTransaction(container, txn)
-    console.log(outs, expectedRedeemScript)
-    // expect(outs.length).toStrictEqual(2)
-    // expect(outs[0].value).toStrictEqual(1)
-    // expect(outs[0].n).toStrictEqual(0)
-    // expect(outs[0].tokenId).toStrictEqual(0)
-    // expect(outs[0].scriptPubKey.asm.startsWith('OP_RETURN 4466547843')).toBeTruthy()
-    // expect(outs[0].scriptPubKey.hex).toStrictEqual(expectedRedeemScript)
-    // expect(outs[0].scriptPubKey.type).toStrictEqual('nulldata')
+    expect(outs.length).toStrictEqual(2)
+    expect(outs[0].value).toStrictEqual(0)
+    expect(outs[0].n).toStrictEqual(0)
+    expect(outs[0].tokenId).toStrictEqual(0)
+    expect(outs[0].scriptPubKey.asm.startsWith('OP_RETURN 446654786d')).toBeTruthy()
+    expect(outs[0].scriptPubKey.hex).toStrictEqual(expectedRedeemScript)
+    expect(outs[0].scriptPubKey.type).toStrictEqual('nulldata')
 
-    // expect(outs[1].value).toStrictEqual(2)
-    // expect(outs[1].n).toStrictEqual(1)
-    // expect(outs[1].tokenId).toStrictEqual(0)
-    // expect(outs[1].scriptPubKey.type).toStrictEqual('witness_v0_keyhash')
-    // expect(outs[1].scriptPubKey.reqSigs).toStrictEqual(1)
-    // expect(outs[1].scriptPubKey.addresses[0]).toStrictEqual(await providers.getAddress())
+    expect(outs[1].value).toStrictEqual(6.9999542)
+    expect(outs[1].n).toStrictEqual(1)
+    expect(outs[1].tokenId).toStrictEqual(0)
+    expect(outs[1].scriptPubKey.type).toStrictEqual('witness_v0_keyhash')
+    expect(outs[1].scriptPubKey.reqSigs).toStrictEqual(1)
+    expect(outs[1].scriptPubKey.addresses[0]).toStrictEqual(await providers.getAddress())
+  })
 
+  it('should update reward address with P2WPKH address', async () => {
+    const balance = await container.call('getbalance')
+    expect(balance >= 2).toBeTruthy()
+
+    // enable updating
+    await jsonRpc.masternode.setGov({
+      ATTRIBUTES: {
+        'v0/params/feature/mn-setowneraddress': 'true',
+        'v0/params/feature/mn-setoperatoraddress': 'true',
+        'v0/params/feature/mn-setrewardaddress': 'true'
+      }
+    })
     await container.generate(1)
+
+    const pubKey = await providers.ellipticPair.publicKey()
+    const collateralAddress = Bech32.fromPubKey(pubKey, 'bcrt')
+    const txid = await jsonRpc.masternode.createMasternode(collateralAddress)
+    await container.generate(20)
+
+    const address = await container.getNewAddress('', 'bech32')
+    const addressDest: P2WPKH = P2WPKH.fromAddress(RegTest, address, P2WPKH)
+    const addressDestHex = addressDest.pubKeyHash
+
+    const updateMasternode: UpdateMasternode = {
+      nodeId: txid,
+      updates: [
+        {
+          updateType: 0x03,
+          address: { addressType: 0x04, addressPubKeyHash: addressDestHex }
+        }
+      ]
+    }
+
+    const script = await providers.elliptic.script()
+    const txn: TransactionSegWit = await builder.masternode.update(updateMasternode, script)
+    const encoded: string = OP_CODES.OP_DEFI_TX_UPDATE_MASTER_NODE(updateMasternode).asBuffer().toString('hex')
+    const expectedRedeemScript = `6a${encoded}`
+
+    const outs = await sendTransaction(container, txn)
+    expect(outs.length).toStrictEqual(2)
+    expect(outs[0].value).toStrictEqual(0)
+    expect(outs[0].n).toStrictEqual(0)
+    expect(outs[0].tokenId).toStrictEqual(0)
+    expect(outs[0].scriptPubKey.asm.startsWith('OP_RETURN 446654786d')).toBeTruthy()
+    expect(outs[0].scriptPubKey.hex).toStrictEqual(expectedRedeemScript)
+    expect(outs[0].scriptPubKey.type).toStrictEqual('nulldata')
+
+    expect(outs[1].value).toStrictEqual(6.9999542)
+    expect(outs[1].n).toStrictEqual(1)
+    expect(outs[1].tokenId).toStrictEqual(0)
+    expect(outs[1].scriptPubKey.type).toStrictEqual('witness_v0_keyhash')
+    expect(outs[1].scriptPubKey.reqSigs).toStrictEqual(1)
+    expect(outs[1].scriptPubKey.addresses[0]).toStrictEqual(await providers.getAddress())
   })
 })
