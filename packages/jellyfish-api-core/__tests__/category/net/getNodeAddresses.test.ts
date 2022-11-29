efad7310518b86e88e5892817e6d0cbc8e3de5f1
Shoham Chakraborty <shhmchk@gmail.com>
2022-11-30 00:00:49 +0800

    Complete getNodeAddresses tests

--- packages/jellyfish-api-core/__tests__/category/net/getNodeAddresses.test.ts
+++ packages/jellyfish-api-core/__tests__/category/net/getNodeAddresses.test.ts
@@ -1,7 +1,7 @@
 import { MasterNodeRegTestContainer } from '@defichain/testcontainers'
 import { ContainerAdapterClient } from '../../container_adapter_client'
 
-describe('Network on masternode', () => {
+describe('Network', () => {
   const container = new MasterNodeRegTestContainer()
   const client = new ContainerAdapterClient(container)
 
@@ -9,15 +9,15 @@ describe('Network on masternode', () => {
     await container.start({
       startFlags: [
         {
-          name: '-regtest',
+          name: 'regtest',
           value: 0
         },
         {
-          name: '-testnet',
+          name: 'testnet',
           value: 1
         },
         {
-          name: '-rpcport',
+          name: 'rpcport',
           value: 19554
         }
       ]
@@ -28,11 +28,33 @@ describe('Network on masternode', () => {
     await container.stop()
   })
 
-  it('should get something in the array', async () => {
-    // await setTimeout(50000);
-    console.log(await container.call('getpeerinfo'))
-    console.log(await container.call('getnetworkinfo'))
-    const info = await client.net.getNodeAddresses()
-    console.log(info)
+  it('should be empty if no available nodes', async () => {
+    const res = await client.net.getNodeAddresses(1)
+    expect(res).toMatchObject([])
+  })
+
+  it('should have available nodes', async () => {
+    // mydeficha.in nodes
+    await container.call('addnode', ['89.58.14.177:18555', 'add'])
+    await container.call('addnode', ['89.58.26.201:18555', 'add'])
+    await container.call('addnode', ['89.58.14.115:18555', 'add'])
+    await container.call('addnode', ['178.254.26.173:18555', 'add'])
+    await container.call('addnode', ['178.254.13.34:18555', 'add'])
+    await container.call('addnode', ['154.53.43.103:18555', 'add'])
+    await container.call('addnode', ['161.97.90.159:18555', 'add'])
+    await container.call('addnode', ['194.233.89.209:18555', 'add'])
+    await container.call('addnode', ['89.163.215.205:18555', 'add'])
+
+    await new Promise((resolve) => {
+      setTimeout(() => resolve(0), 10000)
+    }) // give enough time to find new nodes
+
+    const res = await client.net.getNodeAddresses(1)
+    expect(res).toMatchObject([{
+      time: expect.any(Number),
+      services: expect.any(Number),
+      address: expect.any(String),
+      port: expect.any(Number)
+    }])
   })
 })
