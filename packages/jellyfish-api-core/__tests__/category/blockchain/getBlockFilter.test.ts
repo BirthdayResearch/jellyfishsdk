03594f50c1f25330e2998c8c3e3cfcbcb57c56f8
Kven Ho <kven@cakedefi.com>
2022-12-02 10:33:59 +0800

    Update packages/jellyfish-api-core/__tests__/category/blockchain/getBlockFilter.test.ts
    Co-authored-by: Fuxing Loh <4266087+fuxingloh@users.noreply.github.com>
    Signed-off-by: Kven Ho <kven@cakedefi.com>

--- packages/jellyfish-api-core/__tests__/category/blockchain/getBlockFilter.test.ts
+++ packages/jellyfish-api-core/__tests__/category/blockchain/getBlockFilter.test.ts
@@ -21,7 +21,7 @@ describe('Retrieve a BIP 157 content filter for a particular block', () => {
 
       expect(await promise).toStrictEqual({
         filter: expect.stringMatching(/^[0-9a-f]{1,}$/),
-        header: expect.stringMatching(/^[0-f]{64}$/)
+        header: expect.stringMatching(/^[0-9a-f]{64}$/)
       })
     }
   })
