86a5bb5e59823d50ae31b965a5bc3d662410cc1a
Isaac Yong <isaac@cakedefi.com>
2022-12-12 18:01:02 +0800

    Try fix CI

--- packages/testcontainers/src/index.ts
+++ packages/testcontainers/src/index.ts
@@ -28,3 +28,5 @@ export * from './containers/RegTestContainer/LoanContainer'
 export * from './containers/AppContainer/WhaleSanityContainer'
 
 export * from './containers/NativeChainContainer'
+export * from './containers/NativeChainRpc'
+export * from './containers/NativeChainWaitFor'
