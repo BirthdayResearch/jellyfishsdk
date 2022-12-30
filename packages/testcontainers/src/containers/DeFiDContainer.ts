a8e35d3719b5c5d644af43a64594e78e79c44a5d
Shoham Chakraborty <shhmchk@gmail.com>
2022-12-30 14:26:11 +0530

    Update getBurnInfo and burnTokens

--- packages/testcontainers/src/containers/DeFiDContainer.ts
+++ packages/testcontainers/src/containers/DeFiDContainer.ts
@@ -36,7 +36,7 @@ export abstract class DeFiDContainer extends DockerContainer {
     if (process?.env?.DEFICHAIN_DOCKER_IMAGE !== undefined) {
       return process.env.DEFICHAIN_DOCKER_IMAGE
     }
-    return 'defi/defichain:3.2.0'
+    return 'defi/defichain:HEAD-e0cc2bfe0'
   }
 
   public static readonly DefaultStartOptions = {
