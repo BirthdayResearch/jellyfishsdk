1d88862c2f377f3d438921eda9d4429c22792c83
Dilshan Madushanka <helloscoopz@gmail.com>
2023-01-03 14:08:58 +0800

    Bump image to fe4ccb39d

--- packages/testcontainers/src/containers/DeFiDContainer.ts
+++ packages/testcontainers/src/containers/DeFiDContainer.ts
@@ -36,7 +36,7 @@ export abstract class DeFiDContainer extends DockerContainer {
     if (process?.env?.DEFICHAIN_DOCKER_IMAGE !== undefined) {
       return process.env.DEFICHAIN_DOCKER_IMAGE
     }
-    return 'defi/defichain:HEAD-22a537405'
+    return 'defi/defichain:HEAD-fe4ccb39d'
   }
 
   public static readonly DefaultStartOptions = {
