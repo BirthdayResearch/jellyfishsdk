87e18d09ce65e4bc9b21eed126d2937a69c49475
Shoham Chakraborty <shhmchk@gmail.com>
2023-01-05 18:43:48 +0530

    Bump container to 3.2.1

--- packages/testcontainers/src/containers/DeFiDContainer.ts
+++ packages/testcontainers/src/containers/DeFiDContainer.ts
@@ -36,7 +36,7 @@ export abstract class DeFiDContainer extends DockerContainer {
     if (process?.env?.DEFICHAIN_DOCKER_IMAGE !== undefined) {
       return process.env.DEFICHAIN_DOCKER_IMAGE
     }
-    return 'defi/defichain:HEAD-e0cc2bfe0'
+    return 'defi/defichain:3.2.1'
   }
 
   public static readonly DefaultStartOptions = {
