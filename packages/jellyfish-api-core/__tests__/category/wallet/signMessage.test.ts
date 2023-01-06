1182c56163c1afe0e3e1b442a21909079c908f5e
shancht <shana.tan@cakedefi.com>
2023-01-06 18:14:45 +0800

    fixed tests and added verifyMessage test

--- packages/jellyfish-api-core/__tests__/category/wallet/signMessage.test.ts
+++ packages/jellyfish-api-core/__tests__/category/wallet/signMessage.test.ts
@@ -1,6 +1,7 @@
 import { MasterNodeRegTestContainer } from '@defichain/testcontainers'
 import { ContainerAdapterClient } from '../../container_adapter_client'
 import { RpcApiError } from '@defichain/jellyfish-api-core'
+import { wallet } from '../../../src'
 
 describe('Sign Message', () => {
   const container = new MasterNodeRegTestContainer()
@@ -14,21 +15,39 @@ describe('Sign Message', () => {
     await container.stop()
   })
 
-  it('should throw error if private key is not available', async () => {
-    const promise = client.wallet.signMessage('mpLQjfK79b7CCV4VMJWEWAj5Mpx8Up5zxB', 'This is a test message')
+  it('should throw error if address provided does not to refer to key (BECH32)', async () => {
+    // getNewAddress generates a BECH32 address by default
+    const address = await client.wallet.getNewAddress()
+    const promise = client.wallet.signMessage(address, 'This is a test message')
 
     await expect(promise).rejects.toThrow(RpcApiError)
     await expect(promise).rejects.toMatchObject({
       payload: {
-        code: -4,
-        message: 'Private key not available',
+        code: -3,
+        message: 'Address does not refer to key',
         method: 'signmessage'
       }
     })
   })
 
-  it('should throw error if address is invalid', async () => {
-    const promise = client.wallet.signMessage('test', 'This is a test message')
+  it('should throw error if address provided does not to refer to key (P2SH)', async () => {
+    const address = await client.wallet.getNewAddress('', wallet.AddressType.P2SH_SEGWIT)
+
+    const promise = client.wallet.signMessage(address, 'This is a test message')
+
+    await expect(promise).rejects.toThrow(RpcApiError)
+    await expect(promise).rejects.toMatchObject({
+      payload: {
+        code: -3,
+        message: 'Address does not refer to key',
+        method: 'signmessage'
+      }
+    })
+  })
+
+  it('should throw error if invalid address is provided', async () => {
+    const promise = client.wallet.signMessage('', 'This is a test message')
+
     await expect(promise).rejects.toThrow(RpcApiError)
     await expect(promise).rejects.toMatchObject({
       payload: {
@@ -37,5 +56,15 @@ describe('Sign Message', () => {
         method: 'signmessage'
       }
     })
+  })
+
+  it('should be verifiable', async () => {
+    const address = await client.wallet.getNewAddress('', wallet.AddressType.LEGACY)
+    const message = 'This is a test message'
+
+    const signature = await client.wallet.signMessage(address, message)
+
+    const verify = await client.call('verifymessage', [address, signature, 'This is a test message'], 'number')
+    expect(verify).toStrictEqual(true)
   })
 })
