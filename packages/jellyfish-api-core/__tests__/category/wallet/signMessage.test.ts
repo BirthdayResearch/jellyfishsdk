05ec5e10e9a8e6d4b8493fe66cfd7730787ce457
shancht <shana.tan@cakedefi.com>
2023-01-11 10:59:00 +0800

    added 'privkey not available' test

--- packages/jellyfish-api-core/__tests__/category/wallet/signMessage.test.ts
+++ packages/jellyfish-api-core/__tests__/category/wallet/signMessage.test.ts
@@ -3,7 +3,7 @@ import { ContainerAdapterClient } from '../../container_adapter_client'
 import { RpcApiError } from '@defichain/jellyfish-api-core'
 import { wallet } from '../../../src'
 
-describe('Sign Message', () => {
+describe('Sign Message on masternode', () => {
   const container = new MasterNodeRegTestContainer()
   const client = new ContainerAdapterClient(container)
 
@@ -59,6 +59,21 @@ describe('Sign Message', () => {
       payload: {
         code: -3,
         message: 'Invalid address',
+        method: 'signmessage'
+      }
+    })
+  })
+
+  it('should throw error if address provided does not contain private key', async () => {
+    const message = 'This is a test message'
+    const address = 'mpLQjfK79b7CCV4VMJWEWAj5Mpx8Up5zxB'
+    const promise = client.wallet.signMessage(address, message)
+
+    await expect(promise).rejects.toThrow(RpcApiError)
+    await expect(promise).rejects.toMatchObject({
+      payload: {
+        code: -4,
+        message: 'Private key not available',
         method: 'signmessage'
       }
     })
