f94d943fba173be24fd010c1d0968a1610778951
cedric ogire <cedric.ogire@cakedefi.com>
2023-01-17 17:18:55 +0800

    make error check more specific for deriveAddresses test

--- packages/jellyfish-api-core/__tests__/category/misc/deriveAddresses.test.ts
+++ packages/jellyfish-api-core/__tests__/category/misc/deriveAddresses.test.ts
@@ -28,31 +28,92 @@ describe('derive addresses', () => {
 
   it('should raise an error if the range is specified for un-ranged descriptor', async () => {
     const descriptor = 'wpkh(tprv8ZgxMBicQKsPd7Uf69XL1XwhmjHopUGep8GuEiJDZmbQz6o58LninorQAfcKZWARbtRtfnLcJ5MQ2AtHcQJCCRUcMRvmDUjyEmNUWwx8UbK/1/1/0)#t6wfjs64'
-    await expect(client.misc.deriveAddresses(descriptor, [1, 3])).rejects.toThrow(RpcApiError)
+    const promise = client.misc.deriveAddresses(descriptor, [1, 3])
+    await expect(promise).rejects.toThrow(RpcApiError)
+    await expect(promise).rejects.toMatchObject({
+      payload: {
+        code: -8,
+        message: 'Range should not be specified for an un-ranged descriptor',
+        method: 'deriveaddresses'
+      }
+    })
   })
 
   it('should raise an error if there is no range for a range descriptor', async () => {
-    const descriptor = 'wpkh(tprv8ZgxMBicQKsPd7Uf69XL1XwhmjHopUGep8GuEiJDZmbQz6o58LninorQAfcKZWARbtRtfnLcJ5MQ2AtHcQJCCRUcMRvmDUjyEmNUWwx8UbK/1/1/*)'
-    await expect(client.misc.deriveAddresses(descriptor, [1, 3])).rejects.toThrow(RpcApiError)
+    const descriptor = 'wpkh(tprv8ZgxMBicQKsPd7Uf69XL1XwhmjHopUGep8GuEiJDZmbQz6o58LninorQAfcKZWARbtRtfnLcJ5MQ2AtHcQJCCRUcMRvmDUjyEmNUWwx8UbK/1/1/*)#kft60nuy'
+    const promise = client.misc.deriveAddresses(descriptor)
+    await expect(promise).rejects.toThrow(RpcApiError)
+    await expect(promise).rejects.toMatchObject({
+      payload: {
+        code: -8,
+        message: 'Range must be specified for a ranged descriptor',
+        method: 'deriveaddresses'
+      }
+    })
   })
 
   it('should raise an error if end of range is too high', async () => {
     const descriptor = 'wpkh(tprv8ZgxMBicQKsPd7Uf69XL1XwhmjHopUGep8GuEiJDZmbQz6o58LninorQAfcKZWARbtRtfnLcJ5MQ2AtHcQJCCRUcMRvmDUjyEmNUWwx8UbK/1/1/*)'
-    await expect(client.misc.deriveAddresses(descriptor, [100000])).rejects.toThrow(RpcApiError)
+    const promise = client.misc.deriveAddresses(descriptor, [100000])
+    await expect(promise).rejects.toThrow(RpcApiError)
+    await expect(promise).rejects.toMatchObject({
+      payload: {
+        code: -8,
+        message: 'Range must be specified as end or as [begin,end]',
+        method: 'deriveaddresses'
+      }
+    })
   })
 
   it('should raise an error if range is too large', async () => {
     const descriptor = 'wpkh(tprv8ZgxMBicQKsPd7Uf69XL1XwhmjHopUGep8GuEiJDZmbQz6o58LninorQAfcKZWARbtRtfnLcJ5MQ2AtHcQJCCRUcMRvmDUjyEmNUWwx8UbK/1/1/*)'
-    await expect(client.misc.deriveAddresses(descriptor, [1, 100000])).rejects.toThrow(RpcApiError)
+    const promise = client.misc.deriveAddresses(descriptor, [1, 1000000000])
+    await expect(promise).rejects.toThrow(RpcApiError)
+    await expect(promise).rejects.toMatchObject({
+      payload: {
+        code: -8,
+        message: 'Range is too large',
+        method: 'deriveaddresses'
+      }
+    })
   })
 
   it('should raise an error if range is less than 0', async () => {
     const descriptor = 'wpkh(tprv8ZgxMBicQKsPd7Uf69XL1XwhmjHopUGep8GuEiJDZmbQz6o58LninorQAfcKZWARbtRtfnLcJ5MQ2AtHcQJCCRUcMRvmDUjyEmNUWwx8UbK/1/1/*)'
-    await expect(client.misc.deriveAddresses(descriptor, [-1, 2])).rejects.toThrow(RpcApiError)
+    const promise = client.misc.deriveAddresses(descriptor, [-1, 2])
+    await expect(promise).rejects.toThrow(RpcApiError)
+    await expect(promise).rejects.toMatchObject({
+      payload: {
+        code: -8,
+        message: 'Range should be greater or equal than 0',
+        method: 'deriveaddresses'
+      }
+    })
   })
 
   it('should raise an error if end of range is smaller than the start', async () => {
     const descriptor = 'wpkh(tprv8ZgxMBicQKsPd7Uf69XL1XwhmjHopUGep8GuEiJDZmbQz6o58LninorQAfcKZWARbtRtfnLcJ5MQ2AtHcQJCCRUcMRvmDUjyEmNUWwx8UbK/1/1/*)'
-    await expect(client.misc.deriveAddresses(descriptor, [2, 0])).rejects.toThrow(RpcApiError)
+    const promise = client.misc.deriveAddresses(descriptor, [2, 0])
+    await expect(promise).rejects.toThrow(RpcApiError)
+    await expect(promise).rejects.toMatchObject({
+      payload: {
+        code: -8,
+        message: 'Range specified as [begin,end] must not have begin after end',
+        method: 'deriveaddresses'
+      }
+    })
+  })
+
+  it('should raise an error if there is wrong drescriptor', async () => {
+    const descriptor = 'descriptor'
+    const promise = client.misc.deriveAddresses(descriptor, [0, 2])
+    await expect(promise).rejects.toThrow(RpcApiError)
+    await expect(promise).rejects.toMatchObject({
+      payload: {
+        code: -5,
+        message: 'Missing checksum',
+        method: 'deriveaddresses'
+      }
+    })
   })
 })
