22d3cd5163345aeea8ef8e1ca403dae0054b97c9
shancht <shana.tan@cakedefi.com>
2023-01-30 13:56:19 +0800

    added empty script test

--- packages/jellyfish-api-core/__tests__/category/rawtx/decodeScript.test.ts
+++ packages/jellyfish-api-core/__tests__/category/rawtx/decodeScript.test.ts
@@ -13,6 +13,27 @@ describe('Decodescript()', () => {
     await container.stop()
   })
 
+  it('should allow for empty scripts', async () => {
+    const address = ''
+    const decode = await testing.rpc.rawtx.decodeScript(address)
+    
+    expect(decode).toStrictEqual({
+        asm: '',
+        type: 'nonstandard',
+        p2sh: '2N9hLwkSqr1cPQAPxbrGVUjxyjD11G2e1he',
+        segwit: {
+          asm: '0 e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855',
+          hex: '0020e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855',
+          reqSigs: 1,
+          type: 'witness_v0_scripthash',
+          addresses: [
+            'bcrt1quwcvgs5clswpfxhm7nyfjmaeysn6us0yvjdexn9yjkv3k7zjhp2snwgpgy'
+          ],
+          'p2sh-segwit': '2N2CmnxjBbPTHrawgG2FkTuBLcJtEzA86sF'
+        }
+    })
+  })
+
   it('should decode scriptPubKey', async () => {
     const address = await testing.container.getNewAddress()
     const addressInfo = await testing.rpc.wallet.getAddressInfo(address)
@@ -20,11 +41,12 @@ describe('Decodescript()', () => {
     const scriptPK = addressInfo.scriptPubKey
     const decode = await testing.rpc.rawtx.decodeScript(scriptPK)
 
-    //console log results align expectations
+    // //console log results align expectations
+
     console.log(addressInfo)
     console.log(scriptPK)
     console.log(decode) 
-    })
+  })
 
   it('should decode a hardcoded P2PK scriptSig correctly', async () => {
     const signature = '304502207fa7a6d1e0ee81132a269ad84e68d695483745cde8b541e3bf630749894e342a022100c1f7ab20e13e22fb95281a870f3dcf38d782e53023ee313d741ad0cfbc0c509001'
@@ -42,5 +64,5 @@ describe('Decodescript()', () => {
 
     const decodeScriptResult = await testing.rpc.rawtx.decodeScript(pushSig + pushPK)
     expect(decodeScriptResult.asm).toStrictEqual(signature + ' ' + pubKey)
-    })
   })
+})
