8052459cedeabe1e202f7068eb5b12d20d1a2c55
shancht <shana.tan@cakedefi.com>
2023-01-30 12:18:01 +0800

    tidy up the draft

--- packages/jellyfish-api-core/__tests__/category/rawtx/decodeScript.test.ts
+++ packages/jellyfish-api-core/__tests__/category/rawtx/decodeScript.test.ts
@@ -14,32 +14,33 @@ describe('Decodescript()', () => {
   })
 
   it('should decode scriptPubKey', async () => {
-    const address = await testing.container.getNewAddress()
-    const addressInfo = await testing.rpc.wallet.getAddressInfo(address)
+  const address = await testing.container.getNewAddress()
+  const addressInfo = await testing.rpc.wallet.getAddressInfo(address)
 
-    const scriptPK = addressInfo.scriptPubKey
-    const decode = await testing.rpc.rawtx.decodeScript(scriptPK)
+  const scriptPK = addressInfo.scriptPubKey
+  const decode = await testing.rpc.rawtx.decodeScript(scriptPK)
 
-    console.log(addressInfo)
-    console.log(scriptPK)
-    console.log(decode) //console log results align expectations
+  //console log results align expectations
+  console.log(addressInfo)
+  console.log(scriptPK)
+  console.log(decode) 
   })
 
 it('should decode a hardcoded P2PK scriptSig correctly', async () => {
-    const signature = '304502207fa7a6d1e0ee81132a269ad84e68d695483745cde8b541e3bf630749894e342a022100c1f7ab20e13e22fb95281a870f3dcf38d782e53023ee313d741ad0cfbc0c509001'
-    const push_signature = '48' + signature
+  const signature = '304502207fa7a6d1e0ee81132a269ad84e68d695483745cde8b541e3bf630749894e342a022100c1f7ab20e13e22fb95281a870f3dcf38d782e53023ee313d741ad0cfbc0c509001'
+  const push = '48' + signature
 
-    const decodeScriptResult = await testing.rpc.rawtx.decodeScript(push_signature)
-    expect(decodeScriptResult.asm).toStrictEqual(signature)
-  })
+  const decodeScriptResult = await testing.rpc.rawtx.decodeScript(push)
+  expect(decodeScriptResult.asm).toStrictEqual(signature)
+})
 
-  it('should decode a hardcoded P2PKH scriptSig correctly', async () => {
-    const signature = '304502207fa7a6d1e0ee81132a269ad84e68d695483745cde8b541e3bf630749894e342a022100c1f7ab20e13e22fb95281a870f3dcf38d782e53023ee313d741ad0cfbc0c509001'
-    const push_signature = '48' + signature
-    const public_key = '03b0da749730dc9b4b1f4a14d6902877a92541f5368778853d9c4a0cb7802dcfb2'
-    const push_public_key = '21' + public_key
+it('should decode a hardcoded P2PKH scriptSig correctly', async () => {
+  const signature = '304502207fa7a6d1e0ee81132a269ad84e68d695483745cde8b541e3bf630749894e342a022100c1f7ab20e13e22fb95281a870f3dcf38d782e53023ee313d741ad0cfbc0c509001'
+  const pushSig = '48' + signature
+  const pubKey = '03b0da749730dc9b4b1f4a14d6902877a92541f5368778853d9c4a0cb7802dcfb2'
+  const pushPK = '21' + pubKey
 
-    const decodeScriptResult = await testing.rpc.rawtx.decodeScript(push_signature + push_public_key)
-    expect(decodeScriptResult.asm).toStrictEqual(signature + ' ' + public_key)
+  const decodeScriptResult = await testing.rpc.rawtx.decodeScript(pushSig + pushPK)
+  expect(decodeScriptResult.asm).toStrictEqual(signature + ' ' + pubKey)
   })
 })
