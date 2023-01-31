5d4e12eabf628ba8ed24ce0be68a455a3e544211
shancht <shana.tan@cakedefi.com>
2023-01-31 17:18:55 +0800

    added fail test

--- packages/jellyfish-api-core/__tests__/category/rawtx/decodeScript.test.ts
+++ packages/jellyfish-api-core/__tests__/category/rawtx/decodeScript.test.ts
@@ -1,6 +1,6 @@
 import { MasterNodeRegTestContainer } from '@defichain/testcontainers'
 import { Testing } from '@defichain/jellyfish-testing'
-import { RpcApiError, wallet } from '../../../src'
+import { wallet } from '../../../src'
 
 describe('Decodescript()', () => {
   const container = new MasterNodeRegTestContainer()
@@ -17,7 +17,7 @@ describe('Decodescript()', () => {
   it('should decode empty script', async () => {
     const address = ''
     const decode = await testing.rpc.rawtx.decodeScript(address)
-    
+
     expect(decode).toStrictEqual({
       asm: '',
       type: 'nonstandard',
@@ -39,10 +39,9 @@ describe('Decodescript()', () => {
     const address = await testing.container.getNewAddress()
     const addressInfo = await testing.rpc.wallet.getAddressInfo(address)
     const scriptPk = addressInfo.scriptPubKey
-    
     const decode = await testing.rpc.rawtx.decodeScript(scriptPk)
 
-    expect(decode.asm).toStrictEqual(addressInfo.witness_version + ' ' + addressInfo.witness_program)
+    expect(decode.asm).toStrictEqual(`${addressInfo.witness_version} ${addressInfo.witness_program}`)
     expect(decode.reqSigs).toStrictEqual(1)
     expect(decode.type).toStrictEqual('witness_v0_keyhash')
     expect(decode.addresses).toStrictEqual([addressInfo.address])
@@ -56,7 +55,7 @@ describe('Decodescript()', () => {
     const withoutOpCodes = addressInfo.scriptPubKey.substring(4, 44)
     const decode = await testing.rpc.rawtx.decodeScript(scriptPk)
 
-    expect(decode.asm).toStrictEqual('OP_HASH160 ' + withoutOpCodes + ' OP_EQUAL')
+    expect(decode.asm).toStrictEqual(`OP_HASH160 ${withoutOpCodes} OP_EQUAL`)
     expect(decode.reqSigs).toStrictEqual(1)
     expect(decode.type).toStrictEqual('scripthash')
     expect(decode.addresses).toStrictEqual([addressInfo.address])
@@ -65,21 +64,20 @@ describe('Decodescript()', () => {
   it('should decode scriptPubKey from LEGACY address', async () => {
     const address = 'n2H1Mrobo5PH2grhDa1Kkz6ThYkLmyXLwR'
     const scriptPk = (await testing.rpc.wallet.getAddressInfo(address)).scriptPubKey
-
     const decode = await testing.rpc.rawtx.decodeScript(scriptPk)
 
     expect(decode).toStrictEqual({
       asm: 'OP_DUP OP_HASH160 e3b7608590327594e40ec1412ec27681086959d9 OP_EQUALVERIFY OP_CHECKSIG',
       reqSigs: 1,
       type: 'pubkeyhash',
-      addresses: [ 'n2H1Mrobo5PH2grhDa1Kkz6ThYkLmyXLwR' ],
+      addresses: ['n2H1Mrobo5PH2grhDa1Kkz6ThYkLmyXLwR'],
       p2sh: '2NC1bsXjkxWvmhp7TNiFgPF3yBRVCrYXzhb',
       segwit: {
         asm: '0 e3b7608590327594e40ec1412ec27681086959d9',
         hex: '0014e3b7608590327594e40ec1412ec27681086959d9',
         reqSigs: 1,
         type: 'witness_v0_keyhash',
-        addresses: [ 'bcrt1quwmkppvsxf6efeqwc9qjasnksyyxjkwejd3yaw' ],
+        addresses: ['bcrt1quwmkppvsxf6efeqwc9qjasnksyyxjkwejd3yaw'],
         'p2sh-segwit': '2N2GB1X4scdpHDBgtQxLyLnjL9mrdhpKczw'
       }
     })
@@ -87,25 +85,17 @@ describe('Decodescript()', () => {
 
   it('should flag random data as nulldata', async () => {
     const random = '48304502207fa7a6d1e0ee81132a269ad84e68d695483745cde8b541e3bf630749894e342a022100c1f7ab20e13e22fb95281a870f3dcf38d782e53023ee313d741ad0cfbc0c509001'
-    const decode = await testing.rpc.rawtx.decodeScript('6a' + random)
+    const decode = await testing.rpc.rawtx.decodeScript(`6a${random}`)
     const withoutOpCodes = random.substring(2)
 
-    expect(decode.asm).toStrictEqual('OP_RETURN ' + withoutOpCodes)
+    expect(decode.asm).toStrictEqual(`OP_RETURN ${withoutOpCodes}`)
     expect(decode.type).toStrictEqual('nulldata')
     expect(decode.p2sh).toStrictEqual('2N9YehGXCtVh6nsnkd1ptpoavdSRvod9RAb')
   })
 
   it('should throw error for a non-hexadecimal string', async () => {
     const address = 'text'
-    const decode = await testing.rpc.rawtx.decodeScript(address)
-    
-    //the following is a WIP
-    expect(decode).toMatchObject({        
-      payload: {
-        code: -8,
-        message: "argument must be hexadecimal string (not 'text')", 
-        method: 'decodescript'
-      }
-    })
+    return await expect(testing.rpc.rawtx.decodeScript(address))
+      .rejects.toThrow("RpcApiError: 'argument must be hexadecimal string (not 'text')', code: -8, method: decodescript")
   })
 })
