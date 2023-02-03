b171d774e02f87b1af1793d7b16c0851998aeed6
Fuxing Loh <git@fuxing.dev>
2023-02-03 09:57:49 +0800

    feat(packages/testcontainers): add WhaleApiContainer

---
+++ packages/testcontainers/src/containers/AppContainer/WhaleApiContainer.ts
@@ -0,0 +1,76 @@
+import { GenericContainer, StartedNetwork } from 'testcontainers'
+import { AbstractStartedContainer } from 'testcontainers/dist/modules/abstract-started-container'
+import { StartedNativeChainContainer, waitForCondition } from '@defichain/testcontainers'
+import packageJson from '../../../package.json'
+import fetch from 'cross-fetch'
+
+const WHALE_API_PORT = 3000
+
+/**
+ * For local environment, `:latest` tag will be used as there isn't pipeline to automatically rebuild image locally.
+ */
+const WHALE_VERSION = packageJson.version === '0.0.0' ? 'latest' : packageJson.version
+
+export class WhaleApiContainer extends GenericContainer {
+  // when 0.0.0 use latest
+  constructor (image: string = `ghcr.io/jellyfishsdk/whale-api:${WHALE_VERSION}`) {
+    super(image)
+    this.withExposedPorts(WHALE_API_PORT).withStartupTimeout(120_000)
+  }
+
+  public withNativeChain (
+    container: StartedNativeChainContainer,
+    network: StartedNetwork
+  ): this {
+    const ipAddress = container.getIpAddress(network.getName())
+    this.withEnvironment({
+      WHALE_DEFID_URL: `http://${container.rpcUser}:${container.rpcPassword}@${ipAddress}:19554/`,
+      WHALE_DATABASE_PROVIDER: 'level',
+      WHALE_DATABASE_LEVEL_LOCATION: '.level/index',
+      WHALE_NETWORK: 'regtest',
+      WHALE_VERSION: 'v0'
+    })
+    return this
+  }
+
+  public async start (): Promise<StartedWhaleApiContainer> {
+    return new StartedWhaleApiContainer(await super.start())
+  }
+}
+
+export class StartedWhaleApiContainer extends AbstractStartedContainer {
+  public getContainerPort (): number {
+    return WHALE_API_PORT
+  }
+
+  public getPort (): number {
+    return this.getMappedPort(this.getContainerPort())
+  }
+
+  getEndpoint (): string {
+    return `http://localhost:${this.getPort()}`
+  }
+
+  getWhaleApiClientOptions (): { url: string, version: 'v0', network: 'regtest' } {
+    return {
+      url: this.getEndpoint(),
+      version: 'v0',
+      network: 'regtest'
+    }
+  }
+
+  async waitForIndexedBlockHeight (height: number, timeout: number = 590000): Promise<void> {
+    const blockEndpoint = `${this.getEndpoint()}/v0/regtest/blocks?size=1`
+
+    return await waitForCondition(async () => {
+      const response = await fetch(blockEndpoint, {
+        method: 'GET'
+      })
+      const { data } = await response.json()
+      if (data[0].height > height) {
+        return true
+      }
+      return false
+    }, timeout, 200, 'waitForIndexedBlockHeight')
+  }
+}
