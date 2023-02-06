50a2561bfe9222ca84146a09586d02498f9aaa18
shana <shana.tan@cakedefi.com>
2023-02-06 13:45:54 +0800

    added tests to encryptWallet.test.ts

---
+++ packages/jellyfish-api-core/__tests__/category/wallet/encryptWallet.test.ts
@@ -0,0 +1,60 @@
+import { MasterNodeRegTestContainer, RegTestContainer } from '@defichain/testcontainers'
+import { ContainerAdapterClient } from '../../container_adapter_client'
+
+describe('Wallet without masternode', () => {
+  const container = new RegTestContainer()
+  const client = new ContainerAdapterClient(container)
+
+  beforeAll(async () => {
+    await container.start()
+  })
+
+  afterAll(async () => {
+    await container.stop()
+  })
+
+  it('should throw error if passphrase for encryptWallet is empty', async () => {
+    return await expect(client.wallet.encryptWallet(''))
+      .rejects.toThrow("RpcApiError: 'passphrase can not be empty', code: -8, method: encryptwallet")
+  })
+
+  it('should encryptWallet with a given passphrase', async () => {
+    const promise = await client.wallet.encryptWallet('yourpassphrase')
+
+    expect(promise).toStrictEqual('wallet encrypted; The keypool has been flushed and a new HD seed was generated (if you are using HD). You need to make a new backup.')
+  })
+
+  it('should throw error when encryptWallet is called again after encryption', async () => {
+    return await expect(client.wallet.encryptWallet('yourpassphrase'))
+      .rejects.toThrow("RpcApiError: 'Error: running with an encrypted wallet, but encryptwallet was called.', code: -15, method: encryptwallet")
+  })
+})
+
+describe('Wallet on masternode', () => {
+  const container = new MasterNodeRegTestContainer()
+  const client = new ContainerAdapterClient(container)
+
+  beforeAll(async () => {
+    await container.start()
+  })
+
+  afterAll(async () => {
+    await container.stop()
+  })
+
+  it('should throw error if passphrase for encryptWallet is empty', async () => {
+    return await expect(client.wallet.encryptWallet(''))
+      .rejects.toThrow("RpcApiError: 'passphrase can not be empty', code: -8, method: encryptwallet")
+  })
+
+  it('should encryptWallet with a given passphrase', async () => {
+    const promise = await client.wallet.encryptWallet('yourpassphrase')
+
+    expect(promise).toStrictEqual('wallet encrypted; The keypool has been flushed and a new HD seed was generated (if you are using HD). You need to make a new backup.')
+  })
+
+  it('should throw error when encryptWallet is called again after encryption', async () => {
+    return await expect(client.wallet.encryptWallet('yourpassphrase'))
+      .rejects.toThrow("RpcApiError: 'Error: running with an encrypted wallet, but encryptwallet was called.', code: -15, method: encryptwallet")
+  })
+})
