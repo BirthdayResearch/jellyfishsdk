5ff776eeb7b4598b6991ebc2570f38c1dfac7e35
Harsh <harshrathi.dev@gmail.com>
2023-03-10 18:03:19 +0530

    fixed testcase

--- apps/playground-api/src/setups/setup.dex.ts
+++ apps/playground-api/src/setups/setup.dex.ts
@@ -269,19 +269,13 @@ export class SetupDex extends PlaygroundSetup<PoolPairSetup> {
     const poolPairs = await this.client.poolpair.listPoolPairs()
     const poolPairIds = Object.keys(poolPairs)
 
-    // apply `toFixed(8)` due to 1 / 15 = 0.06666667 which is invalid amount on setgov
+    // apply `toFixed(8)` due to 1 / 16 = 0.0625 which is valid amount on setgov
     const splits = Number(new BigNumber(1 / poolPairIds.length).toFixed(8))
 
     const lpSplits: any = {}
     for (const k in poolPairs) {
       lpSplits[parseInt(k)] = splits
     }
-
-    // to fix: LP_SPLITS: total = 100000005 vs expected 100000000', code: -32600, method: setgov
-    // 0.066666667 * 15 !== 100000000
-    const lstKey = Object.keys(lpSplits)[0]
-    lpSplits[lstKey] = Number(new BigNumber(lpSplits[lstKey]).minus(0.00000005).toFixed(8))
-
     await this.client.masternode.setGov({ LP_SPLITS: lpSplits })
     await this.generate(1)
   }
