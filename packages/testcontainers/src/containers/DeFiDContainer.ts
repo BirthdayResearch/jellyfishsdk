af7cfe63648b0151324c3b9c3c5905bd31efb62a
Harsh <harshrathi.dev@gmail.com>
2023-05-25 11:17:58 +0530

    added stable docker version for testcontainer

--- packages/testcontainers/src/containers/DeFiDContainer.ts
+++ packages/testcontainers/src/containers/DeFiDContainer.ts
@@ -36,7 +36,7 @@ export abstract class DeFiDContainer extends DockerContainer {
     if (process?.env?.DEFICHAIN_DOCKER_IMAGE !== undefined) {
       return process.env.DEFICHAIN_DOCKER_IMAGE
     }
-    return 'defi/defichain:HEAD-fd79aec14' // renovate.json regexManagers
+    return 'defi/defichain:feature-evm-87f295438' // renovate.json regexManagers
   }
 
   public static readonly DefaultStartOptions = {
