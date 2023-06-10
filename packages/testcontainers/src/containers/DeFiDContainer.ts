ecd803f84c1ae4ff611b873d1ef1e0d27cc15e1b
canonbrother <w.canonbrother@gmail.com>
2023-06-10 11:53:01 +0800

    HEAD-f3b7f7dd8 changi testnet

--- packages/testcontainers/src/containers/DeFiDContainer.ts
+++ packages/testcontainers/src/containers/DeFiDContainer.ts
@@ -36,7 +36,7 @@ export abstract class DeFiDContainer extends DockerContainer {
     if (process?.env?.DEFICHAIN_DOCKER_IMAGE !== undefined) {
       return process.env.DEFICHAIN_DOCKER_IMAGE
     }
-    return 'defi/defichain:HEAD-39f369032' // renovate.json regexManagers
+    return 'defi/defichain:HEAD-f3b7f7dd8' // renovate.json regexManagers
   }
 
   public static readonly DefaultStartOptions = {
