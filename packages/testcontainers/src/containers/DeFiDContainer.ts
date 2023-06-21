bf73ba49c498340add235c9a0b26376736f923e3
JJ Adonis <work@josejai.me>
2023-06-21 13:50:07 +0800

    chore(ops): updated to node to prevent test fails

--- packages/testcontainers/src/containers/DeFiDContainer.ts
+++ packages/testcontainers/src/containers/DeFiDContainer.ts
@@ -36,7 +36,7 @@ export abstract class DeFiDContainer extends DockerContainer {
     if (process?.env?.DEFICHAIN_DOCKER_IMAGE !== undefined) {
       return process.env.DEFICHAIN_DOCKER_IMAGE
     }
-    return 'defi/defichain:master-b6fc51097' // renovate.json regexManagers
+    return 'defi/defichain:master-92f1c507b' // renovate.json regexManagers
   }
 
   public static readonly DefaultStartOptions = {
