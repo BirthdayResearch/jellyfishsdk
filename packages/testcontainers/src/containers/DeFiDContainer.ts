9636d6ded54950286fb16517ab43afdba7ce75b6
canonbrother <w.canonbrother@gmail.com>
2023-07-04 20:05:54 +0800

    HEAD-669ed01fc

--- packages/testcontainers/src/containers/DeFiDContainer.ts
+++ packages/testcontainers/src/containers/DeFiDContainer.ts
@@ -36,7 +36,7 @@ export abstract class DeFiDContainer extends DockerContainer {
     if (process?.env?.DEFICHAIN_DOCKER_IMAGE !== undefined) {
       return process.env.DEFICHAIN_DOCKER_IMAGE
     }
-    return 'defi/defichain:master-c14a2ba13' // renovate.json regexManagers
+    return 'defi/defichain:HEAD-669ed01fc' // renovate.json regexManagers
   }
 
   public static readonly DefaultStartOptions = {
