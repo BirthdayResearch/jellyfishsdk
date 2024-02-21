8f9d465305657339a86892d68c82bcb79e2c4ef0
canonbrother <w.canonbrother@gmail.com>
2024-02-21 11:56:01 +0800

    4.0.9

--- packages/testcontainers/src/containers/DeFiDContainer.ts
+++ packages/testcontainers/src/containers/DeFiDContainer.ts
@@ -36,7 +36,7 @@ export abstract class DeFiDContainer extends DockerContainer {
     if (process?.env?.DEFICHAIN_DOCKER_IMAGE !== undefined) {
       return process.env.DEFICHAIN_DOCKER_IMAGE
     }
-    return 'defi/defichain:4.0.5' // renovate.json regexManagers
+    return 'defi/defichain:4.0.9' // renovate.json regexManagers
   }
 
   public static readonly DefaultStartOptions = {
