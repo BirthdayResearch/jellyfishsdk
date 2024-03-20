a2ab81971ab7c4de5c33a80a21db4913e5f542a9
canonbrother <w.canonbrother@gmail.com>
2024-03-20 22:09:53 +0800

    rename gh_instance to jest-worker

--- jest.sequencer.js
+++ jest.sequencer.js
@@ -4,9 +4,9 @@ const ShuffleSeed = require('shuffle-seed')
 class CustomSequencer extends Sequencer {
   sort (tests) {
     // In CI, GitHub Actions tests are parallelized across multiple instance
-    if (process.env.GH_INSTANCE_TOTAL) {
-      const total = parseInt(process.env.GH_INSTANCE_TOTAL, 10)
-      const index = parseInt(process.env.GH_INSTANCE_INDEX, 10)
+    if (process.env.JEST_WORKER_TOTAL) {
+      const total = parseInt(process.env.JEST_WORKER_TOTAL, 10)
+      const index = parseInt(process.env.JEST_WORKER_INDEX, 10)
 
       return ShuffleSeed.shuffle(tests, 'deterministic')
         .filter((_, i) => i % total === index)
