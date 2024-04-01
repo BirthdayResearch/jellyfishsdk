44d76fcd79b30398d5ec7154df6458218555225e
canonbrother <w.canonbrother@gmail.com>
2024-04-01 15:50:32 +0800

    HEAD-6fa363a26a

--- packages/testcontainers/src/containers/NativeChainContainer.ts
+++ packages/testcontainers/src/containers/NativeChainContainer.ts
@@ -29,7 +29,7 @@ export class NativeChainContainer extends GenericContainer {
     if (process?.env?.DEFICHAIN_DOCKER_IMAGE !== undefined) {
       return process.env.DEFICHAIN_DOCKER_IMAGE
     }
-    return 'defi/defichain:4.0.9' // renovate.json regexManagers
+    return 'defi/defichain:HEAD-6fa363a26a' // renovate.json regexManagers
   }
 
   public static readonly PREFIX = 'defichain-testcontainers-'
