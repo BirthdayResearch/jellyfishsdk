3eac12cbfc5a572e1b921c6e9e1626ccfc1fe673
canonbrother <w.canonbrother@gmail.com>
2024-04-02 23:28:04 +0800

    HEAD-b3f4954724

--- packages/testcontainers/src/containers/DeFiDContainer.ts
+++ packages/testcontainers/src/containers/DeFiDContainer.ts
@@ -36,7 +36,7 @@ export abstract class DeFiDContainer extends DockerContainer {
     if (process?.env?.DEFICHAIN_DOCKER_IMAGE !== undefined) {
       return process.env.DEFICHAIN_DOCKER_IMAGE
     }
-    return 'defi/defichain:HEAD-6fa363a26a' // renovate.json regexManagers
+    return 'defi/defichain:HEAD-b3f4954724' // renovate.json regexManagers
   }
 
   public static readonly DefaultStartOptions = {
